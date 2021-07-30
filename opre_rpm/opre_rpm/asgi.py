d50989051ca1231b2368381cf86151ebc925d8cc
Carly Jugler <carjug@gmail.com>
2021-07-30 11:34:16 -0600

    Fresh django install

---
+++ opre_rpm/opre_rpm/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for opre_rpm project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opre_rpm.settings')
+
+application = get_asgi_application()
