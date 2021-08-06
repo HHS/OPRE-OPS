038e57f12d47970c926a75cf3fd4bfbc9415f2b7
Carly Jugler <carjug@gmail.com>
2021-08-06 12:05:57 -0600

    add arg parser to secret.py

--- opre_ops/secret.py
+++ opre_ops/secret.py
@@ -12,3 +12,11 @@ def generate_random_string(length):
         output += secrets.choice(choice)
 
     return output
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Generate a secret key/password.  The string will include digits, letters, "
+                                     "and punctuation, and has a default length of 50.")
+    parser.add_argument("-l", "--length", help="Specify secret length", type=int, default=50)
+
+    args = parser.parse_args()
+    print(generate_random_string(args.length))
