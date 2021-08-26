51e063da40d96633d22588df678136586740f1ae
Alex Soble <alex.soble@gsa.gov>
2021-08-26 08:25:10 -0700

    Remove unused dj_database_url

--- opre_ops/opre_ops/settings.py
+++ opre_ops/opre_ops/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import dj_database_url
 
 from pathlib import Path
 from .env import env
