f292c6ee5128bd3309e1c903f1e323dbae551ddc
Carly Jugler <carjug@gmail.com>
2021-08-27 16:02:07 -0600

    Add pipenv install to start script

--- start-web.sh
+++ start-web.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # todo: django docs say don't use this `runserver` command in prod
+pip install --upgrade pip pipenv && pipenv install
+
 python ./opre_ops/manage.py runserver 0.0.0.0:8080
