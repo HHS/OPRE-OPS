5332f67391cd3c9c723fb876e6b21beb090e568c
Carly Jugler <carjug@gmail.com>
2021-09-01 16:49:11 -0600

    Add initial migration for agency model

--- opre_ops/opre_ops/settings/common.py
+++ opre_ops/opre_ops/settings/common.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'opre_ops',
+    'ops_site',
 ]
 
 MIDDLEWARE = [
--- opre_ops/ops_site/models.py
+++ opre_ops/ops_site/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Agency(models.Model):
-    name = models.CharField()
-    nickname = models.CharField()
+    name = models.CharField(max_length=80)
+    nickname = models.CharField(max_length=40)
