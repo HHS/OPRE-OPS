ca8bfce999691d6cbe9adb828851f58fa70f2312
Carly Jugler <carjug@gmail.com>
2021-09-03 10:38:01 -0600

    Pluralize agency properly

--- opre_ops/ops_site/models.py
+++ opre_ops/ops_site/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Agency(models.Model):
     name = models.CharField(max_length=100)
     nickname = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "agencies"
