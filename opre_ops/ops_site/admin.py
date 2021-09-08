831cd229e48ba2addb7a76e215947836410f190d
Carly Jugler <carjug@gmail.com>
2021-09-08 16:49:15 -0600

    Add role model with migration and fake data

--- opre_ops/ops_site/admin.py
+++ opre_ops/ops_site/admin.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from ops_site.models import Agency
+from ops_site.models import Role
 
 
 @admin.register(Agency)
 class AgencyAdmin(admin.ModelAdmin):
     list_display = ("name", "nickname")
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ("name",)
