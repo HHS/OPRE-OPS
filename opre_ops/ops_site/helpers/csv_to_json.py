7d30672e95a6008c6583db01e10a118735d34510
Carly Jugler <carjug@gmail.com>
2021-09-14 13:31:36 -0600

    fix typo in docstring

--- opre_ops/ops_site/helpers/csv_to_json.py
+++ opre_ops/ops_site/helpers/csv_to_json.py
@@ -9,7 +9,7 @@ def convert_csv_to_json(inputf, outputf, keys, model_name):
     for fixture data.
 
     How to use:
-    In CLI, run python convert_csv_to_json.py <input filepath > <output filepath> <model name> <list of fields/columns to parse>
+    In CLI, run python csv_to_json.py <input filepath > <output filepath> <model name> <list of fields/columns to parse>
 
     Do not attempt to output to the existing fixture/fake_data.json file, this will 
     override the existing data in that file. Instead, output to a newly created JSON 
