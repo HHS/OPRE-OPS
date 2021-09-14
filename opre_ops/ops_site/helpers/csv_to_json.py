b21e298346dd9e980f902314953782f3458a1c37
Carly Jugler <carjug@gmail.com>
2021-09-14 13:19:57 -0600

    Add docstring to csv to JSON

--- opre_ops/ops_site/helpers/csv_to_json.py
+++ opre_ops/ops_site/helpers/csv_to_json.py
@@ -4,6 +4,46 @@ import argparse
 
 
 def convert_csv_to_json(inputf, outputf, keys, model_name):
+    """
+    Takes csv of model data and converts it into formatted JSON
+    for fixture data.
+
+    How to use:
+    In CLI, run python convert_csv_to_json.py <arg1> <arg2> <list of args 3> <arg 4>
+
+    Do not attempt to output to the existing fixture/fake_data.json file, this will 
+    override the existing data in that file. Instead, output to a newly created JSON 
+    file from which you can copy/paste the generated JSON to the fixture file.
+
+    Sample output JSON:
+    [
+    {
+        "model": "ops_site.<model_name>",
+        "pk": 1,
+        "fields": {
+            "key1": "Test Data 1",
+            "key2": "Field 2",
+            "key3": [list, of, things],
+            "key4": "2"
+        }
+    },
+    {
+        "model": "ops_site.<model_name>",
+        "pk": 2,
+        "fields": {
+            "key1": "Test Data 2",
+            "key2": "Field 2",
+            "key3": [list, of, things],
+            "key4": "1"
+        }
+    }
+    ]
+
+    :param inputf: string, file path to input csv file
+    :param outputf: string, file path to output json file
+    :param keys: list, fields/column names to parse as model fields
+    :param model_name: string, model name to for which to build the fixture
+    """
     with open(inputf) as csv_f:
         reader = csv.DictReader(csv_f)
         reader.fieldnames = [name.lower() for name in reader.fieldnames]
