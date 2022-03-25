440db51962e758c4f62c28602966ac01f0680b64
John Skiles Skinner <john.skinner@gsa.gov>
2022-03-25 16:20:38 -0500

    Add static directory and use relative pathname

--- opre_ops/opre_ops/settings/common.py
+++ opre_ops/opre_ops/settings/common.py
@@ -101,7 +101,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
