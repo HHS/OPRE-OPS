281c64439cba45f0b28a09fdf6fc040d35f330e5
John Skiles Skinner <john.skinner@gsa.gov>
2022-03-29 13:21:59 -0500

    Experiment with putting static files in parent directory

--- opre_ops/opre_ops/settings/common.py
+++ opre_ops/opre_ops/settings/common.py
@@ -103,7 +103,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Default primary key field type
