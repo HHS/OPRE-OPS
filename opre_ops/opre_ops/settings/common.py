636c6528d57c2c4a3e1ccac114f2f0c3f8ccfee5
John Skiles Skinner <john.skinner@gsa.gov>
2022-03-30 16:18:49 -0500

    Remove hardcoded path, try nested `dirname()`s

--- opre_ops/opre_ops/settings/common.py
+++ opre_ops/opre_ops/settings/common.py
@@ -103,7 +103,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-PROJECT_DIR = '/home/vcap/app/opre_ops/opre_ops'
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Default primary key field type
