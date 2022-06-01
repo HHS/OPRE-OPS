89d0627678205948728997a2c3ea1238a01ddb79
John Skiles Skinner <john.skinner@gsa.gov>
2022-06-01 14:19:28 -0500

    Create a detail view of individual CANs

--- opre_ops/ops_site/views.py
+++ opre_ops/ops_site/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import datetime
 
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from ops_site.models import CANInfo
 
 def home(request):
@@ -12,3 +13,11 @@ def home(request):
 
 class CANInfoListView(ListView):
     model = CANInfo
+
+class CANInfoDetailView(DetailView):
+
+    queryset = CANInfo.objects.all()
+
+    def get_object(self):
+        obj = super().get_object()
+        return obj
