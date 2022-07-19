06d9b2e43c2a6f3e7a251700b94ab50e3903978a
halprin <halprin@users.noreply.github.com>
2022-07-19 12:56:00 -0600

    Set-up nox to run default flake8 stuff

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+import nox
+
+python_source = ['opre_ops', 'noxfile.py']
+
+@nox.session
+def lint(session):
+    session.run('pipenv', 'install', '--dev', external=True)
+
+    args = session.posargs or python_source
+    session.run("flake8", *args)
