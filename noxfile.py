e9bd4e7dbd59e5c17d20a4acb9c5e834c4e0e19b
halprin <halprin@users.noreply.github.com>
2022-07-19 13:01:17 -0600

    304: Install black code formatter

--- noxfile.py
+++ noxfile.py
@@ -7,4 +7,11 @@ def lint(session):
     session.run('pipenv', 'install', '--dev', external=True)
 
     args = session.posargs or python_source
-    session.run("flake8", *args)
+    session.run('flake8', *args)
+
+@nox.session
+def black(session):
+    session.run('pipenv', 'install', '--dev', external=True)
+
+    args = session.posargs or python_source
+    session.run('black', *args)
