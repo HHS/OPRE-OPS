66e342a7ed2ec73ef454efaa5603db90c0c78bf3
halprin <halprin@users.noreply.github.com>
2022-07-20 14:42:09 -0600

    304: Use Django ReST plugin and implement a basic JSON ReST API

--- backend/opre_ops/ops_site/urls.py
+++ backend/opre_ops/ops_site/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
+from ops_site.cans.controller import CanListController, CanReadController
 from ops_site.views import CANInfoDetailView
 from ops_site.views import CANInfoListView
 from . import views
 
 
 urlpatterns = [
-    path("home", views.home),
-    path("cans/", CANInfoListView.as_view()),
-    path("cans/<int:pk>/", CANInfoDetailView.as_view(), name="can-detail"),
+    path("cans", CanListController.as_view()),
+    path("cans/<int:pk>", CanReadController.as_view()),
+    # old views
+    path("view/home", views.home),
+    path("view/cans/", CANInfoListView.as_view()),
+    path("view/cans/<int:pk>/", CANInfoDetailView.as_view(), name="can-detail"),
 ]
