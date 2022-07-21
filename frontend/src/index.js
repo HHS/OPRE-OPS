8fa5d1f62f2f9197de1d97f378f23fa6e94fd78d
halprin <halprin@users.noreply.github.com>
2022-07-21 13:06:30 -0600

    304: Add ESLint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { Provider } from 'react-redux'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import store from './store';
+import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+    <React.StrictMode>
+        <Provider store={store}>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<App />} />
+                    <Route path="/" element={<App />} />
                 </Routes>
             </BrowserRouter>
         </Provider>
-  </React.StrictMode>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
