6936a53caa747cd936e499f34b27d161ea087ac9
halprin <halprin@users.noreply.github.com>
2022-07-21 11:16:12 -0600

    304: Add some frontend foundation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import store from './store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<App />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
   </React.StrictMode>
 );
 
