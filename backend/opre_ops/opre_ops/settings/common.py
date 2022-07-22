082c6bf1f641aeeece956bae0256d4dafaacc423
halprin <halprin@users.noreply.github.com>
2022-07-22 15:21:17 -0600

    304: Add CORS to the backend

--- backend/opre_ops/opre_ops/settings/common.py
+++ backend/opre_ops/opre_ops/settings/common.py
@@ -33,9 +33,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -109,3 +111,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOWED_ORIGIN_REGEXES = [r"http://localhost(:\d{1,4})?"]
