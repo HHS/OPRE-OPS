357380c65f31c9188955b66ae844417f93024fc5
halprin <halprin@users.noreply.github.com>
2022-07-22 17:47:13 -0600

    304: Nuke the frontend out of Django

--- backend/opre_ops/opre_ops/settings/common.py
+++ backend/opre_ops/opre_ops/settings/common.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
 ]
@@ -98,19 +97,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = "/static/"
-
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-CORS_ALLOWED_ORIGIN_REGEXES = [r"http://localhost(:\d{1,4})?"]
