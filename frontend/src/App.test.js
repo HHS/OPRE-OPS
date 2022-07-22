0ae34c3c443c04c802977c997a15e192afab0131
halprin <halprin@users.noreply.github.com>
2022-07-22 17:33:19 -0600

    304: Fix unit test for now

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 test("renders learn react link", () => {
-    render(<App />);
+    render(<App />, { wrapper: BrowserRouter });
     const linkElement = screen.getByText(/OPS system/i);
     expect(linkElement).toBeInTheDocument();
 });
