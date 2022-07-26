61d9602eaa16bc6c4acfbc5e6e6fb95d4acdccad
halprin <halprin@users.noreply.github.com>
2022-07-26 10:48:09 -0600

    304: Change the logging for the frontend

--- frontend/src/helpers/backend.js
+++ frontend/src/helpers/backend.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const BACKEND_DOMAIN = process.env.REACT_APP_BACKEND_DOMAIN;
 
 export const callBackend = async (urlPath, action, requestBody) => {
-    console.log(`${BACKEND_DOMAIN}${urlPath}`);
+    console.log(`Calling backend at ${urlPath}`);
+
     const response = await axios({
         method: action,
         url: `${BACKEND_DOMAIN}${urlPath}`,
