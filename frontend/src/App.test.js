dd10681887b3a2deba8ff8eba3ebe41a67eb27e4
halprin <halprin@users.noreply.github.com>
2022-08-01 13:52:11 -0600

    304: Be more specific in the frontend coverage ignores

--- frontend/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-
-test("renders learn react link", () => {
-    render(<App />, { wrapper: BrowserRouter });
-    const linkElement = screen.getByText(/OPS system/i);
-    expect(linkElement).toBeInTheDocument();
-});
