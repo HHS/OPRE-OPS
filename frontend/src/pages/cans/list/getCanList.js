ca9b449d4631236ec58fa051ffb26dafdbdf0974
halprin <halprin@users.noreply.github.com>
2022-08-01 10:12:06 -0600

    1: Clean up getCanList

--- frontend/src/pages/cans/list/getCanList.js
+++ frontend/src/pages/cans/list/getCanList.js
@@ -1,10 +1,8 @@
 import { setCanList } from "./canListSlice";
-// import { callBackend } from "../../../helpers/backend";
 import ApplicationContext from "../../../applicationContext/ApplicationContext";
 
 export const getCanList = () => {
     return async (dispatch, getState) => {
-        // const responseData = await callBackend("/ops/cans", "get");
         const responseData = await ApplicationContext.get().helpers().callBackend("/ops/cans", "get");
         dispatch(setCanList(responseData));
     };
