2983e8758477d2c18145ce69fcb2fbffd9485c88
halprin <halprin@users.noreply.github.com>
2022-08-02 14:09:04 -0600

    304: Try a different one

--- backend/opre_ops/manage.py
+++ backend/opre_ops/manage.py
@@ -8,7 +8,7 @@ def main():
     # Default to running with cloud.gov settings for fail-safe; allow override to use local settings.
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opre_ops.django_config.settings.cloudgov")
 
-    print('PAK::PYTHONPATH')
+    print('PAK::PYTHONPATH2')
     print(sys.path)
 
     try:
