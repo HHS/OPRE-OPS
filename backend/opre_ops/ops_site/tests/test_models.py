951f68edefd8371eb1475eb8d024b36ee7732fb6
Tim Donaworth <tdonaworth@flexion.us>
2022-08-04 16:09:12 -0400

    inital attempt at CAN model tests

--- backend/opre_ops/ops_site/tests/test_models.py
+++ backend/opre_ops/ops_site/tests/test_models.py
@@ -1,6 +1,21 @@
-def test_foo():
-    pass
+import pytest
+
+from model_bakery import baker
+from urllib import response
+from rest_framework.test import APIClient
+
+from opre_ops.ops_site.cans.models import CommonAccountingNumber as CAN
 
 
-def test_bar():
-    pass
+client = APIClient()
+
+@pytest.fixture()
+def can(db):
+    return baker.make(CAN)
+
+def test_using_can(can):
+    assert isinstance(can, CAN)
+
+def test_api_getAllCans(can):
+    response = client.get('/ops/cans')
+    assert response.status_code == 200
