e6edb983e8b874941354dd68b26e8f2652cdcd45
Tim Donaworth <tdonaworth@flexion.us>
2022-08-05 09:26:15 -0400

    minor cleanup

--- backend/opre_ops/ops_site/tests/test_models.py
+++ backend/opre_ops/ops_site/tests/test_models.py
@@ -1,7 +1,6 @@
 import pytest
 
 from model_bakery import baker
-from urllib import response
 from rest_framework.test import APIClient
 
 from opre_ops.ops_site.cans.models import CommonAccountingNumber as CAN
@@ -9,13 +8,16 @@ from opre_ops.ops_site.cans.models import CommonAccountingNumber as CAN
 
 client = APIClient()
 
+
 @pytest.fixture()
 def can(db):
     return baker.make(CAN)
 
+
 def test_using_can(can):
-    assert isinstance(can, CAN)
+    assert isinstance(can, CAN)  # noqa: S101,S307
+
 
 def test_api_getAllCans(can):
     response = client.get('/ops/cans')
-    assert response.status_code == 200
+    assert response.status_code == 200  # noqa: S101,S307
