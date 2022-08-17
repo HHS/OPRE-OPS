37ca35cc7badb2581b8eb9d8e87f01295609a800
halprin <halprin@users.noreply.github.com>
2022-08-17 16:05:21 -0600

    304: Play around with coverage and pytest

---
+++ backend/tests/ops_site/cans/test_controller.py
@@ -0,0 +1,11 @@
+import pytest
+
+from opre_ops.ops_site.cans.controller import CommonAccountingNumberSerializer
+
+
+def test_CAN_serializer_has_depth_of_one():
+    assert CommonAccountingNumberSerializer.Meta.depth == 1
+
+
+def test_CAN_serializer_returns_all_models_fields():
+    assert CommonAccountingNumberSerializer.Meta.fields == "__all__"
