97f07b9407663489097a110736a148a30d0c5dcd
Tim Donaworth <tdonaworth@flexion.us>
2022-08-17 13:02:48 -0400

    cleanup

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -1,23 +1,47 @@
 /* eslint-disable no-undef */
-test("List CANs", () => {
-    it("passes", () => {
+describe("List CANs", () => {
+    it("list cans", () => {
         cy.visit("/");
         cy.contains("/cans").click();
         cy.url().should("include", "/cans");
     });
 });
 
-test("CAN Details", () => {
-    it("passes", () => {
+describe("CAN Details", () => {
+    it("get can details", () => {
         cy.visit("/cans");
         cy.contains("G99PHS9").click();
         cy.url().should("include", "/cans/3");
     });
 });
 
-test("Select Fiscal Year", () => {
-    it("passes", () => {
+describe("Select Fiscal Year", () => {
+    it("get can fiscal year details", () => {
         cy.visit("/cans/3");
         cy.contains("G99PHS9").click();
+        cy.wait(100);
+        cy.get("[class*='-control']")
+            .click(0, 0, { force: true })
+            .get("[class*='-menu']")
+            .find("[id*='-option']")
+            .eq(2)
+            .click(0, 0, { force: true });
+        cy.contains("7512000");
+    });
+});
+
+describe("Test Negative Value", () => {
+    it("get a negative value", () => {
+        cy.visit("/cans/3");
+        cy.contains("G99PHS9").click();
+        cy.wait(100);
+        cy.get("[class*='-control']")
+            .click(0, 0, { force: true })
+            .get("[class*='-menu']")
+            .find("[id*='-option']")
+            .eq(3)
+            .click(0, 0, { force: true });
+        cy.contains("-300000");
+        cy.get("[class='red-negative']").contains("-300000");
     });
 });
