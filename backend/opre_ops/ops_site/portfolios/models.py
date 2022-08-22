b122e3068ab7b6b32bedc9351b7c35794aa06bd8
Tim Donaworth <tdonaworth@flexion.us>
2022-08-22 15:22:49 -0400

    Creatd portfolio model

---
+++ backend/opre_ops/ops_site/portfolios/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from opre_ops.ops_site.cans.models import CommonAccountingNumber
+
+
+class Portfolio(models.Model):
+    """
+    A Portfolio is ___
+    """
+    PORTFOLIO_STATUS = [
+        ("In-Process", "In-Process"),
+        ("Not-Started", "Not-Started"),
+        ("Sandbox", "Sandbox"),
+    ]
+
+    name = models.CharField(max_length=120)
+    description = models.TextField(default="", blank=True)
+    status = models.CharField(max_length=30, choices=PORTFOLIO_STATUS)
+    cans = models.ManyToManyField(CommonAccountingNumber)
+    current_fiscal_year_funding = models.DecimalField(max_digits=12,
+                                                      decimal_places=2)
+
+    @property
+    def get_all_cans(self):
+        return self.cans.all()
+
+    class Meta:
+        verbose_name_plural = "Portfolios"
+
+    def __str__(self):
+        return self.name
