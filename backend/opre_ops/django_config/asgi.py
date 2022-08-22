e1dcc81e6875d3be06a707cb583721045db68679
halprin <halprin@users.noreply.github.com>
2022-08-22 11:56:28 -0600

    376: Install uvicorn

--- backend/opre_ops/django_config/asgi.py
+++ backend/opre_ops/django_config/asgi.py
@@ -10,6 +10,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opre_ops.django_config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opre_ops.django_config.settings.cloudgov")
 
 application = get_asgi_application()
