d4ba5996a2d416b30a776f97be26c89ffef39f2e
Tim Donaworth <tdonaworth@flexion.us>
2022-08-29 17:37:22 -0400

    added Route

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -14,6 +14,7 @@ import App from "./App";
 import CanList from "./pages/cans/list/CanList";
 import CanDetail from "./pages/cans/detail/CanDetail";
 import PortfolioList from "./pages/portfolios/list/PortfolioList";
+import PortfolioDetail from "./pages/portfolios/detail/PortfolioDetail";
 
 ApplicationContext.registerApplicationContext(DeployedApplicationContext);
 
@@ -25,6 +26,7 @@ root.render(
                 <Routes>
                     <Route path="/" element={<App />} />
                     <Route path="/portfolios" element={<PortfolioList />} />
+                        <Route path="/portfolios/:id" element={<PortfolioDetail />} />
                     <Route path="/cans" element={<CanList />}>
                         <Route path="/cans/:id" element={<CanDetail />} />
                     </Route>
