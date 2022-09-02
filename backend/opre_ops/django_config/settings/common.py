f18f175da3fb1dc08005c5fe89ecf0a83034e388
Tim Donaworth <tdonaworth@flexion.us>
2022-09-02 10:01:48 -0400

    linting

--- backend/opre_ops/django_config/settings/common.py
+++ backend/opre_ops/django_config/settings/common.py
@@ -85,9 +85,10 @@ USE_TZ = True
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = "static/"
-STATIC_ROOT = "static"
-STATICFILES_DIRS = "static"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
