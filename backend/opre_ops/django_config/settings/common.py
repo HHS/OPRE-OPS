11a189189e83487209ed21d3eef5433d062e15b5
Tim Donaworth <tdonaworth@flexion.us>
2022-09-02 10:41:26 -0400

    linting

--- backend/opre_ops/django_config/settings/common.py
+++ backend/opre_ops/django_config/settings/common.py
@@ -85,7 +85,7 @@ USE_TZ = True
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
