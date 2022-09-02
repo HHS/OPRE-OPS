49fdb1255c9a37c749df2e36519278ce3905a0e6
Tim Donaworth <tdonaworth@flexion.us>
2022-09-02 10:39:30 -0400

    lint fix

--- frontend/cypress/support/e2e.js
+++ frontend/cypress/support/e2e.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // ***********************************************************
 // This example support/e2e.js is processed and
 // loaded automatically before your test files.
@@ -14,7 +15,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
