8b966bab4580e360d37761d5fbe328d2f98015d9
halprin <halprin@users.noreply.github.com>
2022-09-02 10:29:19 -0600

    376: Convert the CAN detail page to USWDS

--- frontend/src/pages/cans/detail/CanDetail.jsx
+++ frontend/src/pages/cans/detail/CanDetail.jsx
@@ -3,6 +3,7 @@ import { getCan } from "./getCan";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import BudgetSummary from "./budgetSummary/BudgetSummary.jsx";
+import constants from "../../../constants";
 
 const CanDetail = () => {
     const dispatch = useDispatch();
@@ -14,96 +15,34 @@ const CanDetail = () => {
     }, [dispatch, urlPathParams.id]);
 
     return (
-        <main>
-            <section className="flex">
-                <div className="one-flex">
-                    <div className="info-head">
-                        <a className="right-float" href="/custom-report">
-                            Download custom report
-                        </a>
-                        <h1>
-                            {can.number} ({can.nickname})
-                        </h1>
+        <>
+            <h1>
+                {can.number} ({can.nickname})
+            </h1>
+
+            <div className="grid-container">
+                <div className="grid-row">
+                    <div className="grid-col">
+                        <h3>CAN description</h3>
+                        {can.description}
+                        <h3>CAN purpose</h3>
+                        {can.purpose || constants.notFilledInText}
+                        <h3>Arrangement type</h3>
+                        {can.arrangement_type}
+                        <h3>Funding source</h3>
+                        {can.funding_source?.[0]?.nickname || constants.notFilledInText}
+                        <h3>OPRE point of contact</h3>
+                        {can.authorizer?.name}
+                        <h3>OPRE division</h3>
+                        {can.division || constants.notFilledInText}
                     </div>
-                    <div className="rounded-box">
-                        <div className="info-unit">
-                            <h2>CAN information</h2>
-                        </div>
-                        <div className="info-unit">
-                            <h3>CAN description</h3>
-                            {can.description}
-                        </div>
-                        <div className="info-unit">
-                            <h3>CAN purpose</h3>
-                            {can.purpose || "n/a"}
-                        </div>
-                        <div className="info-unit">
-                            <h3>Arrangement type</h3>
-                            {can.arrangement_type}
-                        </div>
-                        <div className="info-unit">
-                            <h3>Funding source</h3>
-                            {can.funding_source?.[0]?.nickname || "n/a"}
-                        </div>
-                        <div className="info-unit">
-                            <h3>OPRE CAN point of contact</h3>
-                            {can.authorizer?.name}
-                        </div>
-                        <div className="info-unit">
-                            <h3>OPRE division</h3>
-                            {can.division || "n/a"}
-                        </div>
+
+                    <div className="grid-col">
+                        <BudgetSummary />
                     </div>
                 </div>
-                <div className="two-flex">
-                    <BudgetSummary />
-                </div>
-            </section>
-            <section className="rounded-box">
-                <div className="info-unit">
-                    <h2>
-                        Breakdown of contracts and budget lines funded by CAN
-                        {can.number} ({can.nickname}) for FY 2022
-                    </h2>
-                </div>
-                <table className="one-flex">
-                    <tbody>
-                        <tr>
-                            <th>Contracts and budget lines</th>
-                            <th>CAN contrib</th>
-                            <th>% of total</th>
-                            <th>Additoinal funding</th>
-                            <th>Research areas</th>
-                            <th>Point of contact</th>
-                        </tr>
-                        <tr>
-                            <td>
-                                <details>
-                                    <summary>NIH VIQI</summary>
-                                    <table>
-                                        <tbody>
-                                            <tr>
-                                                <td>SC 1</td>
-                                                <td>$992,000</td>
-                                            </tr>
-                                            <tr>
-                                                <td>SC 1 Fee</td>
-                                                <td>$8,000</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </details>
-                            </td>
-                            <td>$1,000,000</td>
-                            <td>80%</td>
-                            <td>CAN</td>
-                            <td>Area</td>
-                            <td>👤 Person</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </section>
-        </main>
+            </div>
+        </>
     );
 };
 
