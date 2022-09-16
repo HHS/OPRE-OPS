46cc6731772e8ee04c06cc18e14017fbecf0e874
halprin <halprin@users.noreply.github.com>
2022-09-16 15:39:35 -0600

    447: Start using the Authlib

--- backend/ops_api/ops/auth/oidc.py
+++ backend/ops_api/ops/auth/oidc.py
@@ -1,9 +1,21 @@
 from http import HTTPStatus
+import os
 
+from authlib.integrations.django_client import OAuth
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+
+oauth = OAuth()
+oauth.register(
+    "google",
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_id=os.getenv("OIDC_CLIENT_ID"),
+    client_secret=os.getenv("OIDC_CLIENT_SECRET"),
+    client_kwargs={"scope": "openid"},
+)
 
 
 class OidcController(APIView):
@@ -17,5 +29,8 @@ class OidcController(APIView):
         print(
             f"Got an OIDC request with the callback URL of {callback_url} and a state of {state}"
         )
+
+        # This isn't quite right because this request isn't the actual callback
+        # token = oauth.google.authorize_access_token(request)
 
         return Response({"jwt": "OPS-specific JWT"}, status=HTTPStatus.OK)
