1708a0c2f7895fede902ddf391c2c42df9f9f11e
halprin <halprin@users.noreply.github.com>
2022-09-19 14:27:01 -0600

    447: Add half-finished code_verifier

--- backend/ops_api/ops/auth/oidc.py
+++ backend/ops_api/ops/auth/oidc.py
@@ -17,12 +17,13 @@ class OidcController(APIView):
     def post(request: Request):
         callback_url = request.data["callbackUrl"]
         state = request.data["state"]
+        pkce_code_verifier = request.data["pkceCodeVerifier"]
 
         print(
             f"Got an OIDC request with the callback URL of {callback_url} and a state of {state}"
         )
 
-        token = oauth.logingov.fetch_access_token(redirect_uri=callback_url)
+        token = oauth.logingov.fetch_access_token(redirect_uri=callback_url, code_verifier=pkce_code_verifier)
         print(token)
 
         return Response({"jwt": "OPS-specific JWT"}, status=HTTPStatus.OK)
