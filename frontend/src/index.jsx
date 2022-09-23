fd47fd251d14e2dff7089281c94e03e36f39cb2f
John DeAngelis <john.deangelis2@gmail.com>
2022-09-23 15:08:03 -0400

    fix bug with DeployedApplicationContext.js not loading on start

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,26 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import store from "./store";
-
-import ApplicationContext from "./applicationContext/ApplicationContext";
-import DeployedApplicationContext from "./applicationContext/DeployedApplicationContext";
 
 import HomeRoutes from "./components/HomeRoutes/HomeRoutes";
 
 // eslint-disable-next-line import/no-unresolved
 import "@uswds/uswds/css/uswds.min.css";
 import "@uswds/uswds";
-
-import App from "./App";
-import CanList from "./pages/cans/list/CanList";
-import CanDetail from "./pages/cans/detail/CanDetail";
-import PortfolioList from "./pages/portfolios/list/PortfolioList";
-import PortfolioDetail from "./pages/portfolios/detail/PortfolioDetail";
-
-ApplicationContext.registerApplicationContext(DeployedApplicationContext);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
