5c60157c9584627215faa858ca2b496fa3f6744b
halprin <halprin@users.noreply.github.com>
2022-09-27 12:45:03 -0600

    447: Update Client ID for Login.gov

--- backend/ops_api/django_config/settings/local.py
+++ backend/ops_api/django_config/settings/local.py
@@ -37,7 +37,7 @@ INSTALLED_APPS += ["django_extensions"]  # noqa: F405
 AUTHLIB_OAUTH_CLIENTS = {
     "logingov": {
         "server_metadata_url": "https://idp.int.identitysandbox.gov/.well-known/openid-configuration",
-        "client_id": "urn:gov:gsa:openidconnect.profiles:sp:sso:hhs_acf:opre_ops_jwt",
+        "client_id": "urn:gov:gsa:openidconnect.profiles:sp:sso:hhs_acf:opre_ops",
         "client_kwargs": {"scope": "openid"},
     }
 }
