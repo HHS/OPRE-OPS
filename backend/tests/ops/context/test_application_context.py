6dddd9ff84938b2bed8317d26f15d4740437e704
Tim Donaworth <tdonaworth@flexion.us>
2022-10-04 15:19:23 -0400

    created basic app_context test

---
+++ backend/tests/ops/context/test_application_context.py
@@ -0,0 +1,13 @@
+from ops_api.ops.contexts.application_context import ApplicationContext
+from ops_api.ops.contexts.application_context import TestContext
+
+
+def test_application_context_register():
+    ApplicationContext.register_context(TestContext)
+    assert ApplicationContext.get_context().get_name() == "TestContext"
+    
+def test_test_context_get_name():
+    assert TestContext.get_name() == "TestContext"
+    
+def test_context_auth_library():
+    assert TestContext.auth_library() != None
