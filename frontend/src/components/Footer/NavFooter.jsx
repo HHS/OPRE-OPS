7fc9a5e89446bf2c8a194a5ec421eddd6fd459f7
Tim Donaworth <tdonaworth@flexion.us>
2022-11-01 11:57:29 -0400

    core UI changes

---
+++ frontend/src/components/Footer/NavFooter.jsx
@@ -0,0 +1,52 @@
+export const NavFooter = () => {
+    return (
+        <nav className="usa-footer__nav" aria-label="Footer navigation">
+            <ul className="grid-row grid-gap">
+                <li
+                    className="
+              mobile-lg:grid-col-4
+              desktop:grid-col-auto
+              usa-footer__primary-content
+            "
+                >
+                    <a className="usa-footer__primary-link" href="javascript:void(0);">
+                        &lt;Primary link&gt;
+                    </a>
+                </li>
+                <li
+                    className="
+              mobile-lg:grid-col-4
+              desktop:grid-col-auto
+              usa-footer__primary-content
+            "
+                >
+                    <a className="usa-footer__primary-link" href="javascript:void(0);">
+                        &lt;Primary link&gt;
+                    </a>
+                </li>
+                <li
+                    className="
+              mobile-lg:grid-col-4
+              desktop:grid-col-auto
+              usa-footer__primary-content
+            "
+                >
+                    <a className="usa-footer__primary-link" href="javascript:void(0);">
+                        &lt;Primary link&gt;
+                    </a>
+                </li>
+                <li
+                    className="
+              mobile-lg:grid-col-4
+              desktop:grid-col-auto
+              usa-footer__primary-content
+            "
+                >
+                    <a className="usa-footer__primary-link" href="javascript:void(0);">
+                        &lt;Primary link&gt;
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
---
+++ frontend/src/components/Header/Menu.jsx
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom";
+
+export const Menu = () => {
+    return (
+        <nav aria-label="Primary navigation" className="usa-nav">
+            <div className="usa-nav__inner">
+                <button type="button" className="usa-nav__close">
+                    <img src="/assets/img/usa-icons/close.svg" role="img" alt="Close" />
+                </button>
+                <ul className="usa-nav__primary usa-accordion">
+                    <li className="usa-nav__primary-item">
+                        <button
+                            type="button"
+                            className="usa-accordion__button usa-nav__link usa-current"
+                            aria-expanded="false"
+                            aria-controls="extended-nav-section-one"
+                        >
+                            <span>Section</span>
+                        </button>
+                        <ul id="extended-nav-section-one" className="usa-nav__submenu">
+                            <li className="usa-nav__submenu-item">
+                                <a href="#">
+                                    <span>&lt;Navigation link&gt;</span>
+                                </a>
+                            </li>
+                            <li className="usa-nav__submenu-item">
+                                <a href="#">
+                                    <span>&lt;Navigation link&gt;</span>
+                                </a>
+                            </li>
+                            <li className="usa-nav__submenu-item">
+                                <a href="#">
+                                    <span>&lt;Navigation link&gt;</span>
+                                </a>
+                            </li>
+                            <li className="usa-nav__submenu-item">
+                                <a href="#">
+                                    <span>&lt;Navigation link&gt;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="usa-nav__primary-item">
+                        <button
+                            type="button"
+                            className="usa-accordion__button usa-nav__link"
+                            aria-expanded="false"
+                            aria-controls="extended-nav-section-two"
+                        >
+                            <span>Section</span>
+                        </button>
+                        <ul id="extended-nav-section-two" className="usa-nav__submenu">
+                            <li className="usa-nav__submenu-item">
+                                <a href="#">
+                                    <span>&lt;Navigation link&gt;</span>
+                                </a>
+                            </li>
+                            <li className="usa-nav__submenu-item">
+                                <a href="#">
+                                    <span>&lt;Navigation link&gt;</span>
+                                </a>
+                            </li>
+                            <li className="usa-nav__submenu-item">
+                                <a href="#">
+                                    <span>&lt;Navigation link&gt;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="usa-nav__primary-item">
+                        <Link to="portfolios">/portfolios</Link>
+                    </li>
+                    <li className="usa-nav__primary-item">
+                        <Link to="cans">/cans</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
