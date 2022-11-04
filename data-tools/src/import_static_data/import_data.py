48422f04fe7a52849324931a4887477aec2b58be
John DeAngelis <john.deangelis2@gmail.com>
2022-11-04 17:04:38 -0400

    models created; data importing

---
+++ data-tools/src/import_static_data/import_data.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, insert, MetaData, delete
+from env.local import DATABASE_URL
+import json5
+
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+metadata_obj = MetaData()
+metadata_obj.reflect(bind=engine)
+
+portfolio_data = json5.load(open("../../data/data.json5"))
+
+with engine.connect() as conn:
+
+    for ops_table in portfolio_data:
+        conn.execute(delete(metadata_obj.tables[ops_table]))
+
+    conn.commit()
+
+    for ops_table in portfolio_data:
+        data = portfolio_data[ops_table]
+
+        result = conn.execute(
+            insert(metadata_obj.tables[ops_table]),
+            data,
+        )
+
+    conn.commit()
