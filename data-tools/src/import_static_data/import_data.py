66ac9e2609430c1b8d321968af1f9d2ba4e63883
John DeAngelis <john.deangelis2@gmail.com>
2022-11-08 12:45:14 -0500

    add more unit tests

--- data-tools/src/import_static_data/import_data.py
+++ data-tools/src/import_static_data/import_data.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Dict
 
 import sqlalchemy.engine
 from sqlalchemy import create_engine, insert, MetaData, text
@@ -20,39 +20,44 @@ def load_module(module_name: str):
 
 
 def get_config(environment_name: str = os.getenv("ENV")):
-    module_name = f"environment.{environment_name}" if environment_name else "environment.dev"
+    module_name = (
+        f"environment.{environment_name}" if environment_name else "environment.dev"
+    )
     return load_module(module_name)
 
 
-def get_data_to_import(file_name=os.getenv('DATA')):
+def get_data_to_import(file_name: str = os.getenv("DATA")) -> Dict:
     return json5.load(open(f"data/{file_name}"))
 
 
-def delete_existing_data(conn, portfolio_data):
+def delete_existing_data(conn: sqlalchemy.engine.Engine, portfolio_data: Dict):
     for ops_table in portfolio_data:
         conn.execute(text(f"TRUNCATE {ops_table} CASCADE;"))
 
 
+def load_new_data(
+    conn: sqlalchemy.engine.Engine, portfolio_data, metadata_obj: sqlalchemy.MetaData
+):
+    for ops_table in portfolio_data:
+        data = portfolio_data[ops_table]
+        conn.execute(
+            insert(metadata_obj.tables[ops_table]),
+            data,
+        )
+
+
 def import_data(engine, metadata_obj, portfolio_data):
     with engine.connect() as conn:
-
         delete_existing_data(conn, portfolio_data)
-
         conn.commit()
 
-        for ops_table in portfolio_data:
-            data = portfolio_data[ops_table]
-
-            result = conn.execute(
-                insert(metadata_obj.tables[ops_table]),
-                data,
-            )
-
+        load_new_data(conn, portfolio_data, metadata_obj)
         conn.commit()
 
 
 if __name__ == "__main__":
     import importlib
+
     global_configuration = get_config(module_loader=importlib.import_module)
     global_data = get_data_to_import()
     global_engine, global_metadata_obj = init_db(global_configuration.DATABASE_URL)
--- data-tools/test/import_static_data/test_import_data.py
+++ data-tools/test/import_static_data/test_import_data.py
@@ -1,19 +1,16 @@
-import os
-from unittest.mock import call
 from unittest import mock
-from unittest.mock import MagicMock
 
+import pytest
 import sqlalchemy.engine
-import pytest_mock
-from pytest_mock import mocker
 
-from src.import_static_data.import_data import delete_existing_data, get_config, init_db
+from src.import_static_data.import_data import (
+    delete_existing_data,
+    get_config,
+    init_db,
+    load_new_data,
+    import_data,
+)
 
-
-# def test_delete_existing_data(mocker):
-#     portfolio_data = {"table1": [], "table2": [], "table3": []}
-#     mocker.patch.object("sqlalchemy.create_engine")
-#     delete_existing_data(None, portfolio_data)
 
 def test_init_db():
     engine, metadata_obj = init_db("sqlite://")
@@ -33,3 +30,53 @@ def test_get_config_prod():
         get_config("cloudgov")
         assert mock_load.call_count == 1
         assert mock_load.call_args_list[0].args == ("environment.cloudgov",)
+
+
+def test_delete_existing_data_empty():
+    mock_conn = mock.MagicMock()
+    delete_existing_data(mock_conn, {})
+    assert mock_conn.execute.call_count == 0
+
+
+def test_delete_existing_data():
+    mock_conn = mock.MagicMock()
+    delete_existing_data(mock_conn, {"table1": [], "table2": [], "table3": []})
+    assert mock_conn.execute.call_count == 3
+    assert (
+        mock_conn.execute.call_args_list[0].args[0].text == "TRUNCATE table1 CASCADE;"
+    )
+    assert (
+        mock_conn.execute.call_args_list[1].args[0].text == "TRUNCATE table2 CASCADE;"
+    )
+    assert (
+        mock_conn.execute.call_args_list[2].args[0].text == "TRUNCATE table3 CASCADE;"
+    )
+
+
+def test_load_new_data_empty():
+    mock_conn = mock.MagicMock()
+    mock_meta = mock.MagicMock()
+    load_new_data(mock_conn, {}, mock_meta)
+    assert mock_conn.execute.call_count == 0
+
+
+def test_load_new_data():
+    mock_conn = mock.MagicMock()
+    mock_meta = mock.MagicMock()
+    mock_meta.return_value = {}
+    # Test is a little weak - would be better to fully mock the MetaData obj
+    with pytest.raises(sqlalchemy.exc.ArgumentError):
+        load_new_data(mock_conn, {"table1": [{}]}, mock_meta)
+
+
+def test_import_data():
+    mock_engine = mock.MagicMock()
+    mock_meta = mock.MagicMock()
+    with mock.patch(
+        "src.import_static_data.import_data.delete_existing_data"
+    ) as mock_delete, mock.patch(
+        "src.import_static_data.import_data.load_new_data"
+    ) as mock_load:
+        import_data(mock_engine, mock_meta, {})
+        assert mock_delete.call_count == 1
+        assert mock_load.call_count == 1
