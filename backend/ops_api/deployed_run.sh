19ec751a1960bb363d452c7eeb7222127d68c9a9
Tim Donaworth <tdonaworth@flexion.us>
2022-11-18 16:32:02 -0500

    flask run changes

--- backend/ops_api/deployed_run.sh
+++ backend/ops_api/deployed_run.sh
@@ -6,4 +6,5 @@ export PYTHONPATH="./:${PYTHONPATH}"
 export FLASK_APP=ops
 export FLASK_DEBUG=true
 
-python -m gunicorn -b ":8080" "ops:create_app()"
+#python -m gunicorn -b ":8080" "ops:create_app()"
+python -m flask run -p "8080"
