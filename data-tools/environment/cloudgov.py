2a16c5750d38ca1cf5f795d8499e582827381614
Tim Donaworth <tdonaworth@flexion.us>
2022-11-22 08:36:02 -0500

    updated manifest and notes

--- data-tools/environment/cloudgov.py
+++ data-tools/environment/cloudgov.py
@@ -18,4 +18,4 @@ database_service = vcap_services["aws-rds"][0]
 database_creds = database_service["credentials"]
 
 DATABASE_URL = f'postgresql+psycopg2://{database_creds["username"]}:{database_creds["password"]}@{database_creds["host"]}:{database_creds["port"]}/{database_creds["db_name"]}'
-VERBOSE = False
+VERBOSE = True
