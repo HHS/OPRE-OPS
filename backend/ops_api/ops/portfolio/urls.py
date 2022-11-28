8e50a4b1befde8f88a187e52b3ffcc475f3914e2
Tim Donaworth <tdonaworth@flexion.us>
2022-11-28 17:50:21 -0500

    added new endpoints

--- backend/ops_api/ops/portfolio/urls.py
+++ backend/ops_api/ops/portfolio/urls.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
+from ops.can import views as can_views
 from ops.portfolio import views
 
 bp_portfolio = Blueprint("portfolio", __name__, url_prefix="/ops/portfolios")
 bp_portfolio.add_url_rule("/", view_func=views.portfolio_list)
 bp_portfolio.add_url_rule("/<int:pk>/", view_func=views.get_portfolio)
 bp_portfolio.add_url_rule("/<int:pk>/calcFunding/", view_func=views.calc_funding)
+bp_portfolio.add_url_rule("/<int:pk>/cans/", view_func=can_views.get_portfolio_cans)
 
 
 bp_portfolio_status = Blueprint(
@@ -15,4 +17,4 @@ bp_portfolio_status.add_url_rule("/<int:pk>/", view_func=views.get_portfolio_sta
 
 bp_division = Blueprint("division", __name__, url_prefix="/ops/division")
 bp_division.add_url_rule("/", view_func=views.division_list)
-bp_division.add_url_rule("/<int:pk>", view_func=views.get_division)
+bp_division.add_url_rule("/<int:pk>/", view_func=views.get_division)
