d62fa54df974f35b9beb2138d55c49101cda7c25
Tim Donaworth <tdonaworth@flexion.us>
2022-12-01 18:48:53 -0500

    staging_deploy test009

--- backend/ops_api/deployed_run.sh
+++ backend/ops_api/deployed_run.sh
@@ -6,5 +6,5 @@ export PYTHONPATH="./:${PYTHONPATH}"
 export FLASK_APP=ops
 export FLASK_DEBUG=true
 
-python -m gunicorn -b ":8080" "ops:create_app()"
-#python -m flask run --port=8080 --host=0.0.0.0
+#python -m gunicorn -b ":8080" "ops:create_app()"
+python -m flask run --port=8080 --host=0.0.0.0
