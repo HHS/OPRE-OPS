86a0046c19b37c8592b1426f6da6fcd01f4d5641
Tim Donaworth <tdonaworth@flexion.us>
2022-12-06 10:00:36 -0500

    removed unused style var

--- frontend/src/components/RoundedBox/RoundedBox.jsx
+++ frontend/src/components/RoundedBox/RoundedBox.jsx
@@ -4,34 +4,6 @@ import cssClasses from "./styles.module.css";
 
 library.add(faSquare);
 
-const styles = {
-    root: {
-        width: 175,
-        height: 175,
-    },
-    cardBody: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "left",
-        borderColor: "#FFF",
-    },
-    cardGroup: {
-        display: "flex",
-        marginBottom: "7%",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        width: "250px",
-    },
-    cardItem: {
-        display: "flex",
-        flex: "1",
-    },
-    iconStyle: {
-        verticalAlign: "middle",
-        paddingRight: "4px",
-    },
-};
-
 const RoundedBox = (props) => {
     const cardContainer = `bg-base-lightest font-family-sans ${cssClasses.container}`;
 
