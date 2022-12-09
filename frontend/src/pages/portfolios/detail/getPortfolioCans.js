3098b4692a32c1e4cd55d5629b7293b86616a97c
John DeAngelis <john.deangelis2@gmail.com>
2022-12-09 14:24:01 -0500

    initial code; data rendering but state seems inconsistent

--- frontend/src/pages/portfolios/detail/getPortfolioCans.js
+++ frontend/src/pages/portfolios/detail/getPortfolioCans.js
@@ -1,9 +1,21 @@
-import { setPortfolioCans } from "./portfolioDetailSlice";
+import { setPortfolioCans, setPortfolioCansFundingDetails } from "./portfolioDetailSlice";
 import { getPortfolioCans } from "../../../helpers/api";
+import ApplicationContext from "../../../applicationContext/ApplicationContext";
 
 export const getPortfolioCansAndSetState = (id) => {
     return async (dispatch, getState) => {
         const responseData = await getPortfolioCans(id);
         dispatch(setPortfolioCans(responseData));
+    };
+};
+
+export const getPortfolioCansFundingDetailsAndSetState = (id, fiscalYear) => {
+    return async (dispatch, getState) => {
+        if (id && fiscalYear) {
+            const responseData = await ApplicationContext.get()
+                .helpers()
+                .callBackend(`/ops/fundingSummary?can_id=${id}&fiscal_year=${fiscalYear}`, "get");
+            dispatch(setPortfolioCansFundingDetails(responseData));
+        }
     };
 };
