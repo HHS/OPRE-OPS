15968320a0c33bdc4d5083f436747ad2e443960e
John DeAngelis <john.deangelis2@gmail.com>
2022-12-13 14:12:51 -0500

    implement 0 handling requirements

--- frontend/src/components/UI/CurrencyWithSmallCents/util.js
+++ frontend/src/components/UI/CurrencyWithSmallCents/util.js
@@ -3,13 +3,16 @@ const getCents = (amount) => {
         return s.substring(s.indexOf(".") + 1, s.indexOf(".") + 3);
     };
 
-    // amount is a float currency value - returns the cents as an integer
-    const amount_str = amount.toString();
+    if (amount === 0) return "00";
+
+    // amount is a float currency value - returns the cents as an string
+    const amount_str = parseFloat(amount).toString();
+
     return amount_str.includes(".")
         ? parseInt(getCentsSubstring(amount_str)) < 10
             ? (parseInt(getCentsSubstring(amount_str)) * 10).toString()
             : getCentsSubstring(amount_str)
-        : 0;
+        : "00";
 };
 
 export { getCents };
--- frontend/src/components/UI/CurrencyWithSmallCents/util.test.js
+++ frontend/src/components/UI/CurrencyWithSmallCents/util.test.js
@@ -1,7 +1,7 @@
 import { getCents } from "./util";
 
 test("getCents - integer returns 0 cents", async () => {
-    expect(getCents(100)).toEqual(0);
+    expect(getCents(100)).toEqual("00");
 });
 
 test("getCents - happy path", async () => {
@@ -14,4 +14,20 @@ test("getCents - 1 decimal digit", async () => {
 
 test("getCents - no non-decimal", async () => {
     expect(getCents(0.3)).toEqual("30");
+});
+
+test("getCents - zero", async () => {
+    expect(getCents(0)).toEqual("00");
+});
+
+test("getCents - string with cents", async () => {
+    expect(getCents("1140000.01")).toEqual("01");
+});
+
+test("getCents - string with no cents", async () => {
+    expect(getCents("100")).toEqual("00");
+});
+
+test("getCents - string with tenths", async () => {
+    expect(getCents("100.0")).toEqual("00");
 });
