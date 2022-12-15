cf882f49158aae0f5dd9d24bb7a9960ca6f64ba1
Jonathan Nalley <jnalley@flexion.us>
2022-12-14 23:52:44 -0500

    minor wording changes based on recent user feedback

--- frontend/src/components/PortfolioSummaryCards/PortfolioCarryForwardFunding.jsx
+++ frontend/src/components/PortfolioSummaryCards/PortfolioCarryForwardFunding.jsx
@@ -6,7 +6,7 @@ const PortfolioCarryForwardFunding = () => {
 
     const carryForwardFunding = portfolioFunding.carry_over_funding?.amount || 0;
 
-    return <CurrencySummaryCard headerText="Carry-Forward Funding" amount={carryForwardFunding} />;
+    return <CurrencySummaryCard headerText="Previous FYs Carry-Forward" amount={carryForwardFunding} />;
 };
 
 export default PortfolioCarryForwardFunding;
--- frontend/src/components/PortfolioSummaryCards/PortfolioNewFunding.jsx
+++ frontend/src/components/PortfolioSummaryCards/PortfolioNewFunding.jsx
@@ -1,12 +1,14 @@
 import { useSelector } from "react-redux";
 import CurrencySummaryCard from "../UI/CurrencySummaryCard/CurrencySummaryCard";
 
-const PortfolioNewFunding = () => {
+const PortfolioNewFunding = ({ fiscalYear }) => {
     const portfolioFunding = useSelector((state) => state.portfolioFundingSummary.portfolioFunding);
 
     const newFunding = portfolioFunding.total_funding?.amount - portfolioFunding.carry_over_funding?.amount;
 
-    return <CurrencySummaryCard headerText="New Funding" amount={newFunding} />;
+    const headerText = `FY ${fiscalYear} New Budget`;
+
+    return <CurrencySummaryCard headerText={headerText} amount={newFunding} />;
 };
 
 export default PortfolioNewFunding;
