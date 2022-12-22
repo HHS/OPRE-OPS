2592aa60f9475bfd6c5c367a8e1d0259c2107604
Cliff Hill <chill@flexion.us>
2022-12-22 15:42:48 -0500

    Fixing things, docker now runs.

--- backend/ops_api/ops/user/views.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from flask import Response
-from ops.user.models import User
-
-
-def get_all_users() -> Response:
-    users = User.query.order_by(User.updated.desc()).all()
-    return jsonify([user.to_dict() for user in users])
