9c93fd9fd1388917c3d70b1b227b0bd22e0d274e
Tim Donaworth <tdonaworth@flexion.us>
2023-01-09 08:25:15 -0500

    staging sandbox

--- backend/ops_api/ops/environment/default_settings.py
+++ backend/ops_api/ops/environment/default_settings.py
@@ -6,10 +6,16 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///ops_test.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 FLASK_PORT = 8080
 AUTHLIB_OAUTH_CLIENTS = {
-    "logingov": {
+    "logingov_sandbox_dev": {
         "server_metadata_url": "https://idp.int.identitysandbox.gov/.well-known/openid-configuration",
         "client_id": "urn:gov:gsa:openidconnect.profiles:sp:sso:hhs_acf:opre_ops",
         "client_kwargs": {"scope": "openid"},
+        "logingov_sandbox_staging": {
+            "server_metadata_url": "https://idp.int.identitysandbox.gov/.well-known/openid-configuration",
+            "user_info_url": "https://idp.int.identitysandbox.gov/api/openid_connect/userinfo",
+            "client_id": "urn:gov:gsa:openidconnect.profiles:sp:sso:hhs_acf:opre_ops_staging",
+            "client_kwargs": {"scope": "openid email profile"},
+        },
     }
 }
 JWT_PRIVATE_KEY = os.getenv("JWT_PRIVATE_KEY")
