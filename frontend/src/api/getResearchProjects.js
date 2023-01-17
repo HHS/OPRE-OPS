618fc47483d5082ee201c80e1a5f634a4b808775
John DeAngelis <john.deangelis2@gmail.com>
2023-01-17 14:50:52 -0500

    use axios query params in call to backend

--- frontend/src/api/getResearchProjects.js
+++ frontend/src/api/getResearchProjects.js
@@ -1,12 +1,14 @@
 import ApplicationContext from "../applicationContext/ApplicationContext";
 
 export const getResearchProjects = async (portfolioId, fiscalYear) => {
+    const params = new URLSearchParams({
+        portfolioId,
+        fiscalYear,
+    });
+
     const api_version = ApplicationContext.get().helpers().backEndConfig.apiVersion;
     const responseData = await ApplicationContext.get()
         .helpers()
-        .callBackend(
-            `/api/${api_version}/research-projects/?portfolio_id=${portfolioId}&fiscal_year=${fiscalYear}`,
-            "get"
-        );
+        .callBackend(`/api/${api_version}/research-projects/`, "get", { params });
     return responseData;
 };
