fc1b96f8bc720406db39db8e9edcb011b12f2b56
fpigeonjr <frank.pigeonjr@gmail.com>
2023-01-19 08:37:46 -0600

    replaces hash link for button

--- frontend/src/components/UI/Footer/Footer.jsx
+++ frontend/src/components/UI/Footer/Footer.jsx
@@ -3,10 +3,18 @@ import { Identifier } from "./Idnetifier";
 import { NavFooter } from "./NavFooter";
 
 export const Footer = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
     return (
         <footer className="usa-footer">
             <div className="grid-container usa-footer__return-to-top bg-white">
-                <a href="#">Return to top</a>
+                <button className="text-underline text-primary cursor-pointer" onClick={scrollToTop}>
+                    Return to top
+                </button>
             </div>
             <div className="usa-footer__primary-section bg-base-light">
                 <NavFooter />
