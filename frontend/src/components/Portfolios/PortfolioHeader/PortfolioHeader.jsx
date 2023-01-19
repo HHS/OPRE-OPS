467026a98b279ae33403cbe993fda92fa16034bc
John DeAngelis <john.deangelis2@gmail.com>
2023-01-19 14:52:02 -0500

    use TeamLeaders component in PortfolioHeader.jsx and Research Project; remove unused PortfolioTeamLeaders.jsx

--- frontend/src/components/Portfolios/PortfolioHeader/PortfolioHeader.jsx
+++ frontend/src/components/Portfolios/PortfolioHeader/PortfolioHeader.jsx
@@ -1,8 +1,8 @@
 import { useSelector } from "react-redux";
 
 import style from "./styles.module.css";
-import PortfolioTeamLeaders from "../PortfolioTeamLeaders/PortfolioTeamLeaders";
 import PortfolioDescription from "../PortfolioDescription/PortfolioDescription";
+import TeamLeaders from "../../UI/TeamLeaders/TeamLeaders";
 
 const PortfolioHeader = () => {
     const portfolio = useSelector((state) => state.portfolio.portfolio);
@@ -12,7 +12,7 @@ const PortfolioHeader = () => {
         <section className="padding-4 radius-md bg-brand-neutral-lightest">
             <h1 className={`font-sans-2xl margin-0 text-brand-primary ${style.heading}`}>{portfolio.name}</h1>
             <h2 className={divisionClasses}>{portfolio.division?.name}</h2>
-            <PortfolioTeamLeaders />
+            <TeamLeaders teamLeaders={portfolio.team_leaders} />
             <PortfolioDescription />
         </section>
     );
