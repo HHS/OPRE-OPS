122124d00137b1bc5aae8cde9cab2071400f92ce
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2023-01-20 15:31:54 -0600

    Ops 465 breadcrumbs (#642)
    - updates to createBrowserRouter
    * adds breadcrumbs to detail pages
    - no value-added on list pages
    - opts for uswds helper classes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Link, Navigate } from "react-router-dom";
 import store from "./store";
 
 import "./uswds/css/styles.css";
@@ -18,27 +17,57 @@ import PeopleAndTeams from "./components/Portfolios/PeopleAndTeams/PeopleAndTeam
 import BudgetAndFunding from "./components/Portfolios/BudgetAndFunding/BudgetAndFunding";
 import ResearchProjectDetail from "./pages/researchProjects/detail/ResearchProjectDetail";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolios" element={<PortfolioList />} />
+            <Route
+                path="/portfolios/:id"
+                element={<PortfolioDetail />}
+                handle={{
+                    // you can put whatever you want on a route handle
+                    // here we use "crumb" and return some elements,
+                    // this is what we'll render in the breadcrumbs
+                    // for this route
+                    crumb: () => (
+                        <Link to="/portfolios" className="text-primary">
+                            Portfolios
+                        </Link>
+                    ),
+                }}
+            >
+                {/* Default to BudgetAndFunding */}
+                <Route exact path="" element={<Navigate to={"budget-and-funding"} />} />
+                <Route path="budget-and-funding" element={<BudgetAndFunding />} />
+                <Route path="research-projects" element={<ResearchProjects />} />
+                <Route path="people-and-teams" element={<PeopleAndTeams />} />
+            </Route>
+            <Route path="/cans" element={<CanList />} />
+            <Route
+                path="/cans/:id"
+                element={<CanDetail />}
+                handle={{
+                    // you can put whatever you want on a route handle
+                    // here we use "crumb" and return some elements,
+                    // this is what we'll render in the breadcrumbs
+                    // for this route
+                    crumb: () => (
+                        <Link to="/cans" className="text-primary">
+                            Cans
+                        </Link>
+                    ),
+                }}
+            />
+            <Route path="/research-projects/:id" element={<ResearchProjectDetail />} />
+        </>
+    )
+);
 
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/portfolios" element={<PortfolioList />} />
-                    <Route path="/portfolios/:id" element={<PortfolioDetail />}>
-                        {/* Default to BudgetAndFunding */}
-                        <Route exact path="" element={<Navigate to={"budget-and-funding"} />} />
-                        <Route path="budget-and-funding" element={<BudgetAndFunding />} />
-                        <Route path="research-projects" element={<ResearchProjects />} />
-                        <Route path="people-and-teams" element={<PeopleAndTeams />} />
-                    </Route>
-                    <Route path="/cans" element={<CanList />} />
-                    <Route path="/cans/:id" element={<CanDetail />} />
-                    <Route path="/research-projects/:id" element={<ResearchProjectDetail />} />
-                </Routes>
-            </BrowserRouter>
+            <RouterProvider router={router} />
         </Provider>
     </React.StrictMode>
 );
