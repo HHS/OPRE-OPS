c302956ca8883b6a97cf33679fbb57b4fdb947ea
John DeAngelis <john.deangelis2@gmail.com>
2023-01-21 16:33:37 -0500

    add origination date formatting

--- frontend/src/components/ResearchProjects/HeroFooter/HeroFooter.jsx
+++ frontend/src/components/ResearchProjects/HeroFooter/HeroFooter.jsx
@@ -1,12 +1,23 @@
 import Tag from "../../UI/Tag/Tag";
+import { useSelector } from "react-redux";
 
 const HeroFooter = () => {
+    const researchProject = useSelector((state) => state.researchProject.researchProject);
+
+    const formatOptions = {
+        month: "short",
+        year: "numeric",
+        day: "numeric",
+    };
+
+    const originationDate = new Date(researchProject.origination_date).toLocaleDateString("en-US", formatOptions);
+
     return (
         <div className={`margin-top-2 padding-0 display-flex text-bold font-sans-xs`}>
             <span className={`flex-2`}>
                 Origination Date
                 <div className={"margin-top-1"}>
-                    <Tag text={"Jan 9, 2018"} tagStyle="lightTextDarkBackground" />
+                    <Tag text={originationDate} tagStyle="lightTextDarkBackground" />
                 </div>
             </span>
             <span className={`flex-3`}>Methodologies</span>
