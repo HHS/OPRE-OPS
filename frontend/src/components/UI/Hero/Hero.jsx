da1ce48eca488df814fa045da60f4b0d296c0f6c
John DeAngelis <john.deangelis2@gmail.com>
2023-01-23 15:00:04 -0500

    fix typo

--- frontend/src/components/UI/Hero/Hero.jsx
+++ frontend/src/components/UI/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import HeroDescription from "../../UI/HeroDescription/HeroDescription";
 const Hero = (props) => {
     return (
         <section className={`${props.backgroundColor && "padding-4"} radius-md ${props.backgroundColor}`}>
-            <h1 className={`font-sans-2xl margin-0 text-brand-primary style={backgroundColor && { maxWidth: "70%" }`}>
+            <h1 className={`font-sans-2xl margin-0 text-brand-primary`} style={{ maxWidth: "70%" }}>
                 {props.entityName}
             </h1>
             <h2 className={`font-sans-3xs text-normal margin-top-1 margin-bottom-2`}>{props.divisionName}</h2>
