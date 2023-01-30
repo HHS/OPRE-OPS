f098d0abe0dadbcedfa0886e0bddd99019c43aa3
John DeAngelis <john.deangelis2@gmail.com>
2023-01-30 13:25:19 -0500

    change paths to deployed_run.sh

--- backend/ops_api/deployed_run.sh
+++ backend/ops_api/deployed_run.sh
@@ -3,9 +3,10 @@
 set -eo pipefail
 
 export PYTHONPATH=".:..:${PYTHONPATH}"
-export FLASK_APP=ops_api
+export FLASK_APP=ops_api.ops
 export FLASK_DEBUG=true
 
 cd ..
-python -m gunicorn -b ":8080" "ops_api:create_app()"
+ls -la
+python -m gunicorn -b ":8080" --log-level DEBUG "ops_api.ops:create_app()"
 #python -m flask run --port=8080 --host=0.0.0.0
