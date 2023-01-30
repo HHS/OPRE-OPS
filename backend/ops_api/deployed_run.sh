5ac1ce5697c8df01db5851765d036e1e8dfd7ddd
John DeAngelis <john.deangelis2@gmail.com>
2023-01-30 13:52:01 -0500

    change paths to deployed_run.sh

--- backend/ops_api/deployed_run.sh
+++ backend/ops_api/deployed_run.sh
@@ -11,5 +11,5 @@ cd ops_api
 python -m pip install --no-cache-dir --upgrade pip==22.2.2 pipenv==2022.10.12
 python -m pipenv install --dev --system --deploy
 cd ..
-python -m gunicorn -b ":8080" --log-level DEBUG "ops_api.ops:create_app()"
-#python -m flask run --port=8080 --host=0.0.0.0
+#python -m gunicorn -b ":8080" --log-level DEBUG "ops_api.ops:create_app()"
+python -m flask run --port=8080 --host=0.0.0.0
