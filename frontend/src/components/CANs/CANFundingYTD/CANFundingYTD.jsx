0d4a2ecc2b747e08ece113ce6d4759834ede4e9b
fpigeonjr <frank.pigeonjr@gmail.com>
2023-01-31 16:33:28 -0600

    allows  for passing in classes

--- frontend/src/components/CANs/CANFundingYTD/CANFundingYTD.jsx
+++ frontend/src/components/CANs/CANFundingYTD/CANFundingYTD.jsx
@@ -2,20 +2,20 @@ import styles from "./styles.module.css";
 import CurrencyWithSmallCents from "../../UI/CurrencyWithSmallCents/CurrencyWithSmallCents";
 import CANFundingBar from "../CANFundingBar/CANFundingBar";
 
-const CANFundingYTD = ({ current_funding, expected_funding }) => {
+const CANFundingYTD = ({ current_funding, expected_funding, className = "" }) => {
     const gridRowText = `grid-row padding-top-1 ${styles.gridRowText}`;
 
     return (
-        <div>
-            <div className="grid-row padding-top-1 padding-bottom-1">
-                <div className="grid-col-2">
+        <div className={className}>
+            <div className="grid-row">
+                <div className="grid-col-3">
                     <CurrencyWithSmallCents
                         amount={current_funding || 0}
                         dollarsClasses="font-sans-3xs text-bold"
                         centsStyles={{ fontSize: "8px" }}
                     />
                 </div>
-                <div className="grid-col-2 grid-offset-8">
+                <div className="grid-col-3 grid-offset-6">
                     <div className={styles.right}>
                         <CurrencyWithSmallCents
                             amount={expected_funding || 0}
--- frontend/src/components/UI/Tag/Tag.jsx
+++ frontend/src/components/UI/Tag/Tag.jsx
@@ -1,13 +1,13 @@
 import styles from "./styles.module.css";
 
-const Tag = ({ tagStyle, text }) => {
+const Tag = ({ tagStyle, text, className = "" }) => {
     let tagClasses;
     if (tagStyle === "darkTextLightBackground") {
-        tagClasses = `bg-brand-neutral-lightest text-brand-neutral-dark font-sans-3xs padding-1 ${styles.tag}`;
+        tagClasses = `bg-brand-neutral-lightest text-brand-neutral-dark font-sans-3xs padding-05 ${styles.tag}`;
     } else if (tagStyle === "lightTextDarkBackground") {
-        tagClasses = `bg-brand-dataviz-dark-blue text-brand-neutral-lightest font-sans-3xs padding-1 ${styles.tag}`;
+        tagClasses = `bg-brand-dataviz-dark-blue text-brand-neutral-lightest font-sans-3xs padding-05 ${styles.tag}`;
     }
-    return <div className={tagClasses}>{text}</div>;
+    return <span className={`${tagClasses} ${className}`}>{text}</span>;
 };
 
 export default Tag;
