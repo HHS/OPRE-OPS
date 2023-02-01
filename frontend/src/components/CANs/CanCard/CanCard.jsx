ab2a2df4d91a4e43963052ef1f3b74ba9d71ed0f
fpigeonjr <frank.pigeonjr@gmail.com>
2023-02-01 16:04:02 -0600

    refactored to grid layout

--- frontend/src/components/CANs/CanCard/CanCard.jsx
+++ frontend/src/components/CANs/CanCard/CanCard.jsx
@@ -1,5 +1,6 @@
 import style from "./styles.module.css";
 import CurrencyWithSmallCents from "../../UI/CurrencyWithSmallCents/CurrencyWithSmallCents";
+import CurrencyFormat from "react-currency-format";
 import Tag from "../../UI/Tag/Tag";
 import CANFundingYTD from "../CANFundingYTD/CANFundingYTD";
 import { useEffect, useState } from "react";
@@ -12,7 +13,7 @@ import { faCircle } from "@fortawesome/free-solid-svg-icons";
 const CanCard = ({ can, fiscalYear }) => {
     /* Styling */
     const sectionClasses = `${style.container}`;
-    const leftMarginClasses = `padding-y-205 padding-x-105 ${style.leftMarginContainer}`;
+    const leftMarginClasses = `padding-y-205 ${style.leftMarginContainer}`;
     const cardBodyClasses = `padding-left-3 padding-top-2 ${style.cardBodyDiv}`;
     const fundingYTDClasses = `padding-left-0 grid-container ${style.fundingYTD}`;
     const budgetStatusClasses = `${style.budgetStatus}`;
@@ -38,26 +39,24 @@ const CanCard = ({ can, fiscalYear }) => {
             <div className={sectionClasses}>
                 <dl className={`margin-0 ${leftMarginClasses}`}>
                     <div>
-                        <dt className="font-sans-3xs margin-0 text-brand-neutral">CAN</dt>
-                        <dd className="font-sans-3xs text-semibold margin-0">{can.number}</dd>
+                        <dt className="margin-0 text-brand-neutral">CAN</dt>
+                        <dd className="text-semibold margin-0">{can.number}</dd>
                     </div>
                     <div className="margin-y-3">
-                        <dt className="font-sans-3xs margin-0 text-brand-neutral">Description</dt>
-                        <dd className="font-sans-3xs text-semibold margin-0">{can.nickname}</dd>
+                        <dt className="margin-0 text-brand-neutral">Description</dt>
+                        <dd className="text-semibold margin-0">{can.nickname}</dd>
                     </div>
                     <div className="margin-y-3">
-                        <dt className="font-sans-3xs margin-0 text-brand-neutral">Appropriation</dt>
-                        <dd className="font-sans-3xs text-semibold margin-0">
+                        <dt className="margin-0 text-brand-neutral">Appropriation</dt>
+                        <dd className="text-semibold margin-0">
                             {/* TODO: Get value from backend */}
                             {can.appropriation_date ? can.appropriation_date : "99/99/9999"} ({can.appropriation_term}{" "}
                             {can.appropriation_term > 1 ? "years" : "year"})
                         </dd>
                     </div>
                     <div className="margin-y-3">
-                        <dt className="font-sans-3xs margin-0 text-brand-neutral">Expiration</dt>
-                        <dd className="font-sans-3xs text-semibold margin-0">
-                            {canFundingData?.expiration_date || "---"}
-                        </dd>
+                        <dt className="margin-0 text-brand-neutral">Expiration</dt>
+                        <dd className="text-semibold margin-0">{canFundingData?.expiration_date || "---"}</dd>
                     </div>
                     {/* <div className="margin-y-2">
                         <h2 className="font-sans-3xs margin-0 text-brand-neutral">FY Total Budget</h2>
@@ -70,9 +69,9 @@ const CanCard = ({ can, fiscalYear }) => {
                         </p>
                     </div> */}
                 </dl>
-                <div className="grid-row grid-gap border-accent-cool border-1px padding-205">
-                    {/* LEFT SIDE */}
-                    <div className="grid-col">
+                <div className="grid-row border-accent-cool border-1px padding-205">
+                    {/*NOTE: LEFT SIDE */}
+                    <div className="grid-col-5">
                         <h4 className="font-sans-3xs text-normal">FY {fiscalYear} CAN Total Funding</h4>
                         <CANFundingYTD
                             className="margin-top-5"
@@ -135,47 +134,98 @@ const CanCard = ({ can, fiscalYear }) => {
                             </div>
                         </div>
                     </div>
-                    {/* RIGHT SIDE */}
-                    <div className="tablet:grid-col">
+                    {/* NOTE: RIGHT SIDE */}
+                    <div className="grid-col margin-left-5">
                         <h4 className="font-sans-3xs text-normal">FY {fiscalYear} CAN Budget Status</h4>
 
                         <div className="display-flex flex-justify">
-                            <div>
-                                <div className="display-flex flex-justify">
-                                    <FontAwesomeIcon
-                                        icon={faCircle}
-                                        className="height-2 width-2 margin-right-1px text-brand-dataviz-blue"
-                                    />
-                                    <span>Available</span>
-                                    <span className="margin-left-2">$1,000,000.00</span>
-                                    <Tag tagStyle="darkTextLightBackground" text="20%" className="margin-left-1" />
+                            <div className="grid-row maxw-card-lg font-12px">
+                                <div className="grid-col-5">
+                                    <div className="display-flex flex-align-center">
+                                        <FontAwesomeIcon
+                                            icon={faCircle}
+                                            className="height-1 width-1 margin-right-1px text-brand-dataviz-level-1"
+                                        />
+                                        <span>Available</span>
+                                    </div>
                                 </div>
-                                <div className="display-flex flex-justify margin-top-1">
-                                    <FontAwesomeIcon
-                                        icon={faCircle}
-                                        className="height-2 width-2 margin-right-1px text-brand-dataviz-blue"
+                                <div className="grid-col-5">
+                                    <CurrencyFormat
+                                        value={canFundingData?.available_funding}
+                                        displayType={"text"}
+                                        thousandSeparator={true}
+                                        prefix={"$ "}
+                                        renderText={(value) => <span className="">{value}</span>}
                                     />
-                                    <span>Planned</span>
-                                    <span className="margin-left-2">$1,000,000.00</span>
-                                    <Tag tagStyle="darkTextLightBackground" text="20%" className="margin-left-1" />
                                 </div>
-                                <div className="display-flex flex-justify margin-top-1">
-                                    <FontAwesomeIcon
-                                        icon={faCircle}
-                                        className="height-2 width-2 margin-right-1px text-brand-dataviz-blue"
-                                    />
-                                    <span>Executing</span>
-                                    <span className="margin-left-2">$1,000,000.00</span>
-                                    <Tag tagStyle="darkTextLightBackground" text="20%" className="margin-left-1" />
+                                <div className="grid-col-2">
+                                    <Tag tagStyle="darkTextLightBackground" text="20%" />
                                 </div>
-                                <div className="display-flex flex-justify margin-top-1">
-                                    <FontAwesomeIcon
-                                        icon={faCircle}
-                                        className="height-2 width-2 margin-right-1px text-brand-dataviz-blue"
+                                {/* loop2 */}
+                                <div className="grid-col-5">
+                                    <div className="display-flex flex-align-center">
+                                        <FontAwesomeIcon
+                                            icon={faCircle}
+                                            className="height-1 width-1 margin-right-1px text-brand-dataviz-level-2"
+                                        />
+                                        <span>Planned</span>
+                                    </div>
+                                </div>
+                                <div className="grid-col-5">
+                                    <CurrencyFormat
+                                        value={canFundingData?.planned_funding}
+                                        displayType={"text"}
+                                        thousandSeparator={true}
+                                        prefix={"$ "}
+                                        renderText={(value) => <span>{value}</span>}
                                     />
-                                    <span>Obligated</span>
-                                    <span className="margin-left-2">$1,000,000.00</span>
-                                    <Tag tagStyle="darkTextLightBackground" text="20%" className="margin-left-1" />
+                                </div>
+                                <div className="grid-col-2">
+                                    <Tag tagStyle="darkTextLightBackground" text="20%" />
+                                </div>
+                                {/* loop3 */}
+                                <div className="grid-col-5">
+                                    <div className="display-flex flex-align-center">
+                                        <FontAwesomeIcon
+                                            icon={faCircle}
+                                            className="height-1 width-1 margin-right-1px text-brand-dataviz-level-3"
+                                        />
+                                        <span>Executing</span>
+                                    </div>
+                                </div>
+                                <div className="grid-col-5">
+                                    <CurrencyFormat
+                                        value={canFundingData?.in_execution_funding}
+                                        displayType={"text"}
+                                        thousandSeparator={true}
+                                        prefix={"$ "}
+                                        renderText={(value) => <span>{value}</span>}
+                                    />
+                                </div>
+                                <div className="grid-col-2">
+                                    <Tag tagStyle="darkTextLightBackground" text="20%" />
+                                </div>
+                                {/* loop4 */}
+                                <div className="grid-col-5">
+                                    <div className="display-flex flex-align-center">
+                                        <FontAwesomeIcon
+                                            icon={faCircle}
+                                            className="height-1 width-1 margin-right-1px text-brand-dataviz-level-4"
+                                        />
+                                        <span>Obligated</span>
+                                    </div>
+                                </div>
+                                <div className="grid-col-5">
+                                    <CurrencyFormat
+                                        value={canFundingData?.obligated_funding}
+                                        displayType={"text"}
+                                        thousandSeparator={true}
+                                        prefix={"$ "}
+                                        renderText={(value) => <span>{value}</span>}
+                                    />
+                                </div>
+                                <div className="grid-col-2">
+                                    <Tag tagStyle="darkTextLightBackground" text="20%" />
                                 </div>
                             </div>
 
--- frontend/src/components/UI/Tag/Tag.jsx
+++ frontend/src/components/UI/Tag/Tag.jsx
@@ -3,9 +3,9 @@ import styles from "./styles.module.css";
 const Tag = ({ tagStyle, text, className = "" }) => {
     let tagClasses;
     if (tagStyle === "darkTextLightBackground") {
-        tagClasses = `bg-brand-neutral-lightest text-brand-neutral-dark font-sans-3xs padding-05 ${styles.tag}`;
+        tagClasses = `bg-brand-neutral-lightest text-brand-neutral-dark font-12px padding-05 ${styles.tag}`;
     } else if (tagStyle === "lightTextDarkBackground") {
-        tagClasses = `bg-brand-dataviz-dark-blue text-brand-neutral-lightest font-sans-3xs padding-05 ${styles.tag}`;
+        tagClasses = `bg-brand-dataviz-dark-blue text-brand-neutral-lightest font-12px padding-05 ${styles.tag}`;
     }
     return <span className={`${tagClasses} ${className}`}>{text}</span>;
 };
