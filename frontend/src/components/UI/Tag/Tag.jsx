99576a0bc543a7f5f1f846b2afdcb09fd643af51
fpigeonjr <frank.pigeonjr@gmail.com>
2023-02-09 18:12:07 -0600

    adds active styles

--- frontend/src/components/UI/Tag/Tag.jsx
+++ frontend/src/components/UI/Tag/Tag.jsx
@@ -1,13 +1,36 @@
 import styles from "./styles.module.css";
-
-const Tag = ({ tagStyle, text, className = "" }) => {
+import PropTypes from "prop-types";
+const Tag = ({ tagStyle, text, active = false, label }) => {
     let tagClasses;
+    let activeStyle = "";
+
     if (tagStyle === "darkTextLightBackground") {
-        tagClasses = `bg-brand-neutral-lightest text-brand-neutral-dark font-12px padding-05 ${styles.tag}`;
+        tagClasses = `${styles.tag} bg-brand-neutral-lightest text-brand-neutral-dark font-12px padding-05`;
     } else if (tagStyle === "lightTextDarkBackground") {
-        tagClasses = `bg-brand-dataviz-dark-blue text-brand-neutral-lightest font-12px padding-05 ${styles.tag}`;
+        tagClasses = `${styles.tag} bg-brand-dataviz-dark-blue text-brand-neutral-lightest font-12px padding-05`;
     }
-    return <span className={`${tagClasses} ${className}`}>{text}</span>;
+
+    if (active && label === "Available") {
+        activeStyle = "bg-brand-dataviz-level-1 text-white text-bold";
+    }
+    if (active && label === "Planned") {
+        activeStyle = "bg-brand-dataviz-level-2 text-white text-bold";
+    }
+    if (active && label === "Executing") {
+        activeStyle = "bg-brand-dataviz-level-3 text-bold";
+    }
+    if (active && label === "Obligated") {
+        activeStyle = "bg-brand-dataviz-level-4 text-white text-bold";
+    }
+
+    return <span className={`${tagClasses} ${activeStyle}`}>{text}</span>;
 };
 
 export default Tag;
+
+Tag.propTypes = {
+    tagStyle: PropTypes.string,
+    text: PropTypes.string,
+    active: PropTypes.bool,
+    label: PropTypes.string,
+};
