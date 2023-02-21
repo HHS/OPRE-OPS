4ded42ea3439b402ce1275f3ef05bc4fab0d5bb1
fpigeonjr <frank.pigeonjr@gmail.com>
2023-02-21 10:29:20 -0600

    starts tables

--- frontend/src/components/Portfolios/ResearchProjects/ResearchProjects.jsx
+++ frontend/src/components/Portfolios/ResearchProjects/ResearchProjects.jsx
@@ -22,7 +22,7 @@ const ResearchProjects = () => {
             funding: "6000000",
             fundingToDate: "1900000",
             firstAwardDate: "2018-01-01",
-            cans: 3,
+            cans: "3",
             agreement: "Grant",
         },
         {
@@ -32,7 +32,7 @@ const ResearchProjects = () => {
             funding: "8000000",
             fundingToDate: "1000000",
             firstAwardDate: "2020-01-01",
-            cans: 2,
+            cans: "2",
             agreement: "Mixed",
         },
         {
@@ -42,7 +42,7 @@ const ResearchProjects = () => {
             funding: "1000000",
             fundingToDate: "0",
             firstAwardDate: "2022-01-01",
-            cans: 1,
+            cans: "1",
             agreement: "Contract",
         },
         {
@@ -52,7 +52,7 @@ const ResearchProjects = () => {
             funding: "1000000",
             fundingToDate: "0",
             firstAwardDate: "2022-01-01",
-            cans: 4,
+            cans: "4",
             agreement: "Contract",
         },
         {
@@ -62,15 +62,30 @@ const ResearchProjects = () => {
             funding: "0",
             fundingToDate: "1000000",
             firstAwardDate: "2022-01-01",
-            cans: 5,
+            cans: "5",
             agreement: "Contract",
         },
     ];
+
+    const filteredResearchProjects = data.filter((project) => project.type === "research");
+    const filteredAdminAndSupportProjects = data.filter((project) => project.type === "admin_and_support");
+
     const researchProjectData = researchProjects.map((rp) => (
         <li key={rp.id}>
             <Link to={`/research-projects/${rp.id}`}>{rp.title}</Link>
         </li>
     ));
+
+    const TableRow = ({ name, funding, fundingToDate, firstAwardDate, cans, agreement }) => (
+        <tr>
+            <th scope="row">{name}</th>
+            <td data-sort-value="">{funding}</td>
+            <td data-sort-value="">{fundingToDate}</td>
+            <td data-sort-value="">{firstAwardDate}</td>
+            <td data-sort-value="">{cans}</td>
+            <td data-sort-value="">{agreement}</td>
+        </tr>
+    );
 
     // Get ResearchProject data
     useEffect(() => {
@@ -98,6 +113,125 @@ const ResearchProjects = () => {
             <div className="display-flex flex-justify">
                 <ResearchBudgetVsSpending portfolioId={portfolioId} />
             </div>
+            <h2 className="font-sans-lg">Research Projects</h2>
+            <p className="font-sans-sm">
+                This is a list of all active research projects that this portfolio contributes to for the selected
+                fiscal year.
+            </p>
+
+            <div className="usa-table-container--scrollable" tabIndex="0">
+                <table className="usa-table usa-table--borderless">
+                    <caption></caption>
+                    <thead>
+                        <tr>
+                            <th data-sortable scope="col" role="columnheader">
+                                Project Name
+                                <button
+                                    tabIndex="0"
+                                    className="usa-table__header__button"
+                                    title="Click to sort by Alphabetical in ascending order."
+                                >
+                                    <svg className="usa-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <g className="descending" fill="transparent">
+                                            <path d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"></path>
+                                        </g>
+                                        <g className="ascending" fill="transparent">
+                                            <path
+                                                transform="rotate(180, 12, 12)"
+                                                d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"
+                                            ></path>
+                                        </g>
+                                        <g className="unsorted" fill="transparent">
+                                            <polygon points="15.17 15 13 17.17 13 6.83 15.17 9 16.58 7.59 12 3 7.41 7.59 8.83 9 11 6.83 11 17.17 8.83 15 7.42 16.41 12 21 16.59 16.41 15.17 15"></polygon>
+                                        </g>
+                                    </svg>
+                                </button>
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                FY {fiscalYear.value} Funding
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                Funding to Date
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                First Award
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                CANs
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                Agreements
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredResearchProjects.map((tableData) => (
+                            <TableRow key={tableData.id} {...tableData} />
+                        ))}
+                    </tbody>
+                </table>
+                <div className="usa-sr-only usa-table__announcement-region" aria-live="polite"></div>
+            </div>
+            <h2 className="font-sans-lg">Administrative & Support Projects</h2>
+            <p className="font-sans-sm">
+                This is a list of all active administrative & support projects that this portfolio contributes to for
+                the selected fiscal year.
+            </p>
+            {/* TODO: add Admin and Support table */}
+            <div className="usa-table-container--scrollable" tabIndex="0">
+                <table className="usa-table usa-table--borderless">
+                    <caption></caption>
+                    <thead>
+                        <tr>
+                            <th data-sortable scope="col" role="columnheader">
+                                Project Name
+                                <button
+                                    tabIndex="0"
+                                    className="usa-table__header__button"
+                                    title="Click to sort by Alphabetical in ascending order."
+                                >
+                                    <svg className="usa-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <g className="descending" fill="transparent">
+                                            <path d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"></path>
+                                        </g>
+                                        <g className="ascending" fill="transparent">
+                                            <path
+                                                transform="rotate(180, 12, 12)"
+                                                d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"
+                                            ></path>
+                                        </g>
+                                        <g className="unsorted" fill="transparent">
+                                            <polygon points="15.17 15 13 17.17 13 6.83 15.17 9 16.58 7.59 12 3 7.41 7.59 8.83 9 11 6.83 11 17.17 8.83 15 7.42 16.41 12 21 16.59 16.41 15.17 15"></polygon>
+                                        </g>
+                                    </svg>
+                                </button>
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                FY {fiscalYear.value} Funding
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                Funding to Date
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                First Award
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                CANs
+                            </th>
+                            <th data-sortable scope="col" role="columnheader">
+                                Agreements
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredAdminAndSupportProjects.map((tableData) => (
+                            <TableRow key={tableData.id} {...tableData} />
+                        ))}
+                    </tbody>
+                </table>
+                <div className="usa-sr-only usa-table__announcement-region" aria-live="polite"></div>
+            </div>
+            {/* NOTE: Not sure what to do with this */}
             {researchProjects.length > 0 && <ul>{researchProjectData}</ul>}
             {!researchProjectData && <p>There are no Research Projects.</p>}
         </section>
