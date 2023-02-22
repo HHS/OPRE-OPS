6f2b97453920765c0c6dff668898da5ad1d40f71
fpigeonjr <frank.pigeonjr@gmail.com>
2023-02-22 16:12:30 -0600

    layout improvements on table

--- frontend/src/components/Portfolios/ResearchProjects/ResearchProjects.jsx
+++ frontend/src/components/Portfolios/ResearchProjects/ResearchProjects.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
+import CurrencyFormat from "react-currency-format";
 import { setResearchProjects } from "../../../pages/portfolios/detail/portfolioSlice";
 import { getResearchProjects } from "../../../pages/portfolios/detail/getResearchProjects";
 import ResearchBudgetVsSpending from "./ResearchBudgetVsSpending";
@@ -25,15 +26,49 @@ const ResearchProjects = () => {
         </li>
     ));
 
+    const SortIcon = () => (
+        <button
+            tabIndex="0"
+            className="usa-table__header__button"
+            title="Click to sort by Alphabetical in ascending order."
+        >
+            <svg className="usa-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g className="descending" fill="transparent">
+                    <path d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"></path>
+                </g>
+                <g className="ascending" fill="transparent">
+                    <path
+                        transform="rotate(180, 12, 12)"
+                        d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"
+                    ></path>
+                </g>
+                <g className="unsorted" fill="transparent">
+                    <polygon points="15.17 15 13 17.17 13 6.83 15.17 9 16.58 7.59 12 3 7.41 7.59 8.83 9 11 6.83 11 17.17 8.83 15 7.42 16.41 12 21 16.59 16.41 15.17 15"></polygon>
+                </g>
+            </svg>
+        </button>
+    );
+
     const TableRow = ({ name, link, funding, fundingToDate, firstAwardDate, cans, agreement }) => (
         <tr>
             <th scope="row">
-                <Link className="text-ink text-no-underline hover:text-underline" to={link}>
-                    {name}
+                <Link
+                    to={link}
+                    className="text-ink text-no-underline hover:text-underline usa-tooltip"
+                    data-position="top"
+                    // only show tooltip if name is longer than 30 characters
+                    title={name.length > 30 ? name : ""}
+                >
+                    {/* truncate to 30 characters */}
+                    {name.length > 30 ? name.substring(0, 30) + "..." : name}
                 </Link>
             </th>
-            <td data-sort-value="">{funding}</td>
-            <td data-sort-value="">{fundingToDate}</td>
+            <td data-sort-value="">
+                <CurrencyFormat value={funding} displayType={"text"} thousandSeparator={true} prefix={"$"} />
+            </td>
+            <td data-sort-value="">
+                <CurrencyFormat value={fundingToDate} displayType={"text"} thousandSeparator={true} prefix={"$"} />
+            </td>
             <td data-sort-value="">{firstAwardDate}</td>
             <td data-sort-value="">{cans}</td>
             <td data-sort-value="">{agreement}</td>
@@ -62,6 +97,7 @@ const ResearchProjects = () => {
                 selected fiscal year. An active project has active work happening. It might have funding from a previous
                 fiscal year or no funding within the fiscal year.
             </p>
+            {/* TODO: remove when done TS */}
             {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
             <div className="display-flex flex-justify">
                 <ResearchBudgetVsSpending portfolioId={portfolioId} />
@@ -82,43 +118,24 @@ const ResearchProjects = () => {
                     <table className="usa-table usa-table--borderless">
                         <thead>
                             <tr>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th
+                                    data-sortable
+                                    scope="col"
+                                    role="columnheader"
+                                    style={{ paddingRight: 0, width: "32%" }}
+                                >
                                     Project Name
-                                    <button
-                                        tabIndex="0"
-                                        className="usa-table__header__button"
-                                        title="Click to sort by Alphabetical in ascending order."
-                                    >
-                                        <svg
-                                            className="usa-icon"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 24 24"
-                                        >
-                                            <g className="descending" fill="transparent">
-                                                <path d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"></path>
-                                            </g>
-                                            <g className="ascending" fill="transparent">
-                                                <path
-                                                    transform="rotate(180, 12, 12)"
-                                                    d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"
-                                                ></path>
-                                            </g>
-                                            <g className="unsorted" fill="transparent">
-                                                <polygon points="15.17 15 13 17.17 13 6.83 15.17 9 16.58 7.59 12 3 7.41 7.59 8.83 9 11 6.83 11 17.17 8.83 15 7.42 16.41 12 21 16.59 16.41 15.17 15"></polygon>
-                                            </g>
-                                        </svg>
-                                    </button>
                                 </th>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th data-sortable scope="col" role="columnheader" style={{ paddingRight: 0 }}>
                                     FY {fiscalYear.value} Funding
                                 </th>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th data-sortable scope="col" role="columnheader" style={{ paddingRight: 0 }}>
                                     Funding to Date
                                 </th>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th data-sortable scope="col" role="columnheader" style={{ paddingRight: 0 }}>
                                     First Award
                                 </th>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th data-sortable scope="col" role="columnheader" style={{ paddingRight: 0 }}>
                                     CANs
                                 </th>
                                 <th data-sortable scope="col" role="columnheader">
@@ -146,43 +163,24 @@ const ResearchProjects = () => {
                     <table className="usa-table usa-table--borderless">
                         <thead>
                             <tr>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th
+                                    data-sortable
+                                    scope="col"
+                                    role="columnheader"
+                                    style={{ paddingRight: 0, width: "32%" }}
+                                >
                                     Project Name
-                                    <button
-                                        tabIndex="0"
-                                        className="usa-table__header__button"
-                                        title="Click to sort by Alphabetical in ascending order."
-                                    >
-                                        <svg
-                                            className="usa-icon"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 24 24"
-                                        >
-                                            <g className="descending" fill="transparent">
-                                                <path d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"></path>
-                                            </g>
-                                            <g className="ascending" fill="transparent">
-                                                <path
-                                                    transform="rotate(180, 12, 12)"
-                                                    d="M17 17L15.59 15.59L12.9999 18.17V2H10.9999V18.17L8.41 15.58L7 17L11.9999 22L17 17Z"
-                                                ></path>
-                                            </g>
-                                            <g className="unsorted" fill="transparent">
-                                                <polygon points="15.17 15 13 17.17 13 6.83 15.17 9 16.58 7.59 12 3 7.41 7.59 8.83 9 11 6.83 11 17.17 8.83 15 7.42 16.41 12 21 16.59 16.41 15.17 15"></polygon>
-                                            </g>
-                                        </svg>
-                                    </button>
                                 </th>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th data-sortable scope="col" role="columnheader" style={{ paddingRight: 0 }}>
                                     FY {fiscalYear.value} Funding
                                 </th>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th data-sortable scope="col" role="columnheader" style={{ paddingRight: 0 }}>
                                     Funding to Date
                                 </th>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th data-sortable scope="col" role="columnheader" style={{ paddingRight: 0 }}>
                                     First Award
                                 </th>
-                                <th data-sortable scope="col" role="columnheader">
+                                <th data-sortable scope="col" role="columnheader" style={{ paddingRight: 0 }}>
                                     CANs
                                 </th>
                                 <th data-sortable scope="col" role="columnheader">
