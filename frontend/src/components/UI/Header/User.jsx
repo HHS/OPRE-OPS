d46ca5724d560f4083cd9bed2b9fb539840aa09d
fpigeonjr <frank.pigeonjr@gmail.com>
2023-02-28 14:21:01 -0600

    another attempt

--- frontend/src/components/UI/Header/User.jsx
+++ frontend/src/components/UI/Header/User.jsx
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 
 export const User = () => {
     const user = useSelector((state) => state.auth.activeUser);
-    const doesUserHaveName = user?.first_name && user?.last_name;
-    const userFullName = user.first_name + " " + user.last_name;
 
-    return (
-        // eslint-disable-next-line jsx-a11y/anchor-is-valid
-        <Link to={"/users/" + user?.id}>{user ? (doesUserHaveName ? userFullName : user.email) : "---->"}</Link>
-    );
+    if (user) {
+        return <Link to={`/users/${user?.id}`}>{user?.email}</Link>;
+    }
+    return <span></span>;
 };
