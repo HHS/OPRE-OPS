76d3a4032c7ddb91f54a74acd63c80290d053589
Tim Donaworth <tdonaworth@flexion.us>
2023-03-01 10:59:02 -0500

    Added ProtectedRoute, and enforced auth on a Menu component

--- frontend/src/components/Auth/auth.js
+++ frontend/src/components/Auth/auth.js
@@ -1,5 +1,6 @@
 import ApplicationContext from "../../applicationContext/ApplicationContext";
 import cryptoRandomString from "crypto-random-string";
+import { useSelector } from "react-redux";
 
 const authConfig = ApplicationContext.get().helpers().authConfig;
 
@@ -13,4 +14,32 @@ export const getAuthorizationCode = (stateToken) => {
     providerUrl.searchParams.set("state", stateToken);
     providerUrl.searchParams.set("nonce", cryptoRandomString({ length: 64 }));
     return providerUrl;
+};
+
+export const logoutUser = async (stateToken) => {
+    // As documented here: https://developers.login.gov/oidc/
+    // Example:
+    //
+    // https://idp.int.identitysandbox.gov/openid_connect/logout?
+    //   client_id=${CLIENT_ID}&
+    //   post_logout_redirect_uri=${REDIRECT_URI}&
+    //   state=abcdefghijklmnopabcdefghijklmnop
+    const providerLogout = new URL(authConfig.loginGovLogoutEndpoint);
+    providerLogout.searchParams.set("client_id", authConfig.client_id);
+    providerLogout.searchParams.set("post_logout_redirect_uri", window.location.hostname);
+    providerLogout.searchParams.set("state", stateToken);
+    return providerLogout;
+};
+
+export const CheckAuth = () => {
+    // TODO: We'll most likely want to include multiple checks here to determine if
+    // the user is correctly authenticated and authorized. Hook into the Auth service
+    // at some point.
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn) || false;
+    const tokenExists = localStorage.getItem("access_token");
+    // TODO: Verify access_token's signature
+    // TODO: Verify access_token's claims
+    // TODO: Verify access_token's expiration - maybe perform a refresh()?
+    // TODO: Check Authorization
+    return isLoggedIn && tokenExists; // && payload;
 };
