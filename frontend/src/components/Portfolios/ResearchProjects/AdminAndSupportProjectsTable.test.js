01df3ea1ae74870d8b1517964e2f6fd71b6354ff
fpigeonjr <frank.pigeonjr@gmail.com>
2023-03-02 16:04:52 -0600

    adds basic tests

---
+++ frontend/src/components/Portfolios/ResearchProjects/AdminAndSupportProjectsTable.test.js
@@ -0,0 +1,15 @@
+import store from "../../../store";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import AdminAndSupportProjectsTable from "./AdminAndSupportProjectsTable";
+import { data } from "./data";
+
+const fiscalYear = "2023";
+
+it.skip("renders without crashing", () => {
+    render(
+        <Provider store={store}>
+            <AdminAndSupportProjectsTable fiscalYear={fiscalYear} data={data} />
+        </Provider>
+    );
+});
