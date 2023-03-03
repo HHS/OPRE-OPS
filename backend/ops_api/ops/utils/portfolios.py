2b5c8fbeb454177f284cf9347845279a056d325a
John DeAngelis <john.deangelis2@gmail.com>
2023-03-03 10:12:57 -0500

    intermediate checkin; fixing unit tests

--- backend/ops_api/ops/utils/portfolios.py
+++ backend/ops_api/ops/utils/portfolios.py
@@ -40,10 +40,12 @@ def get_total_funding(portfolio: Portfolio, fiscal_year: Optional[int] = None) -
     carry_over_funding = sum([c.amount if c.amount else 0 for c in can_fiscal_year_carry_over_query]) or 0
 
     # Amount available to a Portfolio budget is the sum of the BLI minus the Portfolio total (above)
-    budget_line_items = BudgetLineItem.query.filter(BudgetLineItem.can.has(CAN.managing_portfolio == portfolio))
+    budget_line_items = BudgetLineItem.query.filter(
+        BudgetLineItem.can_fiscal_year.has(CANFiscalYear.can.managing_portfolio == portfolio)
+    )
 
     if fiscal_year:
-        budget_line_items = budget_line_items.filter(BudgetLineItem.fiscal_year == fiscal_year)
+        budget_line_items = budget_line_items.filter(BudgetLineItem.can_fiscal_year.fiscal_year == fiscal_year)
 
     planned_budget_line_items = budget_line_items.filter(BudgetLineItem.status.has(BudgetLineItemStatus.Planned)).all()
     planned_funding = sum([b.funding for b in planned_budget_line_items]) or 0
