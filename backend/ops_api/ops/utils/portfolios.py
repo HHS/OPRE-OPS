9541cccfbf330f4f4d818aaed877ede6231ff2b3
John DeAngelis <john.deangelis2@gmail.com>
2023-03-06 10:12:12 -0500

    added _get_total_fiscal_year_funding and _get_carry_forward_total

--- backend/ops_api/ops/utils/portfolios.py
+++ backend/ops_api/ops/utils/portfolios.py
@@ -1,10 +1,9 @@
 from decimal import Decimal
-from typing import Optional, TypedDict
+from typing import TypedDict
 
-from models.cans import CAN, BudgetLineItem, BudgetLineItemStatus, CANFiscalYear, CANFiscalYearCarryOver
-from models.portfolios import Portfolio
+from models.cans import CAN, CANFiscalYear, CANFiscalYearCarryOver
 from ops_api.ops import db
-from sqlalchemy import and_, select
+from sqlalchemy import select
 from sqlalchemy.sql.functions import coalesce, sum
 
 
@@ -26,16 +25,17 @@ class TotalFunding(TypedDict):
 
 
 def _get_total_fiscal_year_funding(portfolio_id: int, fiscal_year: int) -> Decimal:
+    # N.B. BudgetLineItem isn't needed here but will be needed in the other calcs.
     stmt = (
         select(coalesce(sum(CANFiscalYear.total_fiscal_year_funding), 0))
         .join(CAN)
-        .join(
-            BudgetLineItem,
-            and_(
-                BudgetLineItem.can_fiscal_year_can_id == CANFiscalYear.can_id,
-                BudgetLineItem.can_fiscal_year_fiscal_year == CANFiscalYear.fiscal_year,
-            ),
-        )
+        # .join(
+        #     BudgetLineItem,
+        #     and_(
+        #         BudgetLineItem.can_fiscal_year_can_id == CANFiscalYear.can_id,
+        #         BudgetLineItem.can_fiscal_year_fiscal_year == CANFiscalYear.fiscal_year,
+        #     ),
+        # )
         .where(CAN.managing_portfolio_id == portfolio_id)
         .where(CANFiscalYear.fiscal_year == fiscal_year)
     )
@@ -43,81 +43,103 @@ def _get_total_fiscal_year_funding(portfolio_id: int, fiscal_year: int) -> Decim
     return db.session.execute(stmt).scalar()
 
 
-def get_total_funding(portfolio: Portfolio, fiscal_year: Optional[int] = None) -> TotalFunding:
-    can_fiscal_year_query = CANFiscalYear.query.filter(CANFiscalYear.can.has(CAN.managing_portfolio == portfolio))
-
-    can_fiscal_year_carry_over_query = CANFiscalYearCarryOver.query.filter(
-        CANFiscalYearCarryOver.can.has(CAN.managing_portfolio == portfolio)
+def _get_carry_forward_total(portfolio_id: int, fiscal_year: int) -> Decimal:
+    stmt = (
+        select(coalesce(sum(CANFiscalYearCarryOver.amount), 0))
+        .join(CAN)
+        .where(CAN.managing_portfolio_id == portfolio_id)
+        .where(CANFiscalYearCarryOver.to_fiscal_year == fiscal_year)
     )
 
-    if fiscal_year:
-        can_fiscal_year_query = can_fiscal_year_query.filter(CANFiscalYear.fiscal_year == fiscal_year).all()
+    return db.session.execute(stmt).scalar()
 
-        can_fiscal_year_carry_over_query = can_fiscal_year_carry_over_query.filter(
-            CANFiscalYearCarryOver.to_fiscal_year == fiscal_year
-        ).all()
 
-    total_funding = sum([c.total_fiscal_year_funding for c in can_fiscal_year_query]) or 0
+def _get_budget_line_item_total_planned(portfolio_id: int, fiscal_year: int) -> Decimal:
+    ...
 
-    carry_over_funding = sum([c.amount if c.amount else 0 for c in can_fiscal_year_carry_over_query]) or 0
 
-    # Amount available to a Portfolio budget is the sum of the BLI minus the Portfolio total (above)
-    budget_line_items = BudgetLineItem.query.filter(
-        BudgetLineItem.can_fiscal_year.has(CANFiscalYear.can.managing_portfolio == portfolio)
-    )
+# def get_total_funding(portfolio: Portfolio, fiscal_year: Optional[int] = None) -> TotalFunding:
+# can_fiscal_year_query = CANFiscalYear.query.filter(CANFiscalYear.can.has(CAN.managing_portfolio == portfolio))
+#
+# can_fiscal_year_carry_over_query = CANFiscalYearCarryOver.query.filter(
+#     CANFiscalYearCarryOver.can.has(CAN.managing_portfolio == portfolio)
+# )
+#
+# if fiscal_year:
+#     can_fiscal_year_query = can_fiscal_year_query.filter(CANFiscalYear.fiscal_year == fiscal_year).all()
+#
+#     can_fiscal_year_carry_over_query = can_fiscal_year_carry_over_query.filter(
+#         CANFiscalYearCarryOver.to_fiscal_year == fiscal_year
+#     ).all()
 
-    if fiscal_year:
-        budget_line_items = budget_line_items.filter(BudgetLineItem.can_fiscal_year.fiscal_year == fiscal_year)
+# total_funding = sum([c.total_fiscal_year_funding for c in can_fiscal_year_query]) or 0
+# total_funding = _get_total_fiscal_year_funding(portfolio_id=portfolio.id, fiscal_year=fiscal_year)
+#
+# carry_over_funding = _get_carry_forward_total(portfolio_id=portfolio.id, fiscal_year=fiscal_year)
 
-    planned_budget_line_items = budget_line_items.filter(BudgetLineItem.status.has(BudgetLineItemStatus.Planned)).all()
-    planned_funding = sum([b.funding for b in planned_budget_line_items]) or 0
-
-    obligated_budget_line_items = budget_line_items.filter(
-        BudgetLineItem.status.has(BudgetLineItemStatus.Obligated)
-    ).all()
-    obligated_funding = sum([b.funding for b in obligated_budget_line_items]) or 0
-
-    in_execution_budget_line_items = budget_line_items.filter(
-        BudgetLineItem.status.has(BudgetLineItemStatus.In_Execution)
-    ).all()
-    in_execution_funding = sum([b.funding for b in in_execution_budget_line_items]) or 0
-
-    total_accounted_for = sum(
-        (
-            planned_funding,
-            obligated_funding,
-            in_execution_funding,
-        )
-    )
-
-    available_funding = float(total_funding) - float(total_accounted_for)
-
-    return {
-        "total_funding": {
-            "amount": float(total_funding),
-            "percent": "Total",
-        },
-        "carry_over_funding": {
-            "amount": float(carry_over_funding),
-            "percent": "Carry Over",
-        },
-        "planned_funding": {
-            "amount": float(planned_funding),
-            "percent": get_percentage(total_funding, planned_funding),
-        },
-        "obligated_funding": {
-            "amount": float(obligated_funding),
-            "percent": get_percentage(total_funding, obligated_funding),
-        },
-        "in_execution_funding": {
-            "amount": float(in_execution_funding),
-            "percent": get_percentage(total_funding, in_execution_funding),
-        },
-        "available_funding": {
-            "amount": float(available_funding),
-            "percent": get_percentage(total_funding, available_funding),
-        },
-    }
+# Amount available to a Portfolio budget is the sum of the BLI minus the Portfolio total (above)
+# budget_line_items = BudgetLineItem.query.filter(
+#     BudgetLineItem.can_fiscal_year.has(
+#         CANFiscalYear.can.managing_portfolio == portfolio
+#     )
+# )
+#
+# if fiscal_year:
+#     budget_line_items = budget_line_items.filter(
+#         BudgetLineItem.can_fiscal_year.fiscal_year == fiscal_year
+#     )
+#
+# planned_budget_line_items = budget_line_items.filter(
+#     BudgetLineItem.status.has(BudgetLineItemStatus.Planned)
+# ).all()
+# planned_funding = sum([b.funding for b in planned_budget_line_items]) or 0
+#
+# obligated_budget_line_items = budget_line_items.filter(
+#     BudgetLineItem.status.has(BudgetLineItemStatus.Obligated)
+# ).all()
+# obligated_funding = sum([b.funding for b in obligated_budget_line_items]) or 0
+#
+# in_execution_budget_line_items = budget_line_items.filter(
+#     BudgetLineItem.status.has(BudgetLineItemStatus.In_Execution)
+# ).all()
+# in_execution_funding = sum([b.funding for b in in_execution_budget_line_items]) or 0
+#
+# total_accounted_for = sum(
+#     (
+#         planned_funding,
+#         obligated_funding,
+#         in_execution_funding,
+#     )
+# )
+#
+# available_funding = float(total_funding) - float(total_accounted_for)
+#
+# return {
+#     "total_funding": {
+#         "amount": float(total_funding),
+#         "percent": "Total",
+#     },
+#     "carry_over_funding": {
+#         "amount": float(carry_over_funding),
+#         "percent": "Carry Over",
+#     },
+#     "planned_funding": {
+#         "amount": float(planned_funding),
+#         "percent": get_percentage(total_funding, planned_funding),
+#     },
+#     "obligated_funding": {
+#         "amount": float(obligated_funding),
+#         "percent": get_percentage(total_funding, obligated_funding),
+#     },
+#     "in_execution_funding": {
+#         "amount": float(in_execution_funding),
+#         "percent": get_percentage(total_funding, in_execution_funding),
+#     },
+#     "available_funding": {
+#         "amount": float(available_funding),
+#         "percent": get_percentage(total_funding, available_funding),
+#     },
+# }
 
 
 def get_percentage(total_funding: float, specific_funding: float) -> float:
--- backend/ops_api/tests/ops/portfolio/test_portfolio.py
+++ backend/ops_api/tests/ops/portfolio/test_portfolio.py
@@ -1,9 +1,13 @@
 from decimal import Decimal
 
 import pytest
-from models.cans import CAN, BudgetLineItem, CANFiscalYear
+from models.cans import CAN, BudgetLineItem, CANFiscalYear, CANFiscalYearCarryOver
 from models.portfolios import Portfolio
-from ops_api.ops.utils.portfolios import _get_total_fiscal_year_funding
+from ops_api.ops.utils.portfolios import (
+    _get_budget_line_item_total_planned,
+    _get_carry_forward_total,
+    _get_total_fiscal_year_funding,
+)
 
 
 @pytest.mark.usefixtures("app_ctx")
@@ -93,17 +97,19 @@ def db_loaded_with_data_for_total_fiscal_year_funding(app, loaded_db):
         can_100 = CAN(id=100, number="CAN100")
         portfolio_100.cans.append(can_100)
         can_100_fy_2023 = CANFiscalYear(can=can_100, fiscal_year=2023, total_fiscal_year_funding=2.0)
-        blin_1 = BudgetLineItem(id=100, amount=1.0)
-        blin_1.can_fiscal_year = can_100_fy_2023
 
-        instances.extend(
-            [
-                portfolio_100,
-                can_100,
-                can_100_fy_2023,
-                blin_1,
-            ]
+        blin_1 = BudgetLineItem(id=100, amount=1.0, status_id=1)
+        blin_2 = BudgetLineItem(id=101, amount=2.0, status_id=2)
+        blin_3 = BudgetLineItem(id=102, amount=3.0, status_id=3)
+        blin_1.can_fiscal_year = can_100_fy_2023
+        blin_2.can_fiscal_year = can_100_fy_2023
+        blin_3.can_fiscal_year = can_100_fy_2023
+
+        can_100_fy_2022_co = CANFiscalYearCarryOver(
+            id=100, amount=1.0, from_fiscal_year=2022, to_fiscal_year=2023, can=can_100
         )
+
+        instances.extend([portfolio_100, can_100, can_100_fy_2023, blin_1, can_100_fy_2022_co])
 
         loaded_db.session.add_all(instances)
 
@@ -118,7 +124,7 @@ def db_loaded_with_data_for_total_fiscal_year_funding(app, loaded_db):
 
 @pytest.mark.usefixtures("app_ctx")
 @pytest.mark.usefixtures("db_loaded_with_data_for_total_fiscal_year_funding")
-def test_get_research_project_funding_summary():
+def test_get_total_fiscal_year_funding():
     result = _get_total_fiscal_year_funding(100, 2023)
     assert result == Decimal(2), "1 CFY in 2023 with $2"
 
@@ -127,3 +133,23 @@ def test_get_research_project_funding_summary():
 
     result = _get_total_fiscal_year_funding(1000, 2023)
     assert result == Decimal(0), "No Portfolio"
+
+
+@pytest.mark.usefixtures("app_ctx")
+@pytest.mark.usefixtures("db_loaded_with_data_for_total_fiscal_year_funding")
+def test_get_carry_forward_total():
+    result = _get_carry_forward_total(100, 2023)
+    assert result == Decimal(1), "$1 CarryForward for FY 2023"
+
+    result = _get_carry_forward_total(100, 1900)
+    assert result == Decimal(0), "No CFY"
+
+    result = _get_carry_forward_total(1000, 2023)
+    assert result == Decimal(0), "No Portfolio"
+
+
+@pytest.mark.usefixtures("app_ctx")
+@pytest.mark.usefixtures("db_loaded_with_data_for_total_fiscal_year_funding")
+def test_get_budget_line_item_total_planned():
+    result = _get_budget_line_item_total_planned(100, 2023)
+    assert result == Decimal(1), "$1 CarryForward for FY 2023"
