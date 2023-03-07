23841af69c48e1699855930d81590c19e9f88ced
Jonathan Nalley <jnalley@flexion.us>
2023-03-06 23:39:00 -0500

    all the cypress tests plz

--- frontend/cypress/e2e/researchProjectDetail.cy.js
+++ frontend/cypress/e2e/researchProjectDetail.cy.js
@@ -8,7 +8,7 @@ it("loads", () => {
     cy.fakeLogin();
     cy.get("h1").should("contain", "African American Child and Family Research Center");
     cy.get("h2").should("contain", "Division of Child and Family Development");
-    cy.get("span").should("contain", "Emily Ball");
+    cy.get("span").should("contain", "Chris Fortunato");
     cy.get("div").should("contain", "Survey");
     cy.get("div").should("contain", "Population #1");
 });
