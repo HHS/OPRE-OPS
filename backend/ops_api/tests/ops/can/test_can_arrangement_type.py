40db47f17bcb1a2950bc1717010fecd328b2209e
John DeAngelis <john.deangelis2@gmail.com>
2023-03-10 10:22:50 -0500

    fix test for enum count

--- backend/ops_api/tests/ops/can/test_can_arrangement_type.py
+++ backend/ops_api/tests/ops/can/test_can_arrangement_type.py
@@ -4,5 +4,4 @@ from models.cans import CANArrangementType
 
 @pytest.mark.usefixtures("app_ctx")
 def test_can_arrangement_type_retrieve_all(loaded_db):
-    can_at = [t for t in CANArrangementType]
-    assert len(can_at) == 5
+    assert len(CANArrangementType) == 5
