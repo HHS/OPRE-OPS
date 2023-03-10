4cec09b575f43899b67771ac7cfd3f9ec1187ceb
John DeAngelis <john.deangelis2@gmail.com>
2023-03-10 10:16:44 -0500

    fix can (enums) and tests

--- backend/ops_api/tests/ops/can/test_can_arrangement_type.py
+++ backend/ops_api/tests/ops/can/test_can_arrangement_type.py
@@ -4,15 +4,5 @@ from models.cans import CANArrangementType
 
 @pytest.mark.usefixtures("app_ctx")
 def test_can_arrangement_type_retrieve_all(loaded_db):
-    can_at = loaded_db.session.query(CANArrangementType).all()
+    can_at = [t for t in CANArrangementType]
     assert len(can_at) == 5
-
-
-@pytest.mark.parametrize(
-    "id,name",
-    [(1, "OPRE Appropriation"), (2, "Cost Share"), (3, "IAA"), (4, "IDDA"), (5, "MOU")],
-)
-@pytest.mark.usefixtures("app_ctx")
-def test_can_arrangement_type_lookup(loaded_db, id, name):
-    can_at = loaded_db.session.get(CANArrangementType, id)
-    assert can_at.name == name
