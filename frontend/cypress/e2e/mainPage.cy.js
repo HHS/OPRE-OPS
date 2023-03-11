816aff4eeced25598db619ed58ef167d7fab96ac
Tim Donaworth <tdonaworth@flexion.us>
2023-03-10 22:22:10 -0500

    clean up commented code.

--- frontend/cypress/e2e/mainPage.cy.js
+++ frontend/cypress/e2e/mainPage.cy.js
@@ -8,9 +8,6 @@ afterEach(() => {
 });
 
 it("has expected state on initial load", () => {
-    // cy.window()
-    //     .then((win) => win.store.getState().auth)
-    //     .should("deep.include", { isLoggedIn: false, activeUser: null });
     cy.fixture("initial-state").then((initState) => {
         cy.window()
             .then((win) => win.store.getState())
@@ -23,18 +20,12 @@ it("loads", () => {
 });
 
 it("clicking on /cans nav takes you to CAN page", () => {
-    // For this test, we've altered the busienss rule so that /cans loads unauthed.
-    // cy.fakeLogin();
     cy.contains("CANs").click();
-
     cy.url().should("include", "/cans/");
     cy.get("h1").should("have.text", "CANs");
 });
 
 it("clicking on /portfolio nav while unauthenticated, should keep you at home page.", () => {
     cy.get("h1").should("have.text", "This is the OPRE OPS system prototype.");
-    //cy.contains("Portfolios").click();
-
     cy.url().should("include", "/");
-    //cy.get("h1").should("have.text", "Portfolios");
 });
