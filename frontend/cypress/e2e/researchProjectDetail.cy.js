c95d474b7049bb56999fb0b4821d0f15fe48f82a
John DeAngelis <john.deangelis2@gmail.com>
2023-03-13 10:22:58 -0400

    fix bug in tag label names

--- frontend/cypress/e2e/researchProjectDetail.cy.js
+++ frontend/cypress/e2e/researchProjectDetail.cy.js
@@ -14,6 +14,6 @@ it("loads", () => {
     cy.get("h1").should("contain", "African American Child and Family Research Center");
     cy.get("h2").should("contain", "Division of Child and Family Development");
     cy.get("span").should("contain", "Emily Ball");
-    cy.get("div").should("contain", "Survey");
-    cy.get("div").should("contain", "Population #1");
+    cy.get("div").should("contain", "SURVEY");
+    cy.get("div").should("contain", "POPULATION_1");
 });
