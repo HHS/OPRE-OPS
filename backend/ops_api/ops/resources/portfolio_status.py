564ff6a1cac8dbe5eb7680ada2d1fb65a507b6a9
John DeAngelis <john.deangelis2@gmail.com>
2023-03-13 09:45:08 -0400

    remove unneeded decorators as per PR comment

--- backend/ops_api/ops/resources/portfolio_status.py
+++ backend/ops_api/ops/resources/portfolio_status.py
@@ -1,5 +1,4 @@
 from flask import Response, jsonify
-from flask_jwt_extended import jwt_required
 from models.base import BaseModel
 from models.portfolios import PortfolioStatus
 from ops_api.ops.base_views import BaseItemAPI, BaseListAPI
@@ -10,7 +9,6 @@ class PortfolioStatusItemAPI(BaseItemAPI):
     def __init__(self, model: BaseModel):
         super().__init__(model)
 
-    @jwt_required()
     @override
     def get(self, id: int) -> Response:
         item = PortfolioStatus(id)
@@ -21,7 +19,6 @@ class PortfolioStatusListAPI(BaseListAPI):
     def __init__(self, model: BaseModel):
         super().__init__(model)
 
-    @jwt_required()
     @override
     def get(self) -> Response:
         items = [e.name for e in PortfolioStatus]
