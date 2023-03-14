c8ad81348331c8a643aa500950f258917e50ba7f
Tim Donaworth <tdonaworth@flexion.us>
2023-03-14 13:34:48 -0400

    updated data-import to support new Role table.

--- backend/models/users.py
+++ backend/models/users.py
@@ -4,14 +4,14 @@ from typing import List
 from models.base import BaseModel
 from sqlalchemy import Column, DateTime, ForeignKey, Identity, Integer, String, Table, func
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, column_property, relationship
+from sqlalchemy.orm import column_property, relationship
 
 # Define a many-to-many relationship between Users and Roles
 user_role_table = Table(
     "user_role",
     BaseModel.metadata,
-    Column("user_id", Integer, ForeignKey("users.id")),
-    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
+    Column("role_id", Integer, ForeignKey("roles.id"), primary_key=True),
 )
 
 
