6e356bbaacd1a8a0c3bf36aaacd3205fcaf3c5a2
John DeAngelis <john.deangelis2@gmail.com>
2023-03-15 13:25:23 -0400

    refactor:add QueryHelper class

--- backend/ops_api/ops/resources/research_projects.py
+++ backend/ops_api/ops/resources/research_projects.py
@@ -1,15 +1,16 @@
-from flask import Response, jsonify, request
+from flask import Response, current_app, jsonify, request
 from models.base import BaseModel
 from models.cans import CANFiscalYear
 from models.research_projects import ResearchProject
 from ops_api.ops import db
 from ops_api.ops.base_views import BaseItemAPI, BaseListAPI
+from ops_api.ops.utils.query_helpers import QueryHelper
 from sqlalchemy.future import select
 from typing_extensions import override
 
 
 class ResearchProjectItemAPI(BaseItemAPI):
-    def __init__(self, model: BaseModel):
+    def __init__(self, model: BaseModel = ResearchProject):
         super().__init__(model)
 
     @override
@@ -26,11 +27,11 @@ class ResearchProjectItemAPI(BaseItemAPI):
 
 
 class ResearchProjectListAPI(BaseListAPI):
-    def __init__(self, model: BaseModel):
+    def __init__(self, model: BaseModel = ResearchProject):
         super().__init__(model)
 
     @staticmethod
-    def get_query(fiscal_year=None, portfolio_id=None, search=None):
+    def _get_query(fiscal_year=None, portfolio_id=None, search=None):
         stmt = (
             select(ResearchProject)
             .distinct(ResearchProject.id)
@@ -38,14 +39,19 @@ class ResearchProjectListAPI(BaseListAPI):
             .join(CANFiscalYear, isouter=True)
         )
 
+        query_helper = QueryHelper(stmt)
+
         if portfolio_id:
-            stmt = stmt.where(ResearchProject.portfolio_id == portfolio_id)
+            query_helper.add_column_equals(ResearchProject.portfolio_id, portfolio_id)
 
         if fiscal_year:
-            stmt = stmt.where(CANFiscalYear.fiscal_year == fiscal_year)
+            query_helper.add_column_equals(CANFiscalYear.fiscal_year, fiscal_year)
 
         if search:
-            stmt = stmt.where(ResearchProject.title.ilike(f"%{search}%"))
+            query_helper.add_search(ResearchProject.title, search)
+
+        stmt = query_helper.get_stmt()
+        current_app.logger.debug(f"SQL: {stmt}")
 
         return stmt
 
@@ -54,7 +60,7 @@ class ResearchProjectListAPI(BaseListAPI):
         portfolio_id = request.args.get("portfolio_id")
         search = request.args.get("search")
 
-        stmt = ResearchProjectListAPI.get_query(fiscal_year, portfolio_id, search)
+        stmt = self._get_query(fiscal_year, portfolio_id, search)
 
         result = db.session.execute(stmt).all()
 
