cc75c790abf65464152dd29ef33446570bceb0ea
Tim Donaworth <tdonaworth@flexion.us>
2023-03-17 09:16:33 -0400

    created initial Agreements api functions

---
+++ frontend/src/api/getAgreements.js
@@ -0,0 +1,8 @@
+import ApplicationContext from "../applicationContext/ApplicationContext";
+
+export const getAgreementsByResearchProjectFilter = async (id) => {
+    const api_version = ApplicationContext.get().helpers().backEndConfig.apiVersion;
+    const endpoint = `/api/${api_version}/agreements/?project_id=${id}`;
+    const responseData = await ApplicationContext.get().helpers().callBackend(endpoint, "get");
+    return responseData;
+};
