44ef495234179fe96f24d5c2d1db1b9a3657e5ee
John DeAngelis <john.deangelis2@gmail.com>
2023-03-22 10:27:40 -0400

    fix password being echoed to log

--- backend/data_tools/src/import_static_data/import_data.py
+++ backend/data_tools/src/import_static_data/import_data.py
@@ -108,8 +108,6 @@ if __name__ == "__main__":
     script_env = os.getenv("ENV")
     script_config = get_config(script_env)
 
-    print(f"Data-Tools Config: {script_config.db_connection_string}")
-
     db_engine, db_metadata_obj = init_db(script_config)
 
     global_data = get_data_to_import()
