3b4b9b8cce14756a9f9f8eb28f6761f6c83218bd
John DeAngelis <john.deangelis2@gmail.com>
2023-03-24 10:28:01 -0400

    initial add OPS Events

---
+++ backend/models/events.py
@@ -0,0 +1,40 @@
+from enum import Enum
+from typing import Any
+
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
+from typing_extensions import override
+
+from .base import BaseModel
+
+
+class OpsEventType(Enum):
+    LOGIN_ATTEMPT = 1
+
+
+class OpsEventStatus(Enum):
+    SUCCESS = 1
+    FAILED = 2
+    UNKNOWN = 3
+
+
+class OpsEvent(BaseModel):
+    __tablename__ = "ops_event"
+
+    id = sa.Column(sa.Integer, sa.Identity(), primary_key=True)
+    event_type = sa.Column(sa.Enum(OpsEventType))
+    event_status = sa.Column(sa.Enum(OpsEventStatus))
+    event_details = sa.Column(JSONB)
+
+    @override
+    def to_dict(self) -> dict[str, Any]:  # type: ignore [override]
+        d: dict[str, Any] = super().to_dict()  # type: ignore [no-untyped-call]
+
+        d.update(
+            {
+                "event_type": self.event_type.name if self.event_type else None,
+                "event_status": self.event_status.name if self.event_status else None
+            }
+        )
+
+        return d
--- backend/ops_api/tests/ops/auth/test_oidc_controller.py
+++ backend/ops_api/tests/ops/auth/test_oidc_controller.py
@@ -1,7 +1,9 @@
 import pytest
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.serialization import Encoding, NoEncryption, PrivateFormat
+from models.events import OpsEvent, OpsEventStatus, OpsEventType
 from ops_api.ops.utils.auth import create_oauth_jwt
+from sqlalchemy import select
 
 
 def test_auth_post_fails(client):
@@ -17,3 +19,43 @@ def test_get_jwt_not_none(app):
     encoded = key.private_bytes(Encoding.PEM, PrivateFormat.TraditionalOpenSSL, NoEncryption())
     with app.test_request_context("/auth/login", method="POST", data={"code": ""}):
         assert create_oauth_jwt(encoded) is not None
+
+
+def test_auth_post_fails_creates_event(client, loaded_db):
+    data = {"code": "abc1234"}
+
+    res = client.post("/api/v1/auth/login/", json=data)
+    assert res.status_code == 400
+
+    stmt = select(OpsEvent).where(OpsEvent.event_status == OpsEventStatus.FAILED)
+    event = loaded_db.session.scalar(stmt)
+    assert "Code is invalid" in event.event_details["error_message"]
+    assert "<class 'authlib.integrations.base_client.errors.OAuthError'>" == event.event_details["error_type"]
+    assert data == event.event_details["request.json"]
+    # cleanup
+    loaded_db.session.delete(event)
+    loaded_db.session.commit()
+
+
+def test_auth_post_succeeds_creates_event(client, loaded_db, mocker):
+    # setup mocks
+    m1 = mocker.patch("ops_api.ops.utils.auth_views._get_token_and_user_data_from_oauth_provider")
+    m1.return_value = ({"access_token": "blah"}, {})
+    m2 = mocker.patch("ops_api.ops.utils.auth_views._get_token_and_user_data_from_internal_auth")
+    user = mocker.MagicMock()
+    user.to_dict.return_value = {}
+    m2.return_value = ("blah", "blah", user)
+
+    # test
+    res = client.post("/api/v1/auth/login/", json={})
+    assert res.status_code == 200
+
+    stmt = select(OpsEvent).where(OpsEvent.event_status == OpsEventStatus.SUCCESS)
+    event = loaded_db.session.scalar(stmt)
+    assert event.event_type == OpsEventType.LOGIN_ATTEMPT
+    assert event.event_status == OpsEventStatus.SUCCESS
+    assert event.event_details["access_token"] == "blah"
+
+    # cleanup
+    loaded_db.session.delete(event)
+    loaded_db.session.commit()
