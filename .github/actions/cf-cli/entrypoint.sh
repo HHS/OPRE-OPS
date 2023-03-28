3a7ed6ccf79b79c256887a7a00595e5b348e5cfe
Tim Donaworth <tdonaworth@flexion.us>
2023-03-28 13:19:14 -0400

    debug

--- .github/actions/cf-cli/entrypoint.sh
+++ .github/actions/cf-cli/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -l
 
-#cf api "$INPUT_CF_API"
-cf login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD" -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
+# cf login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD" -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
+
+cf api "$INPUT_CF_API"
+cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
   cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
