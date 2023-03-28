3e42686d4106e431251513a0a20ccae7c33a955f
John DeAngelis <john.deangelis2@gmail.com>
2023-03-28 16:38:45 -0400

    remove dead code; remove invalid test scenario comment

--- backend/ops_api/tests/ops/funding_summary/test_research_project_funding_summary.py
+++ backend/ops_api/tests/ops/funding_summary/test_research_project_funding_summary.py
@@ -3,68 +3,6 @@ from models import Agreement
 from models.cans import CAN, BudgetLineItem, CANFiscalYear
 from models.research_projects import ResearchProject
 
-# @pytest.fixture()
-# @pytest.mark.usefixtures("app_ctx")
-# def db_loaded_with_research_projects(app, loaded_db):
-#     """
-#     Given a set of ResearchProject/CAN/Funding data below with the current FY 2023
-#       | Research Project | Managed CAN | CAN FY | Funding |
-#       | 100                | 100           | 2023   | $5      |
-#       | 100                | 200           | 2023   | $7      |
-#       | 100                | 300           | 2023   | $3      |
-#       | 100                | 100           | 2022   | $5      |
-#       | 100                | 200           | 2022   | $5      |
-#       | 200                | 400           | 2023   | $5      |
-#     """
-#     with app.app_context():
-#         instances = []
-#
-#         research_project_100 = ResearchProject(id=100, title="RP100")
-#         research_project_200 = ResearchProject(id=200, title="RP200")
-#
-#         research_project_100.portfolio_id = 1
-#         research_project_200.portfolio_id = 1
-#
-#         instances.extend([research_project_100, research_project_200])
-#
-#         can_100 = CAN(id=100, number="CAN100")
-#         can_200 = CAN(id=200, number="CAN200")
-#         can_300 = CAN(id=300, number="CAN300")
-#         can_400 = CAN(id=400, number="CAN400")
-#
-#         # research_project_100.cans.extend([can_100, can_200, can_300])
-#         # research_project_200.cans.append(can_400)
-#
-#         instances.extend([can_100, can_200, can_300, can_400])
-#
-#         can_fy_100_2023 = CANFiscalYear(can_id=can_100.id, fiscal_year=2023, received_funding=5)
-#         can_fy_200_2023 = CANFiscalYear(can_id=can_200.id, fiscal_year=2023, received_funding=7)
-#         can_fy_300_2023 = CANFiscalYear(can_id=can_300.id, fiscal_year=2023, received_funding=3)
-#         can_fy_100_2022 = CANFiscalYear(can_id=can_100.id, fiscal_year=2022, received_funding=5)
-#         can_fy_200_2022 = CANFiscalYear(can_id=can_200.id, fiscal_year=2022, received_funding=5)
-#         can_fy_400_2023 = CANFiscalYear(can_id=can_400.id, fiscal_year=2023, received_funding=5)
-#
-#         instances.extend(
-#             [
-#                 can_fy_100_2023,
-#                 can_fy_200_2023,
-#                 can_fy_300_2023,
-#                 can_fy_100_2022,
-#                 can_fy_200_2022,
-#                 can_fy_400_2023,
-#             ]
-#         )
-#
-#         loaded_db.add_all(instances)
-#
-#         loaded_db.commit()
-#         yield loaded_db
-#
-#         # Cleanup
-#         for instance in instances:
-#             loaded_db.delete(instance)
-#         loaded_db.commit()
-
 
 @pytest.mark.usefixtures("app_ctx")
 @pytest.mark.usefixtures("db_loaded_with_research_projects")
@@ -121,19 +59,6 @@ def test_get_research_project_funding_summary_no_data(auth_client):
 @pytest.fixture()
 @pytest.mark.usefixtures("app_ctx")
 def db_loaded_with_research_projects(app, loaded_db):
-    """
-    Scenario: Calculate Research Project total spending for a given FY.
-    Given a set of ResearchProject/CAN/Funding data below with the current FY 2023
-      | Research Project | Managed CAN | CAN FY | BLIN | BLIN Amount |
-      | 1                | 1           | 2023 | 1    | $1          |
-      | 1                | 2           | 2023 | 2    | $2          |
-      | 1                | 3           | 2022 | 3    | $3          |
-      | 1                | 3           | 2023 | 4    | $4          |
-      | 2                | 4           | 2023 | 5    | $5          |
-
-    When I calculate the total spending for Research Project 1 in FY 2023
-    Then the result should be $7.
-    """
     with app.app_context():
         instances = []
 
