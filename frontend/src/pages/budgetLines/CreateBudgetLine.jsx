9df06a5021f8c60db8639a48ca9b3f46bd5b32a1
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2023-03-28 13:59:51 -0500

    starts description (#795)
    * starts description
    
    * adds state
    
    does not hold in-between steps

--- frontend/src/pages/budgetLines/CreateBudgetLine.jsx
+++ frontend/src/pages/budgetLines/CreateBudgetLine.jsx
@@ -1,5 +1,5 @@
 import App from "../../App";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { StepIndicatorOne } from "../../components/UI/StepIndicator/StepIndicatorOne";
 import { StepIndicatorTwo } from "../../components/UI/StepIndicator/StepIndicatorTwo";
@@ -10,7 +10,7 @@ import { AgreementSelect } from "./AgreementSelect";
 import { CanSelect } from "./CanSelect";
 import { DesiredAwardDate } from "./DesiredAwardDate";
 import { getAgreementsByResearchProjectFilter } from "../../api/getAgreements";
-import { setAgreements } from "./createBudgetLineSlice";
+import { setAgreements, setBudgetLineAdded } from "./createBudgetLineSlice";
 import { ProcurementShopSelect } from "./ProcurementShopSelect";
 
 const StepOne = ({ goToNext }) => (
@@ -43,82 +43,107 @@ const StepOne = ({ goToNext }) => (
         </div>
     </>
 );
-const StepTwo = ({ goBack, goToNext }) => (
-    <>
-        <h2 className="font-sans-lg">Create New Budget Line</h2>
-        <p>Step Two: Text explaining this page</p>
-        <StepIndicatorTwo />
-        <h2 className="font-sans-lg">Procurement Shop</h2>
-        <p>
-            Select the Procurement Shop, and the fee rates will be populated in the table below. If this is an active
-            agreement, it will default to the procurement shop currently being used.
-        </p>
-        <ProcurementShopSelect />
-        <h2 className="font-sans-lg margin-top-3">Budget Line Details</h2>
-        <p>
-            Complete the information below to create new budget lines. Select Add Budget Line to create multiple budget
-            lines.
-        </p>
-        <div className="grid-row grid-gap">
-            <div className="grid-col-4">
-                <div className="usa-form-group">
-                    <label className="usa-label" htmlFor="bl-description">
-                        Description
-                    </label>
-                    <input className="usa-input" id="bl-description" name="bl-description" type="text" />
-                </div>
-                <div className="usa-form-group">
-                    <CanSelect />
-                </div>
-            </div>
-            <div className="grid-col-4">
-                <DesiredAwardDate />
-                <div className="usa-form-group">
-                    <label className="usa-label" htmlFor="bl-amount">
-                        Amount
-                    </label>
-                    <input className="usa-input" id="bl-amount" name="bl-amount" type="number" />
-                </div>
-            </div>
-            <div className="grid-col-4">
-                <div className="usa-character-count">
+const StepTwo = ({ goBack, goToNext }) => {
+    const dispatch = useDispatch();
+    const budgetLinesAdded = useSelector((state) => state.createBudgetLine.budget_lines_added);
+    const [description, setDescription] = useState("");
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        dispatch(
+            setBudgetLineAdded({
+                line_description: description,
+            })
+        );
+    };
+    return (
+        <>
+            <h2 className="font-sans-lg">Create New Budget Line</h2>
+            <p>Step Two: Text explaining this page</p>
+            <StepIndicatorTwo />
+            <h2 className="font-sans-lg">Procurement Shop</h2>
+            <p>
+                Select the Procurement Shop, and the fee rates will be populated in the table below. If this is an
+                active agreement, it will default to the procurement shop currently being used.
+            </p>
+            <ProcurementShopSelect />
+            <h2 className="font-sans-lg margin-top-3">Budget Line Details</h2>
+            <p>
+                Complete the information below to create new budget lines. Select Add Budget Line to create multiple
+                budget lines.
+            </p>
+            <form className="grid-row grid-gap">
+                <div className="grid-col-4">
                     <div className="usa-form-group">
-                        <label className="usa-label" htmlFor="with-hint-textarea">
-                            Notes (optional)
+                        <label className="usa-label" htmlFor="bl-description">
+                            Description
                         </label>
-                        <span id="with-hint-textarea-hint" className="usa-hint">
-                            Maximum 150 characters
-                        </span>
-                        <textarea
-                            className="usa-textarea usa-character-count__field"
-                            id="with-hint-textarea"
-                            maxLength="150"
-                            name="with-hint-textarea"
-                            rows="5"
-                            aria-describedby="with-hint-textarea-info with-hint-textarea-hint"
-                            style={{ height: "7rem" }}
-                        ></textarea>
+                        <input
+                            className="usa-input"
+                            id="bl-description"
+                            name="bl-description"
+                            type="text"
+                            defaultValue={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
                     </div>
-                    <span id="with-hint-textarea-info" className="usa-character-count__message sr-only">
-                        You can enter up to 150 characters
-                    </span>
+                    <div className="usa-form-group">
+                        <CanSelect />
+                    </div>
                 </div>
-                <button className="usa-button usa-button--outline margin-top-2 float-right margin-right-0">
-                    Add Budget Line
+                <div className="grid-col-4">
+                    <DesiredAwardDate />
+                    <div className="usa-form-group">
+                        <label className="usa-label" htmlFor="bl-amount">
+                            Amount
+                        </label>
+                        <input className="usa-input" id="bl-amount" name="bl-amount" type="number" />
+                    </div>
+                </div>
+                <div className="grid-col-4">
+                    <div className="usa-character-count">
+                        <div className="usa-form-group">
+                            <label className="usa-label" htmlFor="with-hint-textarea">
+                                Notes (optional)
+                            </label>
+                            <span id="with-hint-textarea-hint" className="usa-hint">
+                                Maximum 150 characters
+                            </span>
+                            <textarea
+                                className="usa-textarea usa-character-count__field"
+                                id="with-hint-textarea"
+                                maxLength="150"
+                                name="with-hint-textarea"
+                                rows="5"
+                                aria-describedby="with-hint-textarea-info with-hint-textarea-hint"
+                                style={{ height: "7rem" }}
+                            ></textarea>
+                        </div>
+                        <span id="with-hint-textarea-info" className="usa-character-count__message sr-only">
+                            You can enter up to 150 characters
+                        </span>
+                    </div>
+                    <button
+                        className="usa-button usa-button--outline margin-top-2 float-right margin-right-0"
+                        onClick={handleSubmitForm}
+                    >
+                        Add Budget Line
+                    </button>
+                </div>
+            </form>
+
+            <div className="grid-row flex-justify-end margin-top-1">
+                <button className="usa-button usa-button--outline" onClick={() => goBack()}>
+                    Back
+                </button>
+                <button className="usa-button" onClick={() => goToNext({ name: "John Doe" })}>
+                    Continue
                 </button>
             </div>
-        </div>
+        </>
+    );
+};
 
-        <div className="grid-row flex-justify-end margin-top-1">
-            <button className="usa-button usa-button--outline" onClick={() => goBack()}>
-                Back
-            </button>
-            <button className="usa-button" onClick={() => goToNext({ name: "John Doe" })}>
-                Continue
-            </button>
-        </div>
-    </>
-);
 const StepThree = ({ goBack, goToNext }) => (
     <>
         <h2 className="font-sans-lg">Create New Budget Line</h2>
