0c48ae30689ee302e414c240d693ec0f0d126d9a
Cliff Hill <chill@flexion.us>
2023-03-29 13:23:34 -0400

    Fixing the views.
    Signed-off-by: Cliff Hill <chill@flexion.us>

--- backend/ops_api/ops/resources/procurement_shops.py
+++ backend/ops_api/ops/resources/procurement_shops.py
@@ -1,6 +1,11 @@
 """Module containing views for Procurement Shops."""
+from typing import cast
+
+from flask import Response, jsonify
 from models.base import BaseModel
+from models.procurement_shops import ProcurementShop
 from ops_api.ops.base_views import BaseItemAPI, BaseListAPI
+from typing_extensions import override
 
 
 class ProcurementShopsItemAPI(BaseItemAPI):  # type: ignore [misc]
@@ -17,3 +22,12 @@ class ProcurementShopsListAPI(BaseListAPI):  # type: ignore [misc]
     def __init__(self, model: BaseModel):
         """Initialize the class."""
         super().__init__(model)
+
+    @override
+    def get(self) -> Response:
+        procurement_shop_query = self.model.query
+
+        procurement_shops = cast(list[ProcurementShop], procurement_shop_query.all())
+        response = jsonify([ps.to_dict() for ps in procurement_shops])
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return cast(Response, response)
