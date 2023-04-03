1a64b46e289826995e02f74e92bb8b372c3afb61
John DeAngelis <john.deangelis2@gmail.com>
2023-04-03 09:38:45 -0400

    also return engine so that additional parallel sessions can be created in the app

--- backend/ops_api/ops/db.py
+++ backend/ops_api/ops/db.py
@@ -1,11 +1,15 @@
+from __future__ import annotations
+
 from typing import Optional
 
 from models import *  # noqa: F403, F401
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session, scoped_session, sessionmaker
 
 
-def init_db(conn_string: str, is_unit_test: Optional[bool] = False) -> Session:
+def init_db(
+    conn_string: str, is_unit_test: Optional[bool] = False
+) -> tuple[scoped_session[Session | Any], Engine]:  # noqa: F405
     engine = create_engine(conn_string)
     db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
     Base = declarative_base(cls=BaseModel)  # noqa: F405
@@ -13,4 +17,4 @@ def init_db(conn_string: str, is_unit_test: Optional[bool] = False) -> Session:
     if is_unit_test:
         Base.metadata.create_all(bind=engine)
 
-    return db_session
+    return db_session, engine
