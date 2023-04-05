8069952a783d451ff511d7b04fbfdd19cf50aeb4
Tim Donaworth <tdonaworth@flexion.us>
2023-04-05 14:47:14 -0400

    added await to api

--- frontend/src/pages/budgetLines/getProcurementShopList.js
+++ frontend/src/pages/budgetLines/getProcurementShopList.js
@@ -3,6 +3,6 @@ import ApplicationContext from "../../applicationContext/ApplicationContext";
 export const getProcurementShopList = async () => {
     const api_version = ApplicationContext.get().helpers().backEndConfig.apiVersion;
     const endpoint = `/api/${api_version}/procurement-shops/`;
-    const responseData = ApplicationContext.get().helpers().callBackend(endpoint, "get");
+    const responseData = await ApplicationContext.get().helpers().callBackend(endpoint, "get");
     return responseData;
 };
