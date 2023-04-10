4fde34f92ccba49a177aafce70a91e18923facfe
Tim Donaworth <tdonaworth@flexion.us>
2023-04-10 17:20:44 -0400

    added product_service_code GET endpoint

---
+++ backend/ops_api/ops/resources/product_service_code.py
@@ -0,0 +1,23 @@
+"""Module containing views for Product Service Codes."""
+from flask import Response
+from models.base import BaseModel
+from ops_api.ops.base_views import BaseItemAPI, BaseListAPI
+from typing_extensions import override
+
+
+class ProductServiceCodeItemAPI(BaseItemAPI):  # type: ignore [misc]
+    def __init__(self, model: BaseModel):
+        super().__init__(model)
+
+    @override
+    def get(self, id: int) -> Response:
+        return self._get_item_with_try(id)
+
+
+class ProductServiceCodeListAPI(BaseListAPI):  # type: ignore [misc]
+    def __init__(self, model: BaseModel):
+        super().__init__(model)
+
+    @override
+    def get(self) -> Response:
+        return self._get_all_items_with_try()
