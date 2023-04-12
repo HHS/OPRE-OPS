bf2a6bf88d7daac86680f15701bdbc985245b835
fpigeonjr <fpigeon@flexion.us>
2023-04-12 08:45:32 -0500

    adds keyboard trap

--- frontend/src/components/UI/Modal/Modal.jsx
+++ frontend/src/components/UI/Modal/Modal.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 export const Modal = ({
@@ -7,6 +8,47 @@ export const Modal = ({
     actionButtonText,
     handleConfirm = () => {},
 }) => {
+    const modalRef = useRef(null);
+
+    useEffect(() => {
+        // set initial focus to the modal
+        const currentModalRef = modalRef.current;
+        currentModalRef.focus();
+
+        const handleKeydown = (event) => {
+            // get all focusable elements in the modal container
+            const focusableElements = currentModalRef.querySelectorAll(
+                'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+            );
+            const firstElement = focusableElements[0];
+            const lastElement = focusableElements[focusableElements.length - 1];
+
+            if (event.key === "Tab") {
+                // handle focus wraparound
+                if (document.activeElement === lastElement && !event.shiftKey) {
+                    event.preventDefault();
+                    firstElement.focus();
+                }
+                if (document.activeElement === firstElement && event.shiftKey) {
+                    event.preventDefault();
+                    lastElement.focus();
+                }
+            }
+            if (event.key === "Escape") {
+                // close the modal on Escape key press
+                setShowModal(false);
+            }
+        };
+
+        // add event listener for keyboard navigation
+        currentModalRef.addEventListener("keydown", handleKeydown);
+
+        // clean up the event listener when the component unmounts
+
+        return () => {
+            currentModalRef.removeEventListener("keydown", handleKeydown);
+        };
+    }, [setShowModal]);
     return (
         <>
             <div
@@ -18,7 +60,7 @@ export const Modal = ({
                 onClick={() => setShowModal(false)}
             >
                 <div className="usa-modal-overlay" aria-controls="example-modal-1">
-                    <div className="usa-modal" tabIndex="-1" onClick={(e) => e.stopPropagation()}>
+                    <div className="usa-modal" tabIndex="-1" onClick={(e) => e.stopPropagation()} ref={modalRef}>
                         <div className="usa-modal__content">
                             <div className="usa-modal__main">
                                 <h2 className="usa-modal__heading" id="modal-1-heading">
