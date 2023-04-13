b9885d2b0799e3b878178518578c4e46d82ef2ba
John DeAngelis <john.deangelis2@gmail.com>
2023-04-13 13:57:35 -0400

    remove Portfolio from ResearchProject on frontend and fix tests
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/cypress/e2e/researchProjectDetail.cy.js
+++ frontend/cypress/e2e/researchProjectDetail.cy.js
@@ -12,7 +12,7 @@ afterEach(() => {
 
 it("loads", () => {
     cy.get("h1").should("contain", "African American Child and Family Research Center");
-    cy.get("h2").should("contain", "Division of Child and Family Development");
+    cy.get("h2").should("contain", "Division");
     cy.get("span").should("contain", "Chris Fortunato");
     cy.get("div").should("contain", "SURVEY");
     cy.get("div").should("contain", "POPULATION_1");
