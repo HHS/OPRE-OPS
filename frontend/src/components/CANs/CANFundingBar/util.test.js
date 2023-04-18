378e8b6920477f8369f500b807c27fdfbec93967
fpigeonjr <fpigeon@flexion.us>
2023-04-18 12:24:42 -0500

    small refactor

--- frontend/src/components/CANs/CANFundingBar/util.test.js
+++ frontend/src/components/CANs/CANFundingBar/util.test.js
@@ -1,8 +1,19 @@
 import { calculateRatio } from "./util";
 
-test("calculates ratio correctly", async () => {
-    expect(calculateRatio({})).toEqual(0);
-    expect(calculateRatio({ expected: 101.1, received: 101.1 })).toEqual(1);
-    expect(calculateRatio({ expected: 0, received: 101.1 })).toEqual(10000);
-    expect(calculateRatio({ expected: 101.1, received: 0 })).toEqual(0);
+describe("calculateRatio", () => {
+    test("returns 0 when received and expected are not provided", () => {
+        const ratio = calculateRatio({});
+        expect(ratio).toBe(0);
+    });
+
+    test("calculates ratio correctly", () => {
+        const ratio1 = calculateRatio({ expected: 101.1, received: 101.1 });
+        expect(ratio1).toBe(1);
+
+        const ratio2 = calculateRatio({ expected: 0, received: 101.1 });
+        expect(ratio2).toBe(10000);
+
+        const ratio3 = calculateRatio({ expected: 101.1, received: 0 });
+        expect(ratio3).toBe(0);
+    });
 });
