2d357b942e083748cb43e4ffbaab14e9e5b39922
Tim Donaworth <tdonaworth@flexion.us>
2023-04-19 13:22:58 -0400

    updated e2e to comply with code.

--- frontend/cypress/e2e/researchProjectDetail.cy.js
+++ frontend/cypress/e2e/researchProjectDetail.cy.js
@@ -12,7 +12,7 @@ afterEach(() => {
 
 it("loads", () => {
     cy.get("h1").should("contain", "Human Services Interoperability Support");
-    cy.get("h2").should("contain", "Division of Child and Family Development");
+    cy.get("h2").should("contain", "Division");
     cy.get("span").should("contain", "Chris Fortunato");
     cy.get("div").should("contain", "SURVEY");
     cy.get("div").should("contain", "POPULATION_1");
