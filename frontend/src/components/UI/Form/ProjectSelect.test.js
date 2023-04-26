891307e7525b0b78bbf34d476cb02dd8e84aa269
fpigeonjr <fpigeon@flexion.us>
2023-04-26 16:29:07 -0500

    updates tests

--- frontend/src/components/UI/Form/ProjectSelect.test.js
+++ frontend/src/components/UI/Form/ProjectSelect.test.js
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
-import ProjectSelect from "./ProjectSelect";
 import { configureStore } from "@reduxjs/toolkit";
 import createBudgetLineSlice from "../../../pages/budgetLines/createBudgetLineSlice";
+import ProjectSelect from "./ProjectSelect";
 
 const renderWithRedux = (
     component,
@@ -17,8 +17,21 @@ const renderWithRedux = (
     };
 };
 
+const exampleResearchProjects = [
+    { id: 1, title: "Project 1", description: "Description for Project 1" },
+    { id: 2, title: "Project 2", description: "Description for Project 2" },
+];
+const exampleSelectedResearchProject = { id: 1, title: "Project 1", description: "Description for Project 1" };
+
 describe("ProjectSelect component", () => {
     it("renders without crashing", () => {
-        renderWithRedux(<ProjectSelect />);
+        renderWithRedux(
+            <ProjectSelect
+                researchProjects={exampleResearchProjects}
+                selectedResearchProject={exampleSelectedResearchProject}
+                setSelectedProject={() => {}}
+                clearFunction={() => {}}
+            />
+        );
     });
 });
