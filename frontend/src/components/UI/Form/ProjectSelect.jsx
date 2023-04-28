03a969b526699d48744b72d38efb1712099d5062
fpigeonjr <fpigeon@flexion.us>
2023-04-28 08:47:23 -0500

    cleanup

--- frontend/src/components/UI/Form/ProjectSelect.jsx
+++ frontend/src/components/UI/Form/ProjectSelect.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
-import { setAgreementProject } from "./createAgreementSlice";
+import { setAgreementProject } from "../../../pages/agreements/createAgreementSlice";
 
 export const ProjectSelect = ({
     researchProjects,
