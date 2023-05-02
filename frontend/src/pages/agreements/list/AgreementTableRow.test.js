7b23bb1d364aa1d9193f74deb381fea41f362a41
Tim Donaworth <tdonaworth@flexion.us>
2023-05-02 13:03:35 -0400

    udpated sample time

--- frontend/src/pages/agreements/list/AgreementTableRow.test.js
+++ frontend/src/pages/agreements/list/AgreementTableRow.test.js
@@ -25,8 +25,8 @@ describe("AgreementTableRow", () => {
         agreement_type: "GRANT",
         procurement_shop: { fee: 0.05 },
         budget_line_items: [
-            { amount: 100, date_needed: "2024-05-02", status: "DRAFT" },
-            { amount: 200, date_needed: "2023-03-02", status: "UNDER_REVIEW" },
+            { amount: 100, date_needed: "2024-05-02T11:00:00", status: "DRAFT" },
+            { amount: 200, date_needed: "2023-03-02T11:00:00", status: "UNDER_REVIEW" },
         ],
         created_by: "user1",
         notes: "Test notes",
@@ -40,38 +40,7 @@ describe("AgreementTableRow", () => {
         expect(screen.getByText("Test Project")).toBeInTheDocument();
         expect(screen.getByText("Grant")).toBeInTheDocument();
         expect(screen.getByText("$315.00")).toBeInTheDocument();
-        expect(screen.getByText("3/1/2023")).toBeInTheDocument();
+        expect(screen.getByText("3/2/2023")).toBeInTheDocument();
         expect(screen.getByText("In Review")).toBeInTheDocument();
     });
 });
-// test("expands row when chevron is clicked", () => {
-//     render(<AgreementTableRow agreement={agreement} />);
-//     fireEvent.click(screen.getByLabelText("expand row"));
-
-//     expect(screen.getByLabelText("expandable row")).toBeInTheDocument();
-// });
-
-// test.skip("edit button triggers function", () => {
-//     const mockHandleEdit = jest.fn();
-//     render(<AgreementTableRow agreement={agreement} handleEditAgreement={mockHandleEdit} />);
-//     fireEvent.click(screen.getByTitle("edit"));
-
-//     expect(mockHandleEdit).toBeCalled();
-// });
-
-// test.skip("delete button triggers function", () => {
-//     const mockHandleDelete = jest.fn();
-//     render(<AgreementTableRow agreement={agreement} handleDeleteAgreement={mockHandleDelete} />);
-//     fireEvent.click(screen.getByTitle("delete"));
-
-//     expect(mockHandleDelete).toBeCalled();
-// });
-
-// test("approve button triggers function", () => {
-//     const mockHandleSubmit = jest.fn();
-//     render(<AgreementTableRow agreement={agreement} handleSubmitAgreementForApproval={mockHandleSubmit} />);
-//     fireEvent.click(screen.getByTitle("submit for approval"));
-
-//     expect(mockHandleSubmit).toBeCalled();
-// });
-//});
