5489b622600c24dadeb505691c7d71f18798519d
Tim Donaworth <tdonaworth@flexion.us>
2023-05-02 13:07:30 -0400

    lint

--- frontend/src/pages/agreements/list/AgreementTableRow.test.js
+++ frontend/src/pages/agreements/list/AgreementTableRow.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { AgreementTableRow } from "./AgreementTableRow";
 
