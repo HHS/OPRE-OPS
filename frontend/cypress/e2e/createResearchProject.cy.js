a09ba1be103ad2e81fa01b5da1b19d676ba1f8ed
John DeAngelis <john.deangelis2@gmail.com>
2023-05-03 15:05:18 -0400

    e2e test:project type select has the correct options
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/cypress/e2e/createResearchProject.cy.js
+++ frontend/cypress/e2e/createResearchProject.cy.js
@@ -1,15 +1,20 @@
-import { testLogin } from "./utils";
+import { terminalLog, testLogin } from "./utils";
 
-before(() => {
+beforeEach(() => {
     testLogin("admin");
     cy.visit("/projects/create");
 });
 
 afterEach(() => {
     cy.injectAxe();
-    cy.checkA11y();
+    cy.checkA11y(null, null, terminalLog);
 });
 
 it("loads", () => {
     cy.get("h1").should("contain", "Create New Project");
+    cy.get("h2").should("contain", "Select the Project Type");
+});
+
+it("project type select has the correct options", () => {
+    cy.get("#project-type-select-options").should("contain", "Research");
 });
