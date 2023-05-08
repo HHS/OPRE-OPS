9150e9f9b2381b5d1a05d22e17aea57cc924a2c4
Tim Donaworth <tdonaworth@flexion.us>
2023-05-08 09:41:22 -0400

    Created a testable mock Provider, to use for RTK unit tests

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import canListSlice from "./pages/cans/list/canListSlice";
 import canDetailSlice from "./pages/cans/detail/canDetailSlice";
 import portfolioListSlice from "./pages/portfolios/list/portfolioListSlice";
@@ -14,21 +14,31 @@ import createAgreementSlice from "./pages/agreements/createAgreementSlice";
 import createProjectSlice from "./pages/projects/createProjectSlice";
 import { opsApi } from "./api/opsAPI";
 
+const rootReducer = combineReducers({
+    [opsApi.reducerPath]: opsApi.reducer,
+    canList: canListSlice,
+    canDetail: canDetailSlice,
+    portfolioList: portfolioListSlice,
+    portfolioBudgetSummary: portfolioBudgetSummarySlice,
+    auth: authSlice,
+    portfolio: portfolioSlice,
+    userDetail: userSlice,
+    researchProject: researchProjectSlice,
+    researchProjectFunding: ResearchProjectFundingSlice,
+    createBudgetLine: createBudgetLineSlice,
+    createAgreement: createAgreementSlice,
+    createProject: createProjectSlice,
+});
+
+export const setupStore = (preloadedState = {}) => {
+    return configureStore({
+        reducer: rootReducer,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(opsApi.middleware),
+        preloadedState,
+    });
+};
+
 export default configureStore({
-    reducer: {
-        [opsApi.reducerPath]: opsApi.reducer,
-        canList: canListSlice,
-        canDetail: canDetailSlice,
-        portfolioList: portfolioListSlice,
-        portfolioBudgetSummary: portfolioBudgetSummarySlice,
-        auth: authSlice,
-        portfolio: portfolioSlice,
-        userDetail: userSlice,
-        researchProject: researchProjectSlice,
-        researchProjectFunding: ResearchProjectFundingSlice,
-        createBudgetLine: createBudgetLineSlice,
-        createAgreement: createAgreementSlice,
-        createProject: createProjectSlice,
-    },
+    reducer: rootReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(opsApi.middleware),
 });
