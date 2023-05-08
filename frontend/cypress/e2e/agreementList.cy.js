67a64d73fe9f1157fc5d94feaa1fea02b6733f50
Tim Donaworth <tdonaworth@flexion.us>
2023-05-08 16:12:30 -0400

    added e2e test for clicking "Send for Approval"

--- frontend/cypress/e2e/agreementList.cy.js
+++ frontend/cypress/e2e/agreementList.cy.js
@@ -13,3 +13,11 @@ afterEach(() => {
 it("loads", () => {
     cy.get("h1").should("have.text", "Agreements");
 });
+
+it("navigates to the approveAgreements page when the approve button is clicked", () => {
+    // hover over a row to make the approve button visible
+    cy.get("tbody tr").first().trigger("mouseover");
+    // click the approve svg with an id of "submit-for-approval-x where x is the id of the agreement"
+    cy.get("svg[id^='submit-for-approval-']").first().click();
+    cy.url().should("include", "/agreements/approve");
+});
