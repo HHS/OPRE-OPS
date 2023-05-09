3ebb26e4d875732eda8e6d1f5ab3ada72dd1b4f2
Tim Donaworth <tdonaworth@flexion.us>
2023-05-09 07:57:10 -0400

    added CODEOWNERS

--- frontend/cypress/e2e/agreementList.cy.js
+++ frontend/cypress/e2e/agreementList.cy.js
@@ -17,7 +17,7 @@ it("loads", () => {
 it("navigates to the approveAgreements page when the approve button is clicked", () => {
     // hover over a row to make the approve button visible
     cy.get("tbody tr").first().trigger("mouseover");
-    // click the approve svg with an id of "submit-for-approval-x where x is the id of the agreement"
+    // click the approve svg with an id of "submit-for-approval-x" where x is the id of the agreement
     cy.get("svg[id^='submit-for-approval-']").first().click();
     cy.url().should("include", "/agreements/approve");
 });
