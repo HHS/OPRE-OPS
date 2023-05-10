d66587cfea2db821431cf5ac714621bd068e8bde
John DeAngelis <john.deangelis2@gmail.com>
2023-05-10 16:18:29 -0400

    refactor:remove unused code/file
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/api/getProductServiceCodes.js
+++
@@ -1,8 +0,0 @@
-import ApplicationContext from "../applicationContext/ApplicationContext";
-
-export const getProductServiceCodeById = async (id) => {
-    const api_version = ApplicationContext.get().helpers().backEndConfig.apiVersion;
-    const endpoint = `/api/${api_version}/product-service-codes/${id}`;
-    const responseData = await ApplicationContext.get().helpers().callBackend(endpoint, "get");
-    return responseData;
-};
