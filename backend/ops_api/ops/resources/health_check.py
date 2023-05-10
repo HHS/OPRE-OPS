3e322f9902247bd436be512781bca85be9ccfd85
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-05-10 14:19:46 -0600

    add health checks for the database and OAuth server to generate the response for GET /api/v1/health/

--- backend/ops_api/ops/resources/health_check.py
+++ backend/ops_api/ops/resources/health_check.py
@@ -1,9 +1,26 @@
+import asyncio
+
 from flask import Response, jsonify
 from flask.views import MethodView
+
+from ops_api.ops.utils.health_check import check_all
 
 
 class HealthCheckAPI(MethodView):
     def get(self) -> Response:
-        response = jsonify({"status": "OK"})
+        checks = asyncio.run(check_all())
+        max_alarm_level = max(checks.values(), key=lambda obj: obj["alarm_level"])[
+            "alarm_level"
+        ]
+        status = (
+            "OK"
+            if max_alarm_level == 0
+            else "BAD"
+            if max_alarm_level > 1
+            else "Maybe problems"
+        )
+        response = jsonify(
+            {"status": status, "alarm_level": max_alarm_level, "checks": checks}
+        )
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
---
+++ backend/ops_api/ops/utils/health_check.py
@@ -0,0 +1,69 @@
+import asyncio
+import logging
+
+import requests
+from flask import current_app
+from sqlalchemy import text
+
+
+async def check_auth_services() -> dict:
+    """check the health of the OAuth server with a GET of the server_metadata_url
+    Returns:
+        dict: with alarm_level>0 if there is a problem
+    """
+
+    resp = {"status_code": None, "alarm_level": 0}
+    try:
+        authlib_client_config = current_app.config["AUTHLIB_OAUTH_CLIENTS"]["logingov"]
+        server_metadata_url = authlib_client_config["server_metadata_url"]
+        print(server_metadata_url)
+        r = requests.get(server_metadata_url, timeout=10)
+        resp["status_code"] = r.status_code
+        print(f"{r.status_code=}, {type(r.status_code)=}")
+        if r.status_code != 200:
+            resp["alarm_level"] = 1
+    except Exception as e:
+        logging.error("Auth Services check failed: " + str(e), exc_info=True)
+        resp["alarm_level"] = 2
+    return resp
+
+
+async def check_db_conn() -> dict:
+    """check the DB by executing a test query
+    Returns:
+        dict: alarm_level>0 if there is a problem
+    """
+    resp = {"db_conn_is_ok": True, "alarm_level": 0}
+    try:
+        sess = current_app.db_session
+        print(type(sess))
+        current_app.db_session.execute(
+            text("SELECT 'OK';"),
+            execution_options={"timeout": 5},  # just pool wait timeout ?
+        )
+    except Exception as e:
+        logging.error("Database test query failed: " + str(e), exc_info=True)
+        resp["db_conn_is_ok"] = False
+        resp["alarm_level"] = 2
+    try:
+        pool = current_app.engine.pool
+        pool_status = {
+            "size": pool.size(),
+            "checked_in": pool.checkedin(),
+            "overflow": pool.overflow(),
+            "checked_out": pool.checkedout(),
+        }
+        resp["pool_status"] = pool_status
+    except Exception as e:
+        logging.error("Connection pool status failed: " + str(e), exc_info=True)
+    return resp
+
+
+async def check_all() -> dict:
+    """run all checks (DB and Auth services)
+    Returns:
+        dict: a map of the responses from each check
+    """
+    auth, db_conn = await asyncio.gather(check_auth_services(), check_db_conn())
+    checks = {"auth_services": auth, "database_connection": db_conn}
+    return checks
