bd12946bb9a137cb4d5fe50b3415e76ffa15e7e6
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-05-10 15:20:40 -0600

    use make_response_with_headers

--- backend/ops_api/ops/resources/health_check.py
+++ backend/ops_api/ops/resources/health_check.py
@@ -4,6 +4,7 @@ from flask import Response, jsonify
 from flask.views import MethodView
 
 from ops_api.ops.utils.health_check import check_all
+from ops_api.ops.utils.response import make_response_with_headers
 
 
 class HealthCheckAPI(MethodView):
@@ -12,5 +13,5 @@ class HealthCheckAPI(MethodView):
         max_alarm_level = max(checks.values(), key=lambda obj: obj["alarm_level"])["alarm_level"]
         status = "OK" if max_alarm_level == 0 else "BAD" if max_alarm_level > 1 else "Maybe problems"
         response = jsonify({"status": status, "alarm_level": max_alarm_level, "checks": checks})
-        response.headers.add("Access-Control-Allow-Origin", "*")
+        make_response_with_headers(response)
         return response
