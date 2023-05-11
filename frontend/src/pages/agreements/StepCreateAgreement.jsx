7f11439304f954ff3b73dfcade76809616a2b745
John DeAngelis <john.deangelis2@gmail.com>
2023-05-11 10:08:25 -0400

    refactor:extract ProductServiceCodeSummaryBox to component
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/pages/agreements/StepCreateAgreement.jsx
+++ frontend/src/pages/agreements/StepCreateAgreement.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import StepIndicator from "../../components/UI/StepIndicator/StepIndicator";
@@ -26,6 +25,7 @@ import TeamMemberList from "./TeamMemberList";
 import Modal from "../../components/UI/Modal/Modal";
 import { postAgreement } from "../../api/postAgreements";
 import ProjectSummaryCard from "../../components/ResearchProjects/ProjectSummaryCard/ProjectSummaryCard";
+import ProductServiceCodeSummaryBox from "../../components/Agreements/ProductServiceCodeSummaryBox/ProductServiceCodeSummaryBox";
 
 export const StepCreateAgreement = ({ goBack, goToNext, wizardSteps }) => {
     const dispatch = useDispatch();
@@ -106,34 +106,6 @@ export const StepCreateAgreement = ({ goBack, goToNext, wizardSteps }) => {
                 navigate("/agreements/");
             },
         });
-    };
-
-    const ProductServiceCodeSummaryBox = ({ selectedProductServiceCode }) => {
-        const { naics, support_code } = selectedProductServiceCode;
-
-        return (
-            <div
-                className="bg-base-lightest font-family-sans font-12px border-1px border-base-light radius-sm margin-top-4"
-                style={{ width: "19.5625rem", minHeight: "4.375rem" }}
-            >
-                <dl className="margin-0 padding-y-2 padding-x-105 display-flex flex-justify">
-                    <div>
-                        <dt className="margin-0 text-base-dark">NAICS Code</dt>
-                        <dd className="text-semibold margin-0">{naics}</dd>
-                    </div>
-                    <div>
-                        <dt className="margin-0 text-base-dark">Program Support Code</dt>
-                        <dd className="text-semibold margin-0">{support_code}</dd>
-                    </div>
-                </dl>
-            </div>
-        );
-    };
-    ProductServiceCodeSummaryBox.propTypes = {
-        selectedProductServiceCode: PropTypes.shape({
-            naics: PropTypes.number.isRequired,
-            support_code: PropTypes.string.isRequired,
-        }).isRequired,
     };
 
     return (
