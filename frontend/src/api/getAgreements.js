fd92da3838f2d05934544bdcda80f50aa437277f
John DeAngelis <john.deangelis2@gmail.com>
2023-05-12 10:16:58 -0400

    refactor:remove unused functions
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/api/getAgreements.js
+++ frontend/src/api/getAgreements.js
@@ -1,19 +1,5 @@
 import ApplicationContext from "../applicationContext/ApplicationContext";
 
-export const getAllAgreements = async () => {
-    const api_version = ApplicationContext.get().helpers().backEndConfig.apiVersion;
-    const endpoint = `/api/${api_version}/agreements/`;
-    const responseData = await ApplicationContext.get().helpers().callBackend(endpoint, "get");
-    return responseData;
-};
-
-export const getAgreementById = async (id) => {
-    const api_version = ApplicationContext.get().helpers().backEndConfig.apiVersion;
-    const endpoint = `/api/${api_version}/agreements/${id}`;
-    const responseData = await ApplicationContext.get().helpers().callBackend(endpoint, "get");
-    return responseData;
-};
-
 export const getAgreementsByResearchProjectFilter = async (id) => {
     const api_version = ApplicationContext.get().helpers().backEndConfig.apiVersion;
     const endpoint = `/api/${api_version}/agreements/?research_project_id=${id}`;
