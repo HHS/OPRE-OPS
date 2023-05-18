b5611b34f59ec86ed54a096c85c92095fc98a205
fpigeonjr <fpigeon@flexion.us>
2023-05-18 17:37:40 -0500

    writes test for modal

---
+++ frontend/cypress/e2e/components.cy.js
@@ -0,0 +1,35 @@
+import { terminalLog, testLogin } from "./utils";
+
+beforeEach(() => {
+    testLogin("admin");
+    cy.visit("/cans");
+});
+
+afterEach(() => {
+    cy.injectAxe();
+    cy.checkA11y(null, null, terminalLog);
+});
+
+describe("modal management", () => {
+    it("should open and close modal with cancel button", () => {
+        cy.visit("/projects/create");
+        cy.get("#cancel").click(); // open modal
+        cy.get(".usa-modal").should("exist");
+        cy.get(".usa-modal button").contains("Cancel").click(); // close modal
+        cy.get(".usa-modal").should("not.exist");
+    });
+    it("should open and close modal with clicking background", () => {
+        cy.visit("/projects/create");
+        cy.get("#cancel").click(); // open modal
+        cy.get(".usa-modal").should("exist");
+        cy.get(".usa-modal-overlay").click({ force: true }); // close modal
+        cy.get(".usa-modal").should("not.exist");
+    });
+    it("should open and close modal with esc key", () => {
+        cy.visit("/projects/create");
+        cy.get("#cancel").click(); // open modal
+        cy.get(".usa-modal").should("exist");
+        cy.get("body").type("{esc}"); // close modal
+        cy.get(".usa-modal").should("not.exist");
+    });
+});
