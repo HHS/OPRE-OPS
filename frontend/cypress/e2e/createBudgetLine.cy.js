2ef39982f7675b49be45990100f7c84cc95158a0
fpigeonjr <fpigeon@flexion.us>
2023-05-19 10:02:47 -0500

    starts e2e tests

--- frontend/cypress/e2e/createBudgetLine.cy.js
+++ frontend/cypress/e2e/createBudgetLine.cy.js
@@ -1,15 +1,29 @@
-import { testLogin } from "./utils";
+/// <reference types="cypress" />
+import { terminalLog, testLogin } from "./utils";
 
 before(() => {
     testLogin("admin");
     cy.visit("/budget-lines/create");
 });
 
-afterEach(() => {
-    cy.injectAxe();
-    cy.checkA11y();
-});
+// afterEach(() => {
+//     cy.injectAxe();
+//     cy.checkA11y(null, null, terminalLog);
+// });
 
-it("loads", () => {
-    cy.get("h1").should("contain", "Create New Budget Line");
+describe("create budget lines workflow", () => {
+    it("should complete step one", () => {
+        cy.get(".usa-step-indicator__segment--current").should("contain", "Project & Agreement");
+        // summary card should not exist until project is selected
+        cy.get('[data-cy="project-summary-card"]').should("not.exist");
+        // agreement should be disabled until project is selected
+        cy.get("#agreement").should("be.disabled");
+        // select project
+        cy.get("#project").type("Human Services Interoperability Support{enter}");
+        // summary card should exist after project is selected
+        cy.get('[data-cy="project-summary-card"]').should("exist");
+        // agreement should be enabled after project is selected
+        cy.get("#agreement").should("not.be.disabled");
+        cy.get("#agreement").select(1);
+    });
 });
