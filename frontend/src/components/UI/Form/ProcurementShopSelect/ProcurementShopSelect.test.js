c6503f3fed5d50b97614b29a0aec0f0745dffa08
fpigeonjr <fpigeon@flexion.us>
2023-05-23 17:48:09 -0500

    fixes unit tests?

--- frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.test.js
+++ frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.test.js
@@ -3,12 +3,20 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import createBudgetLineSlice from "../../../../pages/budgetLines/createBudgetLineSlice";
 import ProcurementShopSelect from "./ProcurementShopSelect";
+import { opsApi } from "../../../../api/opsAPI";
 
 const renderWithRedux = (
     component,
     {
         initialState,
-        store = configureStore({ reducer: { createBudgetLine: createBudgetLineSlice }, preloadedState: initialState }),
+        store = configureStore({
+            reducer: {
+                createBudgetLine: createBudgetLineSlice,
+                [opsApi.reducerPath]: opsApi.reducer, // Add the opsApi reducer
+            },
+            middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(opsApi.middleware), // Add the opsApi middleware
+            preloadedState: initialState,
+        }),
     } = {}
 ) => {
     return {
