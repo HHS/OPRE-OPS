88d379fd929b7645c2681a5cba4037b40dcd2d48
fpigeonjr <fpigeon@flexion.us>
2023-05-23 17:38:50 -0500

    found a duplicate component

--- frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.test.js
+++ frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.test.js
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import createBudgetLineSlice from "../../../../pages/budgetLines/createBudgetLineSlice";
-import { ProcurementShopSelect } from "./ProcurementShopSelect";
+import ProcurementShopSelect from "./ProcurementShopSelect";
 
 const renderWithRedux = (
     component,
