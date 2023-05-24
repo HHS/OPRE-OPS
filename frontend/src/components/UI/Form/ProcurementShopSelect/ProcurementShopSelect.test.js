41dfe38e06241cbdecfba6bb40af14f18deff416
fpigeonjr <fpigeon@flexion.us>
2023-05-23 21:04:58 -0500

    updates tests

--- frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.test.js
+++ frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.test.js
@@ -1,32 +1,65 @@
 import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import createBudgetLineSlice from "../../../../pages/budgetLines/createBudgetLineSlice";
-import ProcurementShopSelect from "./ProcurementShopSelect";
-import { opsApi } from "../../../../api/opsAPI";
+import { ProcurementShopSelect } from "./ProcurementShopSelect";
+import { useGetProcurementShopsQuery } from "../../../../api/opsAPI";
 
-const renderWithRedux = (
-    component,
-    {
-        initialState,
-        store = configureStore({
-            reducer: {
-                createBudgetLine: createBudgetLineSlice,
-                [opsApi.reducerPath]: opsApi.reducer, // Add the opsApi reducer
-            },
-            middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(opsApi.middleware), // Add the opsApi middleware
-            preloadedState: initialState,
-        }),
-    } = {}
-) => {
-    return {
-        ...render(<Provider store={store}>{component}</Provider>),
-        store,
-    };
-};
+jest.mock("../../../../api/opsAPI");
 
-describe("ProcureShopSelect component", () => {
-    it("renders without crashing", () => {
-        renderWithRedux(<ProcurementShopSelect />);
+const sampleShops = [
+    { id: 1, name: "Shop1", abbr: "S1", fee: 0.1 },
+    { id: 2, name: "Shop2", abbr: "S2", fee: 0.2 },
+];
+
+describe("ProcurementShopSelect", () => {
+    it("renders loading state", () => {
+        useGetProcurementShopsQuery.mockReturnValue({ isLoading: true });
+        const { getByText } = render(
+            <ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />
+        );
+        expect(getByText("Loading...")).toBeInTheDocument();
+    });
+
+    it("renders error state", () => {
+        useGetProcurementShopsQuery.mockReturnValue({ error: true });
+        const { getByText } = render(
+            <ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />
+        );
+        expect(getByText("Oops, an error occurred")).toBeInTheDocument();
+    });
+
+    it("renders initial state with no shop selected", () => {
+        useGetProcurementShopsQuery.mockReturnValue({ data: sampleShops });
+        const { getByLabelText } = render(
+            <ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />
+        );
+        const select = getByLabelText("Procurement Shop");
+        expect(select.value).toBe("0");
+    });
+
+    it("displays all shops in the dropdown", () => {
+        useGetProcurementShopsQuery.mockReturnValue({ data: sampleShops });
+        const { getByLabelText, getByTestId } = render(
+            <ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />
+        );
+        const select = getByLabelText("Procurement Shop");
+
+        sampleShops.forEach((shop) => {
+            const option = Array.from(select.querySelectorAll("option")).find((optionElement) => {
+                const regexpName = new RegExp(shop.name, "i");
+                const regexpCode = new RegExp(shop.abbr, "i");
+                return regexpName.test(optionElement.textContent) && regexpCode.test(optionElement.textContent);
+            });
+            expect(option).toBeInTheDocument();
+        });
+    });
+
+    it("sets default selected shop if provided", () => {
+        useGetProcurementShopsQuery.mockReturnValue({ data: sampleShops });
+        const selectedShop = sampleShops[1];
+        const { getByLabelText } = render(
+            <ProcurementShopSelect selectedProcurementShop={selectedShop} onChangeSelectedProcurementShop={jest.fn()} />
+        );
+
+        const select = getByLabelText("Procurement Shop");
+        expect(select.value).toBe(selectedShop.id.toString());
     });
 });
