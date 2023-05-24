e868e012039327f55e412cd805560440170c8f41
fpigeonjr <fpigeon@flexion.us>
2023-05-23 21:21:57 -0500

    fixes linter issues

--- frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.test.js
+++ frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import { ProcurementShopSelect } from "./ProcurementShopSelect";
 import { useGetProcurementShopsQuery } from "../../../../api/opsAPI";
 
@@ -12,54 +12,31 @@ const sampleShops = [
 describe("ProcurementShopSelect", () => {
     it("renders loading state", () => {
         useGetProcurementShopsQuery.mockReturnValue({ isLoading: true });
-        const { getByText } = render(
-            <ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />
-        );
-        expect(getByText("Loading...")).toBeInTheDocument();
+        render(<ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />);
+        expect(screen.getByText("Loading...")).toBeInTheDocument();
     });
 
     it("renders error state", () => {
         useGetProcurementShopsQuery.mockReturnValue({ error: true });
-        const { getByText } = render(
-            <ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />
-        );
-        expect(getByText("Oops, an error occurred")).toBeInTheDocument();
+        render(<ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />);
+        expect(screen.getByText("Oops, an error occurred")).toBeInTheDocument();
     });
 
     it("renders initial state with no shop selected", () => {
         useGetProcurementShopsQuery.mockReturnValue({ data: sampleShops });
-        const { getByLabelText } = render(
-            <ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />
-        );
-        const select = getByLabelText("Procurement Shop");
+        render(<ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />);
+        const select = screen.getByLabelText("Procurement Shop");
         expect(select.value).toBe("0");
     });
 
-    it("displays all shops in the dropdown", () => {
+    it("displays all shops in the dropdown", async () => {
         useGetProcurementShopsQuery.mockReturnValue({ data: sampleShops });
-        const { getByLabelText, getByTestId } = render(
-            <ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />
-        );
-        const select = getByLabelText("Procurement Shop");
+        render(<ProcurementShopSelect selectedProcurementShop={null} onChangeSelectedProcurementShop={jest.fn()} />);
 
-        sampleShops.forEach((shop) => {
-            const option = Array.from(select.querySelectorAll("option")).find((optionElement) => {
-                const regexpName = new RegExp(shop.name, "i");
-                const regexpCode = new RegExp(shop.abbr, "i");
-                return regexpName.test(optionElement.textContent) && regexpCode.test(optionElement.textContent);
-            });
+        for (const shop of sampleShops) {
+            const fullShopName = `${shop.name} (${shop.abbr})`;
+            const option = await screen.findByText(fullShopName);
             expect(option).toBeInTheDocument();
-        });
-    });
-
-    it("sets default selected shop if provided", () => {
-        useGetProcurementShopsQuery.mockReturnValue({ data: sampleShops });
-        const selectedShop = sampleShops[1];
-        const { getByLabelText } = render(
-            <ProcurementShopSelect selectedProcurementShop={selectedShop} onChangeSelectedProcurementShop={jest.fn()} />
-        );
-
-        const select = getByLabelText("Procurement Shop");
-        expect(select.value).toBe(selectedShop.id.toString());
+        }
     });
 });
