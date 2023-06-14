27ed1722e70309c056ea24e327154434ea0013f9
John DeAngelis <john.deangelis2@gmail.com>
2023-06-14 15:04:43 -0400

    add test debug message
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/ops_api/ops/resources/health_check.py
+++ backend/ops_api/ops/resources/health_check.py
@@ -1,14 +1,14 @@
 import asyncio
 
-from flask import Response, jsonify
+from flask import Response, current_app, jsonify
 from flask.views import MethodView
-
 from ops_api.ops.utils.health_check import check_all
 from ops_api.ops.utils.response import make_response_with_headers
 
 
 class HealthCheckAPI(MethodView):
     def get(self) -> Response:
+        current_app.logger.debug("Health check request received")
         checks = asyncio.run(check_all())
         max_alarm_level = max(checks.values(), key=lambda obj: obj["alarm_level"])["alarm_level"]
         status = "OK" if max_alarm_level == 0 else "BAD" if max_alarm_level > 1 else "Maybe problems"
