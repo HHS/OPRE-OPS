176b57cd57d0d96fb205161d61f13eb629d24a17
John DeAngelis <john.deangelis2@gmail.com>
2023-06-15 14:12:51 -0400

    add token_endpoint to hhsams config
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/ops_api/ops/environment/default_settings.py
+++ backend/ops_api/ops/environment/default_settings.py
@@ -27,6 +27,7 @@ AUTHLIB_OAUTH_CLIENTS = {
     },
     "hhsams": {
         "server_metadata_url": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/.well-known/openid-configuration",
+        "token_endpoint": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/protocol/openid-connect/token",
         "user_info_url": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/protocol/openid-connect/userinfo",
         "client_id": "44fe2c7a-e9c5-43ec-87e9-3de78d2d3a11",
         "client_kwargs": {"scope": "openid email"},
