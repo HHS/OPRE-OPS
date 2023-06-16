0f824b192332526610d64a1ad7fafabe442f1ccc
fpigeonjr <fpigeon@flexion.us>
2023-06-16 11:36:16 -0500

    adds default procurement shop GCS

--- frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.jsx
+++ frontend/src/components/UI/Form/ProcurementShopSelect/ProcurementShopSelect.jsx
@@ -1,5 +1,21 @@
 import { useGetProcurementShopsQuery } from "../../../../api/opsAPI";
 
+/**
+ * Object representing a procurement shop.
+ * @typedef {Object} selectedProcurementShop - The currently selected procurement shop.
+ * @property {string} id - The procurement shop id.
+ * @property {number} fee - The procurement shop fee rate.
+ * @property {string} name - The procurement shop name.
+ * @property {string} abbr - The procurement shop abbreviation.
+ */
+
+/**
+ * A select input for choosing a procurement shop.
+ * @param {Object} props - The component props.
+ * @param {selectedProcurementShop} props.selectedProcurementShop - The currently selected procurement shop object.
+ * @param {Function} props.onChangeSelectedProcurementShop - A function to call when the selected procurement shop changes.
+ * @returns {JSX.Element} - The procurement shop select element.
+ */
 export const ProcurementShopSelect = ({ selectedProcurementShop, onChangeSelectedProcurementShop }) => {
     const {
         data: procurementShops,
@@ -16,6 +32,10 @@ export const ProcurementShopSelect = ({ selectedProcurementShop, onChangeSelecte
 
     const handleChange = (e) => {
         const procurementShopId = e.target.value;
+        if (procurementShopId === "0") {
+            onChangeSelectedProcurementShop(0);
+            return;
+        }
         const procurementShop = {
             id: procurementShops[procurementShopId - 1].id,
             name: procurementShops[procurementShopId - 1].name,
@@ -24,6 +44,27 @@ export const ProcurementShopSelect = ({ selectedProcurementShop, onChangeSelecte
         onChangeSelectedProcurementShop(procurementShop);
     };
 
+    /**
+     * Displays the fee rate for a selected procurement shop.
+     * @param {Object} props - The component props.
+     * @param {selectedProcurementShop} props.selectedProcurementShop - The selected procurement shop object.
+     * @returns {React.JSX.Element} - The fee rate element.
+     */
+    const FeeRate = ({ selectedProcurementShop }) => {
+        if (!selectedProcurementShop || selectedProcurementShop === 0) {
+            return <></>;
+        }
+
+        if (selectedProcurementShop?.id) {
+            return (
+                <span className="margin-left-1 text-base-dark font-12px">
+                    Fee Rate: {selectedProcurementShop?.fee * 100}%
+                </span>
+            );
+        } else {
+            return <span className="margin-left-1 text-base-dark font-12px">Fee Rate:0%</span>;
+        }
+    };
     return (
         <fieldset className="usa-fieldset">
             <label className="usa-label margin-top-0" htmlFor="procurement-shop-select">
@@ -36,6 +77,7 @@ export const ProcurementShopSelect = ({ selectedProcurementShop, onChangeSelecte
                     id="procurement-shop-select"
                     onChange={handleChange}
                     value={selectedProcurementShop?.id}
+                    defaultValue={2}
                     required
                 >
                     <option value={0}>- Select a Procurement Shop -</option>
@@ -45,11 +87,8 @@ export const ProcurementShopSelect = ({ selectedProcurementShop, onChangeSelecte
                         </option>
                     ))}
                 </select>
-                {selectedProcurementShop?.id && (
-                    <span className="margin-left-1 text-base-dark font-12px">
-                        Fee Rate: {selectedProcurementShop?.fee * 100}%
-                    </span>
-                )}
+
+                <FeeRate selectedProcurementShop={selectedProcurementShop} />
             </div>
         </fieldset>
     );
