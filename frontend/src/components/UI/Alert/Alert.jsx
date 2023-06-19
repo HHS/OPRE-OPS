ed555514876c5c7a5191bfc0da3be75754268485
fpigeonjr <fpigeon@flexion.us>
2023-06-19 12:29:12 -0500

    refactors to add alert data from state

--- frontend/src/components/UI/Alert/Alert.jsx
+++ frontend/src/components/UI/Alert/Alert.jsx
@@ -1,19 +1,46 @@
-import PropTypes from "prop-types";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { setIsActive, clearState } from "./alertSlice";
 
 /**
  * A component that displays an alert.
  * @param {object} props - The component props.
  * @param {("success"|"warning"|"error")} props.type - The type of the alert to be styled.
  * @param {string} props.heading - The heading of the alert.
- * @param {string} props.children - The message of the alert.
- * @param {function} props.setIsAlertActive - A function that sets whether the alert is active.
+ * @param {string} props.message - The message of the alert.
+ * @param {string} [props.navigateUrl] - The URL to navigate to - if any.
  * @returns {JSX.Element} The JSX element to render.
  * @see {@link https://designsystem.digital.gov/components/alerts/}
  */
-export const Alert = ({ type, heading, children, setIsAlertActive }) => {
+export const Alert = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isActive = useSelector((state) => state.alert.isActive);
+    const navigateUrl = useSelector((state) => state.alert.navigateUrl);
+    const heading = useSelector((state) => state.alert.heading);
+    const message = useSelector((state) => state.alert.message);
+    const type = useSelector((state) => state.alert.type);
     let classNames = "usa-alert margin-left-neg-4 margin-right-neg-4";
+
+    React.useEffect(() => {
+        const showAlert = async () => {
+            if (navigateUrl) {
+                navigate(navigateUrl);
+            }
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            window.scrollTo(0, 0);
+            dispatch(setIsActive(true));
+
+            await new Promise((resolve) => setTimeout(resolve, 6000));
+            dispatch(setIsActive(false));
+            dispatch(clearState());
+        };
+
+        showAlert();
+    }, [dispatch, navigate, navigateUrl]);
 
     switch (type) {
         case "success":
@@ -29,29 +56,24 @@ export const Alert = ({ type, heading, children, setIsAlertActive }) => {
     }
 
     return (
-        <div className={classNames}>
-            <div className="usa-alert__body display-flex flex-justify">
-                <div>
-                    <h1 className="usa-alert__heading">{heading}</h1>
-                    <p className="usa-alert__text">{children}</p>
+        isActive && (
+            <div className={classNames}>
+                <div className="usa-alert__body display-flex flex-justify">
+                    <div>
+                        <h1 className="usa-alert__heading">{heading}</h1>
+                        <p className="usa-alert__text">{message}</p>
+                    </div>
+                    <FontAwesomeIcon
+                        icon={faClose}
+                        className="height-2 width-2 margin-right-1 hover: cursor-pointer usa-tooltip"
+                        title="close"
+                        data-position="top"
+                        onClick={() => dispatch(setIsActive(false))}
+                    />
                 </div>
-                <FontAwesomeIcon
-                    icon={faClose}
-                    className="height-2 width-2 margin-right-1 hover: cursor-pointer usa-tooltip"
-                    title="close"
-                    data-position="top"
-                    onClick={() => setIsAlertActive(false)}
-                />
             </div>
-        </div>
+        )
     );
-};
-
-Alert.propTypes = {
-    type: PropTypes.oneOf(["success", "warning", "error"]),
-    heading: PropTypes.string.isRequired,
-    children: PropTypes.node,
-    setIsAlertActive: PropTypes.func.isRequired,
 };
 
 export default Alert;
