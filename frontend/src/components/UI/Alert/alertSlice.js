16bbe429e74cd3da7909868272c34b0202cce574
fpigeonjr <fpigeon@flexion.us>
2023-06-19 12:22:26 -0500

    adds Alert slice

---
+++ frontend/src/components/UI/Alert/alertSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    type: "",
+    heading: "",
+    message: "",
+    isActive: false,
+    redirectUrl: "",
+};
+
+const alertSlice = createSlice({
+    name: "alert",
+    initialState,
+    reducers: {
+        setAlert: (state, action) => {
+            state.type = action.payload.type;
+            state.heading = action.payload.heading;
+            state.message = action.payload.message;
+            state.redirectUrl = action.payload.redirectUrl;
+            state.isActive = true;
+        },
+        setIsActive: (state, action) => {
+            state.isActive = action.payload;
+        },
+        clearState: (state) => {
+            state = initialState;
+        },
+    },
+});
+
+export const { setAlert, setIsActive, clearState } = alertSlice.actions;
+
+export default alertSlice.reducer;
