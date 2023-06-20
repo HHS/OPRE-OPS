8bbee476f2474186eb7012f9adb394e872017ed2
John DeAngelis <john.deangelis2@gmail.com>
2023-06-20 16:04:15 -0400

    add Notification model
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

---
+++ backend/models/notifications.py
@@ -0,0 +1,26 @@
+from models import User
+from models.base import BaseModel
+from sqlalchemy import Boolean, Column, ForeignKey, Identity, Integer, String, Table
+from sqlalchemy.orm import relationship
+
+notification_recipients = Table(
+    "notification_recipients",
+    BaseModel.metadata,
+    Column("notification_id", ForeignKey("notification.id"), primary_key=True),
+    Column(
+        "user_id",
+        ForeignKey("users.id"),
+        primary_key=True,
+    ),
+)
+
+
+class Notification(BaseModel):
+    __tablename__ = "notification"
+    id = Column(Integer, Identity(), primary_key=True)
+    title = Column(String)
+    message = Column(String)
+    status = Column(Boolean)
+    recipients = relationship(
+        User, secondary=notification_recipients, back_populates="notifications"
+    )
