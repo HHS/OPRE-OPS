4fad309e2a7a9510fa2f9c65b1b34070971122f2
John DeAngelis <john.deangelis2@gmail.com>
2023-06-27 15:47:05 -0400

    add index.js
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/components/UI/Modal/Modal.jsx
+++ frontend/src/components/UI/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import Notification from "../Notification/Notification";
+import Notification from "../Notification";
 
 export const Modal = ({
     heading,
