8f3565bdb99de3054dd665e3c2bf71827b2062dd
fpigeonjr <fpigeon@flexion.us>
2023-06-28 12:07:06 -0500

    adds JSDocs

--- frontend/src/components/UI/Form/TextArea/TextArea.jsx
+++ frontend/src/components/UI/Form/TextArea/TextArea.jsx
@@ -1,5 +1,18 @@
 import cx from "clsx";
-
+/**
+ * A textarea input component.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.name - The name of the input field.
+ * @param {string} [props.label=name] - The label for the input field.
+ * @param {string} [props.hintMsg] - The hint message for the input field.
+ * @param {function} props.onChange - The change handler for the input field.
+ * @param {boolean} [props.pending=false] - Whether the input field is pending.
+ * @param {string[]} [props.messages=[]] - The error messages for the input field.
+ * @param {string} props.value - The value of the input field.
+ * @param {string} [props.className] - The CSS class for the input field.
+ * @returns {JSX.Element} - The textarea input component.
+ */
 export const TextArea = ({
     name,
     label = name,
@@ -29,7 +42,7 @@ export const TextArea = ({
                 className={`usa-textarea ${messages.length ? "usa-input--error" : null} `}
                 id={name}
                 name="description"
-                rows="5"
+                rows={5}
                 style={{ height: "7rem" }}
                 onChange={handleChange}
                 value={value}
