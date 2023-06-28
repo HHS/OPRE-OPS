5d8d266098732c98e10c939e13611e9d43f48fd7
John DeAngelis <john.deangelis2@gmail.com>
2023-06-28 14:16:34 -0400

    add e2e test for Agreement status in Agreement List
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/cypress/e2e/agreementList.cy.js
+++ frontend/cypress/e2e/agreementList.cy.js
@@ -23,3 +23,16 @@ it("navigates to the approveAgreements page when the approve button is clicked",
     cy.url().should("include", "/agreements/approve");
     cy.get("h1").should("exist");
 });
+
+it("the agreements have the correct status", () => {
+    cy.get("tbody").children().as("table-rows").should("have.length", 8);
+
+    cy.get("@table-rows").eq(0).should("contain", "Draft");
+    cy.get("@table-rows").eq(1).should("contain", "Planned");
+    cy.get("@table-rows").eq(2).should("contain", "Obligated");
+    cy.get("@table-rows").eq(3).should("contain", "Obligated");
+    cy.get("@table-rows").eq(4).should("contain", "Draft");
+    cy.get("@table-rows").eq(5).should("contain", "Draft");
+    cy.get("@table-rows").eq(6).should("contain", "Draft");
+    cy.get("@table-rows").eq(7).should("contain", "Draft");
+});
