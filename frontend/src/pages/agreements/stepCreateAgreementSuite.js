76a90a03e3f63d82e0be8b26cb335cdaca0c5882
fpigeonjr <fpigeon@flexion.us>
2023-06-29 16:41:35 -0500

    adds tests for product_service_code_id

--- frontend/src/pages/agreements/stepCreateAgreementSuite.js
+++ frontend/src/pages/agreements/stepCreateAgreementSuite.js
@@ -2,18 +2,21 @@ import { create, test, enforce, only } from "vest";
 
 const suite = create((data = {}, fieldName) => {
     only(fieldName); // only run the tests for the field that changed
-    // console.log(`data: ${JSON.stringify(data, null, 2)}`);
+    console.log(`data: ${JSON.stringify(data, null, 2)}`);
     test("agreement_type", "Contract is required for now.", () => {
-        // enforce(data["agreement-type"]).equals("CONTRACT");
         enforce(data.agreement_type).equals("CONTRACT");
     });
     test("name", "This is required information", () => {
-        // enforce(data["agreement-title"]).isNotBlank();
         enforce(data.name).isNotBlank();
     });
     test("description", "This is required information", () => {
-        // enforce(data["agreement-description"]).isNotBlank();
         enforce(data.description).isNotBlank();
+    });
+    test("product_service_code_id", "This is required information", () => {
+        enforce(data.product_service_code_id).isNotBlank();
+    });
+    test("product_service_code_id", "This is required information", () => {
+        enforce(data.product_service_code_id).notEquals("0");
     });
 });
 
