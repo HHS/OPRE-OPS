69ea031d7a56f1c0e08a1b79f85bdef29bffa83c
fpigeonjr <fpigeon@flexion.us>
2023-06-30 12:15:21 -0500

    add tests for agreement_reason and incumbent

--- frontend/src/pages/agreements/stepCreateAgreementSuite.js
+++ frontend/src/pages/agreements/stepCreateAgreementSuite.js
@@ -13,10 +13,21 @@ const suite = create((data = {}, fieldName) => {
         enforce(data.description).isNotBlank();
     });
     test("product_service_code_id", "This is required information", () => {
-        enforce(data.product_service_code_id).isNotBlank();
-    });
-    test("product_service_code_id", "This is required information", () => {
         enforce(data.product_service_code_id).greaterThan(0);
+    });
+    test("agreement_reason", "This is required information", () => {
+        enforce(data.agreement_reason).isNotBlank();
+    });
+    test("agreement_reason", "This is required information", () => {
+        enforce(data.agreement_reason).notEquals("0");
+    });
+    test("incumbent", "This is required information", () => {
+        if (
+            (data.agreement_reason && data.agreement_reason === "RECOMPETE") ||
+            data.agreement_reason === "LOGICAL_FOLLOW_ON"
+        ) {
+            enforce(data.incumbent).isNotBlank();
+        }
     });
 });
 
