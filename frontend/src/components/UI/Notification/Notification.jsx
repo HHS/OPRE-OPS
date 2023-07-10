226d0bd1ea835dc186ddb86e245d48cc3011d678
John DeAngelis <john.deangelis2@gmail.com>
2023-07-10 13:18:50 -0400

    refactor css
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/components/UI/Notification/Notification.jsx
+++ frontend/src/components/UI/Notification/Notification.jsx
@@ -7,8 +7,8 @@ export const Notification = ({ data }) => {
     return (
         <li className={customStyles.notificationListItem}>
             <div className={customStyles.flexContainer}>
-                <span className={`${customStyles.notificationHeader} text-bold`}>{data.title}</span>
-                <span className={`${customStyles.notificationDateTimeGroup} font-12px`}>
+                <span className="text-bold">{data.title}</span>
+                <span className="font-12px">
                     <FontAwesomeIcon icon={faClock} className="height-2 width-2 margin-right-1" />
                     <span>{timeAgo(data.created_on)}</span>
                 </span>
@@ -16,7 +16,7 @@ export const Notification = ({ data }) => {
             <div>
                 <p>{data.message}</p>
             </div>
-            <div className={customStyles.hrContainer}>
+            <div>
                 <hr className={customStyles.hrClass} />
             </div>
         </li>
