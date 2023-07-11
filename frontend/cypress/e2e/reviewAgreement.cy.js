56eed73ee374ebf4dbd09ed7bb58b1749ee20dd3
fpigeonjr <fpigeon@flexion.us>
2023-07-11 17:20:45 -0500

    wip
    adding tests for review agreement validations

--- frontend/cypress/e2e/reviewAgreement.cy.js
+++ frontend/cypress/e2e/reviewAgreement.cy.js
@@ -4,24 +4,10 @@ import { terminalLog, testLogin } from "./utils";
 
 const minAgreement = {
     agreement_type: "CONTRACT",
-    // agreement_reason: "NEW_REQ",
     name: "Test Contract",
     number: "TEST001",
-    // description: "Test Description",
     research_project_id: 1,
-    // product_service_code_id: 1,
     procurement_shop_id: 1,
-    // incumbent: "Test Vendor",
-    // project_officer: 1,
-    // team_members: [
-    //     {
-    //         id: 3,
-    //     },
-    //     {
-    //         id: 5,
-    //     },
-    // ],
-    // notes: "Test Notes",
 };
 
 beforeEach(() => {
@@ -34,7 +20,7 @@ afterEach(() => {
     cy.checkA11y(null, null, terminalLog);
 });
 
-it("edit an agreement", () => {
+it("review an agreement", () => {
     expect(localStorage.getItem("access_token")).to.exist;
 
     // create test agreement
@@ -56,10 +42,29 @@ it("edit an agreement", () => {
         cy.intercept("PATCH", "**/agreements/**").as("patchAgreement");
         cy.visit(`/agreements/approve/${agreementId}?mode=review`);
         cy.get("h1").should("have.text", "Please resolve the errors outlined below");
+
         cy.get('[data-cy="error-list"]').should("exist");
         cy.get('[data-cy="error-item"]').should("have.length", 7);
         //send-to-approval button should be disabled
         cy.get('[data-cy="send-to-approval-btn"]').should("be.disabled");
+
+        //fix errors
+        cy.get('[data-cy="edit-agreement-btn"]').click();
+        cy.get("#continue").click();
+        cy.get("#description").type("Test Description");
+        cy.get("#product_service_code_id").select(1);
+        cy.get("#agreement_reason").select("NEW_REQ");
+        cy.get("#project-officer-select-toggle-list").click();
+        cy.get("#project-officer-select-input").invoke("show");
+        cy.get("#users--list").invoke("show");
+        cy.get("li").contains("Chris Fortunato").click();
+        cy.get("#agreementNotes").type("This is a note.");
+        cy.get("[data-cy='continue-btn']").click();
+        //create a budget line with errors
+        cy.get("#enteredDescription").type("Test Budget Line");
+        cy.get("#enteredDescription").clear();
+        cy.get("#input-error-message").should("exist");
+        cy.get("#enteredDescription").type("Test Budget Line");
 
         // delete test agreement
         cy.request({
