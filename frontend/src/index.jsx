07ad39cfc30dc431dbf5ee51d20e01462f1e8dfb
John DeAngelis <john.deangelis2@gmail.com>
2023-07-17 10:36:55 -0400

    use single Route to AgreementsList
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -121,11 +121,7 @@ const router = createBrowserRouter(
                 <Route path="/agreements/create" element={<CreateAgreements />} />
                 <Route path="/projects/create" element={<CreateProject />} />
 
-                <Route path="/agreements/" element={<AgreementsList />}>
-                    <Route exact path="" element={<Navigate to={"all-agreements"} />} />
-                    <Route path="all-agreements" element={<AgreementsList filter="all-agreements" />} />
-                    <Route path="my-agreements" element={<AgreementsList filter="my-agreements" />} />
-                </Route>
+                <Route path="/agreements" element={<AgreementsList />} />
 
                 <Route path="/agreements/edit/:id" element={<EditAgreement />} />
                 <Route path="/agreements/approve/:id" element={<ApproveAgreement />} />
