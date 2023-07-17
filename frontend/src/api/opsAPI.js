b7b270ce985a00b659ca8531a9e240b13b1b71d9
John DeAngelis <john.deangelis2@gmail.com>
2023-07-17 13:39:36 -0400

    back out unneeded code
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/api/opsAPI.js
+++ frontend/src/api/opsAPI.js
@@ -29,10 +29,6 @@ export const opsApi = createApi({
             query: (id) => `/agreements/?research_project_id=${id}`,
             providesTags: ["Agreements", "FilterAgreements"],
         }),
-        getAgreementsByUserId: builder.query({
-            query: (id) => `/agreements/?user_id=${id}`,
-            providesTags: ["Agreements"],
-        }),
         getUserById: builder.query({
             query: (id) => `/users/${id}`,
             providesTags: ["Users"],
@@ -106,7 +102,6 @@ export const {
     useGetAgreementsQuery,
     useGetAgreementByIdQuery,
     useGetAgreementsByResearchProjectFilterQuery,
-    useGetAgreementsByUserIdQuery,
     useGetUserByIdQuery,
     useGetUserByOIDCIdQuery,
     useGetResearchProjectsQuery,
