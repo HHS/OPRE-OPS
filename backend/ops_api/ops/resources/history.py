04990f2ef1d4d9b5717d1ab2c99a5545f55bd02c
Cliff Hill <chill@flexion.us>
2023-07-18 10:55:29 -0400

    Building out the endpoint to handle the requirements from the issue.
    Signed-off-by: Cliff Hill <chill@flexion.us>

--- backend/ops_api/ops/resources/history.py
+++ backend/ops_api/ops/resources/history.py
@@ -1,8 +1,10 @@
 from typing_extensions import override
-from flask import Response
+from flask import current_app, Response, request
 from models.base import BaseModel
-from ops_api.ops.base_views import BaseListAPI
+from ops_api.ops.base_views import BaseListAPI, handle_sql_error
 from ops_api.ops.utils.auth import is_authorized, PermissionType, Permission
+from ops_api.ops.utils.response import make_response_with_headers
+from sqlalchemy import select
 
 
 class OpsDBHistoryListAPI(BaseListAPI):
@@ -12,4 +14,21 @@ class OpsDBHistoryListAPI(BaseListAPI):
     @override
     @is_authorized(PermissionType.GET, Permission.HISTORY)
     def get(self) -> Response:
-        return super().get()
+        class_name = request.args.get("class_name", None)
+        row_key = request.args.get("row_key", None)
+        with handle_sql_error:
+            stmt = select(self.model)
+            if class_name:
+                stmt = stmt.where(self.model.class_name == class_name)
+            if row_key:
+                stmt = stmt.where(self.model.row_key == row_key)
+            stmt = stmt.order_by(self.model.created_on)
+
+            item_list = [row[0] for row in current_app.db_session.execute(stmt).all()]
+
+            if item_list:
+                response = make_response_with_headers([item.to_dict() for item in item_list])
+            else:
+                response = make_response_with_headers({}, 404)
+            return response
+
