6de8a99716aafce435c08c12cb2b5e08349ea6c3
John DeAngelis <john.deangelis2@gmail.com>
2023-07-19 10:12:14 -0400

    do not display agreements table when there are none
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/pages/agreements/list/AgreementsTable.jsx
+++ frontend/src/pages/agreements/list/AgreementsTable.jsx
@@ -5,30 +5,31 @@ import { AgreementTableRow } from "./AgreementTableRow";
  * Agreement table.
  * @param {Object} props - The component props.
  * @param {Object[]} props.agreements - Array of Agreement to display in the table.
- * @returns {ReactNode} The rendered component.
+ * @returns {{agreements}} The rendered component.
  */
 export const AgreementsTable = ({ agreements }) => {
     return (
-        <table className="usa-table usa-table--borderless width-full ">
-            <thead>
-                <tr>
-                    <th scope="col">Agreement</th>
-                    <th scope="col">Project</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">Total</th>
-                    <th scope="col">Need By</th>
-                    <th scope="col" className="padding-0" style={{ width: "6.25rem" }}>
-                        Status
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {agreements?.map((agreement) => (
-                    <AgreementTableRow key={agreement?.id} agreement={agreement} />
-                ))}
-                {agreements?.length === 0 && <div className="padding-1 text-bold">There are no agreements.</div>}
-            </tbody>
-        </table>
+        agreements.length > 0 && (
+            <table className="usa-table usa-table--borderless width-full ">
+                <thead>
+                    <tr>
+                        <th scope="col">Agreement</th>
+                        <th scope="col">Project</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Total</th>
+                        <th scope="col">Need By</th>
+                        <th scope="col" className="padding-0" style={{ width: "6.25rem" }}>
+                            Status
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {agreements?.map((agreement) => (
+                        <AgreementTableRow key={agreement?.id} agreement={agreement} />
+                    ))}
+                </tbody>
+            </table>
+        )
     );
 };
 
