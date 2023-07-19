427fee9229868d58058f2c9101cc1d1a7402ee65
John DeAngelis <john.deangelis2@gmail.com>
2023-07-19 15:48:28 -0400

    fix the overlay
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/pages/agreements/list/AgreementsFilterButton.jsx
+++ frontend/src/pages/agreements/list/AgreementsFilterButton.jsx
@@ -9,18 +9,14 @@ import customStyles from "./AgreementsFilterButton.module.css";
  */
 export const AgreementsFilterButton = ({ setFilterFunctions }) => {
     const [showModal, setShowModal] = React.useState(false);
-    const buttonRef = React.useRef(null);
-
-    const getModalParent = () => buttonRef.current;
 
     Modal.setAppElement("#root");
 
     return (
-        <div className={customStyles.container}>
+        <div className={customStyles.container} id="filter-container">
             <button
                 className={`usa-button display-flex flex-align-center ${customStyles.filterButton}`}
                 onClick={() => setShowModal(true)}
-                ref={buttonRef}
             >
                 <svg
                     className="height-2 width-2 margin-right-05 hover: cursor-pointer usa-tooltip"
@@ -33,24 +29,38 @@ export const AgreementsFilterButton = ({ setFilterFunctions }) => {
             <Modal
                 isOpen={showModal}
                 onRequestClose={() => setShowModal(false)}
-                parentSelector={getModalParent}
+                parentSelector={() => document.querySelector("#filter-container")}
                 className={customStyles.agreementsFilterModal}
                 overlayClassName={customStyles.agreementsFilterOverlay}
             >
-                <div>
-                    <h1 className="text-bold">Filters</h1>
-                    <h3>Upcoming Need By Date</h3>
-                    <h3>Project</h3>
-                    <h3>Project Officer</h3>
-                    <h3>Type</h3>
-                    <h3>Procurement Shop</h3>
-                    <h3>Budget Line Status</h3>
-                    <button onClick={() => setShowModal(false)}>
-                        <span>Reset</span>
-                    </button>
-                    <button onClick={() => setShowModal(false)}>
-                        <span>Apply</span>
-                    </button>
+                <div className="">
+                    <h1 className={`text-bold`}>Filters</h1>
+                    <div>
+                        <h3>Upcoming Need By Date</h3>
+                    </div>
+                    <div>
+                        <h3>Project</h3>
+                    </div>
+                    <div>
+                        <h3>Project Officer</h3>
+                    </div>
+                    <div>
+                        <h3>Type</h3>
+                    </div>
+                    <div>
+                        <h3>Procurement Shop</h3>
+                    </div>
+                    <div>
+                        <h3>Budget Line Status</h3>
+                    </div>
+                    <div>
+                        <button onClick={() => setShowModal(false)}>
+                            <span>Reset</span>
+                        </button>
+                        <button onClick={() => setShowModal(false)}>
+                            <span>Apply</span>
+                        </button>
+                    </div>
                 </div>
             </Modal>
         </div>
