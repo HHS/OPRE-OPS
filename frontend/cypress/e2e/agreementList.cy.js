d27764a738769f627a2c65b4107b7d6cb52912fb
John DeAngelis <john.deangelis2@gmail.com>
2023-07-19 09:37:56 -0400

    remove e2e test for agreement status
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/cypress/e2e/agreementList.cy.js
+++ frontend/cypress/e2e/agreementList.cy.js
@@ -24,36 +24,6 @@ it("navigates to the approveAgreements page when the approve button is clicked",
     cy.get("h1").should("exist");
 });
 
-it("the agreements have the correct status", () => {
-    cy.get("tbody").children().as("table-rows");
-
-    cy.get("@table-rows")
-        .should("contain", "Contract #1: African American Child and Family Research Center")
-        .and("contain", "Draft");
-
-    cy.get("@table-rows")
-        .should("contain", "DIRECT ALLOCATION #2: African American Child and Family Research Center")
-        .and("contain", "Planned");
-
-    cy.get("@table-rows").should("contain", "MIHOPE Check-In").and("contain", "Obligated");
-
-    cy.get("@table-rows").should("contain", "MIHOPE Long-Term").and("contain", "Obligated");
-
-    cy.get("@table-rows")
-        .should("contain", "Grant #1: Early Care and Education Leadership Study (ExCELS)")
-        .and("contain", "Draft");
-
-    cy.get("@table-rows")
-        .should("contain", "IAA #1: Early Care and Education Leadership Study (ExCELS)")
-        .and("contain", "Draft");
-
-    cy.get("@table-rows").should("contain", "IAA-AA #1: Fathers and Continuous Learning (FCL)").and("contain", "Draft");
-
-    cy.get("@table-rows")
-        .should("contain", "CONTRACT #2: Fathers and Continuous Learning (FCL)")
-        .and("contain", "Draft");
-});
-
 it("Agreements Table is correctly filtered on all-agreements or my-agreements", () => {
     cy.visit("/agreements?filter=all-agreements");
     cy.get("tbody").children().should("have.length.at.least", 1);
