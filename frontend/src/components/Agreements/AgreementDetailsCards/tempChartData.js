98616ac9f8abddd5cc6236b4d8d72f194decfde1
fpigeonjr <fpigeon@flexion.us>
2023-07-25 17:59:21 -0500

    updates sample data like Figma

--- frontend/src/components/Agreements/AgreementDetailsCards/tempChartData.js
+++ frontend/src/components/Agreements/AgreementDetailsCards/tempChartData.js
@@ -1,17 +1,17 @@
 export const data = [
     {
         FY: "FY2025",
-        budget: 12000000,
+        budget: 1000000,
         color: "hsla(153, 49%, 47%, 1)",
     },
     {
         FY: "FY2024",
-        budget: 15000000,
+        budget: 500000,
         color: "hsla(157, 33%, 72%, 1)",
     },
     {
         FY: "FY2023",
-        budget: 32000000,
+        budget: 2000000,
         color: "hsla(116, 44%, 32%, 1)",
     },
 ];
