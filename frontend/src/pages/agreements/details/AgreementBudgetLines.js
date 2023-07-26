b59c58ce76abbd6101648f0962526cfe6faac269
fpigeonjr <fpigeon@flexion.us>
2023-07-26 17:16:12 -0500

    adds fallback for no budgetlines

--- frontend/src/pages/agreements/details/AgreementBudgetLines.js
+++ frontend/src/pages/agreements/details/AgreementBudgetLines.js
@@ -1,11 +1,24 @@
+import PropTypes from "prop-types";
 import PreviewTable from "../../../components/UI/PreviewTable/PreviewTable";
 
-const AgreementBudgetLines = ({ agreement }) => {
-    return (
-        <div>
-            <PreviewTable budgetLinesAdded={agreement?.budget_line_items} readOnly={true} />
-        </div>
+/**
+ * Agreement budget lines.
+ * @param {Object} props - The component props.
+ * @param {Object} props.agreement - The agreement to display.
+ * @returns {React.JSX.Element} - The rendered component.
+ */
+export const AgreementBudgetLines = ({ agreement }) => {
+    return agreement?.budget_line_items.length > 0 ? (
+        <PreviewTable budgetLinesAdded={agreement?.budget_line_items} readOnly={true} />
+    ) : (
+        <p>No budget lines.</p>
     );
+};
+
+AgreementBudgetLines.propTypes = {
+    agreement: PropTypes.shape({
+        budget_line_items: PropTypes.arrayOf(PropTypes.object),
+    }),
 };
 
 export default AgreementBudgetLines;
