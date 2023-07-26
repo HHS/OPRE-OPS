3984036f9a1bf4de652c81ce6bda02533c4563fc
fpigeonjr <fpigeon@flexion.us>
2023-07-26 17:40:25 -0500

    adds AgreementDetail Header component

--- frontend/src/pages/agreements/details/AgreementBudgetLines.js
+++ frontend/src/pages/agreements/details/AgreementBudgetLines.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import PreviewTable from "../../../components/UI/PreviewTable/PreviewTable";
-
+import AgreementDetailHeader from "./AgreementDetailHeader";
 /**
  * Agreement budget lines.
  * @param {Object} props - The component props.
@@ -8,15 +8,25 @@ import PreviewTable from "../../../components/UI/PreviewTable/PreviewTable";
  * @returns {React.JSX.Element} - The rendered component.
  */
 export const AgreementBudgetLines = ({ agreement }) => {
-    return agreement?.budget_line_items.length > 0 ? (
-        <PreviewTable budgetLinesAdded={agreement?.budget_line_items} readOnly={true} />
-    ) : (
-        <p>No budget lines.</p>
+    return (
+        <>
+            <AgreementDetailHeader
+                agreementId={agreement?.id}
+                heading="Budget Lines"
+                details="This is a list of all budget lines within this agreement."
+            />
+            {agreement?.budget_line_items.length > 0 ? (
+                <PreviewTable budgetLinesAdded={agreement?.budget_line_items} readOnly={true} />
+            ) : (
+                <p>No budget lines.</p>
+            )}
+        </>
     );
 };
 
 AgreementBudgetLines.propTypes = {
     agreement: PropTypes.shape({
+        id: PropTypes.number,
         budget_line_items: PropTypes.arrayOf(PropTypes.object),
     }),
 };
