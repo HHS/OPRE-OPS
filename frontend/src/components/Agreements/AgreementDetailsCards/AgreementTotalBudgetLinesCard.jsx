1047255d0594017d30df4f980c85e49f0f97998e
fpigeonjr <fpigeon@flexion.us>
2023-07-26 11:16:25 -0500

    adds propTypes and JSDocs

--- frontend/src/components/Agreements/AgreementDetailsCards/AgreementTotalBudgetLinesCard.jsx
+++ frontend/src/components/Agreements/AgreementDetailsCards/AgreementTotalBudgetLinesCard.jsx
@@ -1,6 +1,15 @@
 import RoundedBox from "../../UI/RoundedBox/RoundedBox";
+import PropTypes from "prop-types";
 import { StatusTagList } from "../../UI/Tag/StatusTag";
 
+/**
+ * A component that displays the total budget lines for an agreement.
+ *
+ * @param {Object} props - The component props.
+ * @param {number} props.numberOfAgreements - The number of agreements.
+ * @param {Object} props.countsByStatus - The counts of agreements by status.
+ * @returns {React.JSX.Element} - The agreement total budget lines card component JSX.
+ */
 const AgreementTotalBudgetLinesCard = ({ numberOfAgreements = 0, countsByStatus = {} }) => {
     const headerText = "Total Budget Lines";
 
@@ -10,7 +19,9 @@ const AgreementTotalBudgetLinesCard = ({ numberOfAgreements = 0, countsByStatus 
                 <article data-cy="agreement-total-budget-lines-card-article">
                     <h3 className="margin-0 margin-bottom-3 font-12px text-base-dark text-normal">{headerText}</h3>
                     <div className="display-flex flex-justify width-fit-content">
-                        <span className="font-sans-xl text-bold line-height-sans-1" data-cy="number-of-agreements">{numberOfAgreements}</span>
+                        <span className="font-sans-xl text-bold line-height-sans-1" data-cy="number-of-agreements">
+                            {numberOfAgreements}
+                        </span>
                         <div className="display-flex flex-column margin-left-105 grid-gap">
                             <StatusTagList countsByStatus={countsByStatus} />
                         </div>
@@ -19,6 +30,11 @@ const AgreementTotalBudgetLinesCard = ({ numberOfAgreements = 0, countsByStatus 
             </div>
         </RoundedBox>
     );
+};
+
+AgreementTotalBudgetLinesCard.propTypes = {
+    numberOfAgreements: PropTypes.number.isRequired,
+    countsByStatus: PropTypes.object.isRequired,
 };
 
 export default AgreementTotalBudgetLinesCard;
