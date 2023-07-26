ce6f55a9b8f7b4f1201d00bec37aacfb6da29a2e
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-07-26 07:58:09 -0600

    WIP: nextThreeFyValues

--- frontend/src/components/Agreements/AgreementDetailsCards/AgreementValuesCard.jsx
+++ frontend/src/components/Agreements/AgreementDetailsCards/AgreementValuesCard.jsx
@@ -17,16 +17,32 @@ const AgreementTotalBudgetLinesCard = ({ budgetLineItems }) => {
     }, {});
     const fyValues = Object.keys(fyValuesMap).map((fy) => ({ fiscalYear: fy, amount: fyValuesMap[fy] }));
     const totalValue = fyValues.reduce((acc, cur) => acc + cur.amount, 0);
+    const currentFiscalYear = fiscalYearFromDate(new Date());
+    const nextThreeFyValues = fyValues.filter((fyVal) => {
+        return fyVal.fiscalYear >= currentFiscalYear && fyVal.fiscalYear < currentFiscalYear + 3;
+    })
 
     return (
         <CurrencySummaryCard headerText={headerText} amount={totalValue}>
+            <div>next three</div>
             <ul>
-                {fyValues.map((fyVal) => (
+                {nextThreeFyValues.map((fyVal) => (
                     <li key={fyVal.fiscalYear}>
                         FY {fyVal.fiscalYear}: {fyVal.amount}
                     </li>
                 ))}
             </ul>
+            <hr/>
+            <div style={{color: "#999999", borderTop: "1em"}}>
+                <div>All years</div>
+                <ul style={{color: "#999999"}}>
+                    {fyValues.map((fyVal) => (
+                        <li key={fyVal.fiscalYear}>
+                            FY {fyVal.fiscalYear}: {fyVal.amount}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </CurrencySummaryCard>
     );
 };
