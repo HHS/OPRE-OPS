d30e4c4b0536df19083ac22a269999d859e4161d
Tim Donaworth <tdonaworth@flexion.us>
2023-07-31 09:46:19 -0400

    WIP - Edit User From

--- frontend/src/components/UI/Form/EditUserForm/EditUserForm.jsx
+++ frontend/src/components/UI/Form/EditUserForm/EditUserForm.jsx
@@ -1,28 +1,111 @@
 import PropTypes from "prop-types";
 import Input from "../Input/Input";
+import { useState } from "react";
+import { callBackend } from "../../../../helpers/backend";
 
-export const EditUserForm = ({
-    user,
-    handleEditForm = () => {},
-    handleSubmitForm = () => {},
-    handleResetForm = () => {},
-}) => {
-    const dateJoined = new Date(user?.date_joined).toLocaleDateString("en-US", {
+export const EditUserForm = ({ user }) => {
+    const [formData, setFormData] = useState({
+        firstName: user?.first_name || "",
+        lastName: user?.last_name || "",
+        email: user?.email || "",
+        division: user?.division || "",
+        roles: user?.roles || "",
+        dateJoined: user?.date_joined || "",
+        oidcId: user?.oidc_id || "",
+        hhsId: user?.hhs_id || "",
+    });
+
+    const handleChange = (event) => {
+        if (event && event.target) {
+            const { name, value } = event.target;
+            setFormData((prevFormData) => ({
+                ...prevFormData,
+                [name]: value,
+            }));
+        }
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await callBackend("PUT", `/api/users/${user.id}`, formData);
+            console.log(response.data);
+            // handle success
+        } catch (error) {
+            console.error(error);
+            // handle error
+        }
+    };
+
+    const dateJoined = new Date(formData?.date_joined).toLocaleDateString("en-US", {
         year: "numeric",
         month: "short",
         day: "numeric",
     });
+
     return (
         <form className="usa-form usa-form--large">
             <fieldset className="usa-fieldset">
-                <Input name="first-name" label="First or Given Name" value={user?.first_name} />
-                <Input name="last-name" label="Last or Family Name" value={user?.last_name} />
-                <Input name="email" label="Email Address" value={user?.email} />
-                <Input name="division" label="Division" value={user?.division} />
-                <Input name="roles" label="Roles" value={user?.roles} disabled />
-                <Input name="date-joined" label="Date Joined" value={dateJoined} disabled={true} />
-                <Input name="oidc-id" label="OIDC ID" value={user?.oidc_id} disabled={true} />
-                <Input name="hhs-id" label="HHS ID" value={user?.hhs_id} disabled={true} />
+                <Input
+                    name="first-name"
+                    label="First or Given Name"
+                    value={formData?.first_name}
+                    onChange={(event) => handleChange(event)}
+                />
+                <Input
+                    name="last-name"
+                    label="Last or Family Name"
+                    value={formData?.last_name}
+                    onChange={(event) => handleChange(event)}
+                />
+                <Input
+                    name="email"
+                    label="Email Address"
+                    value={formData?.email}
+                    onChange={(event) => handleChange(event)}
+                />
+                <Input
+                    name="division"
+                    label="Division"
+                    value={formData?.division}
+                    onChange={(event) => handleChange(event)}
+                />
+                <Input
+                    name="roles"
+                    label="Roles"
+                    value={formData?.roles}
+                    disabled
+                    onChange={(event) => handleChange(event)}
+                />
+                <Input
+                    name="date-joined"
+                    label="Date Joined"
+                    value={dateJoined}
+                    disabled={true}
+                    onChange={(event) => handleChange(event)}
+                />
+                <Input
+                    name="oidc-id"
+                    label="OIDC ID"
+                    value={formData?.oidc_id}
+                    disabled={true}
+                    onChange={(event) => handleChange(event)}
+                />
+                <Input
+                    name="hhs-id"
+                    label="HHS ID"
+                    value={formData?.hhs_id}
+                    disabled={true}
+                    onChange={(event) => handleChange(event)}
+                />
+
+                <button
+                    id="edit-user-submit"
+                    className="usa-button usa-button--outline margin-top-2 float-right margin-right-0"
+                    type="submit"
+                >
+                    Save Changes
+                </button>
             </fieldset>
         </form>
     );
@@ -30,9 +113,6 @@ export const EditUserForm = ({
 
 EditUserForm.propTypes = {
     user: PropTypes.object.isRequired,
-    handleEditForm: PropTypes.func.isRequired,
-    handleSubmitForm: PropTypes.func.isRequired,
-    handleResetForm: PropTypes.func.isRequired,
 };
 
 export default EditUserForm;
--- frontend/src/pages/users/edit/EditUser.jsx
+++ frontend/src/pages/users/edit/EditUser.jsx
@@ -1,28 +1,51 @@
 // @ts-nocheck
-import { useSelector, useDispatch } from "react-redux";
-import { getUser } from "../../../api/getUser";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import App from "../../../App";
 import { Breadcrumb } from "../../../components/UI/Header/Breadcrumb";
-import { setUser } from "./userSlice";
-import UserInfoForm from "../../../components/Users/UserInfoForm/UserInfoForm";
 import EditUserForm from "../../../components/UI/Form/EditUserForm/EditUserForm";
+import { useGetUserByIdQuery } from "../../../api/opsAPI";
 
 const UserDetail = () => {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.userDetailEdit.user);
+    //const dispatch = useDispatch();
+    //const user = useSelector((state) => state.userDetailEdit.user);
     const urlPathParams = useParams();
     const userId = urlPathParams.id ? parseInt(urlPathParams.id) : undefined;
 
-    useEffect(() => {
-        const getUserAndSetState = async (userId) => {
-            const results = await getUser(userId);
-            dispatch(setUser(results));
-        };
+    const {
+        data: user,
+        error: errorAgreement,
+        isLoading: isLoadingAgreement,
+        refetch,
+    } = useGetUserByIdQuery(userId, { refetchOnMountOrArgChange: true });
 
-        getUserAndSetState(userId).catch(console.error);
-    }, [dispatch, userId]);
+    useEffect(() => {
+        refetch();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    if (isLoadingAgreement) {
+        return (
+            <App>
+                <h1>Loading...</h1>
+            </App>
+        );
+    }
+    if (errorAgreement) {
+        return (
+            <App>
+                <h1>Oops, an error occurred</h1>
+            </App>
+        );
+    }
+    // useEffect(() => {
+    //     const getUserAndSetState = async (userId) => {
+    //         const results = await getUser(userId);
+    //         dispatch(setUser(results));
+    //     };
+
+    //     getUserAndSetState(userId).catch(console.error);
+    // }, [dispatch, userId]);
 
     return (
         <>
