3b93e716b8cc7a69d535428c213122c849a57e09
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-07-31 14:32:05 -0600

    cleanup debugging

--- frontend/src/components/Agreements/AgreementEditor/AgreementEditorContext.js
+++ frontend/src/components/Agreements/AgreementEditor/AgreementEditorContext.js
@@ -68,7 +68,6 @@ export function useEditAgreementDispatch() {
 }
 
 export function useSetState(key) {
-    console.log("useSetState:", key);
     const dispatch = useContext(EditAgreementDispatchContext);
 
     const setValue = (value) => {
@@ -78,7 +77,6 @@ export function useSetState(key) {
     return setValue;
 }
 export function useUpdateAgreement(key) {
-    console.log("useUpdateAgreement:", key);
     const dispatch = useContext(EditAgreementDispatchContext);
 
     const setValue = (value) => {
