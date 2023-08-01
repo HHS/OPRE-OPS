23c1a982244c723bb132b0e41938ff17d9667d2e
John DeAngelis <john.deangelis2@gmail.com>
2023-08-01 13:32:12 -0400

    filter agreements by upcomingNeedByDate
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/pages/agreements/list/AgreementsList.jsx
+++ frontend/src/pages/agreements/list/AgreementsList.jsx
@@ -10,6 +10,7 @@ import AgreementsTable from "./AgreementsTable";
 import AgreementsFilterHeaderSection from "./AgreementsFilterHeaderSection";
 import { useSearchParams } from "react-router-dom";
 import _ from "lodash";
+import { getCurrentFiscalYear } from "../../../helpers/utils";
 
 /**
  * Page for the Agreements List.
@@ -62,14 +63,39 @@ export const AgreementsList = () => {
     }
 
     let filteredAgreements = _.cloneDeep(agreements);
-    // for (let filter of filters) {
-    //     filteredAgreements = filteredAgreements.filter(filterFunction);
-    // }
 
-    // console.log("filteredAgreements", filteredAgreements);
-    // console.log("filterFunctions", filterFunctions);
-
-    console.log("filters", filters);
+    switch (filters.upcomingNeedByDate) {
+        case "next-30-days":
+            filteredAgreements = filteredAgreements.filter((agreement) => {
+                return agreement.budget_line_items.some((bli) => {
+                    return (
+                        bli.date_needed &&
+                        new Date(bli.date_needed) <= new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000)
+                    );
+                });
+            });
+            break;
+        case "current-fy":
+            filteredAgreements = filteredAgreements.filter((agreement) => {
+                return agreement.budget_line_items.some((bli) => {
+                    const currentFY = Number(getCurrentFiscalYear(new Date()));
+                    const bliFY = new Date(bli.date_needed).getFullYear();
+                    return bli.date_needed && bliFY === currentFY;
+                });
+            });
+            break;
+        case "next-6-months":
+            filteredAgreements = filteredAgreements.filter((agreement) => {
+                const now = new Date();
+                const sixMonthsFromNow = new Date(now.setMonth(now.getMonth() + 6));
+                return agreement.budget_line_items.some((bli) => {
+                    return bli.date_needed && new Date(bli.date_needed) <= sixMonthsFromNow;
+                });
+            });
+            break;
+        case "all-time":
+            break;
+    }
 
     let sortedAgreements;
     if (searchParams.get("filter") === "my-agreements") {
