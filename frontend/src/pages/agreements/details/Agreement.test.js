e237eeedb495a8f7a4f3345a1f784c4c3fc70f45
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-08-01 11:05:56 -0600

    minimal test for Agreement.js

---
+++ frontend/src/pages/agreements/details/Agreement.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Agreement from "./Agreement";
+
+import store from "../../../store";
+import { Provider } from "react-redux";
+
+describe("Agreement", () => {
+    test("renders correctly", () => {
+        render(
+            <Provider store={store}>
+                <Agreement/>
+            </Provider>
+        );
+ });
+});
