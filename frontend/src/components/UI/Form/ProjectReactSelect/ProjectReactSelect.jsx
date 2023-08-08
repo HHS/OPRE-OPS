f4a8ecb92e5a961b4dcaabc94c58dec2b09c4618
John DeAngelis <john.deangelis2@gmail.com>
2023-08-08 10:05:00 -0400

    refactor to use combobox
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/components/UI/Form/ProjectReactSelect/ProjectReactSelect.jsx
+++ frontend/src/components/UI/Form/ProjectReactSelect/ProjectReactSelect.jsx
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
-import Select from "react-select";
-import { useEffect, useState } from "react";
+import ComboBox from "../ComboBox";
 
 /**
  *  A comboBox for choosing a project.
@@ -19,70 +18,6 @@ export const ProjectReactSelect = ({
     legendClassname = "usa-label margin-top-0",
     defaultString = "",
 }) => {
-    const [selectedOption, setSelectedOption] = useState(null);
-
-    const options = researchProjects.map((project) => {
-        return { value: project.id, label: project.title };
-    });
-
-    const customStyles = {
-        control: (provided, state) => ({
-            ...provided,
-            background: "#fff",
-            borderColor: "565c65",
-            minHeight: "40px",
-            height: "40px",
-            boxShadow: state.isFocused ? null : null,
-            outline: state.isFocused ? "0.25rem solid #2491ff" : null,
-            borderRadius: 0,
-        }),
-
-        placeholder: (provided) => ({
-            ...provided,
-            color: "#1b1b1b",
-        }),
-
-        valueContainer: (provided) => ({
-            ...provided,
-            height: "40px",
-            padding: "0 6px",
-        }),
-
-        input: (provided) => ({
-            ...provided,
-            margin: "0px",
-        }),
-        indicatorSeparator: () => ({
-            display: "none",
-        }),
-        indicatorsContainer: (provided) => ({
-            ...provided,
-            height: "40px",
-        }),
-    };
-
-    useEffect(() => {
-        selectedResearchProject === undefined && setSelectedOption(null);
-    }, [selectedResearchProject]);
-
-    const handleChange = (e, actionObj) => {
-        if (actionObj.action === "clear") {
-            setSelectedProject({});
-            setSelectedOption(null);
-        } else {
-            const projId = e.value;
-            const projObj = researchProjects.find((proj) => proj.id === Number(projId));
-            setSelectedProject(projObj);
-
-            const option = options.find((option) => option.value === Number(projId));
-            setSelectedOption(option);
-        }
-    };
-
-    const defaultOption = selectedResearchProject
-        ? options.find((option) => option.value === Number(selectedResearchProject?.id))
-        : null;
-
     return (
         <div className="display-flex flex-justify">
             <div className="left-half width-full">
@@ -90,20 +25,12 @@ export const ProjectReactSelect = ({
                     Project
                 </label>
                 <div className="" data-enhanced="true">
-                    <Select
-                        className="margin-0"
-                        classNamePrefix={"project-react-select"}
-                        data-cy="project-react-select"
-                        data-testid="project-react-select"
-                        name="project-react-select"
-                        tabIndex="0"
-                        value={defaultOption ?? selectedOption}
-                        onChange={handleChange}
-                        options={options}
-                        placeholder={defaultString}
-                        styles={customStyles}
-                        isSearchable={true}
-                        isClearable={true}
+                    <ComboBox
+                        namespace="project-react-select"
+                        data={researchProjects}
+                        selectedData={selectedResearchProject}
+                        setSelectedData={setSelectedProject}
+                        defaultString={defaultString}
                     />
                 </div>
             </div>
