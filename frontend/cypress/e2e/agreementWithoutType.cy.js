6d9848b0c6a343084637c1788964119e3875894f
fpigeonjr <fpigeon@flexion.us>
2023-08-14 09:35:18 -0500

    WIP

--- frontend/cypress/e2e/agreementWithoutType.cy.js
+++ frontend/cypress/e2e/agreementWithoutType.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 import { terminalLog, testLogin } from "./utils";
 const testAgreement = {
-    agreement_type: "CONTRACT",
+    // agreement_type: "CONTRACT",
     agreement_reason: "NEW_REQ",
     name: "Test Contract",
     number: "TEST001",
@@ -22,9 +22,26 @@ const testAgreement = {
     notes: "Test Notes",
 };
 
+const addAgreement = (agreement) => {
+    expect(localStorage.getItem("access_token")).to.exist;
+
+    // create test agreement
+    const bearer_token = `Bearer ${window.localStorage.getItem("access_token")}`;
+    cy.request({
+        method: "POST",
+        url: "http://localhost:8080/api/v1/agreements/",
+        body: agreement,
+        headers: {
+            Authorization: bearer_token,
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+    });
+};
+
 beforeEach(() => {
     testLogin("admin");
-    cy.visit("/agreements/create");
+    cy.visit("/agreements/");
 });
 
 afterEach(() => {
@@ -32,7 +49,19 @@ afterEach(() => {
     cy.checkA11y(null, null, terminalLog);
 });
 
+it("loads with 8 agreements", () => {
+    cy.get("tbody").children().as("table-rows").should("have.length", 8);
+});
 
-it("it handles errors by redirecting to error page", () => {
-    cy.get("h2").should("contain", "Select a Project");
+it("handles agreementwithout Type by redirecting to Error page", () => {
+    cy.intercept("POST", "**/agreements").as("postAgreement");
+    cy.get("tbody").children().as("table-rows").should("have.length", 8);
+    addAgreement(testAgreement);
+    cy.wait("@postAgreement")
+        .then((interception) => {
+            const { statusCode, body } = interception.response;
+            expect(statusCode).to.equal(401);
+            // expect(body.message).to.equal("Agreement created");
+        })
+        .then(cy.log);
 });
