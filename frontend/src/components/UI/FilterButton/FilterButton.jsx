a0b53ad7ba31e395c964edb6612d09a395f8a8a8
John DeAngelis <john.deangelis2@gmail.com>
2023-08-15 10:22:40 -0400

    refactor:rename css class names
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/components/UI/FilterButton/FilterButton.jsx
+++ frontend/src/components/UI/FilterButton/FilterButton.jsx
@@ -44,8 +44,8 @@ export const FilterButton = ({ applyFilter, resetFilter, fieldsetList }) => {
                 isOpen={showModal}
                 onRequestClose={() => setShowModal(false)}
                 parentSelector={() => document.querySelector("#filter-container")}
-                className={`${customStyles.agreementsFilterModal} ${customStyles.modalBackgroundColor}`}
-                overlayClassName={customStyles.agreementsFilterOverlay}
+                className={`${customStyles.filterModal} ${customStyles.modalBackgroundColor}`}
+                overlayClassName={customStyles.filterOverlay}
             >
                 <div className="margin-left-2">
                     <h1 className="text-bold font-sans-lg margin-bottom-205 margin-top-205">Filters</h1>
