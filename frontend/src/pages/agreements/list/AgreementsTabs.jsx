e4c9fffbc602955c4c46e847f898ac7e111c8ac0
fpigeonjr <fpigeon@flexion.us>
2023-08-16 18:14:17 -0500

    rename to AgreementTabs

--- frontend/src/pages/agreements/list/AgreementsTabs.jsx
+++ frontend/src/pages/agreements/list/AgreementsTabs.jsx
@@ -1,13 +1,10 @@
-import "./AgreementsList.scss";
 import { Link, useLocation } from "react-router-dom";
 import styles from "../../../components/Portfolios/PortfolioTabsSection/PortfolioTabsSection.module.scss";
 import TabsSection from "../../../components/UI/TabsSection";
+import "./AgreementsList.scss";
 
 /**
  * A header section of the agreements page that contains the filters.
- * @param {Object} props - The component props.
- * @param {Object} props.filters - The current filters.
- * @param {Function} props.setFilters - A function to call to set the filters.
  * @returns {JSX.Element} - The procurement shop select element.
  */
 export const AgreementsFilterHeaderSection = () => {
