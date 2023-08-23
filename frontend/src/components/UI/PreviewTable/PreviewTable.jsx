99fc66a09cb5aafbf7ee83e2113cbbec5da5fab7
fpigeonjr <fpigeon@flexion.us>
2023-08-23 12:05:06 -0500

    cleanup

--- frontend/src/components/UI/PreviewTable/PreviewTable.jsx
+++ frontend/src/components/UI/PreviewTable/PreviewTable.jsx
@@ -63,9 +63,7 @@ export const PreviewTable = ({
         const isBudgetLineEditable =
             (canUserEditBudgetLines || isUserBudgetLineCreator) &&
             (isBudgetLineDraft || isBudgetLineInReview || isBudgetLinePlanned);
-        console.log({ canUserEditBudgetLines });
-        console.log({ isUserBudgetLineCreator });
-        console.log({ isBudgetLineEditable });
+
         const handleExpandRow = () => {
             setIsExpanded(!isExpanded);
             setIsRowActive(true);
