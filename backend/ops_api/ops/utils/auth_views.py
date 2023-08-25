5f2e921e80421db40e5b78d304d1210713b31f04
Cliff Hill <chill@flexion.us>
2023-08-25 09:33:25 -0400

    Handling freshness for JWT tokens.
    Signed-off-by: Cliff Hill <chill@flexion.us>

--- backend/ops_api/ops/utils/auth_views.py
+++ backend/ops_api/ops/utils/auth_views.py
@@ -110,8 +110,8 @@ def _get_token_and_user_data_from_internal_auth(user_data: dict[str, str]):
         additional_claims = {}
         if user.roles:
             additional_claims["roles"] = [role.name for role in user.roles]
-
-        access_token = create_access_token(identity=user, expires_delta=False, additional_claims=additional_claims)
+        expires = current_app.config["JWT_ACCESS_TOKEN_EXPIRES"]
+        access_token = create_access_token(identity=user, expires_delta=False, additional_claims=additional_claims, fresh=expires)
         refresh_token = create_refresh_token(identity=user, expires_delta=False, additional_claims=additional_claims)
     except Exception as e:
         current_app.logger.exception(e)
@@ -170,5 +170,6 @@ def _get_token_and_user_data_from_oauth_provider(provider: str, auth_code: str):
 @jwt_required(refresh=True)
 def refresh() -> Response:
     identity = get_jwt_identity()
-    access_token = create_access_token(identity=identity)
+    expires = current_app.config["JWT_ACCESS_TOKEN_EXPIRES"]
+    access_token = create_access_token(identity=identity, fresh=expires)
     return make_response_with_headers({"access_token": access_token})
