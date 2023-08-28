24b34b52bed21fce09a7e4e8010b898c08463587
fpigeonjr <fpigeon@flexion.us>
2023-08-28 15:51:59 -0500

    moves query params to component mount

--- frontend/src/pages/agreements/list/AgreementsList.jsx
+++ frontend/src/pages/agreements/list/AgreementsList.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import _ from "lodash";
 import App from "../../../App";
 import { useGetAgreementsQuery } from "../../../api/opsAPI";
@@ -19,6 +19,7 @@ import AgreementsFilterTags from "./AgreementsFilterTags";
  * @returns {React.JSX.Element} - The component JSX.
  */
 export const AgreementsList = () => {
+    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
     const isAlertActive = useSelector((state) => state.alert.isActive);
     const [filters, setFilters] = useState({
@@ -40,6 +41,10 @@ export const AgreementsList = () => {
         error: errorAgreement,
         isLoading: isLoadingAgreement,
     } = useGetAgreementsQuery({ refetchOnMountOrArgChange: true });
+
+    useEffect(() => {
+        navigate("/agreements?filter=all-agreements");
+    }, [navigate]);
 
     const activeUser = useSelector((state) => state.auth.activeUser);
     const myAgreementsUrl = searchParams.get("filter") === "my-agreements";
