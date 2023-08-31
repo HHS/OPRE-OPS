ae9a8e222ee6411a9517efb5b92dcd8a6156cba6
John DeAngelis <john.deangelis2@gmail.com>
2023-08-31 10:12:41 -0400

    add e2e test
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/cypress/e2e/budgetLineItemList.cy.js
+++ frontend/cypress/e2e/budgetLineItemList.cy.js
@@ -26,3 +26,24 @@ it("clicking the add budget lines button takes you to the create budget lines pa
     cy.get("a").contains("Add Budget Lines");
     cy.get('a[href="/budget-lines/create"]').should("exist");
 });
+
+it("pagination on the bli table works as expected", () => {
+    // initial page load
+    cy.get("ul").should("have.class", "usa-pagination__list");
+    cy.get("li").should("have.class", "usa-pagination__item").contains("1");
+    cy.get("a").should("have.class", "usa-current").contains("1");
+    cy.get("li").should("have.class", "usa-pagination__item").contains("2");
+    cy.get("li").should("have.class", "usa-pagination__item").contains("Next");
+    cy.get("li").should("not.contain", "Previous");
+    cy.get("tbody").find("tr").should("have.length", 10);
+
+    // go to the second page
+    cy.get("li").should("have.class", "usa-pagination__item").contains("2").click();
+    cy.get("a").should("have.class", "usa-current").contains("2");
+    cy.get("li").should("have.class", "usa-pagination__item").contains("Previous");
+    cy.get("li").should("not.contain", "Next");
+
+    // go back to the first page
+    cy.get("li").should("have.class", "usa-pagination__item").contains("1").click();
+    cy.get("a").should("have.class", "usa-current").contains("1");
+});
