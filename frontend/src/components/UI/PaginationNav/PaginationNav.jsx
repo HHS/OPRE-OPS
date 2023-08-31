8902ab2da6ce7e166709b3e4a07aac158f3d9277
John DeAngelis <john.deangelis2@gmail.com>
2023-08-31 11:19:17 -0400

    lock Previous and Next buttons in place
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/components/UI/PaginationNav/PaginationNav.jsx
+++ frontend/src/components/UI/PaginationNav/PaginationNav.jsx
@@ -1,16 +1,23 @@
 import cx from "clsx";
 import { useEffect, useState } from "react";
-// https://designsystem.digital.gov/components/pagination/#using-the-pagination-component-2
-/*
-General component properties:
-1. The component features a maximum of seven slots.
-2. Each slot can contain a navigation item or an overflow indicator.
-3. The first slot is always the first page of the set.
-4. If there are fewer than seven pages in the set, show only that number of slots.
-5. The component should always show the first page and current page.
-6. Show the next page, previous page, and last page if those pages exist.
-7. Display the same number of slots for each page in the set.
- */
+import styles from "./PaginationNav.module.scss";
+
+/**
+ * The PaginationNav component provides pagination for a table
+ * ref: https://designsystem.digital.gov/components/pagination/
+ * @param {object} props - The component props.
+ * @param {number} [props.currentPage] - The current page number.
+ * @param {string[]} props.tableHeadings - The table headings to display.
+ * @returns {React.JSX.Element} - The rendered component.
+ * General component properties:
+ * 1. The component features a maximum of seven slots.
+ * 2. Each slot can contain a navigation item or an overflow indicator.
+ * 3. The first slot is always the first page of the set.
+ * 4. If there are fewer than seven pages in the set, show only that number of slots.
+ * 5. The component should always show the first page and current page.
+ * 6. Show the next page, previous page, and last page if those pages exist.
+ * 7. Display the same number of slots for each page in the set.
+ **/
 export const PaginationNav = ({ currentPage, setCurrentPage, items = [], itemsPerPage = 10 }) => {
     const [pageNumberArray, setPageNumberArray] = useState([]); // 7 element array with either a page number or overflow indicator (null)
     const totalPages = Math.ceil(items.length / itemsPerPage);
@@ -80,35 +87,40 @@ export const PaginationNav = ({ currentPage, setCurrentPage, items = [], itemsPe
     return (
         <nav aria-label="Pagination" className="usa-pagination">
             <ul className="usa-pagination__list">
-                {currentPage !== 1 && (
-                    <li key="page-item-previous" className="usa-pagination__item usa-pagination__arrow">
-                        <a
-                            className="usa-pagination__link usa-pagination__previous-page"
-                            aria-label="Previous page"
-                            onClick={() => setCurrentPage(currentPage - 1)}
-                        >
-                            <svg className="usa-icon" aria-hidden="true" role="img">
-                                <use xlinkHref="/assets/img/sprite.svg#navigate_before"></use>
-                            </svg>
-                            <span className="usa-pagination__link-text">Previous</span>
-                        </a>
-                    </li>
-                )}
+                <li
+                    key="page-item-previous"
+                    className={cx(
+                        "usa-pagination__item usa-pagination__arrow",
+                        currentPage === 1 && styles.hideElement
+                    )}
+                >
+                    <a
+                        className="usa-pagination__link usa-pagination__previous-page"
+                        aria-label="Previous page"
+                        onClick={() => setCurrentPage(currentPage - 1)}
+                    >
+                        <svg className="usa-icon" aria-hidden="true" role="img">
+                            <use xlinkHref="/assets/img/sprite.svg#navigate_before"></use>
+                        </svg>
+                        <span className="usa-pagination__link-text">Previous</span>
+                    </a>
+                </li>
                 {pageItems}
-                {currentPage !== totalPages && (
-                    <li key="page-item-next" className="usa-pagination__item usa-pagination__arrow">
-                        <a
-                            className="usa-pagination__link usa-pagination__next-page"
-                            aria-label="Next page"
-                            onClick={() => currentPage < totalPages && setCurrentPage(currentPage + 1)}
-                        >
-                            <span className="usa-pagination__link-text">Next </span>
-                            <svg className="usa-icon" aria-hidden="true" role="img">
-                                <use xlinkHref="/assets/img/sprite.svg#navigate_next"></use>
-                            </svg>
-                        </a>
-                    </li>
-                )}
+                <li key="page-item-next" className="usa-pagination__item usa-pagination__arrow">
+                    <a
+                        className={cx(
+                            "usa-pagination__link usa-pagination__next-page",
+                            currentPage === totalPages && styles.hideElement
+                        )}
+                        aria-label="Next page"
+                        onClick={() => currentPage < totalPages && setCurrentPage(currentPage + 1)}
+                    >
+                        <span className="usa-pagination__link-text">Next </span>
+                        <svg className="usa-icon" aria-hidden="true" role="img">
+                            <use xlinkHref="/assets/img/sprite.svg#navigate_next"></use>
+                        </svg>
+                    </a>
+                </li>
             </ul>
         </nav>
     );
