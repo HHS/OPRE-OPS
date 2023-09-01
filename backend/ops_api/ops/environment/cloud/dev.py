954c47b11f3efb4302ddfc421be10fa09161875f
Tim Donaworth <56687505+tdonaworth@users.noreply.github.com>
2023-09-01 11:05:02 -0400

    Update dev.py
    cleanup

--- backend/ops_api/ops/environment/cloud/dev.py
+++ backend/ops_api/ops/environment/cloud/dev.py
@@ -5,26 +5,6 @@ from ops_api.ops.environment.default_settings import *  # noqa: F403, F401
 
 DEBUG = True
 
-AUTHLIB_OAUTH_CLIENTS = {
-    "logingov": {
-        "server_metadata_url": "https://idp.int.identitysandbox.gov/.well-known/openid-configuration",
-        "user_info_url": "https://idp.int.identitysandbox.gov/api/openid_connect/userinfo",
-        "client_id": "urn:gov:gsa:openidconnect.profiles:sp:sso:hhs_acf:opre_ops",
-        "client_kwargs": {"scope": "openid email"},
-        "redirect_uri": "https://ops-dev.fr.cloud.gov/login",
-    },
-    "hhsams": {
-        "server_metadata_url": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/.well-known/openid-configuration",
-        "token_endpoint": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/protocol/openid-connect/token",
-        "user_info_url": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/protocol/openid-connect/userinfo",
-        "client_id": "44fe2c7a-e9c5-43ec-87e9-3de78d2d3a11",
-        "client_kwargs": {"scope": "openid profile email"},
-        "aud": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/protocol/openid-connect/token",
-        "redirect_uri": "https://ops-dev.fr.cloud.gov/login",
-    },
-}
-
-
 # Helper function
 def get_json_env_var(variable_name):
     """Retrieve and serialize a JSON environment variable."""
