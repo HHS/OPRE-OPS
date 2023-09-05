1735d125e8b6700c4378a719e73d157135f7ebe3
John DeAngelis <john.deangelis2@gmail.com>
2023-09-05 14:03:36 -0400

    enable Reset button
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/pages/budgetLines/list/BLIFilterButton.jsx
+++ frontend/src/pages/budgetLines/list/BLIFilterButton.jsx
@@ -79,6 +79,7 @@ export const BLIFilterButton = ({ setFilters }) => {
         setFilters({
             fiscalYears: [],
         });
+        setFiscalYears([]);
     };
 
     // if (isLoadingProjectData) {
