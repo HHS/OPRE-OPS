9dac422f8fe851191c8ae630f4fe517faa86fddd
John DeAngelis <john.deangelis2@gmail.com>
2023-09-05 15:33:54 -0400

    add portfolios to filter state;add ability to reset portfolio filter
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/pages/budgetLines/list/BLIFilterButton.jsx
+++ frontend/src/pages/budgetLines/list/BLIFilterButton.jsx
@@ -16,23 +16,6 @@ export const BLIFilterButton = ({ setFilters }) => {
     // const currentFY = new Date(getCurrentFiscalYear()).getFullYear();
     const [fiscalYears, setFiscalYears] = React.useState([]);
     const [portfolios, setPortfolios] = React.useState([]);
-    // const [needBy, setNeedBy] = React.useState("all-time");
-    // const [project, setProject] = React.useState({});
-    // const [po, setPO] = React.useState({});
-    // const [agreementType, setAgreementType] = React.useState("");
-    // const [procurementShop, setProcurementShop] = React.useState({});
-    // const [bliStatus, setBliStatus] = React.useState({
-    //     draft: true,
-    //     planned: true,
-    //     executing: true,
-    //     obligated: true,
-    // });
-
-    // const {
-    //     data: projectData,
-    //     error: errorProjectData,
-    //     isLoading: isLoadingProjectData,
-    // } = useGetResearchProjectsQuery();
 
     // The useEffect() hook calls below are used to set the state appropriately when the filter tags (X) are clicked.
     // useEffect(() => {
@@ -52,35 +35,6 @@ export const BLIFilterButton = ({ setFilters }) => {
                 portfolios: portfolios,
             };
         });
-        // setFilters((prevState) => {
-        //     return {
-        //         ...prevState,
-        //         upcomingNeedByDate: needBy,
-        //     };
-        // });
-        // setFilters((prevState) => {
-        //     return setFilterList(prevState, "projects", project, true);
-        // });
-        // setFilters((prevState) => {
-        //     return setFilterList(prevState, "projectOfficers", po, true);
-        // });
-        // setFilters((prevState) => {
-        //     return setFilterList(prevState, "types", agreementType);
-        // });
-        // setFilters((prevState) => {
-        //     return setFilterList(prevState, "procurementShops", procurementShop);
-        // });
-        // setFilters((prevState) => {
-        //     return {
-        //         ...prevState,
-        //         budgetLineStatus: {
-        //             draft: bliStatus.draft,
-        //             planned: bliStatus.planned,
-        //             executing: bliStatus.executing,
-        //             obligated: bliStatus.obligated,
-        //         },
-        //     };
-        // });
     };
 
     const resetFilter = () => {
@@ -91,13 +45,6 @@ export const BLIFilterButton = ({ setFilters }) => {
         setFiscalYears([]);
         setPortfolios([]);
     };
-
-    // if (isLoadingProjectData) {
-    //     return <div>Loading...</div>;
-    // }
-    // if (errorProjectData) {
-    //     return <div>Oops, an error occurred</div>;
-    // }
 
     const fieldStyles = "usa-fieldset margin-bottom-205";
     const legendStyles = `usa-legend font-sans-3xs margin-top-0 padding-bottom-1 ${customStyles.legendColor}`;
