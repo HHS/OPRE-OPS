5d7cecd3e38c42c0893335875af9c19f665ee3b3
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-09-06 13:20:23 -0600

    add labels for ContractAgreement subclass props

--- frontend/src/helpers/utils.js
+++ frontend/src/helpers/utils.js
@@ -110,6 +110,11 @@ const codesToDisplayText = {
         project_officer: "Project Officer",
         research_project: "Research Project",
         team_members: "Team Members",
+        contract_number: "Contract Number",
+        vendor: "Vendor",
+        delivered_status: "Delivered Status",
+        contract_type: "Contract Type",
+        support_contacts: "Support Contacts",
     },
     budgetLineItemPropertyLabels: {
         amount: "Amount",
