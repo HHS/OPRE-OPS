b9b105ad23ccbb7e0bf6b6f8575e5e9a8f7b1135
John DeAngelis <john.deangelis2@gmail.com>
2023-09-07 10:18:24 -0400

    fix tests for FiscalYearComboBox.test.js
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/components/UI/Form/FiscalYearComboBox/FiscalYearComboBox.test.js
+++ frontend/src/components/UI/Form/FiscalYearComboBox/FiscalYearComboBox.test.js
@@ -1,48 +1,39 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import FiscalYearComboBox from "./FiscalYearComboBox";
 
-describe("ProjectReactSelect", () => {
-    const researchProjects = [
-        { id: 1, title: "Project 1", description: "Description 1" },
-        { id: 2, title: "Project 2", description: "Description 2" },
-        { id: 3, title: "Project 3", description: "Description 3" },
-    ];
-    const mockSetSelectedProject = jest.fn();
-    const mockClearFunction = jest.fn();
+describe("FiscalYearComboBox", () => {
+    const mockSetSelectedFiscalYears = jest.fn();
 
     it("renders the component with the correct label", () => {
-        render(
-            <FiscalYearComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={mockSetSelectedProject}
-                clearFunction={mockClearFunction}
-            />
-        );
+        render(<FiscalYearComboBox selectedFiscalYears={null} setSelectedFiscalYears={mockSetSelectedFiscalYears} />);
         expect(screen.getByRole("combobox")).toBeInTheDocument();
+        expect(screen.getByText("Fiscal Year")).toBeInTheDocument();
     });
 
     it("renders the component with the correct options", () => {
-        render(
-            <FiscalYearComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={mockSetSelectedProject}
-            />
+        const { container } = render(
+            <FiscalYearComboBox selectedFiscalYears={null} setSelectedFiscalYears={mockSetSelectedFiscalYears} />
         );
 
-        const select = screen.getByText("Project 1");
-        expect(select).toBeInTheDocument();
+        // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
+        fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
+
+        expect(screen.getByText("2020")).toBeInTheDocument();
+        expect(screen.getByText("2021")).toBeInTheDocument();
+        expect(screen.getByText("2022")).toBeInTheDocument();
     });
 
-    it("updates the selected project when an option is selected", () => {
+    it("updates the input value when the user types in the input field", () => {
+        render(<FiscalYearComboBox selectedFiscalYears={null} setSelectedFiscalYears={mockSetSelectedFiscalYears} />);
+        const input = screen.getByRole("combobox");
+        fireEvent.change(input, { target: { value: "2020" } });
+        expect(input).toHaveValue("2020");
+    });
+
+    it("updates the selected item when multiple options are selected", () => {
         const setSelectedProject = jest.fn();
         const { getByText, container } = render(
-            <FiscalYearComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={setSelectedProject}
-            />
+            <FiscalYearComboBox selectedFiscalYears={null} setSelectedFiscalYears={setSelectedProject} />
         );
         // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
         fireEvent.focus(container.querySelector("input"));
@@ -50,20 +41,15 @@ describe("ProjectReactSelect", () => {
         fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
 
         // eslint-disable-next-line testing-library/prefer-screen-queries
-        fireEvent.click(getByText("Project 2"));
-        expect(setSelectedProject).toHaveBeenCalledWith(researchProjects[1]);
-    });
+        fireEvent.click(getByText("2020"));
+        // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
+        fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
 
-    it("updates the input value when the user types in the input field", () => {
-        render(
-            <FiscalYearComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={mockSetSelectedProject}
-            />
-        );
-        const input = screen.getByRole("combobox");
-        fireEvent.change(input, { target: { value: "Project 2" } });
-        expect(input).toHaveValue("Project 2");
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        fireEvent.click(getByText("2021"));
+        expect(setSelectedProject).toHaveBeenCalledWith([
+            { id: 2020, title: 2020 },
+            { id: 2021, title: 2021 },
+        ]);
     });
 });
