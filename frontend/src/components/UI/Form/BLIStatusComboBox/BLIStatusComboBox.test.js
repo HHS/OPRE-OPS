42ba0cd79cc9416781fdd49b4abc0f02154e3f51
John DeAngelis <john.deangelis2@gmail.com>
2023-09-07 10:25:33 -0400

    fix tests for BLIStatusComboBox.test.js
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/components/UI/Form/BLIStatusComboBox/BLIStatusComboBox.test.js
+++ frontend/src/components/UI/Form/BLIStatusComboBox/BLIStatusComboBox.test.js
@@ -1,48 +1,39 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import BLIStatusComboBox from "./BLIStatusComboBox";
 
-describe("ProjectReactSelect", () => {
-    const researchProjects = [
-        { id: 1, title: "Project 1", description: "Description 1" },
-        { id: 2, title: "Project 2", description: "Description 2" },
-        { id: 3, title: "Project 3", description: "Description 3" },
-    ];
-    const mockSetSelectedProject = jest.fn();
-    const mockClearFunction = jest.fn();
+describe("BLIStatusComboBox", () => {
+    const mockSetSelectedBLIStatus = jest.fn();
 
     it("renders the component with the correct label", () => {
-        render(
-            <BLIStatusComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={mockSetSelectedProject}
-                clearFunction={mockClearFunction}
-            />
-        );
+        render(<BLIStatusComboBox selectedBLIStatus={null} setSelectedBLIStatus={mockSetSelectedBLIStatus} />);
         expect(screen.getByRole("combobox")).toBeInTheDocument();
+        expect(screen.getByText("Budget Lines Status")).toBeInTheDocument();
     });
 
     it("renders the component with the correct options", () => {
-        render(
-            <BLIStatusComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={mockSetSelectedProject}
-            />
+        const { container } = render(
+            <BLIStatusComboBox selectedBLIStatus={null} setSelectedBLIStatus={mockSetSelectedBLIStatus} />
         );
 
-        const select = screen.getByText("Project 1");
-        expect(select).toBeInTheDocument();
+        // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
+        fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
+
+        expect(screen.getByText("Draft")).toBeInTheDocument();
+        expect(screen.getByText("In Review")).toBeInTheDocument();
+        expect(screen.getByText("Planned")).toBeInTheDocument();
     });
 
-    it("updates the selected project when an option is selected", () => {
-        const setSelectedProject = jest.fn();
+    it("updates the input value when the user types in the input field", () => {
+        render(<BLIStatusComboBox selectedBLIStatus={null} setSelectedBLIStatus={mockSetSelectedBLIStatus} />);
+        const input = screen.getByRole("combobox");
+        fireEvent.change(input, { target: { value: "Draft" } });
+        expect(input).toHaveValue("Draft");
+    });
+
+    it("updates the selected item when multiple options are selected", () => {
+        const setSelectedBLIStatus = jest.fn();
         const { getByText, container } = render(
-            <BLIStatusComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={setSelectedProject}
-            />
+            <BLIStatusComboBox selectedBLIStatus={null} setSelectedBLIStatus={setSelectedBLIStatus} />
         );
         // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
         fireEvent.focus(container.querySelector("input"));
@@ -50,20 +41,23 @@ describe("ProjectReactSelect", () => {
         fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
 
         // eslint-disable-next-line testing-library/prefer-screen-queries
-        fireEvent.click(getByText("Project 2"));
-        expect(setSelectedProject).toHaveBeenCalledWith(researchProjects[1]);
-    });
+        fireEvent.click(getByText("Draft"));
+        // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
+        fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
 
-    it("updates the input value when the user types in the input field", () => {
-        render(
-            <BLIStatusComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={mockSetSelectedProject}
-            />
-        );
-        const input = screen.getByRole("combobox");
-        fireEvent.change(input, { target: { value: "Project 2" } });
-        expect(input).toHaveValue("Project 2");
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        fireEvent.click(getByText("Planned"));
+        expect(setSelectedBLIStatus).toHaveBeenCalledWith([
+            {
+                id: 1,
+                title: "Draft",
+                status: "DRAFT",
+            },
+            {
+                id: 3,
+                title: "Planned",
+                status: "PLANNED",
+            },
+        ]);
     });
 });
