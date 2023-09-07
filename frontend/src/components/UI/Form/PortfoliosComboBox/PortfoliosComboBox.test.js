2a5054afebe5c84a5ae56601229b1b8b0e41fa01
John DeAngelis <john.deangelis2@gmail.com>
2023-09-07 14:31:00 -0400

    fix PortfoliosComboBox.test.js
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/components/UI/Form/PortfoliosComboBox/PortfoliosComboBox.test.js
+++ frontend/src/components/UI/Form/PortfoliosComboBox/PortfoliosComboBox.test.js
@@ -1,48 +1,52 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import PortfoliosComboBox from "./PortfoliosComboBox";
+import { useGetPortfoliosQuery } from "../../../../api/opsAPI";
 
-describe("ProjectReactSelect", () => {
-    const researchProjects = [
-        { id: 1, title: "Project 1", description: "Description 1" },
-        { id: 2, title: "Project 2", description: "Description 2" },
-        { id: 3, title: "Project 3", description: "Description 3" },
-    ];
-    const mockSetSelectedProject = jest.fn();
-    const mockClearFunction = jest.fn();
+jest.mock("../../../../api/opsAPI");
+
+const samplePortfolios = [
+    { id: 1, name: "Portfolio1" },
+    { id: 2, name: "Portfolio2" },
+    { id: 3, name: "Portfolio3" },
+];
+
+describe("PortfoliosComboBox", () => {
+    const mockSetSelectedPortfolios = jest.fn();
 
     it("renders the component with the correct label", () => {
-        render(
-            <PortfoliosComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={mockSetSelectedProject}
-                clearFunction={mockClearFunction}
-            />
-        );
+        useGetPortfoliosQuery.mockReturnValue({ data: samplePortfolios });
+        render(<PortfoliosComboBox selectedPortfolios={null} setSelectedPortfolios={mockSetSelectedPortfolios} />);
         expect(screen.getByRole("combobox")).toBeInTheDocument();
+        expect(screen.getByText("Portfolio")).toBeInTheDocument();
     });
 
     it("renders the component with the correct options", () => {
-        render(
-            <PortfoliosComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={mockSetSelectedProject}
-            />
+        useGetPortfoliosQuery.mockReturnValue({ data: samplePortfolios });
+        const { container } = render(
+            <PortfoliosComboBox selectedPortfolios={null} setSelectedPortfolios={mockSetSelectedPortfolios} />
         );
 
-        const select = screen.getByText("Project 1");
-        expect(select).toBeInTheDocument();
+        // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
+        fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
+
+        expect(screen.getByText("Portfolio1")).toBeInTheDocument();
+        expect(screen.getByText("Portfolio2")).toBeInTheDocument();
+        expect(screen.getByText("Portfolio3")).toBeInTheDocument();
     });
 
-    it("updates the selected project when an option is selected", () => {
-        const setSelectedProject = jest.fn();
+    it("updates the input value when the user types in the input field", () => {
+        useGetPortfoliosQuery.mockReturnValue({ data: samplePortfolios });
+        render(<PortfoliosComboBox selectedPortfolios={null} setSelectedPortfolios={mockSetSelectedPortfolios} />);
+        const input = screen.getByRole("combobox");
+        fireEvent.change(input, { target: { value: "Portfolio1" } });
+        expect(input).toHaveValue("Portfolio1");
+    });
+
+    it("updates the selected item when multiple options are selected", () => {
+        const setSelectedPortfolios = jest.fn();
+        useGetPortfoliosQuery.mockReturnValue({ data: samplePortfolios });
         const { getByText, container } = render(
-            <PortfoliosComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={setSelectedProject}
-            />
+            <PortfoliosComboBox selectedPortfolios={null} setSelectedPortfolios={setSelectedPortfolios} />
         );
         // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
         fireEvent.focus(container.querySelector("input"));
@@ -50,20 +54,15 @@ describe("ProjectReactSelect", () => {
         fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
 
         // eslint-disable-next-line testing-library/prefer-screen-queries
-        fireEvent.click(getByText("Project 2"));
-        expect(setSelectedProject).toHaveBeenCalledWith(researchProjects[1]);
-    });
+        fireEvent.click(getByText("Portfolio1"));
+        // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
+        fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
 
-    it("updates the input value when the user types in the input field", () => {
-        render(
-            <PortfoliosComboBox
-                researchProjects={researchProjects}
-                selectedResearchProject={researchProjects[0]}
-                setSelectedProject={mockSetSelectedProject}
-            />
-        );
-        const input = screen.getByRole("combobox");
-        fireEvent.change(input, { target: { value: "Project 2" } });
-        expect(input).toHaveValue("Project 2");
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        fireEvent.click(getByText("Portfolio2"));
+        expect(setSelectedPortfolios).toHaveBeenCalledWith([
+            { id: 1, name: "Portfolio1" },
+            { id: 2, name: "Portfolio2" },
+        ]);
     });
 });
