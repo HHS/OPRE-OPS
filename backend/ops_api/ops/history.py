12aaedb63d11d925ef3397fb6f444be4dfd49f28
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-09-07 06:50:11 -0600

    fix: exclude Enums from changes when they didn't really change (because SQLAlchemy is showing a diff between the new string value and it's equivalent Enum instance)

--- backend/ops_api/ops/history.py
+++ backend/ops_api/ops/history.py
@@ -67,6 +67,9 @@ def build_audit(obj, event_type: OpsDBHistoryType) -> DbRecordAudit:  # noqa: C9
             new_val = convert_for_jsonb(hist.added[0]) if hist.added else None
             if old_val:
                 original[key] = old_val
+            # exclude Enums that didn't really change
+            if old_val == new_val and isinstance(hist.deleted[0], Enum):
+                continue
             diff[key] = new_val
             if event_type == OpsDBHistoryType.NEW:
                 if new_val:
