9c5b1e563c8b796258ff4f2db4e8abd0e334b333
fpigeonjr <fpigeon@flexion.us>
2023-09-12 16:54:05 -0500

    feat: :sparkles: add Summary Card Section

--- frontend/src/components/BudgetLineItems/SummaryCardsSection/SummaryCardsSection.js
+++ frontend/src/components/BudgetLineItems/SummaryCardsSection/SummaryCardsSection.js
@@ -1,5 +1,20 @@
+import RoundedBox from "../../UI/RoundedBox";
+
+const BudgetLineTotalSummaryCard = (title) => {
+    return (
+        <RoundedBox className="padding-y-205 padding-x-4 padding-right-9 ">
+            <h3 className="margin-0 margin-bottom-3 font-12px text-base-dark text-normal">{title}</h3>
+        </RoundedBox>
+    );
+};
+
 const SummaryCardsSection = () => {
-    return <p>SummaryCardsSection</p>;
+    return (
+        <div className="display-flex flex-justify">
+            <BudgetLineTotalSummaryCard title="Budget Lines Total" />
+            <BudgetLineTotalSummaryCard />
+        </div>
+    );
 };
 
 export default SummaryCardsSection;
