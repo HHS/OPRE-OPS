99885fd7c976d2c06db9da512f095685184dd4b0
fpigeonjr <fpigeon@flexion.us>
2023-09-13 16:55:29 -0500

    feat: :sparkles: adds new component SummaryCard
    based on RoundedBox but adds padding and optional header

---
+++ frontend/src/components/UI/SummaryCard/SummaryCard.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import RoundedBox from "../RoundedBox";
+
+/**
+ * SummaryCard component
+ * @param {Object} props - Properties passed to component
+ * @param {string} [props.title] - The title of the card
+ * @param {React.ReactNode} props.children - The children to render
+ * @returns {React.JSX.Element} - The rendered component
+ */
+const SummaryCard = ({ title, children }) => {
+    return (
+        <RoundedBox className="padding-y-205 padding-x-4 padding-right-9 display-inline-block">
+            {title && <h3 className="margin-0 margin-bottom-3 font-12px text-base-dark text-normal">{title}</h3>}
+            {children}
+        </RoundedBox>
+    );
+};
+SummaryCard.propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.node.isRequired,
+};
+
+export default SummaryCard;
