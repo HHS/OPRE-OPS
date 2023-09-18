2e7241700a97b89434143b80ff45fa0098cafe78
fpigeonjr <fpigeon@flexion.us>
2023-09-18 18:17:26 -0500

    refactor: :art: moves BreadCrumb to DefaultLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import PropTypes from "prop-types";
 import DefaultLayout from "./components/UI/Layouts/DefaultLayout";
 
-function App({ children }) {
-    return <DefaultLayout>{children}</DefaultLayout>;
-}
+/**
+ * DefaultLayout component
+ * @param {Object} props - Properties passed to component
+ * @param {string} [props.breadCrumbName] - The name of the current page to be displayed in the breadcrumb
+ * @param {React.ReactNode} props.children - The child nodes to be rendered within the layout
+ */
+const App = ({ children, breadCrumbName }) => <DefaultLayout breadCrumbName={breadCrumbName}>{children}</DefaultLayout>;
 
+App.propTypes = {
+    children: PropTypes.element.isRequired,
+    breadCrumbName: PropTypes.string,
+};
 export default App;
--- frontend/src/components/UI/Layouts/DefaultLayout/DefaultLayout.js
+++ frontend/src/components/UI/Layouts/DefaultLayout/DefaultLayout.js
@@ -3,18 +3,21 @@ import { useSelector } from "react-redux";
 import Header from "../../Header";
 import Footer from "../../Footer";
 import Alert from "../../Alert";
+import Breadcrumb from "../../Header/Breadcrumb";
 
 /**
  * DefaultLayout component
  * @param {Object} props - Properties passed to component
+ * @param {string} [props.breadCrumbName] - The name of the current page to be displayed in the breadcrumb
  * @param {React.ReactNode} props.children - The child nodes to be rendered within the layout
  */
-const DefaultLayout = ({ children }) => {
+const DefaultLayout = ({ children, breadCrumbName }) => {
     const isAlertActive = useSelector((state) => state.alert.isActive);
     return (
         <div className="bg-base-lightest">
             <div className="usa-overlay"></div>
             <Header />
+            {breadCrumbName ? <Breadcrumb currentName={breadCrumbName} /> : null}
             {isAlertActive && <Alert />}
             <main id="main-content" className="grid-container bg-white padding-bottom-6">
                 {children}
@@ -26,5 +29,6 @@ const DefaultLayout = ({ children }) => {
 
 DefaultLayout.propTypes = {
     children: PropTypes.node,
+    breadCrumbName: PropTypes.string,
 };
 export default DefaultLayout;
