ff75d00f1cd0c674f4e7a356c480c68e7d0c1bbc
Tim Donaworth <tdonaworth@flexion.us>
2023-09-19 16:38:27 -0400

    cleanup

--- frontend/src/components/Auth/auth.js
+++ frontend/src/components/Auth/auth.js
@@ -92,7 +92,7 @@ export const CheckAuth = () => {
  * @returns {Promise<void>} A Promise that resolves when the user details are set in the store.
  *
  * @example
- * const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+ * const token = "<token>";
  * const dispatch = useDispatch();
  * setActiveUser(token, dispatch);
  */
