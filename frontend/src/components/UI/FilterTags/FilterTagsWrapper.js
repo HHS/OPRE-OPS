28f30864077fb000cd3cdd41329984ba243f2e68
fpigeonjr <fpigeon@flexion.us>
2023-10-04 11:49:31 -0500

    corrects font

--- frontend/src/components/UI/FilterTags/FilterTagsWrapper.js
+++ frontend/src/components/UI/FilterTags/FilterTagsWrapper.js
@@ -8,7 +8,7 @@
 const FilterTagsWrapper = ({ children }) => {
     return (
         <div className="display-flex flex-align-center flex-wrap padding-bottom-205">
-            <span className="padding-right-205 text-base-dark font-serif-3xs line-height-sans-5 padding-top-05">
+            <span className="padding-right-205 text-base-dark font-sans-3xs line-height-sans-5 padding-top-05">
                 Filters Applied:
             </span>
             {children}
