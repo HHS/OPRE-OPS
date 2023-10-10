e41bc73f0083c72df55e7aabadbbca08c05610fc
John DeAngelis <john.deangelis2@gmail.com>
2023-10-10 14:31:25 -0400

    use Schema inheritance
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/ops_api/ops/schemas/agreements.py
+++ backend/ops_api/ops/schemas/agreements.py
@@ -19,18 +19,7 @@ class AgreementData(Schema):
     notes = fields.String(allow_none=True)
 
 
-class ContractAgreementData(Schema):
-    name = fields.String(required=True)
-    agreement_type = fields.Enum(AgreementType, required=True)
-    description = fields.String(allow_none=True)
-    product_service_code_id = fields.Integer(allow_none=True)
-    agreement_reason = fields.Enum(AgreementReason, allow_none=True)
-    incumbent = fields.String(allow_none=True)
-    project_officer = fields.Integer(allow_none=True)
-    team_members = fields.List(fields.Nested(TeamMembers), default=[], allow_none=True)
-    research_project_id = fields.Integer(allow_none=True)
-    procurement_shop_id = fields.Integer(allow_none=True)
-    notes = fields.String(allow_none=True)
+class ContractAgreementData(AgreementData):
     contract_number = fields.String(allow_none=True)
     vendor = fields.String(allow_none=True)
     delivered_status = fields.Bool(default=False)
@@ -38,61 +27,20 @@ class ContractAgreementData(Schema):
     support_contacts = fields.List(fields.Nested(TeamMembers), default=[], allow_none=True)
 
 
-class GrantAgreementData(Schema):
-    name = fields.String(required=True)
-    agreement_type = fields.Enum(AgreementType, required=True)
-    description = fields.String(allow_none=True)
-    product_service_code_id = fields.Integer(allow_none=True)
-    agreement_reason = fields.Enum(AgreementReason, allow_none=True)
-    incumbent = fields.String(allow_none=True)
-    project_officer = fields.Integer(allow_none=True)
-    team_members = fields.List(fields.Nested(TeamMembers), default=[], allow_none=True)
-    research_project_id = fields.Integer(allow_none=True)
-    procurement_shop_id = fields.Integer(allow_none=True)
-    notes = fields.String(allow_none=True)
+class GrantAgreementData(AgreementData):
     foa = fields.String(allow_none=True)
 
 
-class DirectAgreementData(Schema):
-    name = fields.String(required=True)
-    agreement_type = fields.Enum(AgreementType, required=True)
-    description = fields.String(allow_none=True)
-    product_service_code_id = fields.Integer(allow_none=True)
-    agreement_reason = fields.Enum(AgreementReason, allow_none=True)
-    incumbent = fields.String(allow_none=True)
-    project_officer = fields.Integer(allow_none=True)
-    team_members = fields.List(fields.Nested(TeamMembers), default=[], allow_none=True)
-    research_project_id = fields.Integer(allow_none=True)
-    procurement_shop_id = fields.Integer(allow_none=True)
-    notes = fields.String(allow_none=True)
+class DirectAgreementData(AgreementData):
+    pass
 
 
-class IaaAgreementData(Schema):
-    name = fields.String(required=True)
-    agreement_type = fields.Enum(AgreementType, required=True)
-    description = fields.String(allow_none=True)
-    product_service_code_id = fields.Integer(allow_none=True)
-    agreement_reason = fields.Enum(AgreementReason, allow_none=True)
-    incumbent = fields.String(allow_none=True)
-    project_officer = fields.Integer(allow_none=True)
-    team_members = fields.List(fields.Nested(TeamMembers), default=[], allow_none=True)
-    research_project_id = fields.Integer(allow_none=True)
-    procurement_shop_id = fields.Integer(allow_none=True)
-    notes = fields.String(allow_none=True)
+class IaaAgreementData(AgreementData):
+    pass
 
 
-class IaaAaAgreementData(Schema):
-    name = fields.String(required=True)
-    agreement_type = fields.Enum(AgreementType, required=True)
-    description = fields.String(allow_none=True)
-    product_service_code_id = fields.Integer(allow_none=True)
-    agreement_reason = fields.Enum(AgreementReason, allow_none=True)
-    incumbent = fields.String(allow_none=True)
-    project_officer = fields.Integer(allow_none=True)
-    team_members = fields.List(fields.Nested(TeamMembers), default=[], allow_none=True)
-    research_project_id = fields.Integer(allow_none=True)
-    procurement_shop_id = fields.Integer(allow_none=True)
-    notes = fields.String(allow_none=True)
+class IaaAaAgreementData(AgreementData):
+    pass
 
 
 # TODO: This can go away once we can serialize the Agreement with marshmallow
