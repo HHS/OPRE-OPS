349fdf91248d55e9ea96ae8255730a4c73ec4cec
Frank Pigeon Jr <fpigeon@flexion.us>
2023-10-10 09:44:13 -0500

    feat: ✨ adds AgreementMetaAccordion component (#1549)
    * feat: :sparkles: adds Accordion component
    
    optional heading level. defaults to h4. keyboard accessible.
    
    1546
    
    * test: :test_tube: adds unit test
    
    * cleanup
    
    * linter fixes
    
    * feat: :sparkles: adds AgreementMetaAccordion component
    
    * refactor: :recycle: makes Term as its own component
    
    * feat: :sparkles: adds two-col grid
    
    overrides padding on uswds accordion
    
    * fix: :bug: fixes bug by removing unneccessary re-render
    
    this was causing the vest validation to fail
    
    * fix: :bug: fixes a11y issues with definition list
    
    * test: :test_tube: adds component tests for accordion
    
    * fix: :bug: fixes React unique key warning

--- frontend/src/components/UI/Term/Term.jsx
+++ frontend/src/components/UI/Term/Term.jsx
@@ -1,4 +1,5 @@
 import cx from "clsx";
+import PropType from "prop-types";
 
 /**
  * A form input component.
@@ -8,11 +9,11 @@ import cx from "clsx";
  * @param {string} [props.label] - The label to display for the input field (optional).
  * @param {boolean} [props.pending] - A flag to indicate if the input is pending (optional).
  * @param {Array<String>} [props.messages] - An array of error messages to display (optional).
- * @param {string} [props.value] - The value of the input field.(optional)
+ * @param {string|number} [props.value] - The value of the input field (optional).
  * @param {string} [props.className] - Additional CSS classes to apply to the component (optional).
  * @returns {JSX.Element} - The rendered input component.
  */
-const Terms = ({ name, label = name, pending = false, messages = [], value, className }) => {
+const Term = ({ name, label = name, pending = false, messages = [], value, className }) => {
     return (
         <div className={cx("usa-form-group", pending && "pending", className)}>
             <dt className="margin-0 text-base-dark margin-top-3">{label}</dt>
@@ -31,4 +32,12 @@ const Terms = ({ name, label = name, pending = false, messages = [], value, clas
     );
 };
 
-export default Terms;
+Term.propTypes = {
+    name: PropType.string.isRequired,
+    label: PropType.string,
+    pending: PropType.bool,
+    messages: PropType.arrayOf(PropType.string),
+    value: PropType.oneOfType([PropType.string, PropType.number]),
+    className: PropType.string
+};
+export default Term;
--- frontend/src/pages/agreements/review/ReviewAgreement.jsx
+++ frontend/src/pages/agreements/review/ReviewAgreement.jsx
@@ -5,12 +5,12 @@ import PropTypes from "prop-types";
 import BudgetLinesTable from "../../../components/BudgetLineItems/BudgetLinesTable";
 import SimpleAlert from "../../../components/UI/Alert/SimpleAlert";
 import { useGetAgreementByIdQuery, useUpdateBudgetLineItemStatusMutation } from "../../../api/opsAPI";
+import AgreementMetaAccordion from "../../../components/Agreements/AgreementMetaAccordion";
 import { convertCodeForDisplay } from "../../../helpers/utils";
-import Terms from "./Terms";
 import suite from "./suite";
-import useGetUserFullNameFromId from "../../../hooks/user.hooks";
 import { useIsAgreementEditable, useIsUserAllowedToEditAgreement } from "../../../hooks/agreement.hooks";
 import useAlert from "../../../hooks/use-alert.hooks";
+import useGetUserFullNameFromId from "../../../hooks/user.hooks";
 
 /**
  * Renders a page for reviewing and sending an agreement to approval.
@@ -152,110 +152,13 @@ export const ReviewAgreement = ({ agreement_id }) => {
                     Review and Send Agreement to Approval
                 </h1>
             )}
-            <p>
-                Please review the agreement below and edit any information if necessary. Send to Approval will send the
-                agreement to your Division Director to review for Planned Status.
-            </p>
-
-            <dl className="margin-0 font-12px">
-                <Terms
-                    name="name"
-                    label="Project"
-                    messages={res.getErrors("name")}
-                    className={cn("name")}
-                    value={agreement?.name}
-                />
-                <Terms
-                    name="type"
-                    label="Agreement Type"
-                    messages={res.getErrors("type")}
-                    className={cn("type")}
-                    value={convertCodeForDisplay("agreementType", agreement?.agreement_type)}
-                />
-                <Terms
-                    name="description"
-                    label="Description"
-                    messages={res.getErrors("description")}
-                    className={cn("description")}
-                    value={agreement?.description}
-                />
-                <Terms
-                    name="psc"
-                    label="Product Service Code"
-                    messages={res.getErrors("psc")}
-                    className={cn("psc")}
-                    value={agreement?.product_service_code?.name}
-                />
-                <Terms
-                    name="naics"
-                    label="NAICS Code"
-                    messages={res.getErrors("naics")}
-                    className={cn("naics")}
-                    value={agreement?.product_service_code?.naics}
-                />
-                <Terms
-                    name="program-support-code"
-                    label="Program Support Code"
-                    messages={res.getErrors("program-support-code")}
-                    className={cn("program-support-code")}
-                    value={agreement?.product_service_code?.support_code}
-                />
-                <Terms
-                    name="procurement-shop"
-                    label="Procurement Shop"
-                    messages={res.getErrors("procurement-shop")}
-                    className={cn("procurement-shop")}
-                    value={`${agreement?.procurement_shop?.abbr} - Fee Rate: ${
-                        agreement?.procurement_shop?.fee * 100
-                    }%`}
-                />
-                <Terms
-                    name="reason"
-                    label="Reason for creating the agreement"
-                    messages={res.getErrors("reason")}
-                    className={cn("reason")}
-                    value={convertCodeForDisplay("agreementReason", agreement?.agreement_reason)}
-                />
-
-                {agreement?.incumbent && (
-                    <Terms
-                        name="incumbent"
-                        label="Incumbent"
-                        messages={res.getErrors("incumbent")}
-                        className={cn("incumbent")}
-                        value={agreement?.incumbent}
-                    />
-                )}
-                <Terms
-                    name="project-officer"
-                    label="Project Officer"
-                    messages={res.getErrors("project-officer")}
-                    className={cn("project-officer")}
-                    value={projectOfficerName}
-                />
-
-                {agreement?.team_members.length > 0 ? (
-                    <>
-                        <dt className="margin-0 text-base-dark margin-top-3">Team Members</dt>
-                        {agreement?.team_members.map((member) => (
-                            <dd
-                                key={member.id}
-                                className="text-semibold margin-0 margin-top-05"
-                            >
-                                {member.full_name}
-                            </dd>
-                        ))}
-                    </>
-                ) : (
-                    <Terms
-                        name="team-member"
-                        label="Team Members"
-                        messages={res.getErrors("team-member")}
-                        className={cn("team-member")}
-                        value={agreement?.team_members[0]}
-                    />
-                )}
-            </dl>
+            <AgreementMetaAccordion
+                agreement={agreement}
+                projectOfficerName={projectOfficerName}
+                res={res}
+                cn={cn}
+                convertCodeForDisplay={convertCodeForDisplay}
+            />
             <div className={`font-12px usa-form-group ${areThereBudgetLineErrors ? "usa-form-group--error" : null}`}>
                 <h2
                     className="text-bold"
