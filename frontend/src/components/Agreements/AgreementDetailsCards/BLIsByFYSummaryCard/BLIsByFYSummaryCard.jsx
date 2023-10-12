87f186f4d5b29ccb1fe294b84bc261e93f94c8c6
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-10-12 12:59:53 -0600

    OPS-1538 Update Summary Cards on Agreement Details BL Tab (#1550)
    * WIP: begin new cards for BL details (OPS-1528)
    
    * AgreementTotalCard with total,subtotal, fees, and proc shop
    BudgetLinesByFiscalYear with totals that include fees (still using Nivo bar chart)
    
    * fix text for no data
    
    * remove TotalSummaryCards from below table
    
    * WIP: replacing Nivo bar chart with HTML/CSS
    
    * safety limit of 5
    and cleanup
    
    * style: :lipstick: better horizontal spacing and currency format
    
    * refactor: :recycle: moves summary cards to own folder
    
    * style: :lipstick: adds derived prop for better vert spacing
    
    * fixes linter errors
    
    * test: :test_tube: fixes tests 🤞
    
    brings back Summary Cards to create-budget-line workflow
    
    * style: :lipstick: better vert spacing
    
    * style: :lipstick: better horizontal spacing
    
    * refactor: :recycle: handles zero fees
    
    * refactor: :recycle: adds flag to show TotalSummaryCard
    
    * cleanup
    
    ---------
    
    Co-authored-by: Frank Pigeon Jr <fpigeon@flexion.us>

---
+++ frontend/src/components/Agreements/AgreementDetailsCards/BLIsByFYSummaryCard/BLIsByFYSummaryCard.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import PropTypes from "prop-types";
+import CurrencyFormat from "react-currency-format";
+import { fiscalYearFromDate } from "../../../../helpers/utils";
+import constants from "../../../../constants";
+import SummaryCard from "../../../UI/SummaryCard";
+import styles from "./BLIsByFYSummaryCard.styles.module.scss";
+
+/**
+ * A component that displays the total budget lines for an agreement.
+ *
+ * @param {Object} props - The component props.
+ * @param {Array<any>} props.budgetLineItems - The budget line items for the agreement.
+ * @returns {React.JSX.Element} - The agreement total budget lines card component JSX.
+ */
+const BLIsByFYSummaryCard = ({ budgetLineItems }) => {
+    const id = React.useId();
+    const { blisByFYChartColors } = constants;
+    const blisFy = budgetLineItems.map((bli) => ({ ...bli, fiscalYear: fiscalYearFromDate(bli.date_needed) }));
+    const fyTotalsMap = blisFy.reduce((acc, cur) => {
+        if (!cur.fiscalYear || cur.amount == null) return acc;
+        // TODO: create BLI total function somewhere to use here and in AgreementBudgetLines, TotalSummaryCard, etc
+        let fee = cur.amount * cur?.proc_shop_fee_percentage;
+        let total = cur.amount + fee;
+        if (!(cur.fiscalYear in acc)) {
+            acc[cur.fiscalYear] = total;
+        } else {
+            acc[cur.fiscalYear] = acc[cur.fiscalYear] + total;
+        }
+        return acc;
+    }, {});
+    const fyTotalsAll = Object.keys(fyTotalsMap).map((fy) => ({ fiscalYear: fy, total: fyTotalsMap[fy] }));
+    const fyTotals = fyTotalsAll.slice(0, 5);
+    const maxFyTotal = Math.max(...fyTotals.map((o) => o.total));
+
+    const chartData = fyTotals.map((fyVal, index) => {
+        return {
+            FY: fyVal.fiscalYear,
+            total: fyVal.total,
+            ratio: fyVal.total / maxFyTotal,
+            color: blisByFYChartColors[index].color
+        };
+    });
+
+    return (
+        <SummaryCard
+            title="Budget Lines by Fiscal Year"
+            dataCy="blis-by-fy-card"
+        >
+            <div>
+                {chartData.map((item, index) => (
+                    <div
+                        className="display-flex margin-y-1"
+                        key={`blis-fy-${index}-${id}`}
+                    >
+                        <span>FY {item.FY}</span>
+                        <div
+                            className="margin-x-1"
+                            style={{ flex: item.ratio }}
+                        >
+                            <div className={styles.barBox}>
+                                <div
+                                    className={styles.rightBar}
+                                    style={{ backgroundColor: item.color }}
+                                />
+                            </div>
+                        </div>
+                        <CurrencyFormat
+                            value={item.total}
+                            displayType="text"
+                            thousandSeparator=","
+                            prefix="$"
+                            decimalScale={2}
+                            fixedDecimalScale={true}
+                        />
+                    </div>
+                ))}
+            </div>
+        </SummaryCard>
+    );
+};
+
+BLIsByFYSummaryCard.propTypes = {
+    budgetLineItems: PropTypes.array.isRequired
+};
+
+export default BLIsByFYSummaryCard;
