7910fe4ef9e0da556c0facf365a7d17f8f47a7de
fpigeonjr <fpigeon@flexion.us>
2023-10-13 14:26:44 -0500

    test: :test_tube: fixes tests 🤞

--- frontend/cypress/e2e/agreementList.cy.js
+++ frontend/cypress/e2e/agreementList.cy.js
@@ -16,7 +16,9 @@ it("loads", () => {
 });
 
 it("navigates to the approveAgreements page when the approve button is clicked", () => {
+    cy.wait(1000);
     // hover over a row to make the approve button visible
+    cy.get(".usa-table").should("exist");
     cy.get("tbody tr").first().trigger("mouseover");
     // click the approve svg with an id of "submit-for-approval-x" where x is the id of the agreement
     cy.get("svg[id^='submit-for-approval-']").first().click();
