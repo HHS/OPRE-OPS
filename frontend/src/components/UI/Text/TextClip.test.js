03dfcb3b914b618ff171c7a35fc4769c22c25308
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-10-19 11:42:13 -0600

    linted test

--- frontend/src/components/UI/Text/TextClip.test.js
+++ frontend/src/components/UI/Text/TextClip.test.js
@@ -1,8 +1,4 @@
-import TabsSection from "../../UI/TabsSection";
-import store from "../../../store";
-import { Provider } from "react-redux";
 import { render } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
 import TextClip from "./TextClip";
 
 it("renders without crashing", () => {
