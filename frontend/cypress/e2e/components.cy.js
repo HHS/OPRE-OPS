3ede8fa256c1723143693f147eaa5a998bea7397
fpigeonjr <fpigeon@flexion.us>
2023-10-20 09:54:54 -0500

    test: :test_tube: adds e2e tests

--- frontend/cypress/e2e/components.cy.js
+++ frontend/cypress/e2e/components.cy.js
@@ -3,7 +3,7 @@ import { terminalLog, testLogin } from "./utils";
 
 beforeEach(() => {
     testLogin("admin");
-    cy.visit("/cans");
+    // cy.visit("/cans");
 });
 
 afterEach(() => {
@@ -55,7 +55,7 @@ describe("table row", () => {
 });
 
 describe("agreement meta accordion", () => {
-    it("accordion should open when clicked", () => {
+    it("accordion should close when clicked", () => {
         cy.visit("/agreements/approve/1");
         cy.get(".usa-accordion__heading > .usa-accordion__button").first().as("acc-btn").should("exist");
         cy.get(".usa-accordion__content").should("not.be.hidden");
@@ -63,7 +63,7 @@ describe("agreement meta accordion", () => {
         cy.get(".usa-accordion__content").should("be.hidden");
     });
 
-    it("accordion should open via keyboard enter", () => {
+    it("accordion should close via keyboard enter", () => {
         cy.visit("/agreements/approve/1");
         cy.get(".usa-accordion__heading > .usa-accordion__button").first().as("acc-btn").should("exist");
         cy.get(".usa-accordion__content").should("not.be.hidden");
@@ -73,14 +73,6 @@ describe("agreement meta accordion", () => {
 });
 
 describe("agreement action accordion", () => {
-    it('accordion should open when "enter" is pressed', () => {
-        cy.visit("/agreements/approve/1");
-        cy.get("h2").contains("Choose an Action").as("acc-btn").should("exist");
-        cy.get(".usa-accordion__content").should("not.be.hidden");
-        cy.get("@acc-btn").type("{enter}");
-        cy.get(".usa-accordion__content").should("be.hidden");
-    });
-
     it("should have draft option available on agreement one", () => {
         cy.visit("/agreements/approve/1");
         cy.get("h2").contains("Choose an Action").as("acc-btn").should("exist");
@@ -100,20 +92,49 @@ describe("agreement action accordion", () => {
     });
 });
 
-describe("agreement BLI accordion", () => {
-    it('accordion should open when "enter" is pressed', () => {
-        cy.visit("/agreements/approve/1");
-        cy.get("h2").contains("Select Budget Lines").as("acc-btn").should("exist");
-        cy.get(".usa-accordion__content").should("not.be.hidden");
-        cy.get("@acc-btn").type("{enter}");
-        cy.get(".usa-accordion__content").should("be.hidden");
-    });
+describe.only("agreement BLI accordion", () => {
     it("should contain summary card", () => {
         cy.visit("/agreements/approve/1");
-        cy.get("h2").contains("Select Budget Lines").as("acc-btn").type("{enter}");
+        cy.get("h2").contains("Select Budget Lines").as("acc-btn");
         cy.get('[data-cy="blis-by-fy-card"]').should("exist");
         cy.get('[data-cy="currency-summary-card"]').should("exist");
+    });
+
+    it("allow to select individual budget lines", () => {
+        cy.visit("/agreements/approve/1");
+        cy.get('input[id="Change Draft Budget Lines to Planned Status"]').should("exist").should("not.be.disabled");
+        cy.get('[type="radio"]').should("have.length", 2);
+        cy.get('[type="radio"]').first().check({ force: true });
+        cy.get("#1").check({ force: true });
+        cy.get("#2").check({ force: true });
+    });
+
+    it("should handle check-all and uncheck all", () => {
+        cy.visit("/agreements/approve/1");
+        cy.get("h2").contains("Select Budget Lines").as("acc-btn");
         cy.get(".usa-table").should("exist");
+        cy.get("#check-all").should("exist").should("be.disabled");
+        // click action radio button
+        cy.get("h2").contains("Choose an Action").as("acc-btn").should("exist");
+        cy.get('input[id="Change Draft Budget Lines to Planned Status"]').should("exist").should("not.be.disabled");
+        // check the radio button
+        cy.get('[type="radio"]').should("have.length", 2);
+        cy.get('[type="radio"]').first().check({ force: true });
+        cy.get("#check-all").check({ force: true });
+        // all checkboxes should be checked
+        cy.get('[type="checkbox"]')
+            .should("have.length", 3)
+            .each((checkbox) => {
+                cy.wrap(checkbox).should("be.checked");
+            });
+        // uncheck all
+        cy.get("#check-all").uncheck({ force: true });
+        // all checkboxes should be unchecked
+        cy.get('[type="checkbox"]')
+            .should("have.length", 3)
+            .each((checkbox) => {
+                cy.wrap(checkbox).should("not.be.checked");
+            });
     });
 });
 
