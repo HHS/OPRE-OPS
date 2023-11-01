06ae47a362aabda55205bf8ac7ad97f12620c1a8
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-11-01 08:53:03 -0600

    separating in/out division CANs CANS Outside Division Tag Portfolios Tag formatting and styles

--- frontend/src/components/Agreements/AgreementCANReviewAccordian/AgreementCANReviewAccordion.js
+++ frontend/src/components/Agreements/AgreementCANReviewAccordian/AgreementCANReviewAccordion.js
@@ -1,19 +1,45 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Accordion from "../../UI/Accordion";
 import { totalBudgetLineFeeAmount } from "../../../helpers/utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faToggleOff, faToggleOn } from "@fortawesome/free-solid-svg-icons";
 import CANFundingCard from "../../CANs/CANFundingCard";
+import Tag from "../../UI/Tag";
+import { useSelector } from "react-redux";
+import { useGetPortfoliosQuery } from "../../../api/opsAPI";
 
-const AgreementCANReviewAccordion = ({ selectedBudgetLines }) => {
+const AgreementCANReviewAccordian = ({ selectedBudgetLines }) => {
     // TODO: may need to elevate state for approval toggle
-    const [afterApproval, setAfterApproval] = React.useState(true);
+    const [afterApproval, setAfterApproval] = useState(true);
+    const activeUser = useSelector((state) => state.auth.activeUser);
+    console.log("activeUser:", activeUser);
+    const myDivisionId = activeUser?.division;
+    console.log(`myDivisionId: ${myDivisionId}`);
+
+    const { data: portfolios, error, isLoading } = useGetPortfoliosQuery();
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    if (error) {
+        return <div>An error occurred loading Portfolio data</div>;
+    }
+
+    console.log("portfolios:", portfolios);
+
     const cansWithPendingAmount = selectedBudgetLines.reduce((acc, budgetLine) => {
         const canId = budgetLine?.can?.id;
+        const canPortfolio = portfolios.find((portfolio) => portfolio.id === budgetLine?.can?.managing_portfolio_id);
+        const canDivisionId = canPortfolio.division_id;
+        console.log("canPortfolio", canPortfolio);
+        console.log("canDivision", canPortfolio.division_id);
+        const inMyDivision = myDivisionId === canDivisionId;
+        console.log("inMyDivision", inMyDivision);
         if (!acc[canId]) {
+            console.log("can", budgetLine.can);
             acc[canId] = {
                 can: budgetLine.can,
                 pendingAmount: 0,
+                inMyDivision: inMyDivision,
                 count: 0 // not used but handy for debugging
             };
         }
@@ -23,7 +49,18 @@ const AgreementCANReviewAccordion = ({ selectedBudgetLines }) => {
         return acc;
     }, {});
 
+    let canPortfolios = [];
+    selectedBudgetLines.forEach((budgetLine) => {
+        const canPortfolio = portfolios.find((portfolio) => portfolio.id === budgetLine?.can?.managing_portfolio_id);
+        if (canPortfolios.indexOf(canPortfolio) < 0) canPortfolios.push(canPortfolio);
+    });
+
     console.log("cansWithPendingAmount", cansWithPendingAmount);
+    const myCansWithPendingAmount = Object.values(cansWithPendingAmount).filter((el) => el.inMyDivision);
+    const otherCansWithPendingAmount = Object.values(cansWithPendingAmount).filter((el) => !el.inMyDivision);
+
+    console.log("myCansWithPendingAmount", myCansWithPendingAmount);
+    console.log("otherCansWithPendingAmount", otherCansWithPendingAmount);
 
     return (
         <Accordion
@@ -51,15 +88,38 @@ const AgreementCANReviewAccordion = ({ selectedBudgetLines }) => {
                 </button>
             </div>
             <div
-                className="display-flex flex-wrap"
+                className="display-flex flex-wrap margin-bottom-0"
                 style={{ gap: "32px 28px" }}
             >
-                {Object.entries(cansWithPendingAmount).map(([key, value]) => (
+                {myCansWithPendingAmount.map((value) => (
                     <CANFundingCard
-                        key={key}
+                        key={value.can.id}
                         can={value.can}
                         pendingAmount={value.pendingAmount}
                         afterApproval={afterApproval}
+                    />
+                ))}
+            </div>
+            <div className="text-base-dark font-12px margin-top-1">
+                *Total Spending equals the sum of Budget Lines in Planned, Executing and Obligated
+            </div>
+            <div className="margin-top-4">
+                <span className="text-base-dark font-12px">Other CANs Outside Your Portfolio:</span>
+                {otherCansWithPendingAmount.map((value) => (
+                    <Tag
+                        className="margin-left-1"
+                        text={value.can.number}
+                        tagStyle="primaryDarkTextLightBackground"
+                    />
+                ))}
+            </div>
+            <div className="margin-top-3">
+                <span className="text-base-dark font-12px">Portfolios:</span>
+                {canPortfolios.map((portfolio) => (
+                    <Tag
+                        className="margin-left-1"
+                        text={portfolio.name}
+                        tagStyle="primaryDarkTextLightBackground"
                     />
                 ))}
             </div>
@@ -67,4 +127,4 @@ const AgreementCANReviewAccordion = ({ selectedBudgetLines }) => {
     );
 };
 
-export default AgreementCANReviewAccordion;
+export default AgreementCANReviewAccordian;
