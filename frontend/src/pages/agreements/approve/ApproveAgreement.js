50498f7ae82a4c0f30bb393ef1673fbdf82d5604
fpigeonjr <fpigeon@flexion.us>
2023-11-09 16:33:43 -0600

    feat: :sparkles: Add budget line review accordion and related components

--- frontend/src/pages/agreements/approve/ApproveAgreement.js
+++ frontend/src/pages/agreements/approve/ApproveAgreement.js
@@ -6,6 +6,11 @@ import PageHeader from "../../../components/UI/PageHeader";
 import AgreementMetaAccordion from "../../../components/Agreements/AgreementMetaAccordion";
 import useGetUserFullNameFromId from "../../../hooks/user.hooks";
 import { convertCodeForDisplay } from "../../../helpers/utils";
+import AgreementBLIAccordion from "../../../components/Agreements/AgreementBLIAccordion";
+import BudgetLinesTable from "../../../components/BudgetLineItems/BudgetLinesTable";
+import AgreementCANReviewAccordion from "../../../components/Agreements/AgreementCANReviewAccordion";
+import AgreementChangesAccordion from "../../../components/Agreements/AgreementChangesAccordion";
+import { anyBudgetLinesByStatus, selectedBudgetLinesTotal, getTotalByCans } from "../review/ReviewAgreement.helpers";
 
 const ApproveAgreement = () => {
     const urlPathParams = useParams();
@@ -20,8 +25,6 @@ const ApproveAgreement = () => {
         refetchOnMountOrArgChange: true
     });
     const projectOfficerName = useGetUserFullNameFromId(agreement?.project_officer_id);
-    const cn = "";
-    const res = "";
 
     if (isLoadingAgreement) {
         return <h1>Loading...</h1>;
@@ -29,6 +32,8 @@ const ApproveAgreement = () => {
     if (errorAgreement) {
         return <h1>Oops, an error occurred</h1>;
     }
+
+    const changeInCans = getTotalByCans(agreement?.budget_line_items);
 
     return (
         <App breadCrumbName="Approve BLI Status Change">
@@ -40,6 +45,30 @@ const ApproveAgreement = () => {
                 agreement={agreement}
                 projectOfficerName={projectOfficerName}
                 convertCodeForDisplay={convertCodeForDisplay}
+            />
+
+            <AgreementBLIAccordion
+                title="Review Budget Lines"
+                budgetLineItems={agreement.budget_line_items}
+                agreement={agreement}
+                afterApproval={() => {}}
+                setAfterApproval={() => {}}
+            >
+                <BudgetLinesTable
+                    readOnly={true}
+                    budgetLinesAdded={agreement.budget_line_items}
+                    isReviewMode={false}
+                    showTotalSummaryCard={false}
+                />
+            </AgreementBLIAccordion>
+            <AgreementCANReviewAccordion
+                selectedBudgetLines={agreement.budget_line_items}
+                afterApproval={() => {}}
+                setAfterApproval={() => {}}
+            />
+            <AgreementChangesAccordion
+                changeInBudgetLines={agreement.budget_line_items.reduce((acc, { amount }) => acc + amount, 0)}
+                changeInCans={changeInCans}
             />
 
             <pre className="font-code-2xs border-dashed border-error">{JSON.stringify(agreement, null, 2)}</pre>
