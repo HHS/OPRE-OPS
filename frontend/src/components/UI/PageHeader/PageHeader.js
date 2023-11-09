cc9c0d0b6e883cc58d3bed6f1093cdeefe95698b
fpigeonjr <fpigeon@flexion.us>
2023-11-09 09:56:40 -0600

    feat: :sparkles: Add PageHeader component with title and subtitle props

---
+++ frontend/src/components/UI/PageHeader/PageHeader.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+/**
+ * Renders a page header with a title and optional subtitle.
+ * @param {Object} props - The component props.
+ * @param {string} props.title - The title to display.
+ * @param {string} [props.subTitle] - The optional subtitle to display.
+ * @returns {React.JSX.Element} - The rendered component.
+ */
+const PageHeader = ({ title, subTitle }) => {
+    return (
+        <>
+            <h1 className="margin-0 text-brand-primary font-sans-2xl">{title}</h1>
+            {subTitle && <p className="font-sans-3xs margin-0">{subTitle}</p>}
+        </>
+    );
+};
+
+PageHeader.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string
+};
+export default PageHeader;
