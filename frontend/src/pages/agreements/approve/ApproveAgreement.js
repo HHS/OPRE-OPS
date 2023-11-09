a8bfb162ca03af5461c187bcb67fc4be3d6454ed
fpigeonjr <fpigeon@flexion.us>
2023-11-09 16:51:53 -0600

    feat: :sparkles: Add notes section and useToggle hook to ApproveAgreement component

--- frontend/src/pages/agreements/approve/ApproveAgreement.js
+++ frontend/src/pages/agreements/approve/ApproveAgreement.js
@@ -11,11 +11,14 @@ import BudgetLinesTable from "../../../components/BudgetLineItems/BudgetLinesTab
 import AgreementCANReviewAccordion from "../../../components/Agreements/AgreementCANReviewAccordion";
 import AgreementChangesAccordion from "../../../components/Agreements/AgreementChangesAccordion";
 import { anyBudgetLinesByStatus, selectedBudgetLinesTotal, getTotalByCans } from "../review/ReviewAgreement.helpers";
+import TextArea from "../../../components/UI/Form/TextArea";
+import useToggle from "../../../hooks/useToggle";
 
 const ApproveAgreement = () => {
     const urlPathParams = useParams();
     const agreementId = +urlPathParams.id;
     const navigate = useNavigate();
+    const [notes, setNotes] = React.useState("");
     const {
         isSuccess,
         data: agreement,
@@ -25,6 +28,7 @@ const ApproveAgreement = () => {
         refetchOnMountOrArgChange: true
     });
     const projectOfficerName = useGetUserFullNameFromId(agreement?.project_officer_id);
+    const [afterApproval, setAfterApproval] = useToggle(true);
 
     if (isLoadingAgreement) {
         return <h1>Loading...</h1>;
@@ -51,8 +55,8 @@ const ApproveAgreement = () => {
                 title="Review Budget Lines"
                 budgetLineItems={agreement.budget_line_items}
                 agreement={agreement}
-                afterApproval={() => {}}
-                setAfterApproval={() => {}}
+                afterApproval={afterApproval}
+                setAfterApproval={setAfterApproval}
             >
                 <BudgetLinesTable
                     readOnly={true}
@@ -63,15 +67,53 @@ const ApproveAgreement = () => {
             </AgreementBLIAccordion>
             <AgreementCANReviewAccordion
                 selectedBudgetLines={agreement.budget_line_items}
-                afterApproval={() => {}}
-                setAfterApproval={() => {}}
+                afterApproval={afterApproval}
+                setAfterApproval={setAfterApproval}
             />
             <AgreementChangesAccordion
                 changeInBudgetLines={agreement.budget_line_items.reduce((acc, { amount }) => acc + amount, 0)}
                 changeInCans={changeInCans}
             />
-
-            <pre className="font-code-2xs border-dashed border-error">{JSON.stringify(agreement, null, 2)}</pre>
+            <h2 className="font-sans-lg text-semibold">Notes</h2>
+            <TextArea
+                name="submitter-notes"
+                label="Notes (optional)"
+                maxLength={150}
+                value={notes}
+                onChange={(name, value) => setNotes(value)}
+            />
+            <div className="grid-row flex-justify-end margin-top-8">
+                <button
+                    className={`usa-button usa-button--unstyled margin-right-2`}
+                    data-cy="edit-agreement-btn"
+                    onClick={() => {
+                        alert("Not implemented yet");
+                    }}
+                >
+                    Cancel
+                </button>
+                <button
+                    className={`usa-button usa-button--outline margin-right-2`}
+                    data-cy="edit-agreement-btn"
+                    onClick={() => {
+                        alert("Not implemented yet");
+                    }}
+                >
+                    Decline
+                </button>
+                <button
+                    className="usa-button"
+                    data-cy="send-to-approval-btn"
+                    onClick={() => {
+                        alert("Not implemented yet");
+                    }}
+                >
+                    Approve
+                </button>
+            </div>
+            <pre className="font-code-2xs border-dashed border-error margin-top-10">
+                {JSON.stringify(agreement, null, 2)}
+            </pre>
         </App>
     );
 };
