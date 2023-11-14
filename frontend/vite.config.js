d72050ed5ed419fd021c3f55e687b360bd9223f1
fpigeonjr <fpigeon@flexion.us>
2023-11-14 13:36:34 -0600

    Update frontend dependencies and configuration

---
+++ frontend/vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteJsconfigPaths from "vite-jsconfig-paths";
+import svgr from "vite-plugin-svgr";
+import eslint from "vite-plugin-eslint";
+import fs from "fs/promises";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        viteJsconfigPaths(),
+        eslint(),
+        svgr({
+            include: "**/*.svg?react"
+        })
+    ],
+    define: {
+        "process.env": {}
+    },
+    esbuild: {
+        loader: "jsx",
+        include: /src\/.*\.jsx?$/,
+        exclude: []
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                {
+                    name: "load-js-files-as-jsx",
+                    setup(build) {
+                        build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+                            loader: "jsx",
+                            contents: await fs.readFile(args.path, "utf8")
+                        }));
+                    }
+                }
+            ]
+        }
+    }
+});
