7ae704e5b7d5b60e6392d72c0d21ad73761f6577
fpigeonjr <fpigeon@flexion.us>
2023-11-16 18:38:23 -0600

    wip: vitest

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,9 +1,12 @@
 import ApplicationContext from "./applicationContext/ApplicationContext";
 import TestApplicationContext from "./applicationContext/TestApplicationContext";
 import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
 import { setupStore } from "./store";
 import { server } from "./helpers/mocks";
 import { opsApi } from "./api/opsAPI";
+import { afterEach } from "vitest";
 
 ApplicationContext.registerApplicationContext(TestApplicationContext);
 
@@ -16,6 +19,7 @@ beforeAll(() => {
 afterEach(() => {
     server.resetHandlers();
     store.dispatch(opsApi.util.resetApiState());
+    cleanup();
 });
 
 afterAll(() => server.close());
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteJsconfigPaths from "vite-jsconfig-paths";
@@ -37,6 +38,12 @@ export default defineConfig({
             include: "**/*.svg?react"
         })
     ],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.js",
+        coverage: {}
+    },
     define: {
         "process.env": {}
     },
