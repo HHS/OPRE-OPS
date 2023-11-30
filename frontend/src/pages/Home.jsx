6f953a067d082cdbfad7ba762b6396c9aa75c4f7
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2023-11-30 08:58:44 -0700

    WIP

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import App from "../App";
 import RoundedBox from "../components/UI/RoundedBox";
-import { Tooltip } from "../components/UI/USWDS/Tooltip";
+// import { Tooltip } from "../components/UI/USWDS/Tooltip";
+import * as Tooltip from "@radix-ui/react-tooltip";
+import "./styles.css";
+// import styles from "./styles.css";
 
 const Home = () => {
     return (
@@ -11,10 +14,24 @@ const Home = () => {
                     <p>⚠️Tread with caution</p>
                 </RoundedBox>
             </div>
-            <div>
-                <Tooltip label="Test Tooltip Label">
-                    <span>Tooltip Trigger</span>
-                </Tooltip>
+            <div style={{ marginTop: "20em", marginBottom: "20em" }}>
+                <Tooltip.Provider>
+                    <Tooltip.Root>
+                        <Tooltip.Trigger asChild>
+                            <button>Button Text</button>
+                        </Tooltip.Trigger>
+                        <Tooltip.Portal>
+                            <Tooltip.Content
+                                // className={`${styles.TooltipContent}`}
+                                className="TooltipContent"
+                                sideOffset={5}
+                            >
+                                Add to library
+                                <Tooltip.Arrow className="ZZZTooltipArrow" />
+                            </Tooltip.Content>
+                        </Tooltip.Portal>
+                    </Tooltip.Root>
+                </Tooltip.Provider>
             </div>
         </App>
     );
