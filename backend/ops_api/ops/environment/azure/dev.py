c9ba1c0a7e2466563d5a504fe0825c625d30ab7f
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2023-12-15 11:29:42 -0500

    chore: added azure env config

---
+++ backend/ops_api/ops/environment/azure/dev.py
@@ -0,0 +1,46 @@
+import os
+
+from ops_api.ops.environment.default_settings import *  # noqa: F403, F401
+
+DEBUG = True
+
+# Pull secrets from ACA Environment
+db_username = os.getenv("PGUSER")
+db_password = os.getenv("PGPASSWORD")
+db_host = os.getenv("PGHOST")
+db_port = os.getenv("PGPORT")
+db_name = os.getenv("PGDATABASE")
+
+OPS_FRONTEND_URL = os.getenv("OPS_FRONTEND_URL")
+
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"  # noqa: B950
+
+AUTHLIB_OAUTH_CLIENTS = {
+    "logingov": {
+        "server_metadata_url": "https://idp.int.identitysandbox.gov/.well-known/openid-configuration",
+        "user_info_url": "https://idp.int.identitysandbox.gov/api/openid_connect/userinfo",
+        "client_id": "urn:gov:gsa:openidconnect.profiles:sp:sso:hhs_acf:opre_ops",
+        "client_kwargs": {"scope": "openid email"},
+        "redirect_uri": "http://localhost:3000",
+    },
+    "hhsams": {
+        "server_metadata_url": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/.well-known/openid-configuration",
+        "token_endpoint": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/protocol/openid-connect/token",
+        "user_info_url": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/protocol/openid-connect/userinfo",
+        "client_id": "44fe2c7a-e9c5-43ec-87e9-3de78d2d3a11",
+        "client_kwargs": {"scope": "openid email"},
+        "aud": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO/protocol/openid-connect/token",
+        "redirect_uri": "http://localhost:3000",
+    },
+    "fakeauth": {
+        "server_metadata_url": "http://localhost:5000/oidc/.well-known/openid-configuration",
+        "token_endpoint": "http://localhost:5000/oidc/openid-connect/token",
+        "user_info_url": "http://localhost:5000/oidc/openid-connect/userinfo",
+        "client_id": "blah:blah",
+        "client_kwargs": {"scope": "openid email"},
+        "aud": "http://localhost:5000/oidc/openid-connect/token",
+        "redirect_uri": "http://localhost:3000/login",
+    },
+}
+
+JWT_PUBLIC_KEY_PATH = "static/public.pem"
