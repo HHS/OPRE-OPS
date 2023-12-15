ef01feaae1be18d416a7853b6f64a159792204c2
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2023-12-15 15:37:55 -0500

    chore: Updated the context source

--- terraform/eus/dev/backend_asa/context.tf
+++ terraform/eus/dev/backend_asa/context.tf
@@ -1,5 +1,5 @@
 module "ctx" {
-  source          = "../../../global/context"
+  source          = "git::https://github.com/HHS/OPRE-OPS.git//terraform/global/context?ref=tf-global-context-v0.0.2"
   environment     = var.environment
   custom_workload = "be4s"
 }
