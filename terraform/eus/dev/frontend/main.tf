999db4aee46d9a876b44e6c763efeafe0c80f633
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2023-12-20 13:27:21 -0500

    chore: Output FE Domain

--- terraform/eus/dev/frontend/main.tf
+++ terraform/eus/dev/frontend/main.tf
@@ -29,3 +29,7 @@ resource "azurerm_container_app" "frontend" {
     }
   }
 }
+
+output "domain" {
+  value = "https://${module.ctx.labels.fe.resourceNames["azurerm_container_app"]}.${data.azurerm_container_app_environment.aca_env.default_domain}"
+}
