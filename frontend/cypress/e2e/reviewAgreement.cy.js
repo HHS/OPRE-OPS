9351d72aa8b4b0dbb02c0baaf25e439b4eaabd19
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2024-01-02 13:08:55 -0700

    fix test amounts for new test agreement with one planned BLI

--- frontend/cypress/e2e/reviewAgreement.cy.js
+++ frontend/cypress/e2e/reviewAgreement.cy.js
@@ -527,7 +527,7 @@ describe("agreement review CANS accordion", () => {
         cy.get('[data-cy="can-funding-summary-card-5"]').contains("$40,000,000.00");
     });
 
-    it("should handle after approval toggle", () => {
+    it.only("should handle after approval toggle", () => {
         cy.visit("/agreements/review/1").wait(1000);
         // pre-change
         cy.get('[data-cy="button-toggle-After Approval"]').should("exist");
@@ -547,9 +547,9 @@ describe("agreement review CANS accordion", () => {
                 cy.wrap(checkbox).should("be.checked");
             });
         cy.get('[data-cy="can-funding-summary-card-5"]').should("exist");
-        cy.get('[data-cy="can-funding-summary-card-5"]').contains("4,000,000");
+        cy.get('[data-cy="can-funding-summary-card-5"]').contains("5,000,000");
         cy.get('[data-cy="button-toggle-After Approval"]').first().click({ force: true });
-        cy.get('[data-cy="can-funding-summary-card-5"]').contains("2,000,000");
+        cy.get('[data-cy="can-funding-summary-card-5"]').contains("3,000,000");
     });
 
     it("should handle over budget CANs", () => {
