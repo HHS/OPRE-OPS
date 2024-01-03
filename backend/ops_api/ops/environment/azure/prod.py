3c5b2cef66deefff55464b0b8fa2095d9f07b039
Tim Donaworth <tdonaworth@flexion.us>
2024-01-03 15:46:43 -0500

    linting

--- backend/ops_api/ops/environment/azure/prod.py
+++ backend/ops_api/ops/environment/azure/prod.py
@@ -11,4 +11,6 @@ db_host = os.getenv("PGHOST")
 db_port = os.getenv("PGPORT")
 db_name = os.getenv("PGDATABASE")
 
-SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"  # noqa: B950
+SQLALCHEMY_DATABASE_URI = (
+    f"postgresql+psycopg2://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"  # noqa: B950
+)
