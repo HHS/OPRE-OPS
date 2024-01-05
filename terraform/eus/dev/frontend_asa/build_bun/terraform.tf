89c27484e9dadb2ae1ae0508b357f28005fddf5f
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2024-01-05 09:43:09 -0500

    chore: Fixed linting issues

--- terraform/eus/dev/frontend_asa/build_bun/terraform.tf
+++ terraform/eus/dev/frontend_asa/build_bun/terraform.tf
@@ -1,8 +1,15 @@
 terraform {
+  required_version = "~> 1.6"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.79.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
     }
   }
 
