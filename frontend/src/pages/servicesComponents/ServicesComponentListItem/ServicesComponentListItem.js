8319e6306d4bb2f580fd296076f282f807e12c8a
fpigeonjr <fpigeon@flexion.us>
2024-01-26 15:01:58 -0600

    Add Tag component to ServicesComponentListItem.js

--- frontend/src/pages/servicesComponents/ServicesComponentListItem/ServicesComponentListItem.js
+++ frontend/src/pages/servicesComponents/ServicesComponentListItem/ServicesComponentListItem.js
@@ -1,11 +1,12 @@
 import RoundedBox from "../../../components/UI/RoundedBox";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import Tag from "../../../components/UI/Tag";
 
 function ServicesComponentListItem({ item }) {
     return (
         <RoundedBox
-            className="width-full flex-column padding-2"
+            className="width-full flex-column padding-2 margin-top-4"
             style={{ width: "100%", height: "auto", minHeight: "134px" }}
         >
             <section className="display-flex flex-justify">
@@ -45,18 +46,22 @@ function ServicesComponentListItem({ item }) {
                 <dl className="display-flex font-12px">
                     <div>
                         <dt className="margin-0 text-base-dark margin-top-1px">Period of Performance - Start</dt>
-                        <dd className="margin-0 margin-top-05">
-                            {item.popStartMonth}/{item.popStartDay}/{item.popStartYear}
+                        <dd className="margin-0 margin-top-1">
+                            <Tag tagStyle="primaryDarkTextLightBackground">
+                                {item.popStartMonth}/{item.popStartDay}/{item.popStartYear}
+                            </Tag>
                         </dd>
                     </div>
                     <div className="margin-left-4">
                         <dt className="margin-0 text-base-dark margin-top-1px">Period of Performance - End</dt>
-                        <dd className="margin-0 margin-top-05">
-                            {item.popEndMonth}/{item.popEndDay}/{item.popEndYear}
+                        <dd className="margin-0 margin-top-1">
+                            <Tag tagStyle="primaryDarkTextLightBackground">
+                                {item.popEndMonth}/{item.popEndDay}/{item.popEndYear}
+                            </Tag>
                         </dd>
                     </div>
                     <div
-                        className="margin-left-auto"
+                        className="margin-left-8"
                         style={{ width: "25rem" }}
                     >
                         <dt className="margin-0 text-base-dark margin-top-1px">Description</dt>
