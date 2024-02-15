844b9c2628970e61d0bf139c954195748834590a
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2024-02-15 13:32:04 -0700

    minimal cleanup broken bits of WIP

--- backend/ops_api/tests/ops/workflows/test_workflow_procurement.py
+++ backend/ops_api/tests/ops/workflows/test_workflow_procurement.py
@@ -2,9 +2,8 @@ from datetime import datetime
 from unittest.mock import Mock
 
 import pytest
+from models import Package, PackageSnapshot, WorkflowAction, WorkflowInstance, WorkflowStepInstance, WorkflowStepStatus
 from models.workflows import WorkflowTriggerType
-from models import BudgetLineItem, BudgetLineItemStatus, WorkflowAction, WorkflowStepInstance, WorkflowStepStatus, WorkflowInstance, Package, PackageSnapshot
-from ops_api.ops.resources.workflow_approve import update_blis
 
 
 @pytest.mark.usefixtures("app_ctx", "loaded_db")
@@ -16,16 +15,16 @@ def test_creating_procurement_tracker_workflow(loaded_db):
     procurement_workflow.associated_id = 1
     procurement_workflow.associated_type = WorkflowTriggerType.AGREEMENT
     procurement_workflow.workflow_action = WorkflowAction.GENERIC
-    
-    workflow_template = Mock(spec=WorkflowTemplate)
-    workflow_template = 
-    for step in workflow_template.steps:
-        step_instance = Mock(spec=WorkflowStepInstance)
-        step_instance.workflow_instance_id = procurement_workflow.id
-        step_instance.workflow_step_template_id = step.id
-        step_instance.status = WorkflowStepStatus.REVIEW
-        step_instance.notes = ""
-        step_instance.time_started = datetime.now()
+
+    # workflow_template = Mock(spec=WorkflowTemplate)
+    # workflow_template =
+    # for step in workflow_template.steps:
+    #     step_instance = Mock(spec=WorkflowStepInstance)
+    #     step_instance.workflow_instance_id = procurement_workflow.id
+    #     step_instance.workflow_step_template_id = step.id
+    #     step_instance.status = WorkflowStepStatus.REVIEW
+    #     step_instance.notes = ""
+    #     step_instance.time_started = datetime.now()
 
     workflow_step_instance_acquisition = Mock(spec=WorkflowStepInstance)
     workflow_step_instance_acquisition.workflow_instance_id = procurement_workflow.id
@@ -46,7 +45,7 @@ def test_creating_procurement_tracker_workflow(loaded_db):
 
     package = Mock(Package)
     package.submitter_id = 21
-    package.workflow_id = workflow_step_instance.id
+    package.workflow_id = workflow_step_instance_acquisition.id
     package.notes = ""
 
     package_snapshot = Mock(PackageSnapshot)
@@ -54,18 +53,19 @@ def test_creating_procurement_tracker_workflow(loaded_db):
     package_snapshot.object_type = "AGREEMENT"
     package_snapshot.object_id = 1
 
-    # workflow_step_instance.workflow_instance.workflow_action = WorkflowAction.GENERIC
-    # workflow_step_instance.package_entities = {"budget_line_item_ids": [1, 2, 3]}
-
-    # Use loaded_db to create or mock BudgetLineItems
-    bli1 = loaded_db.get(BudgetLineItem, 1)
-    bli2 = loaded_db.get(BudgetLineItem, 2)
-    bli3 = loaded_db.get(BudgetLineItem, 3)
-    blis = [bli1, bli2, bli3]
-
-    # Call the function
-    update_blis(workflow_step_instance)
-
-    # Assert that the status of each BudgetLineItem is set to BudgetLineItemStatus.PLANNED
-    for bli in blis:
-        assert bli.status == BudgetLineItemStatus.PLANNED
+    #
+    # # workflow_step_instance.workflow_instance.workflow_action = WorkflowAction.GENERIC
+    # # workflow_step_instance.package_entities = {"budget_line_item_ids": [1, 2, 3]}
+    #
+    # # Use loaded_db to create or mock BudgetLineItems
+    # bli1 = loaded_db.get(BudgetLineItem, 1)
+    # bli2 = loaded_db.get(BudgetLineItem, 2)
+    # bli3 = loaded_db.get(BudgetLineItem, 3)
+    # blis = [bli1, bli2, bli3]
+    #
+    # # Call the function
+    # update_blis(workflow_step_instance)
+    #
+    # # Assert that the status of each BudgetLineItem is set to BudgetLineItemStatus.PLANNED
+    # for bli in blis:
+    #     assert bli.status == BudgetLineItemStatus.PLANNED
