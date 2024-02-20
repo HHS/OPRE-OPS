256fb11e39e371f23a1fffae44f1e7aad43a83a2
fpigeonjr <fpigeon@flexion.us>
2024-02-20 15:57:16 -0600

    Refactor ServicesComponents and StepCreateBLIsAndSCs components

--- frontend/src/components/UI/WizardSteps/StepCreateBLIsAndSCs/StepCreateBLIsAndSCs.jsx
+++ frontend/src/components/UI/WizardSteps/StepCreateBLIsAndSCs/StepCreateBLIsAndSCs.jsx
@@ -362,10 +362,6 @@ export const StepCreateBLIsAndSCs = ({
                         serviceRequirementType={selectedAgreement.service_requirement_type}
                         agreementId={selectedAgreement.id}
                     />
-                    <DebugCode
-                        title="selectedAgreement"
-                        data={selectedAgreement}
-                    />
                     <h2 className="font-sans-lg margin-top-3">Budget Line Details</h2>
                     <p>Complete the information below to create new budget lines.</p>
                 </>
--- frontend/src/pages/servicesComponents/ServicesComponents.jsx
+++ frontend/src/pages/servicesComponents/ServicesComponents.jsx
@@ -1,20 +1,25 @@
-// import App from "../../App";
-// import ServiceReqTypeSelect from "./ServiceReqTypeSelect";
+import PropTypes from "prop-types";
 import ServicesComponentForm from "./ServicesComponentForm";
 import ServicesComponentsList from "./ServicesComponentsList";
 import ConfirmationModal from "../../components/UI/Modals/ConfirmationModal";
-// import { initialFormData } from "./servicesComponents.constants";
 import useServicesComponents from "./servicesComponents.hooks";
 import DebugCode from "./DebugCode";
 
+/**
+ * ServicesComponents is a component that handles the display and functionality of service components.
+ *
+ * @component
+ * @param {object} props
+ * @param {string} props.serviceRequirementType - The type of service requirement.
+ * @param {number} props.agreementId - The ID of the agreement.
+ * @returns {JSX.Element}
+ */
 const ServicesComponents = ({ serviceRequirementType, agreementId }) => {
     const {
         formData,
         modalProps,
-        serviceTypeReq,
         servicesComponents,
         setFormData,
-        setServiceTypeReq,
         setShowModal,
         showModal,
         handleSubmit,
@@ -57,4 +62,8 @@ const ServicesComponents = ({ serviceRequirementType, agreementId }) => {
     );
 };
 
+ServicesComponents.propTypes = {
+    serviceRequirementType: PropTypes.string.isRequired,
+    agreementId: PropTypes.string.isRequired
+};
 export default ServicesComponents;
