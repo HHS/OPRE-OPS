66dd9a3e39fae7783e72d9f89c4bb3240e9d7b41
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2024-03-14 13:28:33 -0400

    chore: cleanup

--- terraform/eastus/ops/dev/modules/pg-schema/outputs.tf
+++
@@ -1,8 +0,0 @@
-# # Output the secret name and location
-# output "key_vault_secret" {
-#   value = {
-#     name   = azurerm_key_vault_secret.role_password.name
-#     url    = azurerm_key_vault_secret.role_password.id
-#     raw_pw = local.password
-#   }
-# }
--- terraform/eastus/ops/dev/stacks/data-tools/outputs.tf
+++
@@ -1,4 +0,0 @@
-
-# output "domain" {
-#   value = "https://${module.ctx.labels.fe.resourceNames["azurerm_container_app"]}.${data.azurerm_container_app_environment.aca_env.default_domain}"
-# }
