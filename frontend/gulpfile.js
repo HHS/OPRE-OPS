9b516288636648ee79df8f32261a3bd4492a6e7c
fpigeonjr <fpigeon@flexion.us>
2024-03-18 17:18:21 -0500

    refactor:  gulpfile.js to use ES6 import syntax and update paths

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,29 +3,23 @@
 /**
  * Import uswds-compile
  */
-const uswds = require("@uswds/compile");
+import { settings, paths, init, compile, watch } from "@uswds/compile";
 
 /**
  * USWDS version
  * Set the major version of USWDS you're using
  * (Current options are the numbers 2 or 3)
  */
-uswds.settings.version = 3;
+settings.version = 3;
 
 /**
  * Path settings
  * Set as many as you need
  */
-uswds.paths.dist.css = "./src/uswds/css";
-uswds.paths.dist.theme = "./sass/uswds";
-uswds.paths.dist.img = "./src/uswds/img";
-uswds.paths.dist.fonts = "./src/uswds/fonts";
-uswds.paths.dist.js = "./src/uswds/js";
+paths.dist.css = "./src/uswds/css";
+paths.dist.theme = "./sass/uswds";
+paths.dist.img = "./src/uswds/img";
+paths.dist.fonts = "./src/uswds/fonts";
+paths.dist.js = "./src/uswds/js";
 
-/**
- * Exports
- * Add as many as you need
- */
-exports.init = uswds.init;
-exports.compile = uswds.compile;
-exports.watch = uswds.watch;
+export { init, compile, watch };
