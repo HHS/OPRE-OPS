cab4d35a8d0524cc253e64b5af37ef55b24d88c9
fpigeonjr <fpigeon@flexion.us>
2024-03-19 11:21:13 -0500

    refactor: ♻️ cancel logic in AgreementEditForm and CreateBLIsAndSCs components

--- frontend/src/components/UI/WizardSteps/CreateBLIsAndSCs/CreateBLIsAndSCs.jsx
+++ frontend/src/components/UI/WizardSteps/CreateBLIsAndSCs/CreateBLIsAndSCs.jsx
@@ -87,6 +87,7 @@ export const CreateBLIsAndSCs = ({
         handleCancel,
         handleGoBack
     } = useCreateBLIsAndSCs(
+        isEditMode,
         isReviewMode,
         budgetLines,
         goToNext,
