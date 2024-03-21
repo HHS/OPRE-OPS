23e1b377da597d74076161026438ffecd94f7708
John DeAngelis <john.deangelis2@gmail.com>
2024-03-21 16:55:08 -0400

    add no-permissions user to tests
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/ops_api/tests/ops/auth_client.py
+++ backend/ops_api/tests/ops/auth_client.py
@@ -21,3 +21,27 @@ class AuthClient(FlaskClient):
         access_token = create_access_token(identity=user, additional_claims=additional_claims)
         kwargs.setdefault("headers", {"Authorization": f"Bearer {access_token}"})
         return super().open(*args, **kwargs)
+
+
+class NoPermsAuthClient(FlaskClient):
+    """
+    A test client that creates an access token for a user with no roles or groups.
+
+    N.B. Instead of re-using a user from our test data it would be better to create
+    a new user for this purpose. This would make it clear that the user has no
+    permissions and would prevent the tests from breaking if the user's permissions
+    were changed.
+    """
+
+    def open(self, *args, **kwargs):
+        user = User(
+            id="7",
+            oidc_id="00000000-0000-1111-a111-000000000007",
+            email="unit-test-no-perms@ops-api.gov",
+            first_name="Unit",
+            last_name="Test",
+            division=1,
+        )
+        access_token = create_access_token(identity=user, additional_claims={})
+        kwargs.setdefault("headers", {"Authorization": f"Bearer {access_token}"})
+        return super().open(*args, **kwargs)
