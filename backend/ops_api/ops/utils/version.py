eb7cb14934683c9dd7fedc09dd4430d888c09c14
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2024-04-11 21:41:24 -0400

    style: fix lint

--- backend/ops_api/ops/utils/version.py
+++ backend/ops_api/ops/utils/version.py
@@ -8,17 +8,16 @@ class VersionAPI(MethodView):
 
     @staticmethod
     def get_api_version():
-        # Use the absolute path of the current working directory
         current_dir = os.getcwd()
-        openapi_path = os.path.join(current_dir, 'openapi.yml')
+        openapi_path = os.path.join(current_dir, "openapi.yml")
         try:
-            with open(openapi_path, 'r') as file:
+            with open(openapi_path, "r") as file:
                 openapi_spec = yaml.safe_load(file)
-                return openapi_spec.get('info', {}).get('version', 'Unknown')
+                return openapi_spec.get("info", {}).get("version", "Unknown")
         except FileNotFoundError:
-            return f'OpenAPI spec file not found at {openapi_path}'
+            return f"OpenAPI spec file not found at {openapi_path}"
         except Exception as e:
-            return f'Error reading openapi.yml: {str(e)}'
+            return f"Error reading openapi.yml: {str(e)}"
 
     def get(self):
-        return jsonify({'version': self.get_api_version()})
+        return jsonify({"version": self.get_api_version()})
