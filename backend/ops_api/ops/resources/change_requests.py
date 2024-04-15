f793c2c292749ab6aed9efa2582d4a8d352c9a0c
Steve Tekell <127965976+stevtek@users.noreply.github.com>
2024-04-15 11:07:20 -0600

    support for rejecting changes

--- backend/ops_api/ops/resources/change_requests.py
+++ backend/ops_api/ops/resources/change_requests.py
@@ -10,36 +10,35 @@ from ops_api.ops.resources.budget_line_items import validate_and_prepare_change_
 from ops_api.ops.schemas.budget_line_items import PATCHRequestBody
 
 
-def approve_change_request(change_request_id: int, user_id: int) -> ChangeRequest:
+def review_change_request(change_request_id: int, change_to_status: ChangeRequestStatus, user_id: int) -> ChangeRequest:
     session = current_app.db_session
     change_request = session.get(ChangeRequest, change_request_id)
     change_request.reviewed_by_id = user_id
     change_request.reviewed_on = datetime.now()
-    change_request.status = ChangeRequestStatus.APPROVED
+    change_request.status = change_to_status
 
-    if isinstance(change_request, BudgetLineItemChangeRequest):
-        print("~~~BudgetLineItemChangeRequest~~~")
-        budget_line_item = session.get(BudgetLineItem, change_request.budget_line_item_id)
-        # need to copy to avoid changing the original data in the ChangeRequest and triggering an update
-        data = copy.deepcopy(change_request.requested_changes)
-        print(f"~~~data~~~\n{data}")
-        schema = mmdc.class_schema(PATCHRequestBody)()
-        schema.context["id"] = change_request.budget_line_item_id
-        schema.context["method"] = "PATCH"
+    # If approved, then apply the changes
+    if change_to_status == ChangeRequestStatus.APPROVED:
+        if isinstance(change_request, BudgetLineItemChangeRequest):
+            print("~~~BudgetLineItemChangeRequest~~~")
+            budget_line_item = session.get(BudgetLineItem, change_request.budget_line_item_id)
+            # need to copy to avoid changing the original data in the ChangeRequest and triggering an update
+            data = copy.deepcopy(change_request.requested_changes)
+            print(f"~~~data~~~\n{data}")
+            schema = mmdc.class_schema(PATCHRequestBody)()
+            schema.context["id"] = change_request.budget_line_item_id
+            schema.context["method"] = "PATCH"
 
-        change_data, changing_from_data = validate_and_prepare_change_data(
-            data,
-            budget_line_item,
-            schema,
-            ["id", "status", "agreement_id"],
-            partial=False,
-        )
+            change_data, changing_from_data = validate_and_prepare_change_data(
+                data,
+                budget_line_item,
+                schema,
+                ["id", "status", "agreement_id"],
+                partial=False,
+            )
 
-        budget_line_items.update_data(budget_line_item, change_data)
-        session.add(budget_line_item)
-
-    # if change_request.type == "budget_line_item_change_request":
-    #     print("~~~budget_line_item_change_request~~~")
+            budget_line_items.update_data(budget_line_item, change_data)
+            session.add(budget_line_item)
 
     session.add(change_request)
     session.commit()
