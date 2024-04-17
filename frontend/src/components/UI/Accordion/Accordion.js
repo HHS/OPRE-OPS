0d1a4c7876a2dc2b5a6fc3fdbf2076f66d08eabc
fpigeonjr <frank.pigeonjr@gmail.com>
2024-04-17 13:59:58 -0500

    fix: adds refs to open/close accordion

--- frontend/src/components/UI/Accordion/Accordion.js
+++ frontend/src/components/UI/Accordion/Accordion.js
@@ -1,16 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
+import accordion from "@uswds/uswds/js/usa-accordion";
 /**
  * Renders an accordion component with a heading and content.
+ * @component
  * @param {Object} props - The component props.
  * @param {string} props.heading - The heading text for the accordion.
  * @param {number} [props.level=4] - The heading level for the accordion. Defaults to 4.
  * @param {React.ReactNode} props.children - The content to display in the accordion.
- * @returns {React.JSX.Element} - The rendered accordion component.
+ * @returns {JSX.Element} - The rendered accordion component.
  */
 const Accordion = ({ heading, level = 4, children }) => {
     const accordionId = React.useId();
     const AccordionHeading = `h${level}`;
+
+    // Ensure accordion JS is loaded
+    const accordionRef = React.useRef(null);
+    React.useEffect(() => {
+        const accordionElement = accordionRef.current;
+        /* v8 ignore else */
+        if (accordionElement) {
+            accordionElement.querySelectorAll("button").forEach((elem) => {
+                accordion.on(elem);
+            });
+        }
+
+        // Ensure cleanup after the effect
+        return () => {
+            /* v8 ignore else */
+            if (accordionElement) {
+                accordionElement.querySelectorAll("button").forEach((elem) => {
+                    accordion.off(elem);
+                });
+            }
+        };
+    });
 
     if (typeof level !== "number" || level < 1 || level > 6) {
         throw new Error(`Unrecognized heading level: ${level}`);
@@ -20,6 +44,7 @@ const Accordion = ({ heading, level = 4, children }) => {
         <div
             className="usa-accordion padding-bottom-6"
             style={{ lineHeight: "inherit" }}
+            ref={accordionRef}
         >
             <AccordionHeading className="usa-accordion__heading">
                 <button
