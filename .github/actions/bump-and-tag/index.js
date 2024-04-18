8b991926bbe53dd2d935d6f58b80d5b585923a97
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2024-04-18 12:35:09 -0400

    chore: testing

--- .github/actions/bump-and-tag/index.js
+++ .github/actions/bump-and-tag/index.js
@@ -1,12 +1,12 @@
-const { execSync, spawn } = require('child_process');
-const { existsSync, readFileSync, writeFileSync } = require('fs');
+const { execSync } = require('child_process');
+const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
-// Set working directory if defined in environment variables
-const packageJsonDir = process.env.OPENAPI_DIR || process.env.INPUT_OPENAPI_DIR;
-if (packageJsonDir) {
-    process.chdir(path.join(process.env.GITHUB_WORKSPACE, packageJsonDir));
+// Set working directory based on specified environment variables
+const openApiDir = process.env.OPENAPI_DIR || process.env.INPUT_OPENAPI_DIR;
+if (openApiDir) {
+    process.chdir(path.join(process.env.GITHUB_WORKSPACE, openApiDir));
 }
 console.log('Current working directory:', process.cwd());
 
@@ -14,12 +14,12 @@ const openApiFilePath = path.join(process.cwd(), 'openapi.yml');
 const openapi = getOpenApi(openApiFilePath);
 
 function getOpenApi(filePath) {
-    if (!existsSync(filePath)) throw new Error(`File not found: ${filePath}`);
-    return yaml.load(readFileSync(filePath, 'utf8'));
+    if (!fs.existsSync(filePath)) throw new Error(`File not found: ${filePath}`);
+    return yaml.load(fs.readFileSync(filePath, 'utf8'));
 }
 
 function writeOpenApi(filePath, content) {
-    writeFileSync(filePath, yaml.dump(content), 'utf8');
+    fs.writeFileSync(filePath, yaml.dump(content), 'utf8');
 }
 
 function updateVersion(currentVersion) {
@@ -29,18 +29,17 @@ function updateVersion(currentVersion) {
 }
 
 (async () => {
-    const currentVersion = openapi.info.version;
-    const newVersion = updateVersion(currentVersion);
+    const oldVersion = openapi.info.version;
+    const newVersion = updateVersion(oldVersion);
     openapi.info.version = newVersion;
     writeOpenApi(openApiFilePath, openapi);
 
-    // Configure Git
-    execSync('git config user.name "Automated Version Bump"', { stdio: 'inherit' });
+    execSync('git config user.name "GitHub Action"', { stdio: 'inherit' });
     execSync('git config user.email "action@github.com"', { stdio: 'inherit' });
 
     execSync(`git add ${openApiFilePath}`, { stdio: 'inherit' });
     if (process.env.INPUT_SKIP_COMMIT !== 'true') {
-        execSync(`git commit -m "Bump OpenAPI version from ${currentVersion} to ${newVersion}"`, { stdio: 'inherit' });
+        execSync(`git commit -m "Bump OpenAPI version from ${oldVersion} to ${newVersion}"`, { stdio: 'inherit' });
     }
 
     if (process.env.INPUT_SKIP_TAG !== 'true') {
@@ -58,6 +57,10 @@ function updateVersion(currentVersion) {
         }
     }
 
-    console.log(`::set-output name=newVersion::${newVersion}`);
+    // Update to using environment files if set-output is deprecated
+    fs.writeFileSync(`${process.env.GITHUB_ENV}`, `newVersion=${newVersion}\n`, { flag: 'a' });
+    if (process.env.INPUT_SKIP_TAG !== 'true') {
+        fs.writeFileSync(`${process.env.GITHUB_ENV}`, `newTag=${newTag}\n`, { flag: 'a' });
+    }
 })();
 
