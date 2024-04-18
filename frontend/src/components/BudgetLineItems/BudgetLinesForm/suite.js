4235ddcd6ae42ac91a47c7694a7c40589a661aea
fpigeonjr <frank.pigeonjr@gmail.com>
2024-04-18 09:02:23 -0500

    feat: Add validation for needByDate in BudgetLinesForm
    The needByDate field in the BudgetLinesForm component now has validation to ensure that the date is in the format MM/DD/YYYY. This prevents users from entering invalid dates and improves data integrity.

--- frontend/src/components/BudgetLineItems/BudgetLinesForm/suite.js
+++ frontend/src/components/BudgetLineItems/BudgetLinesForm/suite.js
@@ -18,6 +18,9 @@ const suite = create((data) => {
     test("enteredAmount", "This is required information", () => {
         enforce(data.enteredAmount).isNotEmpty();
     });
+    test("needByDate", "Date must be in this format MM/DD/YYYY", () => {
+        enforce(data.needByDate).matches(/^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/);
+    });
     test("needByDate", "This is required information", () => {
         enforce(data.needByDate).isNotBlank();
     });
