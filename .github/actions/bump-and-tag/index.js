a48c0b30c7d1981f86740cbd87da36aa430a9914
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2024-04-18 12:46:30 -0400

    chore: testing

--- .github/actions/bump-and-tag/index.js
+++ .github/actions/bump-and-tag/index.js
@@ -42,10 +42,12 @@ function updateVersion(currentVersion) {
         execSync(`git commit -m "Bump OpenAPI version from ${oldVersion} to ${newVersion}"`, { stdio: 'inherit' });
     }
 
+    let newTag;
+    const tagPrefix = process.env.INPUT_TAG_PREFIX || '';
+    const tagSuffix = process.env.INPUT_TAG_SUFFIX || '';
     if (process.env.INPUT_SKIP_TAG !== 'true') {
-        const tagPrefix = process.env.INPUT_TAG_PREFIX || '';
-        const tagSuffix = process.env.INPUT_TAG_SUFFIX || '';
-        const newTag = `${tagPrefix}${newVersion}${tagSuffix}`;
+        newTag = `${tagPrefix}${newVersion}${tagSuffix}`;
+        console.log(`Tag Prefix: ${tagPrefix}`);
         console.log(`Creating new tag: ${newTag}`);
         execSync(`git tag ${newTag}`, { stdio: 'inherit' });
         fs.writeFileSync(`${process.env.GITHUB_ENV}`, `newTag=${newTag}\n`, { flag: 'a' });
@@ -53,7 +55,7 @@ function updateVersion(currentVersion) {
 
     if (process.env.INPUT_SKIP_PUSH !== 'true') {
         execSync('git push', { stdio: 'inherit' });
-        if (process.env.INPUT_SKIP_TAG !== 'true') {
+        if (newTag) {
             execSync('git push --tags', { stdio: 'inherit' });
         }
     }
