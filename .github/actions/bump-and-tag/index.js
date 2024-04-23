d7809dec20734fdd7135275987937b1f172c4f3e
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2024-04-23 11:16:51 -0400

    feat: added automated versioning

---
+++ .github/actions/bump-and-tag/index.js
@@ -0,0 +1,48 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+const openApiDir = process.env.INPUT_OPENAPI_DIR || '.';
+process.chdir(path.join(process.env.GITHUB_WORKSPACE, openApiDir));
+console.log('Current working directory:', process.cwd());
+
+const openApiFilePath = path.join(process.cwd(), 'openapi.yml');
+const openapi = yaml.load(fs.readFileSync(openApiFilePath, 'utf8'));
+
+function updateVersion(currentVersion) {
+    const parts = currentVersion.split('.');
+    parts[2] = parseInt(parts[2], 10) + 1; // Increment patch version
+    return parts.join('.');
+}
+
+const oldVersion = openapi.info.version;
+const newVersion = updateVersion(oldVersion);
+openapi.info.version = newVersion;
+fs.writeFileSync(openApiFilePath, yaml.dump(openapi), 'utf8');
+
+execSync('git config user.name "GitHub Action"', { stdio: 'inherit' });
+execSync('git config user.email "action@github.com"', { stdio: 'inherit' });
+
+execSync(`git add ${openApiFilePath}`, { stdio: 'inherit' });
+if (process.env.INPUT_SKIP_COMMIT !== 'true') {
+    execSync(`git commit -m "Bump OpenAPI version from ${oldVersion} to ${newVersion}"`, { stdio: 'inherit' });
+}
+
+const tagPrefix = process.env['INPUT_TAG-PREFIX'] || '';
+const tagSuffix = process.env['INPUT_TAG_SUFFIX'] || '';
+let newTag = `${tagPrefix}${newVersion}${tagSuffix}`;
+console.log(`Tag Prefix: [${tagPrefix}]`);
+console.log(`Creating new tag: ${newTag}`);
+
+if (process.env.INPUT_SKIP_TAG !== 'true') {
+    execSync(`git tag ${newTag}`, { stdio: 'inherit' });
+    console.log(`::set-output name=newTag::${newTag}`);
+}
+
+if (process.env.INPUT_SKIP_PUSH !== 'true') {
+    execSync('git push', { stdio: 'inherit' });
+    if (newTag) {
+        execSync('git push --tags', { stdio: 'inherit' });
+    }
+}
