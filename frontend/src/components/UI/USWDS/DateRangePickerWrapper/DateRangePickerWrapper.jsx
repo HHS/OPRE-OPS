33e4d3d72d455d25954ad7894f0a71b23faecf08
fpigeonjr <frank.pigeonjr@gmail.com>
2024-04-23 08:41:39 -0500

    refactor: Update DateRangePickerWrapper to use React hooks for mounting and unmounting

--- frontend/src/components/UI/USWDS/DateRangePickerWrapper/DateRangePickerWrapper.jsx
+++ frontend/src/components/UI/USWDS/DateRangePickerWrapper/DateRangePickerWrapper.jsx
@@ -5,12 +5,21 @@ import dateRangePicker from "@uswds/uswds/js/usa-date-range-picker";
  * A date range picker helps users select a range between two dates.
  */
 function DateRangePickerWrapper({ id, children }) {
+    const [isMounted, setIsMounted] = React.useState(false);
     const dateRangePickerRef = React.useRef(null);
-    React.useLayoutEffect(() => {
-        const dateRangePickerElement = dateRangePickerRef.current;
-        dateRangePicker.on(dateRangePickerElement);
-        return () => dateRangePicker.off(dateRangePickerElement);
-    });
+
+    React.useEffect(() => {
+        if (isMounted) {
+            const dateRangePickerElement = dateRangePickerRef.current;
+            dateRangePicker.on(dateRangePickerElement);
+            return () => dateRangePicker.off(dateRangePickerElement);
+        }
+    }, [isMounted]); // Depend on the mounting state
+
+    React.useEffect(() => {
+        setIsMounted(true); // Set mounted state after initial render
+        return () => setIsMounted(false); // Clean up on unmount
+    }, []);
 
     return (
         <div
