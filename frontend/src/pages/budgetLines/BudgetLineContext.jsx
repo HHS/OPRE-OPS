66914f3aaa012a7dabded6372971ee2f580f4f39
fpigeonjr <frank.pigeonjr@gmail.com>
2024-04-24 12:25:39 -0500

    chore: Clean up unused code

--- frontend/src/pages/budgetLines/BudgetLineContext.jsx
+++ frontend/src/pages/budgetLines/BudgetLineContext.jsx
@@ -4,20 +4,6 @@ import { budgetLinesReducer, initialState } from "./BudgetLineContext.hooks";
 export const BudgetLinesContext = createContext(null);
 export const BudgetLinesDispatchContext = createContext(null);
 
-// const initialState = {
-//     projects_list: [],
-//     projects_filter: "",
-//     agreements: [],
-//     procurement_shops: [],
-//     cans: [],
-//     existing_budget_lines: [],
-//     selected_project: {},
-//     selected_agreement: {},
-//     selected_can: {},
-//     selected_procurement_shop: {},
-//     wizardSteps: ["Project & Agreement", "Budget Lines", "Review"]
-// };
-
 export function BudgetLinesProvider({ children }) {
     const [state, dispatch] = useReducer(budgetLinesReducer, initialState);
 
@@ -27,34 +13,3 @@ export function BudgetLinesProvider({ children }) {
         </BudgetLinesContext.Provider>
     );
 }
-
-// export function useBudgetLines() {
-//     return useContext(BudgetLinesContext);
-// }
-
-// export function useBudgetLinesDispatch() {
-//     return useContext(BudgetLinesDispatchContext);
-// }
-// export function useSetState(key) {
-//     const dispatch = useContext(BudgetLinesDispatchContext);
-
-//     const setValue = (value) => {
-//         dispatch({ type: "SET_STATE", key, value });
-//     };
-
-//     return setValue;
-// }
-
-// function budgetLinesReducer(state, action) {
-//     switch (action.type) {
-//         case "SET_STATE": {
-//             return { ...state, [action.key]: action.value };
-//         }
-//         case "RESET_TO_INITIAL_STATE": {
-//             return initialState;
-//         }
-//         default: {
-//             throw Error("Unknown action: " + action.type);
-//         }
-//     }
-// }
