f69a4ede833505d74db5ef17697b54d63252f930
fpigeonjr <frank.pigeonjr@gmail.com>
2024-04-30 09:40:30 -0500

    test: add date range picker tests in Cypress

--- frontend/cypress/e2e/components.cy.js
+++ frontend/cypress/e2e/components.cy.js
@@ -111,6 +111,38 @@ describe("DatePicker", () => {
     });
 });
 
+describe("DateRangePicker", () => {
+    it("should display date range picker", () => {
+        cy.visit("/agreements/1/budget-lines");
+        cy.get("#edit").click();
+        cy.get("#pop-start-date").should("exist");
+        cy.get("#pop-end-date").should("exist");
+    });
+
+    it("should error if end date is not before the start date", () => {
+        cy.visit("/agreements/1/budget-lines");
+        cy.get("#edit").click();
+        cy.get("#servicesComponentSelect").select("4");
+        cy.get("#pop-start-date").type("01/01/2020").blur();
+        cy.get("#pop-end-date").type("01/01/2010").blur();
+        cy.get('[data-cy="add-services-component-btn"]').click();
+        cy.get("h2").contains("Services Component 4").should("not.exist");
+    });
+    it('should error if date is not in the format "MM/DD/YYYY"', () => {
+        cy.visit("/agreements/1/budget-lines");
+        cy.get("#edit").click();
+        cy.get("#servicesComponentSelect").select("4");
+        cy.get("#pop-start-date").type("tacocat").blur();
+        cy.get('[data-cy="add-services-component-btn"]').click();
+        cy.get("h2").contains("Services Component 4").should("not.exist");
+        cy.get("#pop-start-date").clear();
+        cy.get("#pop-start-date").type("01/01/2020").blur();
+        cy.get("#pop-end-date").type("tacocat").blur();
+        cy.get('[data-cy="add-services-component-btn"]').click();
+        cy.get("h2").contains("Services Component 4").should("not.exist");
+    });
+});
+
 const fireModal = () => {
     cy.visit("/projects/create");
     cy.get("#cancel").click(); // open modal
