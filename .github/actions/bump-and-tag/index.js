4a2732344252181691a9d23a124d70d9b290fd52
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2024-05-02 15:55:29 -0400

    chore: testing

--- .github/actions/bump-and-tag/index.js
+++ .github/actions/bump-and-tag/index.js
@@ -7,23 +7,12 @@ const yaml = require('js-yaml');
 
 async function run() {
     try {
-        // Get the custom token or fall back to the default GitHub token
-        const token = core.getInput('custom-token', { required: false }) || process.env.GITHUB_TOKEN;
-        if (!token) {
-            throw new Error('GitHub token is not provided');
-        }
-
-        // Navigate to the specified directory where openapi.yml is expected to be
+        const token = core.getInput('custom_token', { required: false }) || process.env.GITHUB_TOKEN;
+        const openApiDir = core.getInput('openapi_dir', { required: false }) || '.';
         const workspace = process.env.GITHUB_WORKSPACE || '.';
-        const openApiDir = core.getInput('OPENAPI_DIR', { required: false }) || '.';
         const fullPath = path.join(workspace, openApiDir);
         process.chdir(fullPath);
-        console.log(`Working directory set to: ${fullPath}`);
-
         const openApiFilePath = path.join(process.cwd(), 'openapi.yml');
-        if (!fs.existsSync(openApiFilePath)) {
-            throw new Error(`The openapi.yml file does not exist at ${openApiFilePath}`);
-        }
         const openapi = yaml.load(fs.readFileSync(openApiFilePath, 'utf8'));
 
         const octokit = github.getOctokit(token);
@@ -35,7 +24,6 @@ async function run() {
             per_page: 10
         });
         const messages = commits.map(commit => commit.commit.message);
-
         const bumpType = determineBumpType(messages);
         const oldVersion = openapi.info.version;
         const newVersion = updateVersion(oldVersion, bumpType);
@@ -45,20 +33,17 @@ async function run() {
         execSync('git config user.name "GitHub Action"');
         execSync('git config user.email "action@github.com"');
         execSync(`git add ${openApiFilePath}`);
-
-        if (!core.getBooleanInput('SKIP_COMMIT', { required: false })) {
+        if (!core.getBooleanInput('skip_commit', { required: false })) {
             execSync(`git commit -m "Bump OpenAPI version from ${oldVersion} to ${newVersion}"`);
         }
-
-        const tagPrefix = core.getInput('TAG_PREFIX', { required: false });
-        const tagSuffix = core.getInput('TAG_SUFFIX', { required: false });
+        const tagPrefix = core.getInput('tag_prefix', { required: false });
+        const tagSuffix = core.getInput('tag_suffix', { required: false });
         const newTag = `${tagPrefix}${newVersion}${tagSuffix}`;
-        if (!core.getBooleanInput('SKIP_TAG', { required: false })) {
+        if (!core.getBooleanInput('skip_tag', { required: false })) {
             execSync(`git tag ${newTag}`);
-            core.setOutput('newTag', newTag);
+            core.setOutput('new_tag', newTag);
         }
-
-        if (!core.getBooleanInput('SKIP_PUSH', { required: false })) {
+        if (!core.getBooleanInput('skip_push', { required: false })) {
             execSync('git push');
             if (newTag) {
                 execSync('git push --tags');
@@ -70,6 +55,7 @@ async function run() {
 }
 
 run();
+
 
 function determineBumpType(messages) {
   const majorWords = core.getInput('major-wording', { required: false }).split(',').map(word => word.trim());
