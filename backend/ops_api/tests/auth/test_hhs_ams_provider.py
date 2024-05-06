ed8220f0e0515939677c6a02601aa3f2ef9ffb74
John DeAngelis <john.deangelis2@gmail.com>
2024-05-06 10:04:34 -0400

    test with app_ctx.config
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/ops_api/tests/auth/test_hhs_ams_provider.py
+++ backend/ops_api/tests/auth/test_hhs_ams_provider.py
@@ -1,58 +1,30 @@
 import json
-import os
-from datetime import timedelta
 from unittest.mock import MagicMock
 
 import pytest
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
-from flask import Config
 
 from ops_api.ops.auth.authentication_provider.hhs_ams_provider import HhsAmsProvider
 
 
-@pytest.fixture()
-def config() -> Config:
-    config = Config({})
-    config.from_mapping(
-        {
-            "AUTHLIB_OAUTH_CLIENTS": {
-                "hhs_ams": {
-                    "client_id": "client_id",
-                    "server_metadata_url": "https://some.endpoint.test",
-                    "client_kwargs": {
-                        "scope": "openid",
-                    },
-                    "redirect_uri": "https://some.endpoint.test",
-                    "aud": "https://some.endpoint.test",
-                    "token_endpoint": "https://some.endpoint.test",
-                    "user_info_url": "https://some.endpoint.test",
-                }
-            },
-            "JWT_ACCESS_TOKEN_EXPIRES": timedelta(minutes=300),
-            "JWT_PRIVATE_KEY": os.getenv("JWT_PRIVATE_KEY"),
-            "JWT_PUBLIC_KEY_PATH": "./static/public.pem",
-        }
-    )
-    return config
+@pytest.mark.usefixtures("app_ctx")
+def test_create_provider(app):
+    provider = HhsAmsProvider("hhsams", app.config)
+    assert provider.provider_name == "hhsams"
+    assert provider.config == app.config
+    assert provider.client_id == app.config["AUTHLIB_OAUTH_CLIENTS"]["hhsams"]["client_id"]
+    assert provider.server_metadata_url == app.config["AUTHLIB_OAUTH_CLIENTS"]["hhsams"]["server_metadata_url"]
+    assert provider.scope == app.config["AUTHLIB_OAUTH_CLIENTS"]["hhsams"]["client_kwargs"]["scope"]
+    assert provider.redirect_uri == app.config["AUTHLIB_OAUTH_CLIENTS"]["hhsams"]["redirect_uri"]
+    assert provider.JWT_ACCESS_TOKEN_EXPIRES == app.config["JWT_ACCESS_TOKEN_EXPIRES"]
+    assert provider.aud == app.config["AUTHLIB_OAUTH_CLIENTS"]["hhsams"]["aud"]
+    assert provider.token_endpoint == app.config["AUTHLIB_OAUTH_CLIENTS"]["hhsams"]["token_endpoint"]
+    assert provider.user_info_url == app.config["AUTHLIB_OAUTH_CLIENTS"]["hhsams"]["user_info_url"]
 
 
-def test_create_provider(config):
-    provider = HhsAmsProvider("hhs_ams", config)
-    assert provider.client_id == "client_id"
-    assert provider.provider_name == "hhs_ams"
-    assert provider.config == config
-    assert provider.client_id == config["AUTHLIB_OAUTH_CLIENTS"]["hhs_ams"]["client_id"]
-    assert provider.server_metadata_url == config["AUTHLIB_OAUTH_CLIENTS"]["hhs_ams"]["server_metadata_url"]
-    assert provider.scope == config["AUTHLIB_OAUTH_CLIENTS"]["hhs_ams"]["client_kwargs"]["scope"]
-    assert provider.redirect_uri == config["AUTHLIB_OAUTH_CLIENTS"]["hhs_ams"]["redirect_uri"]
-    assert provider.JWT_ACCESS_TOKEN_EXPIRES == config["JWT_ACCESS_TOKEN_EXPIRES"]
-    assert provider.aud == config["AUTHLIB_OAUTH_CLIENTS"]["hhs_ams"]["aud"]
-    assert provider.token_endpoint == config["AUTHLIB_OAUTH_CLIENTS"]["hhs_ams"]["token_endpoint"]
-    assert provider.user_info_url == config["AUTHLIB_OAUTH_CLIENTS"]["hhs_ams"]["user_info_url"]
-
-
-def test_decode_user(config, mocker):
+@pytest.mark.usefixtures("app_ctx")
+def test_decode_user(app, mocker):
     # Generate a public/private key pair for testing
     private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     public_key = (
@@ -82,7 +54,7 @@ def test_decode_user(config, mocker):
         "aud": "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO",
         "jti": "f7f3b3b7-3b7d-4b7b-8b7b-7b7b7b7b7b7b",
         "exp": 1632710400,
-        "sso": "hhs_ams",
+        "sso": "hhsams",
         "name": "John Doe",
         "email": "john.doe@example.com",
     }
@@ -91,7 +63,7 @@ def test_decode_user(config, mocker):
     jws = jose_jwt.encode(header={"alg": "RS256"}, payload=payload, key=private_key)
 
     # Test the decode_user method
-    provider = HhsAmsProvider("hhs_ams", config)
+    provider = HhsAmsProvider("hhsams", app.config)
     claims = provider.decode_user(jws)
 
     assert claims["iss"] == "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO"
@@ -99,13 +71,13 @@ def test_decode_user(config, mocker):
     assert claims["aud"] == "https://sso-stage.acf.hhs.gov/auth/realms/ACF-SSO"
     assert claims["jti"] == "f7f3b3b7-3b7d-4b7b-8b7b-7b7b7b7b7b7b"
     assert claims["exp"] == 1632710400
-    assert claims["sso"] == "hhs_ams"
+    assert claims["sso"] == "hhsams"
     assert claims["name"] == "John Doe"
     assert claims["email"] == "john.doe@example.com"
 
 
 @pytest.mark.usefixtures("app_ctx")
-def test_authenticate(app, config, mocker):
+def test_authenticate(app, mocker):
     # mock member method fetch_token on the HhsAmsProvider class
     mock_fetch_token = mocker.patch(
         "ops_api.ops.auth.authentication_provider.hhs_ams_provider.HhsAmsProvider.fetch_token"
