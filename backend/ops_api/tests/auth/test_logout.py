22de1761305e20ee3c59a17bc99be69883e807bb
John DeAngelis <john.deangelis2@gmail.com>
2024-05-08 15:48:04 -0400

    add re-login to test
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/ops_api/tests/auth/test_logout.py
+++ backend/ops_api/tests/auth/test_logout.py
@@ -37,6 +37,29 @@ def test_logout(app, client, loaded_db, mocker):
     assert not user_sessions[0].is_active
     assert user_sessions[0].last_active_at != last_active_at
 
+    # login again
+    login_response = client.post("/auth/login/", json={"provider": "fakeauth", "code": "admin_user"})
+    assert login_response.status_code == 200
+    access_token = login_response.json["access_token"]
+    refresh_token = login_response.json["refresh_token"]
+
+    stmt = select(UserSession).where(UserSession.user_id == user.id).order_by(UserSession.created_on.desc())
+    user_sessions = loaded_db.execute(stmt).scalars().all()
+    last_active_at = user_sessions[0].last_active_at
+    assert len(user_sessions) == 2
+    assert user_sessions[0].is_active
+    assert user_sessions[0].access_token == access_token
+    assert user_sessions[0].refresh_token == refresh_token
+    assert user_sessions[0].user_id == user.id
+    assert user_sessions[0].ip_address == "127.0.0.1"
+    assert last_active_at is not None
+    assert not user_sessions[1].is_active
+    assert user_sessions[1].last_active_at != last_active_at
+    assert user_sessions[1].access_token != access_token
+    assert user_sessions[1].refresh_token != refresh_token
+    assert user_sessions[1].user_id == user.id
+    assert user_sessions[1].ip_address == "127.0.0.1"
+
     # cleanup
     loaded_db.execute(text("DELETE FROM user_session"))
     loaded_db.commit()
