2cf02547c034c0aa8d02fd6f5835f1375dc0cd71
John DeAngelis <john.deangelis2@gmail.com>
2024-05-16 13:30:19 -0400

    add @check_user_session to notifications.py
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/ops_api/ops/resources/notifications.py
+++ backend/ops_api/ops/resources/notifications.py
@@ -15,7 +15,7 @@ from typing_extensions import override
 
 from models import Notification, OpsEventType, User
 from ops_api.ops.auth.auth_types import Permission, PermissionType
-from ops_api.ops.auth.decorators import is_authorized
+from ops_api.ops.auth.decorators import check_user_session, is_authorized
 from ops_api.ops.base_views import BaseItemAPI, BaseListAPI, handle_api_error
 from ops_api.ops.utils.events import OpsEventHandler
 from ops_api.ops.utils.query_helpers import QueryHelper
@@ -203,9 +203,9 @@ class NotificationListAPI(BaseListAPI):
         stmt = query_helper.get_stmt()
         return stmt
 
-    @override
-    @is_authorized(PermissionType.GET, Permission.NOTIFICATION)
     @handle_api_error
+    @is_authorized(PermissionType.GET, Permission.NOTIFICATION)
+    @check_user_session
     def get(self) -> Response:
         errors = self._get_input_schema.validate(request.args)
 
