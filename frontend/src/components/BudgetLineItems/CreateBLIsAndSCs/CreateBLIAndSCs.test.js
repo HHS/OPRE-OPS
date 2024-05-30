9118d9d5a7f484dc4b2ea1ea85a56dbcb750315c
Frank Pigeon Jr <fpigeon@flexion.us>
2024-05-30 18:23:01 -0500

    refactor: Update CreateBLIsAndSCs test to use agreement data from tests

--- frontend/src/components/BudgetLineItems/CreateBLIsAndSCs/CreateBLIAndSCs.test.js
+++ frontend/src/components/BudgetLineItems/CreateBLIsAndSCs/CreateBLIAndSCs.test.js
@@ -4,38 +4,8 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import CreateBLIsAndSCs from "./CreateBLIsAndSCs"; // replace with your component
 import store from "../../../store";
+import { agreement } from "../../../tests/data";
 
-const agreementMock = {
-    id: 1,
-    name: "Agreement 1",
-    description: "Description 1",
-    project_officer: "John Doe",
-    period_of_performance_start: "2022-01-01",
-    period_of_performance_end: "2022-12-31",
-    budget_line_items: [
-        {
-            id: 1,
-            description: "Budget Line 1",
-            comments: "Comments 1",
-            can: {
-                id: 1,
-                code: "CAN 1",
-                name: "CAN 1"
-            }
-        },
-        {
-            id: 2,
-            description: "Budget Line 2",
-            comments: "Comments 2",
-            can: {
-                id: 2,
-                code: "CAN 2",
-                name: "CAN 2"
-            }
-        }
-    ],
-    procurement_shop: 1
-};
 const wizardSteps = ["Project", "Agreement", "Budget Lines"];
 
 test("renders without crashing", () => {
@@ -43,10 +13,10 @@ test("renders without crashing", () => {
         <Provider store={store}>
             <BrowserRouter>
                 <CreateBLIsAndSCs
-                    budgetLines={agreementMock.budget_line_items}
-                    selectedResearchProject={agreementMock}
-                    selectedAgreement={agreementMock}
-                    selectedProcurementShop={agreementMock.procurement_shop}
+                    budgetLines={agreement.budget_line_items}
+                    selectedResearchProject={agreement}
+                    selectedAgreement={agreement}
+                    selectedProcurementShop={agreement.procurement_shop}
                     isEditMode={true}
                     continueBtnText="Save Changes"
                     wizardSteps={wizardSteps}
@@ -55,6 +25,8 @@ test("renders without crashing", () => {
                     isReviewMode={false}
                     canUserEditBudgetLines={false}
                     setIsEditMode={() => {}}
+                    includeDrafts={true}
+                    setIncludeDrafts={() => {}}
                 />
             </BrowserRouter>
         </Provider>
