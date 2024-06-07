db31734722216f6239e93a10d33935e97bde39a6
Frank Pigeon Jr <fpigeon@flexion.us>
2024-06-07 14:50:37 -0500

    chore: Update TablePageLayout props to make FilterTags, FilterButton, and TableSection optional

--- frontend/src/components/Layouts/TablePageLayout/TablePageLayout.jsx
+++ frontend/src/components/Layouts/TablePageLayout/TablePageLayout.jsx
@@ -13,9 +13,9 @@ import icons from "../../../uswds/img/sprite.svg";
  * @param {string} props.subtitle - The subtitle to display.
  * @param {string} props.details - The details to display.
  * @param {React.ReactNode} props.TabsSection - The tabs to display.
- * @param {React.ReactNode} props.FilterTags - The filter tags to display.
- * @param {React.ReactNode} props.FilterButton - The filter button to display.
- * @param {React.ReactNode} props.TableSection - The table to display.
+ * @param {React.ReactNode} [props.FilterTags] - The filter tags to display.
+ * @param {React.ReactNode} [props.FilterButton] - The filter button to display.
+ * @param {React.ReactNode} [props.TableSection] - The table to display.
  * @param {React.ReactNode} [props.SummaryCardsSection] - The summary cards to display.
  * @param {string} props.buttonText - The text to display on the button.
  * @param {string} props.buttonLink - The link to navigate to when the button is clicked.
@@ -27,10 +27,10 @@ export const TablePageLayout = ({
     subtitle,
     details,
     TabsSection,
-    FilterTags,
+    FilterTags = null,
     SummaryCardsSection,
-    FilterButton,
-    TableSection,
+    FilterButton = null,
+    TableSection = null,
     buttonText,
     buttonLink
 }) => {
@@ -79,9 +79,9 @@ TablePageLayout.propTypes = {
     subtitle: PropTypes.string.isRequired,
     details: PropTypes.string.isRequired,
     TabsSection: PropTypes.node.isRequired,
-    FilterTags: PropTypes.node.isRequired,
-    FilterButton: PropTypes.node.isRequired,
-    TableSection: PropTypes.node.isRequired,
+    FilterTags: PropTypes.node,
+    FilterButton: PropTypes.node,
+    TableSection: PropTypes.node,
     SummaryCardsSection: PropTypes.node,
     buttonText: PropTypes.string.isRequired,
     buttonLink: PropTypes.string.isRequired
