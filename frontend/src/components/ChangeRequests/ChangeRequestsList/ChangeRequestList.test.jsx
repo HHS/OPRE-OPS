29056bb4a060051a13df61c7bf262d394da357ef
Frank Pigeon Jr <fpigeon@flexion.us>
2024-06-10 09:58:45 -0500

    test: adds BrowserRouter to fix tests

--- frontend/src/components/ChangeRequests/ChangeRequestsList/ChangeRequestList.test.jsx
+++ frontend/src/components/ChangeRequests/ChangeRequestsList/ChangeRequestList.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import { vi } from "vitest";
-import ChangeRequestList from "./ChangeRequestsList";
-import { useGetChangeRequestsListQuery, useGetAgreementByIdQuery } from "../../../api/opsAPI";
+import { useGetAgreementByIdQuery, useGetChangeRequestsListQuery } from "../../../api/opsAPI";
 import { changeRequests } from "../../../tests/data";
+import ChangeRequestList from "./ChangeRequestsList";
 // import TestApplicationContext from "../../../applicationContext/TestApplicationContext";
 
 // const mockFn = TestApplicationContext.helpers().mockFn;
@@ -12,13 +13,21 @@ describe("ChangeRequestList", () => {
     it("renders without any change requests", () => {
         useGetChangeRequestsListQuery.mockReturnValue({ data: {} });
         useGetAgreementByIdQuery.mockReturnValue({ data: {} });
-        render(<ChangeRequestList />);
+        render(
+            <BrowserRouter>
+                <ChangeRequestList />
+            </BrowserRouter>
+        );
         expect(screen.getByText(/no changes/i)).toBeInTheDocument();
     });
     it("renders with change requests", () => {
         useGetChangeRequestsListQuery.mockReturnValue({ data: changeRequests });
         useGetAgreementByIdQuery.mockReturnValue({ data: { display_name: "Agreement Name" } });
-        render(<ChangeRequestList />);
+        render(
+            <BrowserRouter>
+                <ChangeRequestList />
+            </BrowserRouter>
+        );
         const heading = screen.getByRole("heading", { name: "DEBUG CODE" });
         expect(heading).toBeInTheDocument();
     });
