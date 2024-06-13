66436583d7308cddf09b14581c9f714157089fb5
Frank Pigeon Jr <fpigeon@flexion.us>
2024-06-13 15:03:33 -0500

    test: Refactor ReviewCard component and tests

--- frontend/src/components/ChangeRequests/ReviewCard/ReviewCard.test.jsx
+++ frontend/src/components/ChangeRequests/ReviewCard/ReviewCard.test.jsx
@@ -2,11 +2,21 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 import { vi } from "vitest";
-import { useGetAgreementByIdQuery } from "../../../api/opsAPI";
+import { useGetAgreementByIdQuery, useGetBudgetLineItemQuery, useGetCansQuery } from "../../../api/opsAPI";
+import { useGetAgreementName, useGetBLIStatus } from "../../../hooks/lookup.hooks";
+import { agreement, budgetLine } from "../../../tests/data";
 import ReviewCard from "./ReviewCard";
 
 vi.mock("../../../api/opsAPI");
+vi.mock("../../../hooks/lookup.hooks", () => ({
+    useGetNameForCanId: vi.fn(),
+    useGetBLIStatus: vi.fn(),
+    useGetAgreementName: vi.fn()
+}));
+
 describe("ReviewCard", () => {
+    useGetBLIStatus.mockReturnValue("Draft");
+    useGetAgreementName.mockReturnValue("Agreement Name");
     const initialProps = {
         type: "Budget Change",
         agreementId: 1,
@@ -14,8 +24,10 @@ describe("ReviewCard", () => {
         requesterName: "Jane Doe",
         requestDate: "2024-06-12T21:25:25.744930Z"
     };
-    it("should render the ReviewCard component", () => {
-        useGetAgreementByIdQuery.mockReturnValue({ data: { display_name: "TBD" } });
+    it("should render the ReviewCard component", async () => {
+        useGetAgreementByIdQuery.mockReturnValue({ data: { agreement } });
+        useGetBudgetLineItemQuery.mockReturnValue({ data: { budgetLine } });
+        useGetCansQuery.mockReturnValue({ data: [agreement.budget_line_items[0].can] });
         render(
             <BrowserRouter>
                 <ReviewCard {...initialProps}>
@@ -23,8 +35,9 @@ describe("ReviewCard", () => {
                 </ReviewCard>
             </BrowserRouter>
         );
+
         const type = screen.getByText("Budget Change");
-        const agreementName = screen.getByText("TBD");
+        const agreementName = await screen.findByText("Agreement Name");
         const requesterName = screen.getByText("Jane Doe");
         const requestDate = screen.getByText("June 12, 2024");
         const actionIcons = screen.queryByText("icons");
@@ -37,7 +50,11 @@ describe("ReviewCard", () => {
     });
     it("should render the ReviewCard component with action icons", async () => {
         const user = userEvent.setup();
-        useGetAgreementByIdQuery.mockReturnValue({ data: { display_name: "TBD" } });
+
+        useGetAgreementName.mockReturnValue("Agreement Name");
+        useGetAgreementByIdQuery.mockReturnValue({ data: { agreement } });
+        useGetBudgetLineItemQuery.mockReturnValue({ data: { budgetLine } });
+        useGetCansQuery.mockReturnValue({ data: [agreement.budget_line_items[0].can] });
         render(
             <BrowserRouter>
                 <ReviewCard
