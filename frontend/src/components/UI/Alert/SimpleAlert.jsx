39064ee63b07ecde0a6211146a84a6b0089ee4ba
Jonathan Nalley <jnalley@flexion.us>
2024-06-17 21:13:17 -0400

    going back to a simple in-body disclaimer while keeping default exports

--- frontend/src/components/UI/Alert/SimpleAlert.jsx
+++ frontend/src/components/UI/Alert/SimpleAlert.jsx
@@ -7,12 +7,11 @@ import { faClose } from "@fortawesome/free-solid-svg-icons";
  * @param {Object} props - The props object.
  * @param {React.ReactNode} [props.children] - The child elements to render inside the alert. - optional
  * @param {string} props.heading - The heading text for the alert.
- * @param {string} [props.message] - The message text for the alert. - optional
- * @param {("success" | "warning" | "error")} props.type - The type of alert to display.
+ * @param {string} props.message - The message text for the alert.
+ * @param {string} props.type - The type of alert to display.
  * @param {boolean} [props.isClosable] - Whether the alert is closable. - optional
  * @param {boolean} [props.isAlertVisible] - Whether the alert is visible.
  * @param {Function} [props.setIsAlertVisible] - The function to set the alert visibility.
- * @param {string} [props.className] - The class name for the alert. - optional
  * @returns {JSX.Element | null} - The rendered alert component.
  */
 const SimpleAlert = ({
@@ -22,10 +21,9 @@ const SimpleAlert = ({
     type,
     isClosable = false,
     isAlertVisible = true,
-    setIsAlertVisible = () => {},
-    className
+    setIsAlertVisible = () => {}
 }) => {
-    let classNames = `usa-alert margin-left-neg-4 margin-right-neg-4 ${className}`;
+    let classNames = "usa-alert margin-left-neg-4 margin-right-neg-4";
 
     switch (type) {
         case "success":
@@ -76,8 +74,7 @@ SimpleAlert.propTypes = {
     type: PropTypes.oneOf(["success", "warning", "error"]).isRequired,
     isClosable: PropTypes.bool,
     isAlertVisible: PropTypes.bool,
-    setIsAlertVisible: PropTypes.func,
-    className: PropTypes.string
+    setIsAlertVisible: PropTypes.func
 };
 
 export default SimpleAlert;
--- frontend/src/components/UI/Footer/AgencyInfo.jsx
+++ frontend/src/components/UI/Footer/AgencyInfo.jsx
@@ -1,8 +1,37 @@
 function AgencyInfo() {
     return (
         <div className="grid-container">
-            <div className="grid-row">
-                <p className="usa-footer__logo-heading">OPRE Portfolio Management System</p>
+            <div className="grid-row grid-gap">
+                <div
+                    className="
+              usa-footer__logo
+              grid-row
+              mobile-lg:grid-col-6 mobile-lg:grid-gap-2
+            "
+                >
+                    <div className="mobile-lg:grid-col-auto">
+                        <img
+                            className="usa-footer__logo-img"
+                            src="/assets/img/logo-iomg.png"
+                            alt=""
+                        />
+                    </div>
+                    <div className="mobile-lg:grid-col-auto">
+                        <p className="usa-footer__logo-heading">OPRE Portfolio Management System</p>
+                    </div>
+                </div>
+                <div className="usa-footer__contact-links mobile-lg:grid-col-6">
+                    <div className="usa-footer__social-links grid-row grid-gap-1"></div>
+                    <p className="usa-footer__contact-heading"> </p>
+                    <address className="usa-footer__address">
+                        <div className="usa-footer__contact-info grid-row grid-gap">
+                            <div className="grid-col-auto">
+                            </div>
+                            <div className="grid-col-auto">
+                            </div>
+                        </div>
+                    </address>
+                </div>
             </div>
         </div>
     );
