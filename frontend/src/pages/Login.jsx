0ecbc6c99fc50f4d51f335c6d9b0fb083e4adb37
Frank Pigeon Jr <fpigeon@flexion.us>
2024-06-20 17:12:48 -0500

    refactor: Update login page layout and remove redundant headings

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -41,17 +41,12 @@ function Login() {
                 <main id="main-content">
                     <div className="bg-base-lightest">
                         <section className="grid-container usa-section">
-                            <div className="grid-row margin-x-neg-205 margin-bottom-7 flex-justify-center">
+                            <div className="grid-row margin-x-neg-205 margin-bottom-6 flex-justify-center">
                                 <div className="grid-col-6 padding-x-205 margin-bottom-4">
-                                    <h1 className="desktop:display-none font-sans-lg margin-bottom-4 tablet:margin-top-neg-3">
-                                        Access to OPS requires proper authentication.
-                                    </h1>
                                     <MultiAuthSection />
                                 </div>
                                 <div className="grid-col-6 padding-x-205">
-                                    <h2 className="display-none desktop:display-block">
-                                        Access to OPS requires proper authentication.
-                                    </h2>
+                                    <h2>Access to OPS requires proper authentication.</h2>
                                     <div className="border-top border-base-lighter margin-top-3 padding-top-1">
                                         <h2>Are you a federal employee?</h2>
                                         <div className="usa-prose">
