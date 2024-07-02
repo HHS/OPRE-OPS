7e13028f12d38f16ba1d47a64a21ce2e131f26e2
Frank Pigeon Jr <fpigeon@flexion.us>
2024-07-02 09:46:49 -0500

    docs: Update DocumentView component props
    In JSDoc, the convention is to use uppercase for object types (like Object, Array, Function, etc.) and lowercase for primitive types (like string, number, boolean, etc.).

--- frontend/src/components/Agreements/Documents/DocumentView.jsx
+++ frontend/src/components/Agreements/Documents/DocumentView.jsx
@@ -2,12 +2,13 @@ import PropTypes from "prop-types";
 import { document } from "../../../tests/data";
 import { DocumentCollectionHeader } from "./DocumentCollectionHeader";
 import DocumentCollectionView from "./DocumentCollectionView";
+
 /**
  * Renders the document collection view
- * @param {object} props - The component props.
- * @param {boolean} props.isEditMode - Whether the edit mode is on.
- * @param {function} props.setIsEditMode - The function to set the edit mode.
  * @component
+ * @param {Object} props - The component props.
+ * @param {boolean} props.isEditMode - Whether the edit mode is on.
+ * @param {Function} props.setIsEditMode - The function to set the edit mode.
  * @returns {JSX.Element} - The rendered component
  */
 const DocumentView = ({ isEditMode, setIsEditMode }) => {
