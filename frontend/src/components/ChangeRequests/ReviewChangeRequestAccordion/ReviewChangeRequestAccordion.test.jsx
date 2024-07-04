dd76a24044967977a64ae0a3e69d6f54e19da5db
Frank Pigeon Jr <fpigeon@flexion.us>
2024-07-04 11:32:08 -0500

    test: adds tests for status changes

--- frontend/src/components/ChangeRequests/ReviewChangeRequestAccordion/ReviewChangeRequestAccordion.test.jsx
+++ frontend/src/components/ChangeRequests/ReviewChangeRequestAccordion/ReviewChangeRequestAccordion.test.jsx
@@ -1,15 +1,39 @@
 import { render, screen } from "@testing-library/react";
-import ReviewChangeRequestAccordion from "./ReviewChangeRequestAccordion";
 import { changeRequests } from "../../../tests/data";
+import ReviewChangeRequestAccordion from "./ReviewChangeRequestAccordion";
 
 describe("ReviewChangesAccordion", () => {
     const initialProps = {
         changeType: "budget change",
         changeRequests: changeRequests
     };
-    it("should render the review accordion", () => {
+    it("should render the review accordion for budget change", () => {
         render(<ReviewChangeRequestAccordion {...initialProps} />);
         expect(screen.getByText(/review changes/i)).toBeInTheDocument();
         expect(screen.getByText(/budget changes/i)).toBeInTheDocument();
+    });
+    it("should render the review accordion for status change to planned", () => {
+        render(
+            <ReviewChangeRequestAccordion
+                {...initialProps}
+                changeType="status change"
+                statusChangeTo="planned"
+            />
+        );
+        expect(screen.getByText(/review changes/i)).toBeInTheDocument();
+        expect(screen.getByText(/status changes/i)).toBeInTheDocument();
+        expect(screen.getByText(/planned/i)).toBeInTheDocument;
+    });
+    it("should render the review accordion for status change to executing", () => {
+        render(
+            <ReviewChangeRequestAccordion
+                {...initialProps}
+                changeType="status change"
+                statusChangeTo="executing"
+            />
+        );
+        expect(screen.getByText(/review changes/i)).toBeInTheDocument();
+        expect(screen.getByText(/status changes/i)).toBeInTheDocument();
+        expect(screen.getByText(/executing/i)).toBeInTheDocument;
     });
 });
