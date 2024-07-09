9d322ffcb0453380f12329ee1cdc7bc0dcdca5d5
John DeAngelis <john.deangelis2@gmail.com>
2024-07-09 14:10:31 -0400

    remove unneeded roles; fix e2e login test; use current_timestamp in SQL INSERT
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/data_tools/initial_data/013-procurement_shop.sql
+++ backend/data_tools/initial_data/013-procurement_shop.sql
@@ -1,4 +1,4 @@
-INSERT INTO ops.procurement_shop (id, name, abbr, fee, created_by, updated_by, created_on, updated_on) VALUES (1, 'Product Service Center', 'PSC', 0, null, null, '2024-07-08 17:49:15.923946', '2024-07-08 17:49:15.923946');
-INSERT INTO ops.procurement_shop (id, name, abbr, fee, created_by, updated_by, created_on, updated_on) VALUES (2, 'Government Contracting Services', 'GCS', 0, null, null, '2024-07-08 17:49:15.946423', '2024-07-08 17:49:15.946423');
-INSERT INTO ops.procurement_shop (id, name, abbr, fee, created_by, updated_by, created_on, updated_on) VALUES (3, 'National Institute of Health', 'NIH', 0.005, null, null, '2024-07-08 17:49:15.949201', '2024-07-08 17:49:15.949201');
-INSERT INTO ops.procurement_shop (id, name, abbr, fee, created_by, updated_by, created_on, updated_on) VALUES (4, 'Interior Business Center', 'IBC', 0.048, null, null, '2024-07-08 17:49:15.952820', '2024-07-08 17:49:15.952820');
+INSERT INTO ops.procurement_shop (id, name, abbr, fee, created_on, updated_on) VALUES (1, 'Product Service Center', 'PSC', 0, current_timestamp, current_timestamp);
+INSERT INTO ops.procurement_shop (id, name, abbr, fee, created_on, updated_on) VALUES (2, 'Government Contracting Services', 'GCS', 0, current_timestamp, current_timestamp);
+INSERT INTO ops.procurement_shop (id, name, abbr, fee, created_on, updated_on) VALUES (3, 'National Institute of Health', 'NIH', 0.005, current_timestamp, current_timestamp);
+INSERT INTO ops.procurement_shop (id, name, abbr, fee, created_on, updated_on) VALUES (4, 'Interior Business Center', 'IBC', 0.048, current_timestamp, current_timestamp);
