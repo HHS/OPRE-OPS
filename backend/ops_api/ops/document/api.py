9fc052568e508ddaacac2bf3182fc42c5d7ada4d
maiyerlee <mlee@flexion.us>
2024-07-10 13:23:34 -0500

    Use decorator with new document permission on endpoints

--- backend/ops_api/ops/document/api.py
+++ backend/ops_api/ops/document/api.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, Response, current_app, request
 
+from ops_api.ops.auth.auth_types import Permission, PermissionType
+from ops_api.ops.auth.decorators import is_authorized
 from ops_api.ops.document.document_gateway import DocumentGateway
 from ops_api.ops.document.schema import DocumentRequestSchema, DocumentResponseSchema, GetDocumentRequestSchema
 from ops_api.ops.document.service import DocumentService
@@ -10,24 +12,25 @@ documents_bp = Blueprint("documents", __name__)
 
 
 @documents_bp.route("/documents/", methods=["GET"])
+@is_authorized(PermissionType.GET, Permission.UPLOAD_DOCUMENT)
 def get_documents_by_agreement_id():
 
     # Use schema to validate request data and get agreement_id
     request_schema = GetDocumentRequestSchema()
     request_data = request_schema.dump(request_schema.load(request.args))
-    agreement_id = request_data["agreement_id"]
 
     # Call document service to get documents by agreement_id
     document_service = DocumentService(DocumentGateway(current_app.config))
-    documents = document_service.get_documents_by_agreement_id(agreement_id)
+    results = document_service.get_documents_by_agreement_id(request_data["agreement_id"])
 
     # Prepare response
     response_schema = DocumentResponseSchema(many=True)
-    response_data = response_schema.dump({"documents": documents})
-    return make_response_with_headers(data=response_data, status_code=200)
+    response_data = response_schema.dump([row[0] for row in results])
+    return make_response_with_headers(data=response_data)
 
 
 @documents_bp.route("/documents/", methods=["POST"])
+@is_authorized(PermissionType.POST, Permission.UPLOAD_DOCUMENT)
 def create_document() -> Response:
     # Use schema to validate request data and get document data
     request_data = request.get_json()
@@ -36,10 +39,9 @@ def create_document() -> Response:
 
     # Call document service to add the document
     document_service = DocumentService(DocumentGateway(current_app.config))
-    document = document_service.create_document(document_data)
+    result = document_service.create_document(document_data)
 
     # Prepare response
     response_schema = DocumentResponseSchema()
-    response_data = response_schema.dump(document)
-
-    return make_response_with_headers(response_data, 201)
+    response_data = response_schema.dump(result)
+    return make_response_with_headers(data=response_data, status_code=201)
