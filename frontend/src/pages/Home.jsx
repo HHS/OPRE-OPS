f5d76084f776e10127a7cd433f8b0ea22084f9c1
Frank Pigeon Jr <fpigeon@flexion.us>
2024-07-11 09:38:14 -0500

    Update Home.jsx
    update content

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <div className="display-flex flex-justify-center">
                 <RoundedBox className="padding-x-2 margin-top-2 display-inline-block text-center">
                     <h1>This is the OPRE OPS system prototype</h1>
-                    <p>⚠️Tread with caution</p>
+                    <p>⚠️Something New</p>
                 </RoundedBox>
             </div>
         </App>
