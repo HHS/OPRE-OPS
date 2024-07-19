c9a695c6df31c684eca64e287c67f466e872d3f8
Frank Pigeon Jr <fpigeon@flexion.us>
2024-07-19 17:17:24 -0500

    feat: makes api call to render CR total
    - adds unit-tests
    -

--- frontend/src/pages/agreements/list/AgreementsTabs.jsx
+++ frontend/src/pages/agreements/list/AgreementsTabs.jsx
@@ -1,6 +1,8 @@
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import styles from "../../../components/Portfolios/PortfolioTabsSection/PortfolioTabsSection.module.scss";
 import TabsSection from "../../../components/UI/TabsSection";
+import { useChangeRequestTotal } from "../../../hooks/useChangeRequests.hooks";
 import tabStyles from "./AgreementTabs.module.css";
 
 /**
@@ -8,49 +10,57 @@ import tabStyles from "./AgreementTabs.module.css";
  * @returns {JSX.Element} - The procurement shop select element.
  */
 
-/* TODO: make api call to replace hard-coded value */
+const paths = [
+    {
+        name: "",
+        label: "All Agreements"
+    },
+    {
+        name: "?filter=my-agreements",
+        label: "My Agreements"
+    },
+    {
+        name: "?filter=change-requests",
+        label: "For Review"
+    }
+];
 
-export const AgreementTabs = () => {
-    const location = useLocation();
-    const selected = `font-sans-2xs text-bold ${styles.listItemSelected}`;
-    const notSelected = `font-sans-2xs text-bold ${styles.listItemNotSelected}`;
-    const paths = [
-        {
-            name: "",
-            label: "All Agreements"
-        },
-        {
-            name: "?filter=my-agreements",
-            label: "My Agreements"
-        },
-        {
-            name: "?filter=change-requests",
-            label: "For Review"
-        }
-    ];
+const AgreementTabs = () => {
+    const { search } = useLocation();
+    const changeRequestsTotal = useChangeRequestTotal();
+    /**
+     * @param {string} queryString - The query string of the tab.
+     */
+    const getClassName = (queryString) =>
+        `font-sans-2xs text-bold ${search === queryString ? styles.listItemSelected : styles.listItemNotSelected}`;
 
-    const links = paths.map((path) => {
-        const queryString = `${path.name}`;
+    const renderLinks = () =>
+        paths.map((path) => {
+            const queryString = `${path.name}`;
+            const hasChangeRequestsOnReviewTab = path.label === "For Review" && changeRequestsTotal > 0;
 
-        return (
-            <>
-                <Link
-                    to={queryString}
-                    className={location.search === queryString ? selected : notSelected}
-                    key={queryString}
-                >
-                    {path.label}
-                </Link>
-                <span className={`margin-left-neg-1 position-absolute bottom-2 ${tabStyles.notificationCircle}`}>
-                    5
-                </span>
-            </>
-        );
-    });
+            return (
+                <React.Fragment key={queryString}>
+                    <Link
+                        to={queryString}
+                        className={getClassName(queryString)}
+                    >
+                        {path.label}
+                    </Link>
+                    {hasChangeRequestsOnReviewTab && (
+                        <span
+                            className={`margin-left-neg-1 position-absolute bottom-2 ${tabStyles.notificationCircle}`}
+                        >
+                            {changeRequestsTotal}
+                        </span>
+                    )}
+                </React.Fragment>
+            );
+        });
 
     return (
         <TabsSection
-            links={links}
+            links={renderLinks()}
             label="Agreements Tabs Section"
         />
     );
