b8d5099ad46a50a7137d3d23eb45bea6354ccec3
maiyerlee <mlee@flexion.us>
2024-07-22 07:17:00 -0500

    Add methods to create new doc record and update status

--- backend/ops_api/ops/document/utils.py
+++ backend/ops_api/ops/document/utils.py
@@ -1,7 +1,9 @@
 from flask import current_app
+from flask_jwt_extended import current_user
 from sqlalchemy.future import select
 
-from models import Agreement
+from models import Agreement, Document
+from ops_api.ops.document.exceptions import DocumentNotFoundError
 
 
 def is_user_linked_to_agreement(user, agreement_id: int) -> bool:
@@ -24,3 +26,33 @@ def is_user_linked_to_agreement(user, agreement_id: int) -> bool:
             return True
 
     return False
+
+
+def insert_new_document(document_data):
+    """
+    Insert a new document into the database.
+    """
+    document_record = Document(
+        agreement_id=document_data["agreement_id"],
+        created_by=current_user.id,
+        document_type=document_data["document_type"],
+        file_name=document_data["file_name"],
+    )
+    current_app.db_session.add(document_record)
+    current_app.db_session.commit()
+
+    return document_record
+
+
+def set_document_status_by_id(document_id, status):
+    """
+    Update the status of a document in the database.
+    """
+    document_stmt = select(Document).where(Document.document_id == document_id)
+    document = current_app.db_session.scalar(document_stmt)
+
+    if not document:
+        raise DocumentNotFoundError(f"Document with uuid {document_id} not found.")
+
+    document.status = status
+    current_app.db_session.commit()
