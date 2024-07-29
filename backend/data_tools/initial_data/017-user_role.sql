5d0910b65e1418dc7f6728855f0ea61ffa326437
John DeAngelis <john.deangelis2@gmail.com>
2024-07-29 13:18:02 -0400

    add Budget Team Member user with User Admin role
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/data_tools/initial_data/017-user_role.sql
+++ backend/data_tools/initial_data/017-user_role.sql
@@ -1,3 +1,5 @@
 INSERT INTO ops.user_role (user_id, role_id, created_on, updated_on) VALUES (521, 2, current_timestamp, current_timestamp);
 INSERT INTO ops.user_role (user_id, role_id, created_on, updated_on) VALUES (520, 1, current_timestamp, current_timestamp);
 INSERT INTO ops.user_role (user_id, role_id, created_on, updated_on) VALUES (520, 5, current_timestamp, current_timestamp);
+INSERT INTO ops.user_role (user_id, role_id, created_on, updated_on) VALUES (523, 5, current_timestamp, current_timestamp);
+INSERT INTO ops.user_role (user_id, role_id, created_on, updated_on) VALUES (523, 6, current_timestamp, current_timestamp);
