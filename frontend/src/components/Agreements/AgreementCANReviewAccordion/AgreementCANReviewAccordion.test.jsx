656d9f9b03c2649f5f9bdc9951d12e170631e60d
Frank Pigeon Jr <fpigeon@flexion.us>
2024-08-02 09:47:48 -0500

    test: Add test for rendering from approve page with budget change to CAN

--- frontend/src/components/Agreements/AgreementCANReviewAccordion/AgreementCANReviewAccordion.test.jsx
+++ frontend/src/components/Agreements/AgreementCANReviewAccordion/AgreementCANReviewAccordion.test.jsx
@@ -153,8 +153,9 @@ describe("AgreementCANReview", () => {
         expect(totalSpendingCardAfterApproval2).toBeInTheDocument();
         expect(remainingBudgetCardAfterApproval2).toBeInTheDocument();
     });
-    it.todo("should render from approve page with budget change to CAN", async () => {
+    it("should render from approve page with budget change to CAN", async () => {
         const mockSetAfterApproval = vi.fn();
+        const user = userEvent.setup();
         useGetCanFundingSummaryQuery
             .mockReturnValueOnce({ data: canFundingCardData })
             .mockReturnValueOnce({ data: canFundingCardData2 });
@@ -170,6 +171,51 @@ describe("AgreementCANReview", () => {
                 />
             </Provider>
         );
+
+        const toggle = screen.getByRole("button", { name: "Off (Drafts excluded) After Approval" });
+        const headingCard1 = screen.getByRole("heading", { name: "G99PHS9 (1 Year) CAN Total Budget" });
+        const headingCard2 = screen.getByRole("heading", { name: "G99XXX8 (1 Year) CAN Total Budget" });
+        const totalSpendingCard1 = screen.getByText(/9,700,000/i);
+        const remainingBudgetCard1 = screen.getByText(/14,300,000/i);
+        const totalSpendingCard2 = screen.getByText(/300,500/i);
+        const remainingBudgetCard2 = screen.getByText(/1,979,500/i);
+
+        expect(toggle).toBeInTheDocument();
+        expect(headingCard1).toBeInTheDocument();
+        expect(headingCard2).toBeInTheDocument();
+        expect(totalSpendingCard1).toBeInTheDocument();
+        expect(remainingBudgetCard1).toBeInTheDocument();
+        expect(totalSpendingCard2).toBeInTheDocument();
+        expect(remainingBudgetCard2).toBeInTheDocument();
+
+        await user.click(toggle);
+        expect(mockSetAfterApproval).toHaveBeenCalled();
+        useGetCanFundingSummaryQuery
+            .mockReturnValueOnce({ data: canFundingCardData })
+            .mockReturnValueOnce({ data: canFundingCardData2 });
+        render(
+            <Provider store={store}>
+                <AgreementCANReviewAccordion
+                    action=""
+                    afterApproval={true}
+                    instructions="budget change to CAN"
+                    isApprovePage={true}
+                    selectedBudgetLines={selectedBudgetLinesToCans}
+                    setAfterApproval={mockSetAfterApproval}
+                />
+            </Provider>
+        );
+        const toggleAfterApproval = screen.getByRole("button", { name: "On (Drafts included) After Approval" });
+        const totalSpendingCardAfterApproval1 = screen.getByText(/8,996,500/i);
+        const remainingBudgetCardAfterApproval1 = screen.getByText(/15,003,500/i);
+        const totalSpendingCardAfterApproval2 = screen.getByText(/1,004,000/i);
+        const remainingBudgetCardAfterApproval2 = screen.getByText(/1,276,000/i);
+
+        expect(toggleAfterApproval).toBeInTheDocument();
+        expect(totalSpendingCardAfterApproval1).toBeInTheDocument();
+        expect(remainingBudgetCardAfterApproval1).toBeInTheDocument();
+        expect(totalSpendingCardAfterApproval2).toBeInTheDocument();
+        expect(remainingBudgetCardAfterApproval2).toBeInTheDocument();
     });
     it.todo("should render from approve page with budget change to amount and CAN");
     it.todo("should render from approve page with status change to PLANNED");
