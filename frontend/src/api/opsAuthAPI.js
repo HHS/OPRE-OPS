8a0160bb88be0f861d928bb8665f2f05a1eb7374
John DeAngelis <john.deangelis2@gmail.com>
2024-08-07 09:40:43 -0400

    update http to https in mock unit test (js)
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- frontend/src/api/opsAuthAPI.js
+++ frontend/src/api/opsAuthAPI.js
@@ -4,7 +4,7 @@ import { getAccessToken } from "../components/Auth/auth";
 const BACKEND_DOMAIN =
     window.__RUNTIME_CONFIG__?.REACT_APP_BACKEND_DOMAIN ||
     import.meta.env.VITE_BACKEND_DOMAIN ||
-    "http://localhost:8000"; // Default to localhost if not provided (e.g. in tests)
+    "https://localhost:8000"; // Default to localhost if not provided (e.g. in tests)
 
 export const opsAuthApi = createApi({
     reducerPath: "opsAuthApi",
--- frontend/src/tests/mocks.js
+++ frontend/src/tests/mocks.js
@@ -3,19 +3,19 @@ import { setupServer } from "msw/node";
 import { changeRequests, divisions, roles } from "./data";
 
 export const handlers = [
-    http.get(`http://localhost:8000/api/v1/agreements/`, () => {
+    http.get(`https://localhost:8000/api/v1/agreements/`, () => {
         return HttpResponse.json([
             { id: 1, name: "Agreement 1" },
             { id: 2, name: "Agreement 2" }
         ]);
     }),
-    http.get(`http://localhost:8000/api/v1/agreements/:id`, (req, res, ctx) => {
+    http.get(`https://localhost:8000/api/v1/agreements/:id`, (req, res, ctx) => {
         const { id } = req.params;
 
         return res(ctx.status(200), ctx.json({ id, name: `Agreement ${id}` }));
     }),
 
-    http.get(`http://localhost:8000/api/v1/research-projects/`, (req, res, ctx) => {
+    http.get(`https://localhost:8000/api/v1/research-projects/`, (req, res, ctx) => {
         return res(
             ctx.status(200),
             ctx.json([
@@ -26,21 +26,21 @@ export const handlers = [
         );
     }),
 
-    http.post(`http://localhost:8000/api/v1/research-projects/`, (req, res, ctx) => {
+    http.post(`https://localhost:8000/api/v1/research-projects/`, (req, res, ctx) => {
         const { body } = req;
 
         return res(ctx.status(201), ctx.json({ id: 3, name: body.name }), ctx.delay(150));
     }),
 
-    http.get(`http://localhost:8000/api/v1/change-requests/`, (req, res, ctx) => {
+    http.get(`https://localhost:8000/api/v1/change-requests/`, (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(changeRequests), ctx.delay(150));
     }),
 
-    http.get(`http://localhost:8000/auth/roles/`, () => {
+    http.get(`https://localhost:8000/auth/roles/`, () => {
         return HttpResponse.json(roles);
     }),
 
-    http.get(`http://localhost:8000/api/v1/divisions/`, () => {
+    http.get(`https://localhost:8000/api/v1/divisions/`, () => {
         return HttpResponse.json(divisions);
     })
 ];
