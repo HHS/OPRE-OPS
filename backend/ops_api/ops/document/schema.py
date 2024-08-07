20f0af13be44142a7930eda48336e15ce94ac07e
maiyerlee <mlee@flexion.us>
2024-08-07 11:41:23 -0500

    Add document endpoints to ops api

--- backend/ops_api/ops/document/schema.py
+++ backend/ops_api/ops/document/schema.py
@@ -1,11 +1,12 @@
 from marshmallow import Schema, fields
 
 
-class GetDocumentRequestSchema(Schema):
+class PatchDocumentRequestSchema(Schema):
     agreement_id: int = fields.Int(required=True)
+    status: str = fields.String(required=True)
 
 
-class DocumentRequestSchema(Schema):
+class PostDocumentRequestSchema(Schema):
     agreement_id: int = fields.Int(required=True)
     document_type: str = fields.String(required=True)
     file_name: str = fields.String(required=True)
@@ -16,7 +17,17 @@ class DocumentResponseSchema(Schema):
     uuid: str = fields.String(required=True)
 
 
-class DocumentPatchRequestSchema(Schema):
-    agreement_id: int = fields.Int(required=True)
-    document_id: str = fields.String(required=True)
-    status: str = fields.String(required=True)
+class DocumentSchema(Schema):
+    document_id = fields.UUID(required=True)
+    document_type = fields.String(required=True)
+    file_name = fields.String(required=True)
+    status = fields.String()
+    created_by = fields.Integer(allow_none=True)
+    updated_by = fields.Integer(allow_none=True)
+    created_on = fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", allow_none=True)
+    updated_on = fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", allow_none=True)
+
+
+class DocumentsResponseSchema(Schema):
+    url: str = fields.String(required=True)
+    documents: list = fields.List(fields.Nested(DocumentSchema), required=True)
