749514f3ff04f2144eebaec15bb7e2d09f10b5af
Frank Pigeon Jr <fpigeon@flexion.us>
2024-08-12 17:34:01 -0500

    refactor: better handling of validation

--- frontend/src/components/Agreements/AgreementMetaAccordion/AgreementMetaAccordion.jsx
+++ frontend/src/components/Agreements/AgreementMetaAccordion/AgreementMetaAccordion.jsx
@@ -16,6 +16,23 @@ import Term from "../../UI/Term";
  */
 const AgreementMetaAccordion = ({ agreement, projectOfficerName, res, cn, convertCodeForDisplay, instructions }) => {
     const MORE_THAN_THREE_TEAM_MEMBERS = agreement?.team_members.length > 3;
+    /**
+     * Renders a Term component.
+     * @component
+     * @param {string} name - The name of the input field.
+     * @param {string} [label] - The label to display for the input field (optional)..
+     * @param {string|number} [value] - The value of the input field (optional).
+     * @returns {JSX.Element} - The rendered input component.
+     */
+    const renderTerm = (name, label, value) => (
+        <Term
+            name={name}
+            label={label}
+            value={value}
+            messages={res ? res.getErrors(name) : undefined}
+            className={cn ? cn(name) : undefined}
+        />
+    );
 
     return (
         <Accordion
@@ -23,256 +40,74 @@ const AgreementMetaAccordion = ({ agreement, projectOfficerName, res, cn, conver
             level={2}
         >
             <p>{instructions}</p>
-            {/* WITH VALIDATION */}
-            {res && (
-                <div className="grid-row grid-gap">
-                    <dl className="margin-0 font-12px grid-col">
-                        <Term
-                            name="project"
-                            label="Project"
-                            value={agreement?.project?.title}
-                        />
-                        <Term
-                            name="name"
-                            label="Agreement"
-                            messages={res.getErrors("name")}
-                            className={cn("name")}
-                            value={agreement?.name}
-                        />
-                        <Term
-                            name="description"
-                            label="Description"
-                            messages={res.getErrors("description")}
-                            className={cn("description")}
-                            value={agreement?.description}
-                        />
+            <div className="grid-row grid-gap">
+                <dl className="margin-0 font-12px grid-col">
+                    {renderTerm("project", "Project", agreement?.project?.title)}
+                    {renderTerm("name", "Agreement", agreement?.name)}
+                    {renderTerm("description", "Description", agreement?.description)}
+                </dl>
+
+                <div className="margin-0 font-12px grid-col">
+                    <dl>
+                        {renderTerm(
+                            "type",
+                            "Agreement Type",
+                            convertCodeForDisplay("agreementType", agreement?.agreement_type)
+                        )}
+                        {renderTerm(
+                            "contract-type",
+                            "Contract Type",
+                            convertCodeForDisplay("contractType", agreement?.contract_type)
+                        )}
+                        {renderTerm(
+                            "service-requirement-type",
+                            "Service Requirement Type",
+                            convertCodeForDisplay("serviceRequirementType", agreement?.service_requirement_type)
+                        )}
+                        {renderTerm("psc", "Product Service Code", agreement?.product_service_code?.name)}
+                    </dl>
+                    <dl className="display-flex flex-justify">
+                        {renderTerm("naics", "NAICS Code", agreement?.product_service_code?.naics)}
+                        {renderTerm(
+                            "program-support-code",
+                            "Program Support Code",
+                            agreement?.product_service_code?.support_code
+                        )}
+                    </dl>
+                    <dl>
+                        {renderTerm(
+                            "procurement-shop",
+                            "Procurement Shop",
+                            `${agreement?.procurement_shop?.abbr} - Fee Rate: ${agreement?.procurement_shop?.fee * 100}%`
+                        )}
+                        {renderTerm(
+                            "reason",
+                            "Reason for creating the agreement",
+                            convertCodeForDisplay("agreementReason", agreement?.agreement_reason)
+                        )}
+                        {agreement?.incumbent && renderTerm("incumbent", "Incumbent", agreement?.incumbent)}
+                        {renderTerm("project-officer", "Project Officer", projectOfficerName)}
                     </dl>
 
-                    <div className="margin-0 font-12px grid-col">
-                        <dl>
-                            <Term
-                                name="type"
-                                label="Agreement Type"
-                                messages={res.getErrors("type")}
-                                className={cn("type")}
-                                value={convertCodeForDisplay("agreementType", agreement?.agreement_type)}
-                            />
-                            <Term
-                                name="contract-type"
-                                label="Contract Type"
-                                value={convertCodeForDisplay("contractType", agreement?.contract_type)}
-                            />
-                            <Term
-                                name="service-requirement-type"
-                                label="Service Requirement Type"
-                                value={convertCodeForDisplay(
-                                    "serviceRequirementType",
-                                    agreement?.service_requirement_type
-                                )}
-                            />
-                            <Term
-                                name="psc"
-                                label="Product Service Code"
-                                messages={res.getErrors("psc")}
-                                className={cn("psc")}
-                                value={agreement?.product_service_code?.name}
-                            />
+                    {agreement?.team_members.length > 0 ? (
+                        <dl className="grid-row grid-gap-sm">
+                            <dt className="margin-0 text-base-dark margin-top-3 grid-col-12">Team Members</dt>
+                            {agreement?.team_members.map((member) => (
+                                <dd
+                                    key={member.id}
+                                    className={`text-semibold margin-0 margin-top-05 ${
+                                        MORE_THAN_THREE_TEAM_MEMBERS ? "grid-col-6" : "grid-col-12"
+                                    }`}
+                                >
+                                    {member.full_name}
+                                </dd>
+                            ))}
                         </dl>
-                        <dl className="grid-row">
-                            <Term
-                                name="naics"
-                                label="NAICS Code"
-                                messages={res.getErrors("naics")}
-                                className={`grid-col margin-top-0 ${cn("program-support-code")}`}
-                                value={agreement?.product_service_code?.naics}
-                            />
-                            <Term
-                                name="program-support-code"
-                                label="Program Support Code"
-                                messages={res.getErrors("program-support-code")}
-                                className={`grid-col margin-top-0 ${cn("program-support-code")}`}
-                                value={agreement?.product_service_code?.support_code}
-                            />
-                        </dl>
-                        <dl>
-                            <Term
-                                name="procurement-shop"
-                                label="Procurement Shop"
-                                messages={res.getErrors("procurement-shop")}
-                                className={cn("procurement-shop")}
-                                value={`${agreement?.procurement_shop?.abbr} - Fee Rate: ${
-                                    agreement?.procurement_shop?.fee * 100
-                                }%`}
-                            />
-                            <Term
-                                name="reason"
-                                label="Reason for creating the agreement"
-                                messages={res.getErrors("reason")}
-                                className={cn("reason")}
-                                value={convertCodeForDisplay("agreementReason", agreement?.agreement_reason)}
-                            />
-
-                            {agreement?.incumbent && (
-                                <Term
-                                    name="incumbent"
-                                    label="Incumbent"
-                                    messages={res.getErrors("incumbent")}
-                                    className={cn("incumbent")}
-                                    value={agreement?.incumbent}
-                                />
-                            )}
-                            <Term
-                                name="project-officer"
-                                label="Project Officer"
-                                messages={res.getErrors("project-officer")}
-                                className={cn("project-officer")}
-                                value={projectOfficerName}
-                            />
-                        </dl>
-
-                        {agreement?.team_members.length > 0 ? (
-                            <dl className="grid-row grid-gap-sm">
-                                <dt className="margin-0 text-base-dark margin-top-3 grid-col-12">Team Members</dt>
-                                {agreement?.team_members.map((member) => (
-                                    <dd
-                                        key={member.id}
-                                        className={`text-semibold margin-0 margin-top-05 ${
-                                            MORE_THAN_THREE_TEAM_MEMBERS ? "grid-col-6" : "grid-col-12"
-                                        }`}
-                                    >
-                                        {member.full_name}
-                                    </dd>
-                                ))}
-                            </dl>
-                        ) : (
-                            <dl>
-                                <Term
-                                    name="team-member"
-                                    label="Team Members"
-                                    messages={res.getErrors("team-member")}
-                                    className={cn("team-member")}
-                                    value={agreement?.team_members[0]}
-                                />
-                            </dl>
-                        )}
-                    </div>
+                    ) : (
+                        renderTerm("team-members", "Team Members", agreement?.team_members[0])
+                    )}
                 </div>
-            )}
-            {/* WITHOUT VALIDATION */}
-            {!res && (
-                <div className="grid-row grid-gap">
-                    <dl className="margin-0 font-12px grid-col">
-                        <Term
-                            name="project"
-                            label="Project"
-                            value={agreement?.project?.title}
-                        />
-                        <Term
-                            name="name"
-                            label="Agreement"
-                            value={agreement?.name}
-                        />
-                        <Term
-                            name="description"
-                            label="Description"
-                            value={agreement?.description}
-                        />
-                    </dl>
-
-                    <div className="margin-0 font-12px grid-col">
-                        <dl>
-                            <Term
-                                name="type"
-                                label="Agreement Type"
-                                value={convertCodeForDisplay("agreementType", agreement?.agreement_type)}
-                            />
-                            <Term
-                                name="contract-type"
-                                label="Contract Type"
-                                value={convertCodeForDisplay("contractType", agreement?.contract_type)}
-                            />
-                            <Term
-                                name="service-requirement-type"
-                                label="Service Requirement Type"
-                                value={convertCodeForDisplay(
-                                    "serviceRequirementType",
-                                    agreement?.service_requirement_type
-                                )}
-                            />
-                            <Term
-                                name="psc"
-                                label="Product Service Code"
-                                value={agreement?.product_service_code?.name}
-                            />
-                        </dl>
-                        <dl className="grid-row">
-                            <Term
-                                name="naics"
-                                label="NAICS Code"
-                                className="grid-col margin-top-0"
-                                value={agreement?.product_service_code?.naics}
-                            />
-                            <Term
-                                name="program-support-code"
-                                label="Program Support Code"
-                                className="grid-col margin-top-0"
-                                value={agreement?.product_service_code?.support_code}
-                            />
-                        </dl>
-                        <dl>
-                            <Term
-                                name="procurement-shop"
-                                label="Procurement Shop"
-                                value={`${agreement?.procurement_shop?.abbr} - Fee Rate: ${
-                                    agreement?.procurement_shop?.fee * 100
-                                }%`}
-                            />
-                            <Term
-                                name="reason"
-                                label="Reason for creating the agreement"
-                                value={convertCodeForDisplay("agreementReason", agreement?.agreement_reason)}
-                            />
-
-                            {agreement?.incumbent && (
-                                <Term
-                                    name="incumbent"
-                                    label="Incumbent"
-                                    value={agreement?.incumbent}
-                                />
-                            )}
-                            <Term
-                                name="project-officer"
-                                label="Project Officer"
-                                value={projectOfficerName}
-                            />
-                        </dl>
-
-                        {agreement?.team_members.length > 0 ? (
-                            <dl className="grid-row grid-gap-sm">
-                                <dt className="margin-0 text-base-dark margin-top-3 grid-col-12">Team Members</dt>
-                                {agreement?.team_members.map((member) => (
-                                    <dd
-                                        key={member.id}
-                                        className={`text-semibold margin-0 margin-top-05 ${
-                                            MORE_THAN_THREE_TEAM_MEMBERS ? "grid-col-6" : "grid-col-12"
-                                        }`}
-                                    >
-                                        {member.full_name}
-                                    </dd>
-                                ))}
-                            </dl>
-                        ) : (
-                            <dl>
-                                <Term
-                                    name="team-member"
-                                    label="Team Members"
-                                    value={agreement?.team_members[0]}
-                                />
-                            </dl>
-                        )}
-                    </div>
-                </div>
-            )}
+            </div>
         </Accordion>
     );
 };
@@ -285,4 +120,5 @@ AgreementMetaAccordion.propTypes = {
     convertCodeForDisplay: PropTypes.func.isRequired,
     instructions: PropTypes.string.isRequired
 };
+
 export default AgreementMetaAccordion;
