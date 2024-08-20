59fb469d230b0d8589f944e3900c82081affd283
Frank Pigeon Jr <fpigeon@flexion.us>
2024-08-20 16:48:15 -0500

    test: adds tests from BLI list

--- frontend/cypress/e2e/budgetChangeRequest.cy.js
+++ frontend/cypress/e2e/budgetChangeRequest.cy.js
@@ -267,6 +267,108 @@ describe("Budget Change Requests", () => {
                     });
             });
     });
+    it("should handle a budget change from budgetlines page", () => {
+        expect(localStorage.getItem("access_token")).to.exist;
+
+        // create test agreement
+        const bearer_token = `Bearer ${window.localStorage.getItem("access_token")}`;
+        cy.request({
+            method: "POST",
+            url: "http://localhost:8080/api/v1/agreements/",
+            body: testAgreement,
+            headers: {
+                Authorization: bearer_token,
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            }
+        })
+            .then((response) => {
+                expect(response.status).to.eq(201);
+                expect(response.body.id).to.exist;
+                const agreementId = response.body.id;
+                return agreementId;
+            })
+            // create BLI
+            .then((agreementId) => {
+                const bliData = { ...testBli, agreement_id: agreementId };
+                cy.request({
+                    method: "POST",
+                    url: "http://localhost:8080/api/v1/budget-line-items/",
+                    body: bliData,
+                    headers: {
+                        Authorization: bearer_token,
+                        Accept: "application/json"
+                    }
+                }).then((response) => {
+                    expect(response.status).to.eq(201);
+                    expect(response.body.id).to.exist;
+                    const bliId = response.body.id;
+                    return { agreementId, bliId };
+                });
+            })
+            .then(({ agreementId, bliId }) => {
+                cy.visit(
+                    `/agreements/${agreementId}/budget-lines?mode=edit&budget-line-id=${bliId}#budget-lines-header`
+                );
+                cy.get("#servicesComponentSelect").select("1");
+                cy.get("#pop-start-date").type("01/01/2024");
+                cy.get("#pop-end-date").type("01/01/2025");
+                cy.get("#description").type("This is a description.");
+                cy.get("[data-cy='add-services-component-btn']").click();
+                cy.get("tbody").children().as("table-rows").should("have.length", 1);
+                cy.get("@table-rows").eq(0).find("[data-cy='expand-row']").click();
+                cy.get("[data-cy='edit-row']").click();
+                cy.get("#allServicesComponentSelect").select("SC1");
+                cy.get("#enteredAmount").clear();
+                cy.get("#enteredAmount").type("2_222_222");
+                cy.get('[data-cy="update-budget-line"]').click();
+                cy.get('[data-cy="continue-btn"]').click();
+                cy.get('[data-cy="confirm-action"]').click();
+                cy.get('[data-cy="alert"]').should("exist");
+                cy.get('[data-cy="alert"]').should(($alert) => {
+                    expect($alert).to.contain(`BL ${bliId} Amount: $1,000,000.00 to $2,222,222.00`);
+                });
+                // cy.get("[data-cy='close-alert']").click();
+                cy.visit("/agreements?filter=change-requests").wait(1000);
+                // see if there are any review cards
+                cy.get("[data-cy='review-card']").should("exist").contains("Budget Change");
+                // verify agreement history
+                cy.visit(`/agreements/${agreementId}`);
+                cy.get(".usa-breadcrumb__list > :nth-child(3)").should("have.text", testAgreement.name);
+                cy.get('[data-cy="details-left-col"] > :nth-child(4)').should("have.text", "History");
+                cy.get('[data-cy="agreement-history-container"]').should("exist");
+                cy.get('[data-cy="agreement-history-container"]').scrollIntoView();
+                cy.get('[data-cy="agreement-history-list"]').should("exist");
+                checkHistoryItem(
+                    /Budget Change to Amount In Review/,
+                    `Admin Demo requested a budget change on BL ${bliId} from $1,000,000.00 to $2,222,222.00 and it's currently In Review for approval.`
+                )
+                    .then(() => {
+                        cy.request({
+                            method: "DELETE",
+                            url: `http://localhost:8080/api/v1/budget-line-items/${bliId}`,
+                            headers: {
+                                Authorization: bearer_token,
+                                Accept: "application/json"
+                            }
+                        }).then((response) => {
+                            expect(response.status).to.eq(200);
+                        });
+                    })
+                    .then(() => {
+                        cy.request({
+                            method: "DELETE",
+                            url: `http://localhost:8080/api/v1/agreements/${agreementId}`,
+                            headers: {
+                                Authorization: bearer_token,
+                                Accept: "application/json"
+                            }
+                        }).then((response) => {
+                            expect(response.status).to.eq(200);
+                        });
+                    });
+            });
+    });
 });
 
 const checkHistoryItem = (titleRegex, expectedText) => {
