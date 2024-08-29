a99c57e27ea81567b16cdcec28f8e1c197a1eedb
Frank Pigeon Jr <fpigeon@flexion.us>
2024-08-29 17:53:47 -0500

    feat: adds RTK Query for agreement details

--- frontend/src/pages/agreements/resolve/ResolveAgreement.jsx
+++ frontend/src/pages/agreements/resolve/ResolveAgreement.jsx
@@ -1,8 +1,32 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetAgreementByIdQuery } from "../../../api/opsAPI";
+import App from "../../../App";
+import DebugCode from "../../../components/DebugCode";
+import { useIsAgreementEditable, useIsUserAllowedToEditAgreement } from "../../../hooks/agreement.hooks";
+
+// /agreements/resolve/:id/*
 function ResolveAgreement() {
+    const urlPathParams = useParams();
+    const agreementId = parseInt(urlPathParams.id);
+    const [projectOfficer, setProjectOfficer] = React.useState({});
+    const {
+        data: agreement,
+        error: errorAgreement,
+        isLoading: isLoadingAgreement
+    } = useGetAgreementByIdQuery(agreementId, {
+        refetchOnMountOrArgChange: true
+    });
+
+    const canUserEditAgreement = useIsUserAllowedToEditAgreement(agreement?.id);
+    const isAgreementEditable = useIsAgreementEditable(agreement?.id);
+    const isEditable = isAgreementEditable && canUserEditAgreement;
+
     return (
-        <div>
-            <h1>Resolve Agreement</h1>
-        </div>
+        <App breadCrumbName={agreement?.display_name}>
+            <h2>Edit Agreement Details</h2>
+            <DebugCode data={agreement} />
+        </App>
     );
 }
 
