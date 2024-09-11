8f1695fb31bd3b6df1b074cf929c94d1ee8d3522
Santi-3rd <g.santiago3rd@gmail.com>
2024-09-11 08:24:40 -0600

    Created new tests for 401 codes.

--- frontend/src/errorMiddleware.js
+++ frontend/src/errorMiddleware.js
@@ -1,11 +1,13 @@
 import { isRejected } from "@reduxjs/toolkit";
 import { opsApi } from "./api/opsAPI.js";
-import store from "./store.js";
 import { logout } from "./components/Auth/authSlice.js";
+import store from "./store.js";
+
+export const innerIsRejected = (action) => isRejected(action) || action.type === "REJECTED";
 
 // eslint-disable-next-line no-unused-vars
 export const errorMiddleware = (api) => (next) => (action) => {
-    if (isRejected(action)) {
+    if (innerIsRejected(action)) {
         if (action.payload?.status === 401) {
             store.dispatch(opsApi.util.resetApiState());
             store.dispatch(logout());
