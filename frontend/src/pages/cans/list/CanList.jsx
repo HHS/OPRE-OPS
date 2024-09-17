8544d811b795df73179a66e2f9d6cff35ec9d1b4
Frank Pigeon Jr <fpigeon@flexion.us>
2024-09-17 17:13:21 -0500

    refactor: replaces with RTK Query

--- frontend/src/pages/cans/list/CanList.jsx
+++ frontend/src/pages/cans/list/CanList.jsx
@@ -1,22 +1,34 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
 import App from "../../../App";
 import CANTable from "../../../components/CANs/CANTable";
 import DebugCode from "../../../components/DebugCode";
 import TablePageLayout from "../../../components/Layouts/TablePageLayout";
 import CANTags from "./CanTabs";
-import { getCanList } from "./getCanList";
+// import { getCanList } from "./getCanList";
 import { useSearchParams } from "react-router-dom";
+import { useGetCansQuery } from "../../../api/opsAPI";
+import ErrorPage from "../../ErrorPage";
 
 const CanList = () => {
-    const dispatch = useDispatch();
-    const canList = useSelector((state) => state.canList.cans);
+    // const dispatch = useDispatch();
+    // const canList = useSelector((state) => state.canList.cans);
     const [searchParams] = useSearchParams();
     const myCANsUrl = searchParams.get("filter") === "my-cans";
-    useEffect(() => {
-        dispatch(getCanList());
-    }, [dispatch]);
-
+    // useEffect(() => {
+    //     dispatch(getCanList());
+    // }, [dispatch]);
+    const { data: canList, isError, isLoading } = useGetCansQuery();
+    if (isLoading) {
+        return (
+            <App>
+                <h1>Loading...</h1>
+            </App>
+        );
+    }
+    if (isError) {
+        return <ErrorPage />;
+    }
     // TODO: remove flag once CANS are ready
     return (
         import.meta.env.DEV && (
