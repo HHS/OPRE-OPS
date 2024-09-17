3f816a7afbd5aa568f3c02fdbfde52bff1308b65
Frank Pigeon Jr <fpigeon@flexion.us>
2024-09-17 17:12:46 -0500

    feat: adds method_of_transfer helper

--- frontend/src/helpers/utils.js
+++ frontend/src/helpers/utils.js
@@ -176,12 +176,18 @@ export const codesToDisplayText = {
         can_id: "CAN",
         date_needed: "Obligate By Date",
         status: "Status"
+    },
+    methodOfTransfer: {
+        COST_SHARE: "Cost Share",
+        DIRECT: "Direct",
+        IAA: "IAA",
+        IDDA: "IDDA"
     }
 };
 
 /**
  * Converts a code value into a display text value based on a predefined mapping.
- * @param {("agreementType" | "agreementReason" | "budgetLineStatus" | "validation" | "classNameLabels" | "baseClassNameLabels"| "agreementPropertyLabels" | "budgetLineItemPropertyLabels" | "changeToTypes")} listName - The name of the list to retrieve the mapping from the codesToDisplayText object. This parameter is required.
+ * @param {("agreementType" | "agreementReason" | "budgetLineStatus" | "validation" | "classNameLabels" | "baseClassNameLabels"| "agreementPropertyLabels" | "budgetLineItemPropertyLabels" | "changeToTypes" | "methodOfTransfer")} listName - The name of the list to retrieve the mapping from the codesToDisplayText object. This parameter is required.
  * @param {string} code - The code value to convert. This parameter is required.
  * @returns {string} The display text value for the code, or the original code value if no mapping is found.
  * @throws {Error} If either the listName or code parameter is not provided.
