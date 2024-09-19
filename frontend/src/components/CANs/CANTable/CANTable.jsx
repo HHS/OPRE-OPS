7e26cc4a928e2e96364d8a9145ea8b3942ec7bb7
Frank Pigeon Jr <fpigeon@flexion.us>
2024-09-19 15:39:59 -0500

    feat: adds Tooltip to Table Heading

--- frontend/src/components/CANs/CANTable/CANTable.jsx
+++ frontend/src/components/CANs/CANTable/CANTable.jsx
@@ -1,8 +1,7 @@
 import PropTypes from "prop-types";
-import Table from "../../UI/Table";
-import { CAN_TABLE_HEADINGS } from "./CANTable.constants";
+import Tooltip from "../../UI/USWDS/Tooltip";
 import CANTableRow from "./CANTableRow";
-
+import styles from "./style.module.css";
 /**
  * CANTable component of CanList
  * @component
@@ -15,23 +14,88 @@ const CANTable = ({ cans }) => {
     if (cans.length === 0) {
         return <p className="text-center">No CANs found</p>;
     }
+
     return (
-        <Table tableHeadings={CAN_TABLE_HEADINGS}>
-            {cans.map((can) => (
-                <CANTableRow
-                    key={can.id}
-                    canId={can.id}
-                    name={can.display_name}
-                    nickname={can.nick_name}
-                    portfolio={can.portfolio.abbreviation}
-                    FY={can.funding_budgets[0]?.fiscal_year ?? "TBD"}
-                    activePeriod={can.active_period ?? "TBD"}
-                    obligateBy={can.obligate_by ?? "09/30/25"}
-                    transfer={can.funding_details.method_of_transfer ?? "TBD"}
-                    fyBudget={can.funding_budgets[0]?.budget ?? 0}
-                />
-            ))}
-        </Table>
+        <table className={`usa-table usa-table--borderless width-full ${styles.tableHover}`}>
+            <TableHead />
+            <tbody>
+                {cans.map((can) => (
+                    <CANTableRow
+                        key={can.id}
+                        canId={can.id}
+                        name={can.display_name}
+                        nickname={can.nick_name}
+                        portfolio={can.portfolio.abbreviation}
+                        fiscalYear={can.funding_budgets[0]?.fiscal_year ?? "TBD"}
+                        activePeriod={can.active_period ?? "TBD"}
+                        obligateBy={can.obligate_by ?? "09/30/25"}
+                        transfer={can.funding_details.method_of_transfer ?? "TBD"}
+                        fyBudget={can.funding_budgets[0]?.budget ?? 0}
+                    />
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+const TableHead = () => {
+    const availbleTooltip =
+        "$ Available is the remaining amount of the total budget that is available to plan from (Total FY Budget minus budget lines in Planned, Executing or Obligated Status)";
+    return (
+        <thead>
+            <tr>
+                <th
+                    scope="col"
+                    style={{ whiteSpace: "nowrap" }}
+                >
+                    CAN
+                </th>
+                <th
+                    scope="col"
+                    style={{ whiteSpace: "nowrap" }}
+                >
+                    Portfolio
+                </th>
+                <th
+                    scope="col"
+                    style={{ whiteSpace: "nowrap" }}
+                >
+                    FY
+                </th>
+                <th
+                    scope="col"
+                    style={{ whiteSpace: "nowrap" }}
+                >
+                    Active Period
+                </th>
+                <th
+                    scope="col"
+                    style={{ whiteSpace: "nowrap" }}
+                >
+                    Obligate By
+                </th>
+                <th
+                    scope="col"
+                    style={{ whiteSpace: "nowrap" }}
+                >
+                    Transfer
+                </th>
+                <th
+                    scope="col"
+                    style={{ whiteSpace: "nowrap" }}
+                >
+                    FY Budget
+                </th>
+                <th
+                    scope="col"
+                    style={{ whiteSpace: "nowrap" }}
+                >
+                    <Tooltip label={availbleTooltip}>
+                        <span>$ Available</span>
+                    </Tooltip>
+                </th>
+            </tr>
+        </thead>
     );
 };
 
