ba4ad2c788d2ea9f770dcb1de539a939f12fe0a8
Frank Pigeon Jr <fpigeon@flexion.us>
2024-10-01 11:18:28 -0500

    docs: updates User schema

--- frontend/src/components/Users/UserTypes.d.ts
+++ frontend/src/components/Users/UserTypes.d.ts
@@ -3,3 +3,24 @@ export type SafeUser = {
     full_name: string;
     id: number;
 };
+
+export type User = {
+    created_by?: number;
+    created_on: Date;
+    display_name: string;
+    division: number;
+    email: string;
+    first_name: string;
+    full_name: string;
+    hhs_id?: number;
+    id: number;
+    last_name: string;
+    oidc_id: string;
+    roles: UserRoles[];
+    status: UserStatus;
+    updated_by?: number;
+    updated_on: Date;
+};
+
+type UserRoles = "USER_ADMIN" | "BUDGET_TEAM" | "admin" | "division-director" | "user";
+type UserStatus = "ACTIVE" | "INACTIVE" | "LOCKED";
