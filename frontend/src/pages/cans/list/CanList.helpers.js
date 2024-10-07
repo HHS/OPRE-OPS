4fafb35733b9fa9eca1fb4931c6433600fff8675
Frank Pigeon Jr. <fpigeon@flexion.us>
2024-10-07 12:59:38 -0500

    feat: role-based filtering of My CANs (#2880)
    * docs: adds todos

--- frontend/src/pages/cans/list/CanList.helpers.js
+++ frontend/src/pages/cans/list/CanList.helpers.js
@@ -27,6 +27,8 @@ export const sortAndFilterCANs = (cans, myCANsUrl, activeUser) => {
             return can.portfolio.division_id === userDivisionId;
         }
         // Filter based on team members
+        // TODO: add project officers per #2884
+
         if (roles.includes(USER_ROLES.USER)) {
             return can.budget_line_items.some((bli) => bli.team_members.some((member) => member.id === userId));
         }
