1555750976db4cfde0edac183b4f738f213ec964
Frank Pigeon Jr <fpigeon@flexion.us>
2024-10-08 06:48:41 -0500

    style: label change

--- frontend/src/components/CANs/CANActivePeriodComboBox/CANActivePeriodComboBox.jsx
+++ frontend/src/components/CANs/CANActivePeriodComboBox/CANActivePeriodComboBox.jsx
@@ -37,7 +37,7 @@ const CANActivePeriodComboBox = ({
                     className={legendClassname}
                     htmlFor="can-active-period-combobox-input"
                 >
-                    Fiscal Year
+                    Active Period
                 </label>
                 <div>
                     <ComboBox
