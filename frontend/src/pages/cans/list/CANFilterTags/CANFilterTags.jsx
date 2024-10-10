289a6ffac53a013a70ff59a33bbe54f20f604588
Frank Pigeon Jr <fpigeon@flexion.us>
2024-10-10 14:39:33 -0500

    refactor: adding and removing Pills

--- frontend/src/pages/cans/list/CANFilterTags/CANFilterTags.jsx
+++ frontend/src/pages/cans/list/CANFilterTags/CANFilterTags.jsx
@@ -1,103 +1,119 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import _ from "lodash";
 import FilterTags from "../../../../components/UI/FilterTags";
 import FilterTagsWrapper from "../../../../components/UI/FilterTags/FilterTagsWrapper";
 
 /**
- * A filter tags.
- * @param {Object} props - The component props.
- * @param {import ('../CANFilterButton/CANFilterTypes').Filters} props.filters - The current filters.
- * @param {Function} props.setFilters - A function to call to set the filters.
- * @returns {JSX.Element | boolean} - The procurement shop select element.
+ * @typedef {Object} Tag
+ * @property {string} tagText
+ * @property {string} filter
  */
-export const CANFilterTags = ({ filters, setFilters }) => {
+
+/**
+ * @typedef {Object} FilterItem
+ * @property {string} title
+ */
+
+/**
+ * @typedef {Object} Filters
+ * @property {FilterItem[]} activePeriod
+ * @property {FilterItem[]} portfolio
+ * @property {FilterItem[]} transfer
+ */
+
+/**
+ * Custom hook for managing tags list
+ * @param {Filters} filters
+ * @returns {Tag[]}
+ */
+const useTagsList = (filters) => {
     const [tagsList, setTagsList] = useState([]);
 
+    const updateTags = useCallback(
+        (filterKey, filterName) => {
+            if (!Array.isArray(filters[filterKey])) return;
+
+            const selectedTags = filters[filterKey].map((item) => ({
+                tagText: item.title,
+                filter: filterName
+            }));
+
+            setTagsList((prevState) => [...prevState.filter((t) => t.filter !== filterName), ...selectedTags]);
+        },
+        [filters]
+    );
+
+    useEffect(() => {
+        updateTags("activePeriod", "activePeriod");
+    }, [filters.activePeriod, updateTags]);
+
+    useEffect(() => {
+        updateTags("portfolio", "portfolio");
+    }, [filters.portfolio, updateTags]);
+
+    useEffect(() => {
+        updateTags("transfer", "transfer");
+    }, [filters.transfer, updateTags]);
+
+    return tagsList;
+};
+
+/**
+ * A filter tags component.
+ * @param {Object} props - The component props.
+ * @param {Filters} props.filters - The current filters.
+ * @param {Function} props.setFilters - A function to call to set the filters.
+ * @returns {JSX.Element|null} The filter tags component or null if no tags.
+ */
+export const CANFilterTags = ({ filters, setFilters }) => {
+    const tagsList = useTagsList(filters);
+
+    /**
+     * Removes a filter tag
+     * @param {Tag} tag - The tag to remove
+     */
     const removeFilter = (tag) => {
-        const filteredTagsList = tagsList.filter((t) => t.tagText !== tag.tagText);
-        setTagsList(filteredTagsList);
         switch (tag.filter) {
-            case "fiscalYears":
-                setFilters((prevState) => {
-                    return {
-                        ...prevState,
-                        fiscalYears: prevState.fiscalYears.filter(
-                            (fy) => fy.title.toString() !== tag.tagText.replace("FY ", "")
-                        )
-                    };
-                });
+            case "activePeriod":
+                setFilters((prevState) => ({
+                    ...prevState,
+                    activePeriod: prevState.activePeriod.filter((period) => period.title !== tag.tagText)
+                }));
                 break;
-            case "portfolios":
-                setFilters((prevState) => {
-                    return {
-                        ...prevState,
-                        portfolios: prevState.portfolios.filter((portfolio) => portfolio.name !== tag.tagText)
-                    };
-                });
+            case "portfolio":
+                setFilters((prevState) => ({
+                    ...prevState,
+                    portfolio: prevState.portfolio.filter((portfolio) => portfolio.title !== tag.tagText)
+                }));
                 break;
-            case "bliStatus":
-                setFilters((prevState) => {
-                    return {
-                        ...prevState,
-                        bliStatus: prevState.bliStatus.filter((status) => status.title !== tag.tagText)
-                    };
-                });
+            case "transfer":
+                setFilters((prevState) => ({
+                    ...prevState,
+                    transfer: prevState.transfer.filter((transfer) => transfer.title !== tag.tagText)
+                }));
                 break;
+            default:
+                console.warn(`Unknown filter type: ${tag.filter}`);
         }
     };
 
-    useEffect(() => {
-        const selectedFiscalYears = [];
-        Array.isArray(filters.fiscalYears) &&
-            filters.fiscalYears.forEach((fiscalYear) => {
-                const tag = `FY ${fiscalYear.title}`;
-                selectedFiscalYears.push({ tagText: tag, filter: "fiscalYears" });
-            });
-        setTagsList((prevState) => prevState.filter((t) => t.filter !== "fiscalYears"));
-        setTagsList((prevState) => {
-            return [...prevState, ...selectedFiscalYears];
-        });
-    }, [filters.fiscalYears]);
-
-    useEffect(() => {
-        const selectedPortfolios = [];
-        Array.isArray(filters.portfolios) &&
-            filters.portfolios.forEach((portfolio) => {
-                selectedPortfolios.push({ tagText: portfolio.name, filter: "portfolios" });
-            });
-        setTagsList((prevState) => prevState.filter((t) => t.filter !== "portfolios"));
-        setTagsList((prevState) => {
-            return [...prevState, ...selectedPortfolios];
-        });
-    }, [filters.portfolios]);
-
-    useEffect(() => {
-        const selectedBLIStatus = [];
-        Array.isArray(filters.bliStatus) &&
-            filters.bliStatus.forEach((status) => {
-                selectedBLIStatus.push({ tagText: status.title, filter: "bliStatus" });
-            });
-        setTagsList((prevState) => prevState.filter((t) => t.filter !== "bliStatus"));
-        setTagsList((prevState) => {
-            return [...prevState, ...selectedBLIStatus];
-        });
-    }, [filters.bliStatus]);
-
     const tagsListByFilter = _.groupBy(tagsList, "filter");
-    const tagsListByFilterMerged = [];
-    Array.isArray(tagsListByFilter.fiscalYears) && tagsListByFilterMerged.push(...tagsListByFilter.fiscalYears.sort());
-    Array.isArray(tagsListByFilter.portfolios) && tagsListByFilterMerged.push(...tagsListByFilter.portfolios.sort());
-    Array.isArray(tagsListByFilter.bliStatus) && tagsListByFilterMerged.push(...tagsListByFilter.bliStatus.sort());
+    const tagsListByFilterMerged = Object.values(tagsListByFilter)
+        .flat()
+        .sort((a, b) => a.tagText.localeCompare(b.tagText));
+
+    if (tagsList.length === 0) {
+        return null;
+    }
 
     return (
-        tagsList.length > 0 && (
-            <FilterTagsWrapper>
-                <FilterTags
-                    removeFilter={removeFilter}
-                    tagsList={tagsListByFilterMerged}
-                />
-            </FilterTagsWrapper>
-        )
+        <FilterTagsWrapper>
+            <FilterTags
+                removeFilter={removeFilter}
+                tagsList={tagsListByFilterMerged}
+            />
+        </FilterTagsWrapper>
     );
 };
+
 export default CANFilterTags;
