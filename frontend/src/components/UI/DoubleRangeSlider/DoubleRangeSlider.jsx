54e82c738658b07b5224e6305f3f6b764288b72a
maiyerlee <mlee@flexion.us>
2024-10-11 02:32:31 -0500

    Fix key warnings

--- frontend/src/components/UI/DoubleRangeSlider/DoubleRangeSlider.jsx
+++ frontend/src/components/UI/DoubleRangeSlider/DoubleRangeSlider.jsx
@@ -1,39 +1,49 @@
 import ReactSlider from "react-slider";
 import styled from 'styled-components';
 
-const DoubleRangeSlider = ({min, max}) => {
-    const StyledSlider = styled(ReactSlider)`
-        width: 100%;
-        height: 25px;
-    `;
+const StyledTrack = styled.div`
+    top: 5px;
+    bottom: 0;
+    background: white;
+    border-radius: 999px;
+    border: 1px solid black;
+`;
 
-    const StyledThumb = styled.div`
-        height: 30px;
-        line-height: 30px;
-        width: 30px;
-        text-align: center;
-        background-color: whitesmoke;
-        color: black;
-        border: 2px solid gray;
-        border-radius: 50%;
-        cursor: grab;
-    `;
+const StyledThumb = styled.div`
+    height: 30px;
+    line-height: 30px;
+    width: 30px;
+    background-color: whitesmoke;
+    border: 2px solid gray;
+    border-radius: 50%;
+    cursor: grab;
+`;
 
-    const Thumb = (props, state) => <StyledThumb {...props}>{state.valueNow}</StyledThumb>;
+const StyledSlider = styled(ReactSlider)`
+    width: 100%;
+    height: 25px;
+`;
 
-    const StyledTrack = styled.div`
-        top: 5px;
-        bottom: 0;
-        background: white;
-        border-radius: 999px;
-        border: 1px solid black;
-    `;
+const DoubleRangeSlider = ({min, max, onValueChange}) => {
+    const Thumb = ({ key, ...props }) => (
+        <StyledThumb key={key} {...props}/>
+    );
 
-    const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
+    const Track = ({ key, ...props }) => (
+        <StyledTrack key={key} {...props}/>
+    );
 
     return (
         <div>
-            <StyledSlider defaultValue={[min, max]} renderTrack={Track} renderThumb={Thumb} />
+            <StyledSlider
+                onAfterChange={(value) =>{
+                    if (onValueChange) {
+                        onValueChange(value);
+                    }
+                }}
+                defaultValue={[min, max]}
+                renderTrack={Track}
+                renderThumb={Thumb} />
         </div>
     );
 }
