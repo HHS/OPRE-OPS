c48ff191cda4f7bc0b1b2655a5e125d27af95e30
maiyerlee <mlee@flexion.us>
2024-10-17 12:15:04 -0500

    fix: use oidc_id instead of id

--- backend/data_tools/src/disable_users/disable_users.py
+++ backend/data_tools/src/disable_users/disable_users.py
@@ -3,12 +3,11 @@ import os
 
 from data_tools.src.disable_users.queries import (
     ALL_ACTIVE_USER_SESSIONS_QUERY,
-    EXCLUDED_USER_IDS,
+    EXCLUDED_USER_OIDC_IDS,
+    GET_USER_ID_BY_OIDC_QUERY,
     INACTIVE_USER_QUERY,
-    SYSTEM_USER_EMAIL,
-    SYSTEM_USER_ID,
-    SYSTEM_USER_OIDC_ID,
-    SYSTEM_USER_QUERY,
+    SYSTEM_ADMIN_EMAIL,
+    SYSTEM_ADMIN_OIDC_ID,
 )
 from data_tools.src.import_static_data.import_data import get_config, init_db
 from sqlalchemy import text
@@ -20,36 +19,50 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def create_system_user(se):
+def get_ids_from_oidc_ids(se, oidc_ids: list):
+    """Retrieve user IDs corresponding to a list of OIDC IDs."""
+    if not all(isinstance(oidc_id, str) for oidc_id in oidc_ids):
+        raise ValueError("All oidc_ids must be strings.")
+
+    ids = []
+    for oidc_id in oidc_ids:
+        user_id = se.execute(text(GET_USER_ID_BY_OIDC_QUERY), {"oidc_id": oidc_id}).scalar()
+
+        if user_id is not None:
+            ids.append(user_id)
+
+    return ids
+
+
+def create_system_admin(se):
     """Create system user if it doesn't exist."""
-    system_user = se.execute(
-        text(SYSTEM_USER_QUERY),
-        {"oidc_id": SYSTEM_USER_OIDC_ID}
+    system_admin = se.execute(
+        text(GET_USER_ID_BY_OIDC_QUERY),
+        {"oidc_id": SYSTEM_ADMIN_OIDC_ID}
     ).fetchone()
 
-    if system_user is None:
+    if system_admin is None:
         sys_user = User(
-            id=SYSTEM_USER_ID,
-            email=SYSTEM_USER_EMAIL,
-            oidc_id=SYSTEM_USER_OIDC_ID,
+            email=SYSTEM_ADMIN_EMAIL,
+            oidc_id=SYSTEM_ADMIN_OIDC_ID,
             status=UserStatus.LOCKED
         )
         se.add(sys_user)
         se.commit()
         return sys_user.id
 
-    return system_user[0]
+    return system_admin[0]
 
 
-def disable_user(se, user_id, system_user_id):
+def disable_user(se, user_id, system_admin_id):
     """Deactivate a single user and log the change."""
-    updated_user = User(id=user_id, status=UserStatus.INACTIVE, updated_by=system_user_id)
+    updated_user = User(id=user_id, status=UserStatus.INACTIVE, updated_by=system_admin_id)
     se.merge(updated_user)
 
     db_audit = build_audit(updated_user, OpsDBHistoryType.UPDATED)
     ops_db_history = OpsDBHistory(
         event_type=OpsDBHistoryType.UPDATED,
-        created_by=system_user_id,
+        created_by=system_admin_id,
         class_name=updated_user.__class__.__name__,
         row_key=db_audit.row_key,
         changes=db_audit.changes,
@@ -59,7 +72,7 @@ def disable_user(se, user_id, system_user_id):
     ops_event = OpsEvent(
         event_type=OpsEventType.UPDATE_USER,
         event_status=OpsEventStatus.SUCCESS,
-        created_by=system_user_id,
+        created_by=system_admin_id,
     )
     se.add(ops_event)
 
@@ -68,7 +81,7 @@ def disable_user(se, user_id, system_user_id):
         updated_user_session = UserSession(
             id=session[0],
             is_active=False,
-            updated_by=system_user_id
+            updated_by=system_admin_id
         )
         se.merge(updated_user_session)
 
@@ -77,11 +90,12 @@ def update_disabled_users_status(conn: sqlalchemy.engine.Engine):
     """Update the status of disabled users in the database."""
     with Session(conn) as se:
         logger.info("Checking for System User.")
-        system_user_id = create_system_user(se)
+        system_admin_id = create_system_admin(se)
 
         logger.info("Fetching inactive users.")
         results = se.execute(text(INACTIVE_USER_QUERY)).scalars().all()
-        user_ids = [uid for uid in results if uid not in EXCLUDED_USER_IDS]
+        excluded_ids = get_ids_from_oidc_ids(se, EXCLUDED_USER_OIDC_IDS)
+        user_ids = [uid for uid in results if uid not in excluded_ids]
 
         if not user_ids:
             logger.info("No inactive users found.")
@@ -91,7 +105,7 @@ def update_disabled_users_status(conn: sqlalchemy.engine.Engine):
 
         for user_id in user_ids:
             logger.info("Deactivating user", user_id)
-            disable_user(se, user_id, system_user_id)
+            disable_user(se, user_id, system_admin_id)
 
         se.commit()
 
--- backend/data_tools/tests/disable_users/test_disable_users.py
+++ backend/data_tools/tests/disable_users/test_disable_users.py
@@ -1,98 +1,108 @@
 from unittest.mock import MagicMock, patch
 
-from data_tools.src.disable_users.disable_users import create_system_user, disable_user, update_disabled_users_status
-from data_tools.src.disable_users.queries import SYSTEM_USER_EMAIL, SYSTEM_USER_ID, SYSTEM_USER_OIDC_ID
+import pytest
+from data_tools.src.disable_users.disable_users import (
+    create_system_admin,
+    disable_user,
+    get_ids_from_oidc_ids,
+    update_disabled_users_status,
+)
+from data_tools.src.disable_users.queries import SYSTEM_ADMIN_EMAIL, SYSTEM_ADMIN_OIDC_ID
 
 from models import OpsDBHistoryType, OpsEventStatus, OpsEventType, UserStatus
 
-system_user_id = 111
+system_admin_id = 111
 
-def test_create_system_user():
-    mock_se = MagicMock()
-    mock_se.execute.return_value.fetchone.return_value = None
+@pytest.fixture
+def mock_session():
+    """Fixture for creating a mock SQLAlchemy session."""
+    session = MagicMock()
+    session.execute.return_value.fetchone.return_value = None
+    return session
 
-    result = create_system_user(mock_se)
+def test_create_system_admin(mock_session):
+    create_system_admin(mock_session)
 
-    se_add = mock_se.add.call_args[0][0]
-    mock_se.execute.assert_called_once()
-    mock_se.add.assert_called_once()
-    mock_se.commit.assert_called_once()
-    assert result == SYSTEM_USER_ID
-    assert se_add.id == SYSTEM_USER_ID
-    assert se_add.email == SYSTEM_USER_EMAIL
-    assert se_add.oidc_id == SYSTEM_USER_OIDC_ID
+    se_add = mock_session.add.call_args[0][0]
+    mock_session.execute.assert_called_once()
+    mock_session.add.assert_called_once()
+    mock_session.commit.assert_called_once()
+    assert se_add.email == SYSTEM_ADMIN_EMAIL
+    assert se_add.oidc_id == SYSTEM_ADMIN_OIDC_ID
     assert se_add.first_name is None
     assert se_add.last_name is None
 
+def test_return_existing_system_admin(mock_session):
+    mock_session.execute.return_value.fetchone.return_value = (system_admin_id,)
 
-def test_return_existing_system_user():
-    mock_se = MagicMock()
-    mock_se.execute.return_value.fetchone.return_value = (SYSTEM_USER_ID,)
+    result = create_system_admin(mock_session)
 
-    result = create_system_user(mock_se)
+    assert result == system_admin_id
+    mock_session.add.assert_not_called()
+    mock_session.commit.assert_not_called()
 
-    assert result == SYSTEM_USER_ID
-    mock_se.add.assert_not_called()
-    mock_se.commit.assert_not_called()
-
-def test_deactivate_user():
+def test_deactivate_user(mock_session):
     user_id = 1
     db_history_changes = {
-        "id": {
-            "new": user_id,
-            "old": None
-        },
-        "status": {
-            "new": "INACTIVE",
-            "old": None
-        },
-        "updated_by": {
-            "new": system_user_id,
-            "old": None}
+        "id": {"new": user_id, "old": None},
+        "status": {"new": "INACTIVE", "old": None},
+        "updated_by": {"new": system_admin_id, "old": None}
     }
 
-    mock_se = MagicMock()
-    mock_se.execute.return_value = [(1,), (2,)]
+    mock_session.execute.return_value = [(1,), (2,)]
 
-    disable_user(mock_se, user_id, system_user_id)
+    disable_user(mock_session, user_id, system_admin_id)
 
-    assert mock_se.merge.call_count == 3
-    assert mock_se.add.call_count == 2
+    assert mock_session.merge.call_count == 3
+    assert mock_session.add.call_count == 2
 
-    user_call = mock_se.merge.call_args_list[0]
+    user_call = mock_session.merge.call_args_list[0]
     assert user_call[0][0].id == user_id
     assert user_call[0][0].status == UserStatus.INACTIVE
-    assert user_call[0][0].updated_by == system_user_id
+    assert user_call[0][0].updated_by == system_admin_id
 
-    user_session_call_1 = mock_se.merge.call_args_list[1]
+    user_session_call_1 = mock_session.merge.call_args_list[1]
     assert user_session_call_1[0][0].id == user_id
-    assert user_session_call_1[0][0].is_active == False
-    assert user_session_call_1[0][0].updated_by == system_user_id
+    assert user_session_call_1[0][0].is_active is False
+    assert user_session_call_1[0][0].updated_by == system_admin_id
 
-    user_session_call_2 = mock_se.merge.call_args_list[1]
-    assert user_session_call_2[0][0].id == user_id
-    assert user_session_call_2[0][0].is_active == False
-    assert user_session_call_2[0][0].updated_by == system_user_id
-
-    ops_db_history_call = mock_se.add.call_args_list[0]
+    ops_db_history_call = mock_session.add.call_args_list[0]
     assert ops_db_history_call[0][0].event_type == OpsDBHistoryType.UPDATED
-    assert ops_db_history_call[0][0].created_by == system_user_id
+    assert ops_db_history_call[0][0].created_by == system_admin_id
     assert ops_db_history_call[0][0].class_name == 'User'
     assert ops_db_history_call[0][0].row_key == str(user_id)
     assert ops_db_history_call[0][0].changes == db_history_changes
 
-    ops_events_call = mock_se.add.call_args_list[1]
+    ops_events_call = mock_session.add.call_args_list[1]
     assert ops_events_call[0][0].event_type == OpsEventType.UPDATE_USER
     assert ops_events_call[0][0].event_status == OpsEventStatus.SUCCESS
-    assert ops_events_call[0][0].created_by == system_user_id
+    assert ops_events_call[0][0].created_by == system_admin_id
 
 @patch("data_tools.src.disable_users.disable_users.logger")
-def test_no_inactive_users(mock_logger):
-    mock_conn = MagicMock()
-    mock_se = MagicMock()
-    mock_se.execute.return_value.all.return_value = None
-    update_disabled_users_status(mock_conn)
+def test_no_inactive_users(mock_logger, mock_session):
+    mock_session.execute.return_value.all.return_value = None
+    update_disabled_users_status(mock_session)
 
     mock_logger.info.assert_any_call("Checking for System User.")
     mock_logger.info.assert_any_call("Fetching inactive users.")
     mock_logger.info.assert_any_call("No inactive users found.")
+
+def test_valid_oidc_ids(mock_session):
+    mock_session.execute.return_value.scalar.side_effect = [1, 2, None]  # Mock responses for OIDC IDs
+
+    oidc_ids = ["oidc_1", "oidc_2", "oidc_3"]
+    expected_ids = [1, 2]
+
+    result = get_ids_from_oidc_ids(mock_session, oidc_ids)
+    assert result == expected_ids
+
+    empty_result = get_ids_from_oidc_ids(mock_session, [])
+    assert empty_result == []
+
+def test_invalid_oidc_id_type(mock_session):
+    oidc_ids = ["valid_oidc", 123, "another_valid_oidc"]
+
+    with pytest.raises(ValueError) as context:
+        get_ids_from_oidc_ids(mock_session, oidc_ids)
+
+    assert str(context.value) == "All oidc_ids must be strings."
