213dc136a33ba96aae225178e931d13550fdbddf
John DeAngelis <john.deangelis2@gmail.com>
2024-10-17 16:21:51 -0400

    setup DB metadata in fixture and create DB session
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/data_tools/tests/conftest.py
+++ backend/data_tools/tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm import Session, scoped_session, sessionmaker
+
+from models import BaseModel
 
 
 def is_responsive(db):
@@ -20,3 +23,21 @@ def db_service(docker_ip, docker_services):
     engine = create_engine(connection_string, echo=True, future=True)
     docker_services.wait_until_responsive(timeout=30.0, pause=0.1, check=lambda: is_responsive(engine))
     return engine
+
+
+@pytest.fixture()
+def loaded_db(db_service) -> Session:
+    """Get SQLAlchemy Session."""
+
+    BaseModel.metadata.drop_all(db_service)
+    BaseModel.metadata.create_all(db_service)
+
+    session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=db_service))
+
+    yield session
+
+    # cleanup
+    session.rollback()
+
+    session.commit()
+    session.close()
