2ac21534a273576cf84b91b001805cca1a7148ce
John DeAngelis <john.deangelis2@gmail.com>
2024-10-21 16:05:15 -0400

    use init_db from db module
    Signed-off-by: John DeAngelis <john.deangelis2@gmail.com>

--- backend/data_tools/src/common/utils.py
+++ backend/data_tools/src/common/utils.py
@@ -1,12 +1,13 @@
 from typing import Optional
 
+import data_tools.src.common.db as db_module
 import sqlalchemy
 from data_tools.environment.azure import AzureConfig
 from data_tools.environment.dev import DevConfig
 from data_tools.environment.local import LocalConfig
 from data_tools.environment.pytest import PytestConfig
 from data_tools.environment.types import DataToolsConfig
-from sqlalchemy import Engine, create_engine
+from sqlalchemy import Engine
 
 from models import BaseModel
 
@@ -15,9 +16,7 @@ def init_db(
     config: DataToolsConfig, db: Optional[Engine] = None
 ) -> tuple[sqlalchemy.engine.Engine, sqlalchemy.MetaData]:
     if not db:
-        engine = create_engine(
-            config.db_connection_string, echo=config.verbosity, future=True
-        )
+        _, engine = db_module.init_db(config.db_connection_string)
     else:
         engine = db
     return engine, BaseModel.metadata
