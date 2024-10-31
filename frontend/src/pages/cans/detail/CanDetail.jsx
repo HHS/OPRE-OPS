c88344fc256f1db26d367e53d29f10e1b888f39a
Frank Pigeon Jr <fpigeon@flexion.us>
2024-10-31 17:11:24 -0500

    refactor: adds can terms

--- frontend/src/pages/cans/detail/CanDetail.jsx
+++ frontend/src/pages/cans/detail/CanDetail.jsx
@@ -1,3 +1,8 @@
+import DebugCode from "../../../components/DebugCode";
+import Tag from "../../../components/UI/Tag";
+import Term from "../../../components/UI/Term";
+import TermTag from "../../../components/UI/Term/TermTag";
+
 /**
     @typedef {import("../../../components/CANs/CANTypes").CAN} CAN
 */
@@ -15,15 +20,62 @@
 const CanDetail = ({ can }) => {
     return (
         <article>
-            <h1>
-                {can?.number} ({can?.nick_name})
-            </h1>
-            <div className="grid-row">
-                <div className="grid-col">
-                    <h2>CAN description</h2>
-                    {can?.description}
+            <h2>CAN Details</h2>
+            <div className="grid-row font-12px">
+                {/* // NOTE: Left Column */}
+                <div
+                    className="grid-col"
+                    data-cy="details-left-col"
+                >
+                    <Term
+                        name="Description"
+                        value={can?.description || "TBD"}
+                    />
+                    <section data-cy="history">
+                        <h3 className="text-base-dark margin-top-3 text-normal font-12px">History</h3>
+                        <p>Not yet implemented</p>
+                    </section>
+                </div>
+                {/* // NOTE: Right Column */}
+                <div
+                    className="grid-col"
+                    data-cy="details-right-col"
+                >
+                    <TermTag
+                        term="CAN"
+                        description={can.number}
+                    />
+                    <TermTag
+                        term="Nickname"
+                        description={can.nick_name}
+                    />
+                    <TermTag
+                        term="Portfolio"
+                        description={can.portfolio?.name}
+                    />
+                    <dl>
+                        <dt className="margin-0 text-base-dark margin-top-3">Team Members</dt>
+                        {can.portfolio?.team_leaders &&
+                            can.portfolio?.team_leaders.length > 0 &&
+                            can.portfolio.team_leaders.map((teamLeader) => (
+                                <dd
+                                    key={teamLeader.id}
+                                    className="margin-0 margin-top-1 margin-bottom-2"
+                                >
+                                    <Tag
+                                        tagStyle="primaryDarkTextLightBackground"
+                                        text={teamLeader.full_name}
+                                    />
+                                </dd>
+                            ))}
+                    </dl>
+                    <TermTag
+                        term="Division Director"
+                        description="Coming Soon"
+                    />
                 </div>
             </div>
+            <DebugCode data={can} />
         </article>
     );
 };
