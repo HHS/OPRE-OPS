3021b250897ff94cd2325bef922038183b061671
Frank Pigeon Jr <fpigeon@flexion.us>
2024-10-31 11:22:54 -0500

    wip: continue CAN details page

--- frontend/src/pages/cans/detail/Can.jsx
+++ frontend/src/pages/cans/detail/Can.jsx
@@ -2,20 +2,42 @@ import { Route, Routes, useParams } from "react-router-dom";
 import CanDetail from "./CanDetail";
 import App from "../../../App";
 import CanDetailTabs from "../../../components/CANs/CanDetailTabs/CanDetailTabs";
+import { useGetCanByIdQuery } from "../../../api/opsAPI";
+import PageHeader from "../../../components/UI/PageHeader";
+
+/**
+    @typedef {import("../../../components/CANs/CANTypes").CAN} CAN
+*/
 
 const Can = () => {
     const urlPathParams = useParams();
     const canId = parseInt(urlPathParams.id || "-1");
+    /** @type {{data?: CAN | undefined, isLoading: boolean}} */
+    const { data: can, isLoading } = useGetCanByIdQuery(canId);
+
+    if (isLoading) {
+        return <div> Loading Can... </div>;
+    }
+    if (!can) {
+        return <div>Can not found</div>;
+    }
+
+    const subTitle = `${can.nick_name} - ${can.active_period} ${can?.active_period > 1 ? "Years" : "Year"}`;
 
     return (
-        <App breadCrumbName="CAN">
+        <App breadCrumbName={can.display_name}>
+            <PageHeader
+                title={can.display_name || "TBD"}
+                subTitle={subTitle}
+            />
+
             <section className="display-flex flex-justify margin-top-3">
                 <CanDetailTabs canId={canId} />
             </section>
             <Routes>
                 <Route
                     path=""
-                    element={<CanDetail />}
+                    element={<CanDetail can={can} />}
                 />
             </Routes>
         </App>
--- frontend/src/pages/cans/detail/CanDetail.jsx
+++ frontend/src/pages/cans/detail/CanDetail.jsx
@@ -1,26 +1,18 @@
-import { useSelector, useDispatch } from "react-redux";
-import { getCan } from "./getCan";
-import { useEffect } from "react";
-import CANBudgetSummary from "../../../components/CANs/CANBudgetSummary/CANBudgetSummary.jsx";
-import constants from "../../../constants";
-import { setCan } from "./canDetailSlice";
-import { useParams } from "react-router-dom";
+/**
+    @typedef {import("../../../components/CANs/CANTypes").CAN} CAN
+*/
 
-const CanDetail = () => {
-    const dispatch = useDispatch();
-    const can = useSelector((state) => state.canDetail.can);
-    const urlPathParams = useParams();
-    const canId = parseInt(urlPathParams.id);
+/**
+ * @typedef {Object} CanDetailProps
+ * @property {CAN} can
+ */
 
-    useEffect(() => {
-        const getCANAndSetState = async (canId) => {
-            const results = await getCan(canId);
-            dispatch(setCan(results));
-        };
-
-        getCANAndSetState(canId).catch(console.error);
-    }, [dispatch, canId]);
-
+/**
+ * @component - The CAN detail page.
+ * @param {CanDetailProps} props
+ * @returns  {JSX.Element} - The component JSX.
+ */
+const CanDetail = ({ can }) => {
     return (
         <article>
             <h1>
@@ -30,20 +22,6 @@ const CanDetail = () => {
                 <div className="grid-col">
                     <h2>CAN description</h2>
                     {can?.description}
-                    <h2>CAN purpose</h2>
-                    {can.purpose || constants.notFilledInText}
-                    <h2>Arrangement type</h2>
-                    {can?.arrangement_type?.name}
-                    <h2>Funding source</h2>
-                    {can.funding_source?.[0]?.nick_name || constants.notFilledInText}
-                    <h2>OPRE point of contact</h2>
-                    {can.authorizer?.name}
-                    <h2>OPRE division</h2>
-                    {can.division || constants.notFilledInText}
-                </div>
-
-                <div className="grid-col">
-                    <CANBudgetSummary />
                 </div>
             </div>
         </article>
