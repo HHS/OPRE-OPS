db3a8bedf35fa4213568ab8076f1c0132b668c89
Frank Pigeon Jr. <fpigeon@flexion.us>
2024-11-19 09:40:33 -0600

    feat: ✨adds CAN Funding Cards to CAN Funding page (#3084)
    * feat: adds CANFunding received card
    
    * feat: adds CANBudgetByFYCard
    
    * feat: adds LineBar component
    
    * style: updates chart colors

---
+++ frontend/src/components/CANs/CANBudgetByFYCard/CANBudgetByFYCard.jsx
@@ -0,0 +1,42 @@
+import Card from "../../UI/Cards/Card";
+import LineBar from "../../UI/DataViz/LineBar";
+import { summaryCard } from "./CANBudgetByFYCard.helpers";
+
+/**
+ *  @typedef {import("../../../components/CANs/CANTypes").FundingBudget} FundingBudget
+ */
+
+/**
+ * @typedef {Object} CANBudgetByFYCard
+ * @property {FundingBudget[]} fundingBudgets
+ */
+
+/**
+ * @component - The CAN Budget by Fiscal Year Card
+ * @param {CANBudgetByFYCard} props
+ * @returns  {JSX.Element} - The component JSX.
+ */
+const CANBudgetByFYCard = ({ fundingBudgets }) => {
+    const { chartData } = summaryCard(fundingBudgets);
+
+    return (
+        <>
+            <Card
+                title="CAN Budget by FY"
+                dataCy="can-budget-fy-card"
+            >
+                {chartData.map((item) => (
+                    <LineBar
+                        key={`budget-fy-${item.FY}`}
+                        color={item.color}
+                        ratio={item.ratio}
+                        title={`FY ${item.FY}`}
+                        total={item.total}
+                    />
+                ))}
+            </Card>
+        </>
+    );
+};
+
+export default CANBudgetByFYCard;
