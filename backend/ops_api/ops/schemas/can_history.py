60a4ab1c37edb3de993633bcde5d0fdba1de888c
rajohnson90 <rjohnson@flexion.us>
2025-01-07 16:26:24 -0600

    chore: adding marshmallow schema for can history list

---
+++ backend/ops_api/ops/schemas/can_history.py
@@ -0,0 +1,21 @@
+from marshmallow import EXCLUDE, Schema, fields
+from models import CANHistoryType
+
+
+class CANHistoryListSchema(Schema):
+    id = fields.Integer(required=True)
+    can_id = fields.Integer(required=True)
+    ops_event_id = fields.Integer(required=True)
+    history_title = fields.String(required=True)
+    history_message = fields.String(required=True)
+    timestamp = fields.String(required=True)
+    history_type = fields.Enum(CANHistoryType)
+
+
+class GetHistoryListQueryParametersSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE  # Exclude unknown fields
+
+    can_id = fields.Integer(required=True)
+    limit = fields.Integer(default=10, allow_none=True)
+    offset = fields.Integer(default=0, allow_none=True)
