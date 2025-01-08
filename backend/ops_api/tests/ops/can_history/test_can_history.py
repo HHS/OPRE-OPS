592c409d1c33e2e20943381d478d20074117df90
Santi-3rd <g.santiago3rd@gmail.com>
2025-01-08 15:27:55 -0700

    test: Created test data and first test for can history

--- backend/ops_api/tests/ops/can_history/test_can_history.py
+++ backend/ops_api/tests/ops/can_history/test_can_history.py
@@ -1,12 +1,16 @@
 import pytest
 
 from models import CANHistory
-
-# from ops.services.can_history import CANHistoryService
+from ops.services.can_history import CANHistoryService
 
 # from sqlalchemy import select
 
 
 @pytest.mark.usefixtures("app_ctx")
 def test_get_can_history(loaded_db):
-    loaded_db.query(CANHistory)
+    test_can_id = 500
+    count = loaded_db.query(CANHistory).where(CANHistory.can_id == test_can_id).count()
+    can_history_service = CANHistoryService()
+    # Set a limit higher than our test data so we can get all results
+    response = can_history_service.get(test_can_id, 1000, 0)
+    assert len(response) == count
