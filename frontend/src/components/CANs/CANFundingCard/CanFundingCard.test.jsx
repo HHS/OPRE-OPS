ff882fd596e52616d86b1c4dc875bfece51a1d78
maiyerlee <mlee@flexion.us>
2025-01-20 20:57:45 -0600

    test: fix fe data

--- frontend/src/components/CANs/CANFundingCard/CanFundingCard.test.jsx
+++ frontend/src/components/CANs/CANFundingCard/CanFundingCard.test.jsx
@@ -55,45 +55,47 @@ const canData = {
 
 const canFundingCardData = {
     available_funding: "14300000.00",
-    cans: [{
-        can: {
-            appropriation_date: "2023-10-01T00:00:00.000000Z",
-            active_period: 1,
-            arrangement_type: "OPRE_APPROPRIATION",
-            authorizer: 26,
-            authorizer_id: 26,
-            budget_line_items: [15011, 15017, 15020],
-            can_type: null,
-            created_by: null,
-            created_by_user: null,
-            created_on: "2024-07-29T14:44:58.757452Z",
-            description: "Social Science Research and Development",
-            display_name: "G99PHS9",
-            division_id: 6,
-            expiration_date: "2024-09-01T00:00:00.000000Z",
-            external_authorizer_id: null,
-            funding_sources: [26],
-            id: 502,
-            managing_portfolio: 8,
-            portfolio_id: 8,
-            nick_name: "SSRD",
-            number: "G99PHS9",
-            projects: [],
-            shared_portfolios: [],
-            updated_by: null,
-            updated_by_user: null,
-            updated_on: "2024-07-29T14:44:58.757452Z",
-            versions: [
-                {
-                    id: 502,
-                    transaction_id: 208
-                }
-            ]
-        },
-        carry_forward_label: "Carry-Forward",
-        expiration_date: "09/01/2024",
-    }],
-    carry_forward_funding: 0,
+    cans: [
+        {
+            can: {
+                appropriation_date: "2023-10-01T00:00:00.000000Z",
+                active_period: 1,
+                arrangement_type: "OPRE_APPROPRIATION",
+                authorizer: 26,
+                authorizer_id: 26,
+                budget_line_items: [15011, 15017, 15020],
+                can_type: null,
+                created_by: null,
+                created_by_user: null,
+                created_on: "2024-07-29T14:44:58.757452Z",
+                description: "Social Science Research and Development",
+                display_name: "G99PHS9",
+                division_id: 6,
+                expiration_date: "2024-09-01T00:00:00.000000Z",
+                external_authorizer_id: null,
+                funding_sources: [26],
+                id: 502,
+                managing_portfolio: 8,
+                portfolio_id: 8,
+                nick_name: "SSRD",
+                number: "G99PHS9",
+                projects: [],
+                shared_portfolios: [],
+                updated_by: null,
+                updated_by_user: null,
+                updated_on: "2024-07-29T14:44:58.757452Z",
+                versions: [
+                    {
+                        id: 502,
+                        transaction_id: 208
+                    }
+                ]
+            },
+            carry_forward_label: "Carry-Forward",
+            expiration_date: "09/01/2024"
+        }
+    ],
+    carry_forward_funding: "14300000.00",
     expected_funding: "5000000.00",
     in_draft_funding: 0,
     in_execution_funding: "2000000.00",
