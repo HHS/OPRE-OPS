69c197b1ec6c0b43184c5f8e941a8267535a50bf
Frank Pigeon Jr. <frank.pigeonjr@gmail.com>
2025-01-28 17:08:01 -0600

    fix: else case for no budget needs to trigger edit-mode

--- frontend/src/pages/cans/detail/Can.hooks.js
+++ frontend/src/pages/cans/detail/Can.hooks.js
@@ -105,6 +105,8 @@ export default function useCan() {
                     setModalProps(initialModalProps);
                 }
             });
+        } else {
+            setIsEditMode({ ...isEditMode, fundingPage: !isEditMode.fundingPage });
         }
     };
 
