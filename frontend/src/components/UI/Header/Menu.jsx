0477c1d3138c02dfd1cc49b12488b9bb184e35cf
Jonathan Nalley <jnalley@flexion.us>
2025-01-30 10:47:15 -0500

    chore: updating to use vite env variables to do the hiding

--- frontend/src/components/UI/Header/Menu.jsx
+++ frontend/src/components/UI/Header/Menu.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { CheckAuth } from "../../Auth/auth";
+import { CheckAuth } from "../../Auth/auth";
 import { useSelector } from "react-redux";
 
 export const Menu = () => {
-   // const isAuthorized = CheckAuth();
+    const isAuthorized = CheckAuth();
     const activeUser = useSelector((state) => state.auth.activeUser);
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
@@ -22,15 +22,11 @@ export const Menu = () => {
                 />
             </button>
             <ul className="usa-nav__primary usa-accordion">
-                {/* {isAuthorized ? (
+                {isAuthorized && import.meta.env.MODE === "development" (
                     <li className="usa-nav__primary-item">
                         <Link to="/portfolios/">Portfolios</Link>
                     </li>
-                ) : (
-                    <li>
-                        <span></span>
-                    </li>
-                )} */}
+                )}
                 <li className="usa-nav__primary-item">
                     <Link to="/cans/">CANs</Link>
                 </li>
