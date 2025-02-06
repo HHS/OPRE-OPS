8800a7195f67a10b341f6a1e650443e61cb5373c
weimiao67 <wmiao@flexion.us>
2025-02-05 21:39:54 -0700

    feat: adds feedback content

--- frontend/src/pages/help/Feedback.jsx
+++ frontend/src/pages/help/Feedback.jsx
@@ -1,8 +1,20 @@
 const Feedback = () => {
     return (
         <>
-            <h1>Feedback</h1>
-            <p>Coming Soon</p>
+            <h2>We&rsquo;d love to hear from you!</h2>
+            <p>
+                You’re feedback is important to us. If you have feedback on something we can improve or an idea for a
+                new feature, please let us know!
+            </p>
+            <p>
+                Click the link here to get started:{" "}
+                <a href="ops-feedback@formsgoogle.com">ops-feedback@formsgoogle.com</a>
+            </p>
+            <p>For immediate assistance:</p>
+            <ul>
+                <li>Call (555) 555-5555</li>
+                <li>Email <a href="mailto:acf-ops-help@acf.org">acf-ops-help@acf.org</a> </li>
+            </ul>
         </>
     );
 };
