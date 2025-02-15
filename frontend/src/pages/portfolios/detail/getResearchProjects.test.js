91d39984f41d2dd94b6edec71a97ccea0c5ed9c2
Jonathan Nalley <jnalley@flexion.us>
2025-02-15 01:56:49 -0500

    chore: updating to new acf-gov urls

--- frontend/src/pages/portfolios/detail/getResearchProjects.test.js
+++ frontend/src/pages/portfolios/detail/getResearchProjects.test.js
@@ -16,7 +16,7 @@ test("successfully gets the ResearchProject data with all query params", async (
             portfolio_id: 1,
             short_title: "",
             title: "African American Child and Family Research Center",
-            url: "https://www.acf.hhs.gov/opre/project/african-american-child-and-family-research-center"
+            url: "https://acf.gov/opre/project/african-american-child-and-family-research-center"
         }
     ];
 
