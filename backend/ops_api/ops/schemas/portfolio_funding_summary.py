b777e498e90cc2f3fa5df5c0cb5d67e554428878
maiyerlee <mlee@flexion.us>
2025-02-20 16:44:32 -0600

    fix: add response schema

--- backend/ops_api/ops/schemas/portfolio_funding_summary.py
+++ backend/ops_api/ops/schemas/portfolio_funding_summary.py
@@ -3,3 +3,17 @@ from marshmallow import Schema, fields
 
 class RequestSchema(Schema):
     fiscal_year = fields.Integer(allow_none=True)
+
+
+class FundingLineItem(Schema):
+    amount = fields.Float(required=True)
+    percent = fields.String(required=True)
+
+
+class ResponseSchema(Schema):
+    total_funding = fields.Nested(FundingLineItem)
+    carry_forward_funding = fields.Nested(FundingLineItem)
+    planned_funding = fields.Nested(FundingLineItem)
+    obligated_funding = fields.Nested(FundingLineItem)
+    in_execution_funding = fields.Nested(FundingLineItem)
+    available_funding = fields.Nested(FundingLineItem)
