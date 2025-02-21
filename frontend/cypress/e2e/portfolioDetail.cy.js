b4045151849d69ef8065895ac9a939726eaafa10
Frank Pigeon Jr. <frank.pigeonjr@gmail.com>
2025-02-21 07:03:22 -0600

    fix: update portfolio detail page URL and reduce wait time

--- frontend/cypress/e2e/portfolioDetail.cy.js
+++ frontend/cypress/e2e/portfolioDetail.cy.js
@@ -3,7 +3,7 @@ import { terminalLog, testLogin } from "./utils";
 
 beforeEach(() => {
     testLogin("system-owner");
-    cy.visit("/portfolios/1").wait(2000);
+    cy.visit("/portfolios/1/spending").wait(1000);
     cy.get("#fiscal-year-select").select("2021");
 });
 
