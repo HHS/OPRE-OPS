e27da8514939a4ed2f9d23ef39c60ab7c02a931a
John DeAngelis <john.deangelis2@gmail.com>
2025-03-11 17:10:13 -0400

    build: add dotenv; use dotenv in load scripts

--- backend/data_tools/src/load_cans/main.py
+++ backend/data_tools/src/load_cans/main.py
@@ -7,11 +7,14 @@ from data_tools.src.azure_utils.utils import get_csv
 from data_tools.src.common.db import init_db_from_config, setup_triggers
 from data_tools.src.common.utils import get_config, get_or_create_sys_user
 from data_tools.src.load_cans.utils import transform
+from dotenv import load_dotenv
 from loguru import logger
 from sqlalchemy import select, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from models import Portfolio
+
+load_dotenv(os.getenv("ENV_FILE", ".env"))
 
 # Set the timezone to UTC
 os.environ["TZ"] = "UTC"
@@ -24,8 +27,11 @@ format = (
     "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
     "<level>{message}</level>"
 )
-logger.add(sys.stdout, format=format, level="INFO")
-logger.add(sys.stderr, format=format, level="INFO")
+# Configure logger with global level set to INFO by default
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+logger.remove()  # Remove default handlers
+logger.configure(handlers=[{"sink": sys.stdout, "format": format, "level": LOG_LEVEL}])
+logger.add(sys.stderr, format=format, level=LOG_LEVEL)
 
 
 @click.command()
