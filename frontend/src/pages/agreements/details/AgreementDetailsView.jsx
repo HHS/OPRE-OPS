8bcce0d9166d87388ba5b15ceff3c02e7f0f6371
weimiao67 <wmiao@flexion.us>
2025-03-14 09:03:10 -0600

    fix: add contract type to agreement detail page

--- frontend/src/pages/agreements/details/AgreementDetailsView.jsx
+++ frontend/src/pages/agreements/details/AgreementDetailsView.jsx
@@ -63,6 +63,18 @@ const AgreementDetailsView = ({ agreement, projectOfficer }) => {
                                 text={convertCodeForDisplay("agreementType", agreement?.agreement_type)}
                             />
                         </dd>
+                        {agreement?.contract_type && (
+                            <>
+                                <dt className="margin-0 text-base-dark margin-top-3">Contract Type</dt>
+                                <dd className="margin-0 margin-top-1">
+                                    <Tag
+                                        tagStyle="primaryDarkTextLightBackground"
+                                        text={convertCodeForDisplay("contractType", agreement?.contract_type)}
+                                    />
+                                </dd>
+                            </>
+                        )}
+
                         <dt className="margin-0 text-base-dark margin-top-3">Product Service Code</dt>
                         <dd className="margin-0 margin-top-1">
                             <Tag
