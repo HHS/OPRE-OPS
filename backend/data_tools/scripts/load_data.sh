f2d85ad45fcf1817e9201a616680e1ec6d000bf9
John DeAngelis <john.deangelis2@gmail.com>
2025-03-17 16:30:50 -0400

    chore: add load_data.sh

--- backend/data_tools/scripts/load_data.sh
+++ backend/data_tools/scripts/load_data.sh
@@ -4,16 +4,18 @@ set -eo pipefail
 export PYTHONPATH=.:$PYTHONPATH
 
 ENV=$1
-INPUT_CSV=$2
+DATA_TYPE=$2
+INPUT_CSV=$3
 
 echo "Activating virtual environment..."
 . .venv/bin/activate
 
 echo "ENV is $ENV"
+echo "DATA_TYPE is $DATA_TYPE"
 echo "INPUT_CSV is $INPUT_CSV"
 
 echo "Running script..."
 python data_tools/src/load_data.py \
 --env "${ENV}" \
---type grants \
+--type "${DATA_TYPE}" \
 --input-csv "${INPUT_CSV}"
