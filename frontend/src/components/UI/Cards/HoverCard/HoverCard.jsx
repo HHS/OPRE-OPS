6825f923893bcadc7b703e5dc81a91a0df619db1
Frank Pigeon Jr. <frank.pigeonjr@gmail.com>
2025-03-19 19:17:01 -0500

    feat: adds hover state

--- frontend/src/components/UI/Cards/HoverCard/HoverCard.jsx
+++ frontend/src/components/UI/Cards/HoverCard/HoverCard.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 /**
  * @typedef {Object} HoverCardProps
  * @property {string} title - The title of the card.
@@ -13,6 +14,8 @@
  */
 
 const HoverCard = ({ title, description, variant, level = 3 }) => {
+    const [isHovered, setIsHovered] = React.useState(false);
+
     if (typeof level !== "number" || level < 1 || level > 6) {
         throw new Error(`Unrecognized heading level: ${level}`);
     }
@@ -22,7 +25,10 @@ const HoverCard = ({ title, description, variant, level = 3 }) => {
         <div
             className={`usa-card grid-col-4 display-flex flex-column flex-align-center ${variant === "dark" ? "bg-brand-primary-dark text-white" : ""}`}
             style={{ padding: "53px 23px" }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
         >
+            {isHovered ? <span className="text-red">active</span> : <span className="text-red">inactive</span>}
             {/* @ts-ignore */}
             <HeadingTag className="margin-0 margin-bottom-1 font-sans-lg">{title}</HeadingTag>
             <p className="text-center margin-0">{description}</p>
