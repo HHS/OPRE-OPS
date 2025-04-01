34ab8869d7972f479ca04ff858f3631ba90d08c3
weimiao67 <wmiao@flexion.us>
2025-04-01 11:17:48 -0600

    test: update CAN list filtering tests for budget values and adjust wait times

--- frontend/cypress/e2e/canList.cy.js
+++ frontend/cypress/e2e/canList.cy.js
@@ -3,8 +3,11 @@ import { terminalLog, testLogin } from "./utils";
 
 beforeEach(() => {
     testLogin("division-director");
-    cy.visit("/cans").wait(1000);
+    cy.visit("/cans").wait(2000);
     cy.get("#fiscal-year-select").select("2023");
+    cy.get("button").contains("Filter").click();
+    cy.get("button").contains("Reset").click();
+    cy.get("button").contains("Apply").click();
 });
 
 afterEach(() => {
@@ -200,7 +203,7 @@ describe("CAN List Filtering", () => {
         cy.get("tbody").find("tr").should("have.length.greaterThan", 3);
     });
 
-    it("fiscal year filtering with FY budgets equalling 500,000", () => {
+    it("fiscal year filtering with FY budgets equalling 5,000,000", () => {
         cy.get("button").contains("Filter").click();
 
         cy.get(".sc-blHHSb").within(() => {
@@ -219,7 +222,7 @@ describe("CAN List Filtering", () => {
         cy.get("button").contains("Apply").click();
 
         cy.get("tbody").find("tr").should("have.length.above", 0);
-        cy.get("[data-cy='line-graph-with-legend-card']").contains("$ 500,000.00");
+        cy.get("[data-cy='line-graph-with-legend-card']").contains("500,000.00");
     });
 
     it("fiscal year filtering with FY budgets over 5,000,000", () => {
@@ -241,7 +244,7 @@ describe("CAN List Filtering", () => {
         cy.get("button").contains("Apply").click();
 
         cy.get("tbody").find("tr").should("have.length.above", 0);
-        cy.get("[data-cy='line-graph-with-legend-card']").contains("$ 70,000,000.00");
+        cy.get("[data-cy='line-graph-with-legend-card']").contains("78,200,000.00");
     });
 
     it("fiscal year filtering with FY budgets under 1,450,000", () => {
