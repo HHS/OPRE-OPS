7965ab76c6618079502ead141e74de541ece0f40
Frank Pigeon Jr. <fpigeon@flexion.us>
2025-04-10 10:21:38 -0500

    refactor: update PortfolioList component to use API query and improve layout

--- frontend/src/pages/portfolios/list/PortfolioList.jsx
+++ frontend/src/pages/portfolios/list/PortfolioList.jsx
@@ -1,45 +1,53 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useGetPortfoliosQuery } from "../../../api/opsAPI";
 import App from "../../../App";
-import { getPortfolioList } from "./getPortfolioList";
+import Card from "../../../components/UI/Cards/Card";
 
+/**
+ * @typedef {import("../../../components/Portfolios/PortfolioTypes").Portfolio} Portfolio
+ * @typedef {import("../../../components/Portfolios/PortfolioTypes").Division} Division
+ */
+
+/**
+ * Component that displays a list of portfolios grouped by division
+ * @returns {JSX.Element} The rendered component
+ */
 const PortfolioList = () => {
-    const dispatch = useDispatch();
-    const portfolioList = useSelector((state) => state.portfolioList.portfolios);
+    const { data: portfolios, isLoading } = useGetPortfoliosQuery({});
 
-    const tableClasses = "usa-table usa-table--borderless margin-x-auto";
+    /** @type {Record<string, Portfolio[]>} */
+    const porfolioListGroupedByDivision = portfolios?.reduce((acc, portfolio) => {
+        const division = portfolio.division.name;
+        if (!acc[division]) {
+            acc[division] = [];
+        }
+        acc[division].push(portfolio);
+        return acc;
+    }, {});
 
-    useEffect(() => {
-        dispatch(getPortfolioList());
-    }, [dispatch]);
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
     return (
-        <App>
-            <h1 className="text-center">Portfolios</h1>
-            <nav>
-                <table className={tableClasses}>
-                    <caption>List of all Portfolios</caption>
-                    <thead>
-                        <tr>
-                            <th scope="col">name</th>
-                            <th scope="col">status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {portfolioList.map((portfolio) => (
-                            <tr key={portfolio.id}>
-                                <th scope="row">
-                                    <Link to={"./" + portfolio.id}>{portfolio.name}</Link>
-                                </th>
-                                <td>{portfolio.status}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </nav>
+        <App breadCrumbName="Portfolios">
+            <h1 className="margin-0 text-brand-primary font-sans-2xl">Portfolios</h1>
 
-            <Outlet />
+            {Object.keys(porfolioListGroupedByDivision).map((division) => (
+                <section key={division}>
+                    <h2 className="font-12px text-base-dark">{division}</h2>
+                    {porfolioListGroupedByDivision[division].map((portfolio) => (
+                        <Link
+                            key={portfolio.id}
+                            to={`/portfolios/${portfolio.id}/spending`}
+                        >
+                            <Card>
+                                <h3 className="font-sans-lg">{portfolio.name}</h3>
+                            </Card>
+                        </Link>
+                    ))}
+                </section>
+            ))}
         </App>
     );
 };
