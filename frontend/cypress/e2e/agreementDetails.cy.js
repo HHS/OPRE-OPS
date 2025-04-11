0575b5fda9ab202a3a4ce7935de6b475bf2ea43c
weimiao67 <wmiao@flexion.us>
2025-04-11 09:14:41 -0600

    fix: update agreement detail tests for contract types and improve alert handling

--- frontend/cypress/e2e/agreementDetails.cy.js
+++ frontend/cypress/e2e/agreementDetails.cy.js
@@ -3,7 +3,6 @@ import { terminalLog, testLogin } from "./utils";
 
 beforeEach(() => {
     testLogin("system-owner");
-    cy.visit("/agreements/9");
 });
 
 afterEach(() => {
@@ -11,12 +10,16 @@ afterEach(() => {
     cy.checkA11y(null, null, terminalLog);
 });
 
-it("agreement loads with details", () => {
+it("Contract type agreement loads with details", () => {
     cy.visit("/agreements/1");
+    cy.wait(2000);
     cy.get('[data-cy="alert"]').should("exist");
-    cy.get('[data-cy="alert"]').find("exist");
+    cy.get('[data-cy="close-alert"]').click();
     cy.get("h1").contains("Contract #1: African American Child and Family Research Center");
     cy.get("h2").first().contains("Human Services Interoperability Support");
+    cy.get('[data-cy="details-tab-Award & Modifications"]').should("be.disabled");
+    cy.get('[data-cy="details-tab-Procurement Tracker"]').should("be.disabled");
+    cy.get('[data-cy="details-tab-Documents"]').should("be.disabled");
     cy.get("h2").eq(1).contains("Agreement Details");
     cy.get('[data-cy="details-left-col"] > :nth-child(1) > .text-base-dark').contains("Description");
     cy.get('[data-cy="details-right-col"] > :nth-child(1) > :nth-child(1)').contains("Agreement Type");
@@ -32,7 +35,24 @@ it("agreement loads with details", () => {
     cy.get("p.font-12px").contains("There are currently no notes for this agreement.");
 });
 
+it("Non contract type agreement loads with details", () => {
+    cy.visit("/agreements/11");
+    cy.get('[data-cy="alert"]').should("exist");
+    cy.get('[data-cy="close-alert"]').click();
+    cy.get("h1").contains("Support Contract #1");
+    cy.get("h2").first().contains("Support Project #1");
+    cy.get("h2").eq(1).contains("Agreement Details");
+    cy.get('[data-cy="details-left-col"] > :nth-child(1) > .text-base-dark').contains("Description");
+    cy.get('[data-cy="details-right-col"] > :nth-child(1) > :nth-child(1)').contains("Agreement Type");
+    cy.get('[data-cy="details-right-col"] > :nth-child(1) > :nth-child(2) > .font-12px').contains("Contract");
+    cy.get('[data-cy="details-right-col"] > :nth-child(2) > :nth-child(1)').contains("COR");
+    cy.get('[data-cy="details-right-col"] > :nth-child(2) > :nth-child(2)').contains("Alternate COR");
+    cy.get('[data-cy="details-right-col"] > :nth-child(3) > :nth-child(1)').contains("Team Members");
+
+});
+
 it("agreement loads with budget lines", () => {
+    cy.visit("/agreements/9");
     cy.get('[data-cy="details-tab-SCs & Budget Lines"]').click();
     cy.get('[data-cy="currency-summary-card"]').contains("Agreement Total");
     cy.get('[data-cy="currency-summary-card"]').contains("$ 1,005,000.00"); // agreement total
@@ -46,13 +66,14 @@ it("agreement loads with budget lines", () => {
 });
 
 it("should not warn when not making changes to agreement and tabbing to BLI tab", () => {
-    // cy.visit("/agreements/9");
+    cy.visit("/agreements/9");
     cy.get("#edit").click();
     cy.get('[data-cy="details-tab-SCs & Budget Lines"]').click();
     cy.get("#ops-modal").should("not.exist");
 });
 
 it("should warn when making changes to agreement and tabbing out", () => {
+    cy.visit("/agreements/9");
     cy.get("#edit").click();
     cy.get("#contract-type").select("Firm Fixed Price (FFP)");
     cy.get('[data-cy="details-tab-Agreement Details"]').click();
@@ -60,6 +81,7 @@ it("should warn when making changes to agreement and tabbing out", () => {
 });
 
 it("should handle cancel edits", () => {
+    cy.visit("/agreements/9");
     // Agreement Details Tab
     cy.get("#edit").click();
     cy.get("#contract-type").select("Firm Fixed Price (FFP)");
