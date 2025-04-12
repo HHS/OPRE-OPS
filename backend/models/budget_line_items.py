7854bab19cd547a899785c48119c44579d2e4cd2
Jonathan Nalley <jnalley@flexion.us>
2025-04-11 20:48:54 -0400

    chore: typo

--- backend/models/budget_line_items.py
+++ backend/models/budget_line_items.py
@@ -278,7 +278,7 @@ class StateCode(Enum):
     SC = "South Carolina"
     SD = "South Dakota"
     TN = "Tennessee"
-    TX - "Texas"
+    TX = "Texas"
     VT = "Vermont"
     VA = "Virginia"
     WA = "Washington"
