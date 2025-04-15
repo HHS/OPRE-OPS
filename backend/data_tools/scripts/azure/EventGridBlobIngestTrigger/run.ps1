7027e3c622d0c55e2e1417013cae907121b741c5
rajohnson90 <rjohnson@flexion.us>
2025-04-15 15:39:33 -0500

    chore: work in progress powershell script to run az container app job. still need image ver from env

---
+++ backend/data_tools/scripts/azure/EventGridBlobIngestTrigger/run.ps1
@@ -0,0 +1,6 @@
+# Input bindings are passed in via param block.
+param([byte[]] $InputBlob, $TriggerMetadata)
+
+# Write out the blob name and size to the information log.
+Write-Host "PowerShell Blob trigger function Processed blob! Name: $($TriggerMetadata.Name) Size: $($InputBlob.Length) bytes"
+az containerapp job start --name "opre-ops-dev-app-blis-myltest" --resource-group "opre-ops-dev-app-rg" --cpu 0.5 --memory "1Gi" --image ghcr.io/hhs/opre-ops/ops-data-tools:unstable --command "/bin/sh" "./data_tools/src/load_data.sh" --args "dev" "budget_lines" $TriggerMetadata.Name
