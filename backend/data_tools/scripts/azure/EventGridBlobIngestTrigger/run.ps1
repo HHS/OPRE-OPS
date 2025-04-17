98f3627f6b9db021ecc8919ef030d8e68dd272c4
rajohnson90 <rjohnson@flexion.us>
2025-04-17 15:57:20 -0500

    chore: fixing format to call load_data.sh script with specific python image

--- backend/data_tools/scripts/azure/EventGridBlobIngestTrigger/run.ps1
+++ backend/data_tools/scripts/azure/EventGridBlobIngestTrigger/run.ps1
@@ -3,4 +3,5 @@ param([byte[]] $InputBlob, $TriggerMetadata)
 
 # Write out the blob name and size to the information log.
 Write-Host "PowerShell Blob trigger function Processed blob! Name: $($TriggerMetadata.Name) Size: $($InputBlob.Length) bytes"
-az containerapp job start --name "opre-ops-dev-app-blis-myltest" --resource-group "opre-ops-dev-app-rg" --cpu 0.5 --memory "1Gi" --image ghcr.io/hhs/opre-ops/ops-data-tools:unstable --command "/bin/sh" "./data_tools/src/load_data.sh" --args "dev" "budget_lines" $TriggerMetadata.Name
+az containerapp job start --name "opre-ops-dev-app-blis-myltest" --resource-group "opre-ops-dev-app-rg" --cpu 0.5 --memory "1Gi" --image ghcr.io/hhs/opre-ops/ops-data-tools:d1d5a0d742c8d60797cdc1ba51b6fe85cf8b0fd9 --command "/bin/sh" "./data_tools/scripts/load_data.sh" --args "dev" "master_spreadsheet_budget_lines" $TriggerMetadata.Name
+# az containerapp job start --name "opre-ops-dev-app-blis-myltest" --resource-group "opre-ops-dev-app-rg" --cpu 0.5 --memory "1Gi" --image ghcr.io/hhs/opre-ops/ops-data-tools:d1d5a0d742c8d60797cdc1ba51b6fe85cf8b0fd9 --command "ls" "./data_tools/scripts"
