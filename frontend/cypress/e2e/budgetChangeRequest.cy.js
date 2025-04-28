e67270da7ef255e1aaf0593c89cb49c8464d45a7
Frank Pigeon Jr <fpigeon@flexion.us>
2025-04-28 09:31:10 -0500

    chore: add TODO to refactor flaky test in "Budget Change in review"

--- frontend/cypress/e2e/budgetChangeRequest.cy.js
+++ frontend/cypress/e2e/budgetChangeRequest.cy.js
@@ -433,6 +433,7 @@ describe("Budget Change Requests", () => {
     });
 });
 
+// TODO: Refactor this test to not be so flaky
 describe.skip("Budget Change in review", () => {
     // testing with agreement 9
     it("should allow editing an agreement if any budget lines are in review", () => {
