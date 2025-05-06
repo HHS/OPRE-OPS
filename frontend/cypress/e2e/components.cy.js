cd32e48c493fa25a3fb865ecd521877a37c9e60d
Frank Pigeon Jr <fpigeon@flexion.us>
2025-05-06 10:32:10 -0500

    refactor: remove redundant date range picker test and update visit path for existing tests

--- frontend/cypress/e2e/components.cy.js
+++ frontend/cypress/e2e/components.cy.js
@@ -12,15 +12,8 @@ afterEach(() => {
 });
 
 describe("DateRangePicker", () => {
-    it("should display date range picker", () => {
-        cy.visit("/agreements/1/budget-lines");
-        cy.get("#edit").click();
-        cy.get("#pop-start-date").should("exist");
-        cy.get("#pop-end-date").should("exist");
-    });
-
     it("should error if end date is not before the start date", () => {
-        cy.visit("/agreements/1/budget-lines");
+        cy.visit("/agreements/9/budget-lines");
         cy.get("#edit").click();
         cy.get("#servicesComponentSelect").select("4");
         cy.get("#pop-start-date").type("01/01/2020");
@@ -31,7 +24,7 @@ describe("DateRangePicker", () => {
         cy.get("h2").contains("Services Component 4").should("not.exist");
     });
     it('should error if date is not in the format "MM/DD/YYYY"', () => {
-        cy.visit("/agreements/1/budget-lines");
+        cy.visit("/agreements/9/budget-lines");
         cy.get("#edit").click();
         cy.get("#servicesComponentSelect").select("4");
         cy.get("#pop-start-date").type("tacocat");
