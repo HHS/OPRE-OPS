b5b06e5fbd35ec37e621fea8fed49a52cda2eb49
John DeAngelis <john.deangelis2@gmail.com>
2025-05-14 10:26:32 -0400

    feat: add current_fee property to ProcurementShop and add type hints

--- backend/models/procurement_shops.py
+++ backend/models/procurement_shops.py
@@ -1,6 +1,5 @@
 """Module containing the Procurement Shop model."""
 
-import decimal
 from decimal import Decimal
 from typing import Optional
 
@@ -9,6 +8,35 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from models.base import BaseModel
+
+
+class ProcurementShopFee(BaseModel):
+    """The Procurement Shop Fee model."""
+
+    __tablename__ = "procurement_shop_fee"
+
+    __table_args__ = (
+        UniqueConstraint(
+            "procurement_shop_id",
+            "end_date",
+            name="procurement_shop_fee_unique_active_period",
+        ),
+    )
+
+    id = BaseModel.get_pk_column()
+    procurement_shop_id: Mapped[int] = mapped_column(
+        ForeignKey("procurement_shop.id"), nullable=False
+    )
+    # fee percentage - if the fee is 5%, it should be stored as 5.0
+    fee: Mapped[Optional[Decimal]] = mapped_column(Numeric(12, 2), default=Decimal(0.0))
+    procurement_shop = relationship(
+        "ProcurementShop", back_populates="procurement_shop_fees"
+    )
+    start_date: Mapped[Optional[Date]] = mapped_column(Date(), nullable=True)
+    end_date: Mapped[Optional[Date]] = mapped_column(Date(), nullable=True)
+    budget_line_items = relationship(
+        "BudgetLineItem", back_populates="procurement_shop_fee"
+    )
 
 
 class ProcurementShop(BaseModel):
@@ -25,7 +53,7 @@ class ProcurementShop(BaseModel):
     )
 
     @hybrid_property
-    def fee_percentage(self):
+    def fee_percentage(self) -> Decimal:
         """Get the current fee percentage for the procurement shop based on the current date."""
         from datetime import date
 
@@ -39,7 +67,7 @@ class ProcurementShop(BaseModel):
         return active_fees[0].fee if active_fees else Decimal(0.0)
 
     @fee_percentage.expression
-    def fee_percentage(cls):
+    def fee_percentage(cls) -> Decimal:
         """Get the current fee percentage for the procurement shop based on the current date."""
         from sqlalchemy import func, select
         from sqlalchemy.orm import aliased
@@ -64,35 +92,45 @@ class ProcurementShop(BaseModel):
         # Return the active fee or 0 if none exists
         return func.coalesce(subq, Decimal("0.0"))
 
+    @hybrid_property
+    def current_fee(self) -> Optional[ProcurementShopFee]:
+        """Get the current fee for the procurement shop based on the current date."""
+        from datetime import date
+
+        today = date.today()
+        active_fees = [
+            fee
+            for fee in self.procurement_shop_fees
+            if (fee.start_date is None or fee.start_date <= today)
+            and (fee.end_date is None or today <= fee.end_date)
+        ]
+        return active_fees[0] if active_fees else None
+
+    @current_fee.expression
+    def current_fee(cls) -> Optional[ProcurementShopFee]:
+        """Get the current fee for the procurement shop based on the current date."""
+        from sqlalchemy import func, select
+        from sqlalchemy.orm import aliased
+
+        today = func.current_date()
+        ProcurementShopFeeAlias = aliased(ProcurementShopFee)
+
+        # Subquery to find the active fee for each procurement shop
+        subq = (
+            select(ProcurementShopFeeAlias)
+            .where(
+                ProcurementShopFeeAlias.procurement_shop_id == cls.id,
+                ProcurementShopFeeAlias.start_date <= today,
+                (ProcurementShopFeeAlias.end_date == None)
+                | (ProcurementShopFeeAlias.end_date >= today),
+            )
+            .order_by(ProcurementShopFeeAlias.start_date.desc())
+            .limit(1)
+        )
+
+        # Return the active fee or None if none exists
+        return subq.scalar_subquery()
+
     @BaseModel.display_name.getter
     def display_name(self):
         return self.name
-
-
-class ProcurementShopFee(BaseModel):
-    """The Procurement Shop Fee model."""
-
-    __tablename__ = "procurement_shop_fee"
-
-    __table_args__ = (
-        UniqueConstraint(
-            "procurement_shop_id",
-            "end_date",
-            name="procurement_shop_fee_unique_active_period",
-        ),
-    )
-
-    id = BaseModel.get_pk_column()
-    procurement_shop_id: Mapped[int] = mapped_column(
-        ForeignKey("procurement_shop.id"), nullable=False
-    )
-    # fee percentage - if the fee is 5%, it should be stored as 5.0
-    fee: Mapped[Optional[decimal]] = mapped_column(Numeric(12, 2), default=Decimal(0.0))
-    procurement_shop = relationship(
-        "ProcurementShop", back_populates="procurement_shop_fees"
-    )
-    start_date: Mapped[Optional[Date]] = mapped_column(Date(), nullable=True)
-    end_date: Mapped[Optional[Date]] = mapped_column(Date(), nullable=True)
-    budget_line_items = relationship(
-        "BudgetLineItem", back_populates="procurement_shop_fee"
-    )
