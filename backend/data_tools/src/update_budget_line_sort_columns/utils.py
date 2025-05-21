98e5f168bad67fc56579c4f8e35ad340036484c1
rajohnson90 <rjohnson@flexion.us>
2025-05-21 15:10:40 -0500

    chore: more setup work for accessing db in script

--- backend/data_tools/src/update_budget_line_sort_columns/utils.py
+++ backend/data_tools/src/update_budget_line_sort_columns/utils.py
@@ -1,7 +1,9 @@
 import os
+import sys
 
 from data_tools.src.common.db import init_db_from_config, setup_triggers
 from data_tools.src.import_static_data.import_data import get_config
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 from models import *  # noqa: F403, F401
 
@@ -12,5 +14,15 @@ if __name__ == "__main__":
     script_config = get_config(script_env)
     db_engine, db_metadata_obj = init_db_from_config(script_config)
 
+    if db_engine is None:
+        logger.error("Failed to initialize the database engine.")
+        sys.exit(1)
+
+    with db_engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
+        logger.info("Successfully connected to the database.")
+
+
+    Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=db_engine))
 
     logger.info("Update of Budget Lines and Service Component columns complete.")
