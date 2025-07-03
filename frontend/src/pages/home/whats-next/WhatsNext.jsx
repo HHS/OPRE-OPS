ae9e27c2b00e0e435c803634797a40a3eb70aed7
Frank Pigeon Jr <fpigeon@flexion.us>
2025-07-03 14:41:09 -0500

    docs: updates content with a little help from UX

--- frontend/src/pages/home/whats-next/WhatsNext.jsx
+++ frontend/src/pages/home/whats-next/WhatsNext.jsx
@@ -3,7 +3,7 @@ import WhatsNextTable from "./WhatsNextTable";
 const WhatsNext = () => {
     return (
         <>
-            <h1>What&apos;s Next</h1>
+            <h1 className="font-24px">What&apos;s Next</h1>
             <p>
                 This is a list of what upcoming features will be available in OPS soon including their status.
                 Priorities are subject to change based on decisions by leadership.
--- frontend/src/pages/home/whats-next/data.js
+++ frontend/src/pages/home/whats-next/data.js
@@ -10,19 +10,41 @@ export const data = [
     {
         id: 1,
         priority: 1,
-        title: "AAs",
-        levelOfEffort: "Large",
-        status: "In Progress-Testing",
-        expandedHeading: "Feature Functionality",
-        expandedDescription: "Viewing, creating and editing Assisted Acquisitions (AAs) agreements"
+        title: "Data clean-up",
+        levelOfEffort: "Medium",
+        status: "In-Progress-Development",
+        expandedHeading: "Description",
+        expandedDescription:
+            "The budget team has been working with each division to update data as needed. The OPS team is regularly importing these updates from the budget spreadsheet into OPS."
     },
     {
         id: 2,
         priority: 2,
-        title: "Grants",
+        title: "Procurement shops & fees",
         levelOfEffort: "Large",
-        status: "In Progress-Research",
-        expandedHeading: "Feature Functionality",
-        expandedDescription: "Viewing, creating and editing Grants"
+        status: "In Progress-Development",
+        expandedHeading: "Description",
+        expandedDescription:
+            "We are working to make the procurement shop fee rates dynamic, so that if a fee changes, all budget lines using that shop will automatically receive the new fee rate. There will also be an approval required to change the procurement shop on an agreement, as it changes the total agreement amount. Currently, you will see Fee Amounts on each budget line, but the Agreement Total does not yet include the fees."
+    },
+    {
+        id: 3,
+        priority: 3,
+        title: "Assisted Acquisitions",
+        levelOfEffort: "Large",
+        status: "In Progress-Design",
+        expandedHeading: "Description",
+        expandedDescription:
+            "OPS has so far been mostly developed around contracts, but we are now working to add in other agreement types, starting with assisted acquisitions. This means you’ll be able to create, view, and edit assisted acquisitions in addition to contracts."
+    },
+    {
+        id: 4,
+        priority: 4,
+        title: "Login improvements",
+        levelOfEffort: "Small",
+        status: "In Progress-Design",
+        expandedHeading: "Description",
+        expandedDescription:
+            "We are working on an improvement to the login experience so you can see when there are issues that require further action to resolve"
     }
 ];
