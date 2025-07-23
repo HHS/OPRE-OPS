1d81eec1fb774b19ab2e0cbee539ec7a6428c17b
Frank Pigeon Jr <fpigeon@flexion.us>
2025-07-23 10:14:12 -0500

    docs: update the change requests type
    updated from response on an Agreement CR

--- frontend/src/types/ChangeRequestsTypes.d.ts
+++ frontend/src/types/ChangeRequestsTypes.d.ts
@@ -1,6 +1,6 @@
 export type ChangeRequest = {
     agreement_id: number;
-    budget_line_item_id: number;
+    budget_line_item_id?: number;
     change_request_type: string;
     created_by: number;
     created_by_user: {
@@ -9,15 +9,16 @@ export type ChangeRequest = {
     };
     created_on: string;
     display_name: string;
-    has_budget_change: boolean;
-    has_status_change: boolean;
+    has_budget_change?: boolean;
+    has_status_change?: boolean;
     id: number;
-    managing_division_id: number;
+    managing_division_id: number | null;
     requested_change_data: {
-        amount: number;
-        date_needed: string;
-        can_id: number;
-        status: string;
+        amount?: number;
+        date_needed?: string;
+        can_id?: number;
+        status?: string;
+        awarding_entity_id?: number;
     };
     requested_change_diff: {
         amount?: {
@@ -44,7 +45,7 @@ export type ChangeRequest = {
     requestor_notes: string | null;
     reviewed_on: string | null;
     reviewer_notes: string | null;
-    status: string;
+    status: string; // TODO: what are the status options?
     updated_by: number;
     updated_on: string;
 };
