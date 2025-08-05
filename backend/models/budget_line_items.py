53dcbfbf14fef1897cf327ef74a055df23d7b951
maiyerlee <mlee@flexion.us>
2025-08-05 13:49:48 -0500

    feat: update in_review logic to include agreement change requests
    Signed-off-by: maiyerlee <mlee@flexion.us>

--- backend/models/budget_line_items.py
+++ backend/models/budget_line_items.py
@@ -228,13 +228,29 @@ class BudgetLineItem(BaseModel):
 
         agreement_id = self.agreement_id if hasattr(self, 'agreement_id') else None
 
-        stmt = select(BudgetLineItemChangeRequest).where(
+        bli_stmt = select(BudgetLineItemChangeRequest).where(
             BudgetLineItemChangeRequest.status == ChangeRequestStatus.IN_REVIEW,
             BudgetLineItemChangeRequest.change_request_type == ChangeRequestType.BUDGET_LINE_ITEM_CHANGE_REQUEST,
             BudgetLineItemChangeRequest.budget_line_item_id == self.id,
         )
 
-        results = session.execute(stmt).all()
+        agreement_stmt = select(AgreementChangeRequest).where(
+            AgreementChangeRequest.status == ChangeRequestStatus.IN_REVIEW,
+            AgreementChangeRequest.change_request_type == ChangeRequestType.AGREEMENT_CHANGE_REQUEST,
+            AgreementChangeRequest.agreement_id == agreement_id,
+            # ~AgreementChangeRequest.id.in_(
+            #     select(BudgetLineItemChangeRequest.id).where(
+            #         BudgetLineItemChangeRequest.budget_line_item_id == self.id
+            #     )
+            # ),
+        ) if agreement_id else None
+
+        results = session.execute(bli_stmt).all()
+
+        if agreement_stmt is not None:
+            agreement_stmt_results = session.execute(agreement_stmt).all()
+            results.extend(agreement_stmt_results)
+
         change_requests = [row[0] for row in results] if results else None
         return change_requests
 
