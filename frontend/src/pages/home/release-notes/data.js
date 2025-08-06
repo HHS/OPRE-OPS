fac646179e628c318bc5332040384bf2dde11441
Kim Schulke <65555918+kimschulke@users.noreply.github.com>
2025-08-06 16:53:17 -0400

    docs: update changelog
    updated release date and whats news

--- frontend/src/pages/home/release-notes/data.js
+++ frontend/src/pages/home/release-notes/data.js
@@ -7,43 +7,37 @@ NOTE: types of changes are 'New Feature', 'Improvements', 'Fixes'
 */
 export const data = [
     {
-        releaseDate: "2025-07-02",
-        version: "1.135.0",
+        releaseDate: "2025-07-23",
+        version: "1.147.1",
         changes: [
             {
                 id: "0001",
-                subject: "OPS Release Notes & What's Next",
+                subject: "Overcome by Events (OBE) budget lines",
                 type: "New Feature",
                 description:
-                    "You can now view details about what's new and what's next directly in OPS instead of referencing the google document."
+                    "Any budget lines that have been defined as “overcome by events” or no longer happening have been labeled as such. These budget lines remain included in the agreement total, but are not subtracted from any CANs or your budget."
             },
             {
                 id: "0002",
-                subject: "Sorting on Table Columns",
-                type: "Improvements",
+                subject: "Editing procurement shops with approvals",
+                type: "New Feature",
                 description:
-                    "You can now click on any column in a table to change how the table is sorted from top to bottom."
+                    "Now when you edit a procurement shop on an agreement, it will trigger an approval from your Division Director so they can maintain oversight on budget changes and any changes to budget line fees that might impact the agreement total."
             },
             {
                 id: "0003",
-                subject: "Alphabetical Sorting on Team Members Selection/Dropdown Menu",
-                type: "Improvements",
+                subject: "Agreements updated with correct procurement shops",
+                type: "New Feature",
                 description:
-                    "When you add a Project Officer or team member to an agreement, the list of OPRE staff is now sorted alphabetically by first name so it’s easier to find a specific individual."
+                    "All agreements in OPS have been recently updated with their correct procurement shop. However, the fee rate of each procurement shop is not currently displayed, but will be soon. This means your agreement totals might still be a little off until the total fees are applied.."
             },
             {
                 id: "0004",
-                subject: "Division Directors & Team Leaders Included as Editors on Their Agreements",
+                subject: "Number formatting on exports",
                 type: "Improvements",
                 description:
-                    "Division Directors and Team Leaders will be automatically added as team members to any agreements using their Portfolios/Division’s CANs. This will also enable them to edit those agreements."
+                    "When you export a spreadsheet from OPS, columns for currency ($) will now appear as plain text which enables pivot tables and calculations on the data."
             },
-            {
-                id: "0005",
-                subject: "OCDO Portfolios",
-                type: "Improvements",
-                description: "The OCDO-related portfolios were recently added into OPS."
-            }
         ]
     }
 ];
