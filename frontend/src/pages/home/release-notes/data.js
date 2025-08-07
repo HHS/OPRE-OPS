001f4d2c1745ec23678cd2d13946937e8dbf7c1a
Frank Pigeon Jr <fpigeon@flexion.us>
2025-08-07 17:21:49 -0500

    docs: brings back previous change set

--- frontend/src/pages/home/release-notes/data.js
+++ frontend/src/pages/home/release-notes/data.js
@@ -1,5 +1,4 @@
 /*
-TODO: update this data with actual release notes
 NOTE: This file contains the release notes data for the OPS application.
 NOTE: Each release is an object with a version, date, and an array of changes.
 NOTE: Each change has an id, subject, type, and description.
@@ -37,7 +36,47 @@ export const data = [
                 type: "Improvements",
                 description:
                     "When you export a spreadsheet from OPS, columns for currency ($) will now appear as plain text which enables pivot tables and calculations on the data."
+            }
+        ]
+    },
+    {
+        releaseDate: "2025-07-02",
+        version: "1.135.0",
+        changes: [
+            {
+                id: "0001",
+                subject: "OPS Release Notes & What's Next",
+                type: "New Feature",
+                description:
+                    "You can now view details about what's new and what's next directly in OPS instead of referencing the google document."
             },
+            {
+                id: "0002",
+                subject: "Sorting on Table Columns",
+                type: "Improvements",
+                description:
+                    "You can now click on any column in a table to change how the table is sorted from top to bottom."
+            },
+            {
+                id: "0003",
+                subject: "Alphabetical Sorting on Team Members Selection/Dropdown Menu",
+                type: "Improvements",
+                description:
+                    "When you add a Project Officer or team member to an agreement, the list of OPRE staff is now sorted alphabetically by first name so it’s easier to find a specific individual."
+            },
+            {
+                id: "0004",
+                subject: "Division Directors & Team Leaders Included as Editors on Their Agreements",
+                type: "Improvements",
+                description:
+                    "Division Directors and Team Leaders will be automatically added as team members to any agreements using their Portfolios/Division’s CANs. This will also enable them to edit those agreements."
+            },
+            {
+                id: "0005",
+                subject: "OCDO Portfolios",
+                type: "Improvements",
+                description: "The OCDO-related portfolios were recently added into OPS."
+            }
         ]
     }
 ];
