172a654fb658dc6427d30ed3c6e3ff77e31bb108
John DeAngelis <john.deangelis2@gmail.com>
2025-08-14 15:53:00 -0400

    fix: ensure database commit occurs after handling procurement shop changes

--- backend/ops_api/ops/services/agreements.py
+++ backend/ops_api/ops/services/agreements.py
@@ -65,8 +65,8 @@ class AgreementsService(OpsService[Agreement]):
         change_request_id = None
         if awarding_entity_id:
             change_request_id = self._handle_proc_shop_change(agreement, awarding_entity_id)
-            if change_request_id:
-                self.db_session.commit()
+
+        self.db_session.commit()
 
         return agreement, 202 if change_request_id else 200
 
