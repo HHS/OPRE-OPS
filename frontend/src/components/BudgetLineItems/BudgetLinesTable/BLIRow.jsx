e87073cadbc909b81f69040004f1245a5eb5d5ea
weimiao67 <wmiao@flexion.us>
2025-08-28 09:38:18 -0600

    fix: add dynamic decimal scale for budget line fees display

--- frontend/src/components/BudgetLineItems/BudgetLinesTable/BLIRow.jsx
+++ frontend/src/components/BudgetLineItems/BudgetLinesTable/BLIRow.jsx
@@ -160,6 +160,7 @@ const BLIRow = ({
                             thousandSeparator={true}
                             prefix={"$"}
                             renderText={(value) => value}
+                            decimalScale={getDecimalScale(budgetLine?.fees || 0)}
                             fixedDecimalScale={true}
                         />
                     </span>
