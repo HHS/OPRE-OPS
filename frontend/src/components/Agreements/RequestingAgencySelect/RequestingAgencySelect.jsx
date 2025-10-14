188f2d1ea6deb040de81f96eea9fbd06e4479f52
Frank Pigeon Jr. <fpigeon@flexion.us>
2025-10-14 10:54:07 -0500

    feat: displays agency options

--- frontend/src/components/Agreements/RequestingAgencySelect/RequestingAgencySelect.jsx
+++ frontend/src/components/Agreements/RequestingAgencySelect/RequestingAgencySelect.jsx
@@ -1,8 +1,12 @@
 import { useGetAgreementAgenciesQuery } from "../../../api/opsAPI";
 import DebugCode from "../../DebugCode";
+import Select from "../../UI/Form/Select";
 
 const RequestingAgencySelect = () => {
+    /** @typedef {import("../../../types/AgreementTypes").Agency} Agency */
+    /** @type {{data?: Agency[] | undefined, isError: boolean,  isLoading: boolean}} */
     const { data, isLoading, isError } = useGetAgreementAgenciesQuery({ servicing: true });
+
     if (isLoading) {
         return <div>Loading...</div>;
     }
@@ -11,7 +15,22 @@ const RequestingAgencySelect = () => {
         return <div>Error loading agencies</div>;
     }
 
-    return <DebugCode data={data} />;
+    const options = data?.map((agency) => ({
+        label: `${agency.name} (${agency.abbreviation})`,
+        value: agency.id
+    }));
+
+    return (
+        <>
+            <Select
+                name="requesting-agency"
+                label="Requesting Agency"
+                options={options}
+                onChange={() => {}}
+            />
+            <DebugCode data={data} />
+        </>
+    );
 };
 
 export default RequestingAgencySelect;
