5c06f55eed7cfefd93bf926fc8c9b3a959f82d2b
rajohnson90 <rjohnson@flexion.us>
2025-10-23 09:47:11 -0500

    feat: sort agreement agencies by agency name and paginate agencies

--- backend/ops_api/ops/schemas/agreement_agency.py
+++ backend/ops_api/ops/schemas/agreement_agency.py
@@ -1,4 +1,5 @@
 from marshmallow import Schema, fields
+from marshmallow.validate import Range
 
 
 class AgreementAgencySchema(Schema):
@@ -12,3 +13,15 @@ class AgreementAgencySchema(Schema):
 class AgreementAgencyRequestSchema(Schema):
     requesting = fields.Boolean(required=False, load_default=False)
     servicing = fields.Boolean(required=False, load_default=False)
+    limit = fields.Integer(
+        load_default=10,
+        dump_default=10,
+        validate=Range(min=1, error="Limit must be greater than 0"),
+        allow_none=True,
+    )
+    offset = fields.Integer(
+        load_default=0,
+        dump_default=0,
+        validate=Range(min=0, error="Offset must be greater than 0"),
+        allow_none=True,
+    )
