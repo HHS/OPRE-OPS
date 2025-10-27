128e0f87ef276443c42fad7fddef876033610916
rajohnson90 <rjohnson@flexion.us>
2025-10-27 08:46:27 -0500

    refactor: create non-list pagination schema and use it replace other paginated schemas

--- backend/ops_api/ops/schemas/agreement_agency.py
+++ backend/ops_api/ops/schemas/agreement_agency.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema, fields
-from marshmallow.validate import Range
+
+from ops_api.ops.schemas.pagination import PaginationSchema
 
 
 class AgreementAgencySchema(Schema):
@@ -10,18 +11,6 @@ class AgreementAgencySchema(Schema):
     servicing = fields.Boolean(required=True)
 
 
-class AgreementAgencyRequestSchema(Schema):
+class AgreementAgencyRequestSchema(PaginationSchema):
     requesting = fields.Boolean(required=False, load_default=False)
     servicing = fields.Boolean(required=False, load_default=False)
-    limit = fields.Integer(
-        load_default=10,
-        dump_default=10,
-        validate=Range(min=1, error="Limit must be greater than 0"),
-        allow_none=True,
-    )
-    offset = fields.Integer(
-        load_default=0,
-        dump_default=0,
-        validate=Range(min=0, error="Offset must be greater than 0"),
-        allow_none=True,
-    )
