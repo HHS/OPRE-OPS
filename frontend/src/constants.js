393a92e38c06d07db4ac908b048013551d50fa55
Frank Pigeon Jr. <fpigeon@flexion.us>
2025-11-20 08:53:45 -0600

    chore: cleanup

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -46,6 +46,7 @@ const constants = {
 
 export const NO_DATA = "TBD";
 export const LAST_DATA_UPDATE = "2025-11-03";
+//NOTE: import.meta.env.PROD ? 25 : 10 is not supported in all environments yet
 export const ITEMS_PER_PAGE = process.env.NODE_ENV === "production" ? 25 : 10;
 
 export default constants;
