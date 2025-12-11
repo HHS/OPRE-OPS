749fe821d4d3145bbd773b56dad12797f9a681da
John DeAngelis <john.deangelis2@gmail.com>
2025-12-11 13:36:43 -0500

    refactor: remove requisition-related code from budget line item tests and utility functions

--- backend/data_tools/tests/load_master_spreadsheet_budget_lines_v2/test_load_master_spreadsheet_budget_lines_items_v2.py
+++ backend/data_tools/tests/load_master_spreadsheet_budget_lines_v2/test_load_master_spreadsheet_budget_lines_items_v2.py
@@ -331,7 +331,6 @@ def test_create_model(db_with_data_v2):
     assert bli_model.extend_pop_to is None
     assert bli_model.start_date is None
     assert bli_model.end_date is None
-    assert bli_model.requisition is None
     assert bli_model.object_class_code_id is None
     assert bli_model.object_class_code is None
     assert bli_model.doc_received is False
