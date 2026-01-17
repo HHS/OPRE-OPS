9f0cf10b3639333fe570245c0b39d4236afea306
Santi-3rd <g.santiago3rd@gmail.com>
2026-01-16 20:49:44 -0500

    test: added unit test

--- frontend/src/pages/agreements/details/AgreementProcurementTracker.jsx
+++ frontend/src/pages/agreements/details/AgreementProcurementTracker.jsx
@@ -1,14 +1,32 @@
 import StepIndicator from "../../../components/UI/StepIndicator";
 
 const AgreementProcurementTracker = () => {
-    const wizardSteps = ["Acquisition Planning", "Pre-Solicitation", "Solicitation", "Evaluation", "Pre-Award", "Award"]
+
+    //The data within wizardSteps and currentStep will be provided by the backend.
+    const wizardSteps = [
+        "Acquisition Planning",
+        "Pre-Solicitation",
+        "Solicitation",
+        "Evaluation",
+        "Pre-Award",
+        "Award"
+    ];
+    const currentStep = 2
 
     return (
-        <StepIndicator
-            steps={wizardSteps}
-            currentStep={3}
-        />
+        <>
+            <div className="display-flex flex-justify flex-align-center">
+                <h2 className="font-sans-lg">Procurement Tracker</h2>
+            </div>
+                <p className="font-sans-sm margin-bottom-4">Follow the steps below to complete the procurement process for Budget Lines in Executing Status.</p>
+
+            <StepIndicator
+                steps={wizardSteps}
+                currentStep={currentStep}
+            />
+            {/* Accordions */}
+        </>
     );
 };
 
-export default AgreementProcurementTracker
+export default AgreementProcurementTracker;
