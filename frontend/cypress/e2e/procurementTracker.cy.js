b1fcb1de82eda153833c2f42c5a78b3bc0a6b477
Santi-3rd <g.santiago3rd@gmail.com>
2026-01-20 12:34:44 -0500

    test: removed unit test and added e2e test

--- frontend/cypress/e2e/procurementTracker.cy.js
+++ frontend/cypress/e2e/procurementTracker.cy.js
@@ -1,3 +1,20 @@
 /// <reference types="cypress" />
 
-//test that the procurement tracker tab is accesible when the contract is awarded
+import { testLogin } from "./utils";
+
+beforeEach(() => {
+    testLogin("system-owner");
+});
+
+describe("Procuerement Tracker page", () => {
+    it("procurement tracker details tab should exist and be clickable for awarded agreements", () => {
+        cy.visit(`/agreements/7`);
+        cy.get('[data-cy="details-tab-Procurement Tracker"]').should("exist");
+        cy.get('[data-cy="details-tab-Procurement Tracker"]').click();
+        cy.get("h2").contains("Procurement Tracker")
+    })
+        it("procurement tracker details tab should not exist for unawarded", () => {
+        cy.visit(`/agreements/9`);
+        cy.get('[data-cy="details-tab-Procurement Tracker"]').should("not.exist");
+    })
+})
