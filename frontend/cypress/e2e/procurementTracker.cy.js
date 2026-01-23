14040caf928d93c506f7dc56387bafcb5c9b48e3
weimiao67 <wmiao@flexion.us>
2026-01-23 12:42:55 -0700

    test: add e2e test coverage

--- frontend/cypress/e2e/procurementTracker.cy.js
+++ frontend/cypress/e2e/procurementTracker.cy.js
@@ -7,7 +7,7 @@ beforeEach(() => {
 });
 
 describe("Procurement Tracker page", () => {
-    it("details tab should exist and be clickable for developed agreements", () => {
+    it("Procurement tracker tab should exist and be clickable for developed agreements", () => {
         cy.visit(`/agreements/13`);
         cy.get('[data-cy="details-tab-Procurement Tracker"]').should("exist");
         cy.get('[data-cy="details-tab-Procurement Tracker"]').click();
@@ -15,8 +15,16 @@ describe("Procurement Tracker page", () => {
 
         cy.get('[data-cy="step-indicator-0"]').should("have.class", "usa-step-indicator__segment--current");
     });
-    it("details tab should not exist for non developed agreements", () => {});
-    it("details tab should be disabled for agreements with budget lines not in executing status", () => {});
-    it("should display message if user navigates through url to an agreement with budget lines not in executing status", () => {});
-    it("should display message if no active trackers are found", () => {});
+    it("Procurement tracker tab should not exist for non developed agreements", () => {
+        cy.visit(`/agreements/3`);
+        cy.get('[data-cy="details-tab-Procurement Tracker"]').should("not.exist");
+    });
+    it("Procurement tracker tab should be disabled for agreements with budget lines not in executing status", () => {
+        cy.visit(`/agreements/6`);
+        cy.get('[data-cy="details-tab-Procurement Tracker"]').should("be.disabled");
+    });
+    it("should display message if user navigates through url to an agreement with budget lines not in executing status", () => {
+        cy.visit(`/agreements/6/procurement-tracker`);
+        cy.get("div").contains("No active Procurement Tracker found.");
+    });
 });
