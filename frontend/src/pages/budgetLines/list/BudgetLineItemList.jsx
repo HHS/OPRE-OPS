e89bb0899cff2d52e8b8278a1a131c3fcb55450c
josbell <josbell.quiros@gmail.com>
2026-03-02 11:26:13 -0800

    fix: prevent duplicate 'All' option in FiscalYear component when fiscalYear is 'All'

--- frontend/src/pages/budgetLines/list/BudgetLineItemList.jsx
+++ frontend/src/pages/budgetLines/list/BudgetLineItemList.jsx
@@ -235,7 +235,7 @@ const BudgetLineItemList = () => {
                     <FiscalYear
                         fiscalYear={fiscalYearShortcut}
                         handleChangeFiscalYear={handleChangeFiscalYear}
-                        showAllOption={true}
+                        showAllOption={fiscalYearShortcut !== "All"}
                     />
                 }
                 SummaryCardsSection={
