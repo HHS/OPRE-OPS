f5f75eb45e45307eb48e150dc3c413cdba7cecea
Santi-3rd <g.santiago3rd@gmail.com>
2026-03-11 10:55:52 -0400

    fix: review tab dsplays for reviewer approver role

--- frontend/src/pages/agreements/list/AgreementsTabs.jsx
+++ frontend/src/pages/agreements/list/AgreementsTabs.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import styles from "../../../components/Portfolios/PortfolioTabsSection/PortfolioTabsSection.module.scss";
 import TabsSection from "../../../components/UI/TabsSection";
+import { USER_ROLES } from "../../../components/Users/User.constants";
 import { useChangeRequestTotal } from "../../../hooks/useChangeRequests.hooks";
 import tabStyles from "./AgreementTabs.module.css";
 
@@ -10,24 +12,30 @@ import tabStyles from "./AgreementTabs.module.css";
  * @returns {JSX.Element} - The procurement shop select element.
  */
 
-const paths = [
-    {
-        name: "",
-        label: "All Agreements"
-    },
-    {
-        name: "?filter=my-agreements",
-        label: "My Agreements"
-    },
-    {
-        name: "?filter=change-requests",
-        label: "For Review"
-    }
-];
-
 const AgreementTabs = () => {
     const { search } = useLocation();
     const changeRequestsTotal = useChangeRequestTotal();
+    const userRoles = useSelector((state) => state.auth?.activeUser?.roles) ?? [];
+    const isReviewerApprover = userRoles.some((role) => role?.name === USER_ROLES.REVIEWER_APPROVER);
+
+    const paths = [
+        {
+            name: "",
+            label: "All Agreements"
+        },
+        {
+            name: "?filter=my-agreements",
+            label: "My Agreements"
+        },
+        ...(isReviewerApprover
+            ? [
+                  {
+                      name: "?filter=change-requests",
+                      label: "For Review"
+                  }
+              ]
+            : [])
+    ];
     /**
      * @param {string} queryString - The query string of the tab.
      */
