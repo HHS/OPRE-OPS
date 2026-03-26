99da4c3d9047a20398e15c11328959d8acc3ddb5
rajohnson90 <rjohnson@flexion.us>
2026-03-26 09:51:41 -0500

    fix: fixing one other fiscal year filter test

--- backend/ops_api/tests/ops/project/test_project.py
+++ backend/ops_api/tests/ops/project/test_project.py
@@ -66,12 +66,12 @@ def test_projects_serialization(auth_client, loaded_db, test_user, test_project)
     assert response.json["team_leaders"][0]["full_name"] == "Chris Fortunato"
 
 
-def test_projects_with_fiscal_year_found(auth_client, loaded_db, test_project):
-    response = auth_client.get(url_for("api.projects-group", fiscal_year=[2023]))
+def test_projects_with_fiscal_year_found(auth_client, loaded_db):
+    response = auth_client.get(url_for("api.projects-group", fiscal_year=[2045]))
     assert response.status_code == 200
     assert response.json["count"] == 1
     assert len(response.json["data"]) == 1
-    assert response.json["data"][0]["title"] == "Support Project #1"
+    assert response.json["data"][0]["title"] == "Human Services Interoperability Support"
 
 
 def test_projects_with_fiscal_year_not_found(auth_client, loaded_db):
