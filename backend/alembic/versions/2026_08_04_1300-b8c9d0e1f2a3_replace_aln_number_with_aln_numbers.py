850cdcff88e82694ef90af2248dcbe42402ac354
rajohnson90 <rjohnson@flexion.us>
2026-08-13 16:10:57 -0500

    fix: rebase aln_numbers migration onto latest main head

--- backend/alembic/versions/2026_08_04_1300-b8c9d0e1f2a3_replace_aln_number_with_aln_numbers.py
+++ backend/alembic/versions/2026_08_04_1300-b8c9d0e1f2a3_replace_aln_number_with_aln_numbers.py
@@ -1,7 +1,7 @@
 """Replace aln_number string field with aln_numbers integer array
 
 Revision ID: b8c9d0e1f2a3
-Revises: a7c3f1e9b2d4
+Revises: 5bbc500dc6a1
 Create Date: 2026-08-04 13:00:00.000000+00:00
 
 """
@@ -14,7 +14,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = "b8c9d0e1f2a3"
-down_revision: Union[str, None] = "a7c3f1e9b2d4"
+down_revision: Union[str, None] = "5bbc500dc6a1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
